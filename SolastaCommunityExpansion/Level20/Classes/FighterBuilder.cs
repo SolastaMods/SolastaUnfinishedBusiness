f102e42c23d6b007865b423f041f467442b76d3e
leprechaun7377 <chris.p.john@gmail.com>
2021-11-06 23:49:30 -0700

    Re-add the second action surge as an overriding power.

--- SolastaCommunityExpansion/Level20/Classes/FighterBuilder.cs
+++ SolastaCommunityExpansion/Level20/Classes/FighterBuilder.cs
@@ -3,7 +3,7 @@ using static SolastaModApi.DatabaseHelper.CharacterClassDefinitions;
 using static SolastaModApi.DatabaseHelper.FeatureDefinitionAttributeModifiers;
 using static SolastaModApi.DatabaseHelper.FeatureDefinitionFeatureSets;
 using static SolastaCommunityExpansion.Level20.Features.AttributeModifierFighterIndomitableBuilder;
-//using static SolastaCommunityExpansion.Level20.Features.PowerFighterActionSurge2Builder;
+using static SolastaCommunityExpansion.Level20.Features.PowerFighterActionSurge2Builder;
 
 namespace SolastaCommunityExpansion.Level20.Classes
 {
@@ -15,7 +15,7 @@ namespace SolastaCommunityExpansion.Level20.Classes
                 new FeatureUnlockByLevel(AttributeModifierFighterIndomitableAdd, 13),
                 new FeatureUnlockByLevel(FeatureSetAbilityScoreChoice, 14),
                 new FeatureUnlockByLevel(FeatureSetAbilityScoreChoice, 16),
-                //new FeatureUnlockByLevel(PowerFighterActionSurge2, 17),
+                new FeatureUnlockByLevel(PowerFighterActionSurge2, 17),
                 new FeatureUnlockByLevel(AttributeModifierFighterIndomitableAdd, 17),
                 new FeatureUnlockByLevel(FeatureSetAbilityScoreChoice, 19),
                 new FeatureUnlockByLevel(AttributeModifierFighterExtraAttack, 20)
