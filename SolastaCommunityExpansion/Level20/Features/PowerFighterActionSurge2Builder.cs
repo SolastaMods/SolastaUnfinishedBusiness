515d539179464c3588b9ff29dd42294ebf04e714
Paulo Monteiro <pauloesquilo@gmail.com>
2021-11-06 17:06:00 -0700

    disables fighter ActionSurge to let Chris fix it ;-)

--- SolastaCommunityExpansion/Level20/Features/PowerFighterActionSurge2Builder.cs
+++ SolastaCommunityExpansion/Level20/Features/PowerFighterActionSurge2Builder.cs
@@ -1,23 +1,28 @@
-﻿using SolastaModApi;
-using SolastaModApi.Extensions;
-using static SolastaModApi.DatabaseHelper.FeatureDefinitionPowers;
+﻿//
+// Chris - please review this feature
+//
 
-namespace SolastaCommunityExpansion.Level20.Features
-{
-    internal class PowerFighterActionSurge2Builder : BaseDefinitionBuilder<FeatureDefinitionPower>
-    {
-        const string PowerFighterActionSurgeName = "ZSPowerFighterActionSurge2";
-        const string PowerFighterActionSurgeGuid = "a20a3955a66142e5ba9d2580a71b6c36";
 
-        protected PowerFighterActionSurge2Builder(string name, string guid, int challengeRating) : base(PowerFighterActionSurge, name, guid)
-        {
-            //Definition.SetFixedUsesPerRecharge(2);
-        }
+//using SolastaModApi;
+//using SolastaModApi.Extensions;
+//using static SolastaModApi.DatabaseHelper.FeatureDefinitionPowers;
 
-        private static FeatureDefinitionPower CreateAndAddToDB(string name, string guid, int challengeRating)
-            => new PowerFighterActionSurge2Builder(name, guid, challengeRating).AddToDB();
+//namespace SolastaCommunityExpansion.Level20.Features
+//{
+//    internal class PowerFighterActionSurge2Builder : BaseDefinitionBuilder<FeatureDefinitionPower>
+//    {
+//        const string PowerFighterActionSurgeName = "ZSPowerFighterActionSurge2";
+//        const string PowerFighterActionSurgeGuid = "a20a3955a66142e5ba9d2580a71b6c36";
 
-        internal static readonly FeatureDefinitionPower PowerFighterActionSurge2 =
-            CreateAndAddToDB(PowerFighterActionSurgeName, PowerFighterActionSurgeGuid, 3);
-    }
-}
+//        protected PowerFighterActionSurge2Builder(string name, string guid, int challengeRating) : base(PowerFighterActionSurge, name, guid)
+//        {
+//            //Definition.SetFixedUsesPerRecharge(2);
+//        }
+
+//        private static FeatureDefinitionPower CreateAndAddToDB(string name, string guid, int challengeRating)
+//            => new PowerFighterActionSurge2Builder(name, guid, challengeRating).AddToDB();
+
+//        internal static readonly FeatureDefinitionPower PowerFighterActionSurge2 =
+//            CreateAndAddToDB(PowerFighterActionSurgeName, PowerFighterActionSurgeGuid, 3);
+//    }
+//}
