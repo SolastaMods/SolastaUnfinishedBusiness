07c15196a4e5dd7809a914f4a0c744cfb39a126c
leprechaun7377 <chris.p.john@gmail.com>
2021-11-07 23:42:58 -0800

    Show actual recipes in recipe tooltips.

---
+++ SolastaCommunityExpansion/Patches/GameUi/TooltipFeatureRecipeDescriptionPatcher.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using SolastaModApi.Infrastructure;
+
+namespace SolastaCommunityExpansion.Patches.GameUi
+{
+
+    [HarmonyPatch(typeof(TooltipFeatureDescription), "Bind")]
+    internal static class TooltipFeatureDescription_Bind
+    {
+        internal static void Postfix(TooltipFeatureDescription __instance, ITooltip tooltip)
+        {
+            if (!Main.Settings.RecipeTooltipShowsRecipe)
+            {
+                return;
+            }
+            if (tooltip.DataProvider is IItemDefinitionProvider)
+            {
+                ItemDefinition item = (tooltip.DataProvider as IItemDefinitionProvider).ItemDefinition;
+                if (item.IsDocument && item.DocumentDescription.LoreType == RuleDefinitions.LoreType.CraftingRecipe)
+                {
+                    foreach (ContentFragmentDescription contentFragmentDescription in item.DocumentDescription.ContentFragments)
+                    {
+                        if (contentFragmentDescription.Type == ContentFragmentDescription.FragmentType.Body)
+                        {
+                            string text = string.Empty;
+                            GuiRecipeDefinition guiRecipeDefinition = ServiceRepository.GetService<IGuiWrapperService>().GetGuiRecipeDefinition(item.DocumentDescription.RecipeDefinition.Name);
+                            text = Gui.Format(contentFragmentDescription.Text, new string[]
+                            {
+                                guiRecipeDefinition.Title,
+                                guiRecipeDefinition.IngredientsText
+                            });
+                            __instance.DescriptionLabel.Text = text;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
