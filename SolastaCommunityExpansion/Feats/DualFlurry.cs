2c7524879a845939a5f03d7260a511227a3f2fa2
leprechaun7377 <chris.p.john@gmail.com>
2021-11-08 01:51:31 -0800

    Initial version, need to refactor still.

---
+++ SolastaCommunityExpansion/Feats/DualFlurry.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolastaModApi;
+using SolastaModApi.Extensions;
+using SolastaModApi.Infrastructure;
+using UnityEngine.AddressableAssets;
+using TA;
+
+namespace SolastaCommunityExpansion.Feats
+{
+    class DualFlurryFeatBuilder : BaseDefinitionBuilder<FeatDefinition>
+    {
+        const string DualFlurryFeatName = "DualFlurryFeat";
+        private static readonly string DualFlurryFeatNameGuid = GuidHelper.Create(Core.FP_GUID, DualFlurryFeatName).ToString();
+
+        protected DualFlurryFeatBuilder(string name, string guid) : base(DatabaseHelper.FeatDefinitions.Ambidextrous, name, guid)
+        {
+            Definition.GuiPresentation.Title = "Feat/&DualFlurryTitle";
+            Definition.GuiPresentation.Description = "Feat/&DualFlurryDescription";
+
+            Definition.Features.Clear();
+            Definition.Features.Add(buildFeatureDualFlurry());
+
+            Definition.SetMinimalAbilityScorePrerequisite(false);
+        }
+
+        public static FeatDefinition CreateAndAddToDB(string name, string guid)
+            => new DualFlurryFeatBuilder(name, guid).AddToDB();
+
+        public static FeatDefinition DualFlurryFeat = CreateAndAddToDB(DualFlurryFeatName, DualFlurryFeatNameGuid);
+
+        public static void AddToFeatList()
+        {
+            var DualFlurryFeat = DualFlurryFeatBuilder.DualFlurryFeat;
+        }
+
+        private static FeatureDefinition buildFeatureDualFlurry()
+        {
+            var cond_apply = ConditionDualFlurryApplyBuilder.GetOrAdd();
+            var cond_grant = ConditionDualFlurryGrantBuilder.GetOrAdd();
+
+            return Helpers.FeatureBuilder<PrereqApplyConditionOnDamageDone>.createFeature
+            (
+                "FeatureDualFlurry",
+                GuidHelper.Create(Core.FP_GUID, "FeatureDualFlurry").ToString(),
+                "Feature/&DualFlurryTitle",
+                "Feature/&DualFlurryDescription",
+                null,
+                a =>
+                {
+                    a.initialize_applicator
+                    (
+                        true,
+                        cond_apply,
+                        cond_grant,
+                        1,
+                        RuleDefinitions.DurationType.Turn,
+                        RuleDefinitions.TurnOccurenceType.EndOfTurn,
+                        true, false, false
+                    );
+                }
+            );
+        }
+    }
+
+    internal class ConditionDualFlurryApplyBuilder : BaseDefinitionBuilder<ConditionDefinition>
+    {
+        protected ConditionDualFlurryApplyBuilder(string name, string guid) : base(DatabaseHelper.ConditionDefinitions.ConditionSurged, name, guid)
+        {
+            Definition.GuiPresentation.Title = "Condition/&ConditionDualFlurryApplyTitle";
+            Definition.GuiPresentation.Description = "Condition/&ConditionDualFlurryApplyDescription";
+
+            Definition.SetAllowMultipleInstances(false);
+            Definition.SetDurationParameter(1);
+            Definition.SetDurationType(RuleDefinitions.DurationType.Turn);
+            Definition.SetTurnOccurence(RuleDefinitions.TurnOccurenceType.EndOfTurn);
+            Definition.SetPossessive(true);
+            Definition.SetSilentWhenAdded(true);
+            Definition.SetSilentWhenRemoved(true);
+            Definition.SetConditionType(RuleDefinitions.ConditionType.Beneficial);
+            Definition.Features.Clear();
+        }
+
+        public static ConditionDefinition CreateAndAddToDB()
+            => new ConditionDualFlurryApplyBuilder("ConditionDualFlurryApply", GuidHelper.Create(Core.FP_GUID, "ConditionDualFlurryApply").ToString()).AddToDB();
+
+        public static ConditionDefinition GetOrAdd()
+        {
+            var db = DatabaseRepository.GetDatabase<ConditionDefinition>();
+            return db.TryGetElement("ConditionDualFlurryApply", GuidHelper.Create(Core.FP_GUID, "ConditionDualFlurryApply").ToString()) ?? CreateAndAddToDB();
+        }
+    }
+
+    internal class ConditionDualFlurryGrantBuilder : BaseDefinitionBuilder<ConditionDefinition>
+    {
+        protected ConditionDualFlurryGrantBuilder(string name, string guid) : base(DatabaseHelper.ConditionDefinitions.ConditionSurged, name, guid)
+        {
+            Definition.GuiPresentation.Title = "Condition/&ConditionDualFlurryGrantTitle";
+            Definition.GuiPresentation.Description = "Condition/&ConditionDualFlurryGrantDescription";
+            Definition.GuiPresentation.SetHidden(true);
+
+            Definition.SetAllowMultipleInstances(false);
+            Definition.SetDurationParameter(1);
+            Definition.SetDurationType(RuleDefinitions.DurationType.Turn);
+            Definition.SetTurnOccurence(RuleDefinitions.TurnOccurenceType.EndOfTurn);
+            Definition.SetPossessive(true);
+            Definition.SetSilentWhenAdded(false);
+            Definition.SetSilentWhenRemoved(false);
+            Definition.SetConditionType(RuleDefinitions.ConditionType.Beneficial);
+            Definition.Features.Clear();
+            Definition.Features.Add(buildAdditionalActionDualFlurry());
+        }
+
+        public static ConditionDefinition CreateAndAddToDB()
+            => new ConditionDualFlurryGrantBuilder("ConditionDualFlurryGrant", GuidHelper.Create(Core.FP_GUID, "ConditionDualFlurryGrant").ToString()).AddToDB();
+
+        public static ConditionDefinition GetOrAdd()
+        {
+            var db = DatabaseRepository.GetDatabase<ConditionDefinition>();
+            return db.TryGetElement("ConditionDualFlurryGrant", GuidHelper.Create(Core.FP_GUID, "ConditionDualFlurryGrant").ToString()) ?? CreateAndAddToDB();
+        }
+
+        private static FeatureDefinition buildAdditionalActionDualFlurry()
+        {
+            return Helpers.CopyFeatureBuilder<FeatureDefinitionAdditionalAction>.createFeatureCopy
+            (
+                "AdditionalActionDualFlurry",
+                GuidHelper.Create(Core.FP_GUID, "AdditionalActionDualFlurry").ToString(),
+                "Feature/&AdditionalActionDualFlurryTitle",
+                "Feature/&AdditionalActionDualFlurryDescription",
+                DatabaseHelper.FeatureDefinitionAdditionalActions.AdditionalActionSurgedMain.GuiPresentation.SpriteReference,
+                DatabaseHelper.FeatureDefinitionAdditionalActions.AdditionalActionSurgedMain,
+                a =>
+                {
+                    a.SetActionType(ActionDefinitions.ActionType.Bonus);
+                    a.AuthorizedActions.Clear();
+                    a.ForbiddenActions.Clear();
+                    a.RestrictedActions.Clear();
+                    a.RestrictedActions.Add(DatabaseHelper.ActionDefinitions.AttackOff.Id);
+                }
+            );
+        }
+    }
+}
---
+++ SolastaCommunityExpansion/Feats/Torchbearer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolastaModApi;
+using SolastaModApi.Extensions;
+using SolastaModApi.Infrastructure;
+using UnityEngine.AddressableAssets;
+using TA;
+
+namespace SolastaCommunityExpansion.Feats
+{
+    class TorchbearerFeatBuilder : BaseDefinitionBuilder<FeatDefinition>
+    {
+        const string TorchbearerFeatName = "TorchbearerFeat";
+        private static readonly string TorchbearerFeatNameGuid = GuidHelper.Create(Core.FP_GUID, TorchbearerFeatName).ToString();
+
+        protected TorchbearerFeatBuilder(string name, string guid) : base(DatabaseHelper.FeatDefinitions.Ambidextrous, name, guid)
+        {
+            Definition.GuiPresentation.Title = "Feat/&TorchbearerTitle";
+            Definition.GuiPresentation.Description = "Feat/&TorchbearerDescription";
+
+            Definition.Features.Clear();
+            Definition.Features.Add(buildFeatureTorchbearer());
+
+            Definition.SetMinimalAbilityScorePrerequisite(false);
+        }
+
+        public static FeatDefinition CreateAndAddToDB(string name, string guid)
+            => new TorchbearerFeatBuilder(name, guid).AddToDB();
+
+        public static FeatDefinition TorchbearerFeat = CreateAndAddToDB(TorchbearerFeatName, TorchbearerFeatNameGuid);
+
+        public static void AddToFeatList()
+        {
+            var TorchbearerFeat = TorchbearerFeatBuilder.TorchbearerFeat;
+        }
+
+        private static FeatureDefinition buildFeatureTorchbearer()
+        {
+            var light_restrict = new HasLightSourceRestriction();
+
+            var burn_effect = new EffectForm();
+            burn_effect.SetFormType(EffectForm.EffectFormType.Condition);
+            burn_effect.ConditionForm = new ConditionForm();
+            burn_effect.ConditionForm.Operation = ConditionForm.ConditionOperation.Add;
+            burn_effect.ConditionForm.ConditionDefinition = DatabaseHelper.ConditionDefinitions.ConditionOnFire1D4;
+
+            var burn_description = new EffectDescription();
+            burn_description.Copy(DatabaseHelper.SpellDefinitions.Fireball.EffectDescription);
+            burn_description.SetCreatedByCharacter(true);
+            burn_description.SetTargetSide(RuleDefinitions.Side.Enemy);
+            burn_description.SetTargetType(RuleDefinitions.TargetType.Individuals);
+            burn_description.SetTargetParameter(1);
+            burn_description.SetRangeType(RuleDefinitions.RangeType.Touch);
+            burn_description.SetDurationType(RuleDefinitions.DurationType.Turn);
+            burn_description.SetDurationParameter(3);
+            burn_description.SetCanBePlacedOnCharacter(false);
+            burn_description.SetHasSavingThrow(true);
+            burn_description.SetSavingThrowAbility(Helpers.Stats.Dexterity);
+            burn_description.SetSavingThrowDifficultyAbility(Helpers.Stats.Dexterity);
+            burn_description.SetDifficultyClassComputation(RuleDefinitions.EffectDifficultyClassComputation.AbilityScoreAndProficiency);
+            burn_description.SetSpeedType(RuleDefinitions.SpeedType.Instant);
+
+            burn_description.EffectForms.Clear();
+            burn_description.EffectForms.Add(burn_effect);
+
+            return Helpers.FeatureBuilder<NewFeatureDefinitions.PowerWithRestrictions>.createFeature
+            (
+                "PowerTorchbearer",
+                GuidHelper.Create(Core.FP_GUID, "PowerTorchbearer").ToString(),
+                "Feature/&PowerTorchbearerTitle",
+                "Feature/&PowerTorchbearerDescription",
+                DatabaseHelper.FeatureDefinitionPowers.PowerDomainElementalFireBurst.GuiPresentation.SpriteReference,
+                a =>
+                {
+                    a.SetActivationTime(RuleDefinitions.ActivationTime.BonusAction);
+                    a.SetCostPerUse(0);
+                    a.SetEffectDescription(burn_description);
+                    a.SetFixedUsesPerRecharge(1);
+                    a.SetUsesDetermination(RuleDefinitions.UsesDetermination.Fixed);
+                    a.SetRechargeRate(RuleDefinitions.RechargeRate.AtWill);
+                    a.SetShowCasting(false);
+                    a.SetUniqueInstance(false);
+                    a.restrictions = new List<NewFeatureDefinitions.IRestriction>()
+                    {
+                        light_restrict
+                    };
+                }
+            );
+        }
+    }
+}
