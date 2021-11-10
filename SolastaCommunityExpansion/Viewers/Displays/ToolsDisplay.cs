69ecff2cdd553ab8ba9d7f32692e97e72c1ad958
leprechaun7377 <chris.p.john@gmail.com>
2021-11-09 22:44:43 -0800

    Add a new ui for setting faction relations.

--- SolastaCommunityExpansion/Viewers/Displays/ToolsDisplay.cs
+++ SolastaCommunityExpansion/Viewers/Displays/ToolsDisplay.cs
@@ -49,6 +49,48 @@ namespace SolastaCommunityExpansion.Viewers.Displays
             {
                 Main.Settings.NoExperienceOnLevelUp = toggle;
             }
+
+            UI.Label("");
+            UI.Label("Faction Relations");
+            UI.Label("");
+            
+
+            bool flip = true;
+            var service = ServiceRepository.GetService<IGameFactionService>();
+            if (service != null) {
+                UI.Label("Does not grant achievements for faction relations, you can modify the faction relationship through gameplay after setting it here to gain the achievements.".red());
+                UI.Label("");
+                foreach (FactionDefinition faction in service.RegisteredFactions)
+                {
+                    if (faction.BuiltIn)
+                    {
+                        // These are things like monster factions, generally set to a specific relation and can't be changed.
+                        continue;
+                    }
+                    if (faction.GuiPresentation.Hidden)
+                    {
+                        // These are things like Silent Whipsers and Church Of Einar that are not fully implemented factions.
+                        continue;
+                    }
+                    string title = Gui.Format(faction.GuiPresentation.Title);
+                    if (flip)
+                    {
+                        title = title.yellow();
+                    } else
+                    {
+                        title = title.white();
+                    }
+                    int intValue = service.FactionRelations[faction.Name];
+                    if (UI.Slider(title, ref intValue, faction.MinRelationCap, faction.MaxRelationCap, 0, "", UI.AutoWidth()))
+                    {
+                        SetFactionRelationsContext.SetFactionRelation(faction.Name, intValue);
+                    }
+                    flip = !flip;
+                }                 
+            } else
+            {
+                UI.Label("Load a game to modify faction relations".red());
+            }
         }
     }
 }
