498f8271b2967fd9c80aa03c210a06b7250b51e7
leprechaun7377 <chris.p.john@gmail.com>
2021-11-09 22:03:23 -0800

    Add a check for if we're doing narration and don't pause the game if so.

--- SolastaCommunityExpansion/Patches/GameUi/BattleState_VictoryUpdatePatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUi/BattleState_VictoryUpdatePatcher.cs
@@ -14,6 +14,17 @@ namespace SolastaCommunityExpansion.Patches.GameUi
             if (battleService == null) return;
             if (battleService.Battle != null) return;
 
+            INarrativeDirectionService narrativeService = ServiceRepository.GetService<INarrativeDirectionService>();
+
+            if (narrativeService != null && narrativeService.CurrentSequence != null)
+            {
+                // Don't pause in the middle of a narrative sequence it hangs the game.
+                // For example during the tutorial shoving the rock to destroy the bridge transitions
+                // directly into a narrative sequence. I believe there are several other battle ->
+                // narrative transitions in the game (like the crown vision paladin fight).
+                return;
+            }
+
             var campaign = ServiceRepository.GetService<IGameService>()?.Game?.GameCampaign;
 
             if (campaign == null) return;
