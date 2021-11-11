676896287064f0a8bdd666a98a28d578a1b7c0e9
leprechaun7377 <chris.p.john@gmail.com>
2021-11-11 11:07:44 -0800

    Clean up some unused using statements.

--- SolastaCommunityExpansion/CustomFeatureDefinitions/CustomFightingStyle.cs
+++ SolastaCommunityExpansion/CustomFeatureDefinitions/CustomFightingStyle.cs
@@ -2,11 +2,7 @@
 using SolastaModApi.Diagnostics;
 using SolastaModApi.Extensions;
 using SolastaModApi.Infrastructure;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SolastaCommunityExpansion.CustomFeatureDefinitions
 {
