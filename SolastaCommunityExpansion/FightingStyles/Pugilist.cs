48fdc17fe5be7d8d63a2ad6167341e059f7bedfa
leprechaun7377 <chris.p.john@gmail.com>
2021-11-11 12:14:08 -0800

    Add the offhand attack in for Pugilist as well as the general ability for fighting styles to grant powers!

--- SolastaCommunityExpansion/FightingStyles/Pugilist.cs
+++ SolastaCommunityExpansion/FightingStyles/Pugilist.cs
@@ -58,15 +58,27 @@ namespace SolastaCommunityExpansion.FightingStyles
         {
             if (instance == null)
             {
-
                 GuiPresentationBuilder gui = new GuiPresentationBuilder("FightingStyle/&PugilistFightingDescription", "FightingStyle/&PugilistFightingTitle");
                 gui.SetSpriteReference(DatabaseHelper.CharacterSubclassDefinitions.RangerShadowTamer.GuiPresentation.SpriteReference);
+
+                EffectDescriptionBuilder offhandEffect = new EffectDescriptionBuilder();
+                offhandEffect.SetTargetingData(RuleDefinitions.Side.Enemy, RuleDefinitions.RangeType.MeleeHit, 1, RuleDefinitions.TargetType.Individuals,
+                    1, 1, ActionDefinitions.ItemSelectionType.None);
+                offhandEffect.AddEffectForm(new EffectFormBuilder().CreatedByCharacter().SetBonusMode(RuleDefinitions.AddBonusMode.AbilityBonus)
+                    .SetDamageForm(false, RuleDefinitions.DieType.D10, "DamageBludgeoning", 1, RuleDefinitions.DieType.D8, 1,
+                    RuleDefinitions.HealFromInflictedDamage.Never, new List<RuleDefinitions.TrendInfo>()).Build());
+                FeatureDefinitionPower offhandAttack = new FeatureDefinitionPowerBuilder("PowerPugilistOffhandAttack", "a97a1c9c-232b-42ae-8003-30d244e958b3",
+                    1, RuleDefinitions.UsesDetermination.Fixed, AttributeDefinitions.Strength, RuleDefinitions.ActivationTime.BonusAction,
+                    0, RuleDefinitions.RechargeRate.AtWill, true, true, AttributeDefinitions.Strength,
+                    offhandEffect.Build(), gui.Build(), false /* unique */).AddToDB();
+
                 CustomizableFightingStyleBuilder builder = new CustomizableFightingStyleBuilder("PugilistFightingStlye", "b14f91dc-8706-498b-a9a0-d583b7b00d09",
                     new List<FeatureDefinition>() {
                         new FeatureDefinitionAdditionalDamageBuilder("AdditionalDamagePugilist", "36d24b2e-8ef4-4037-a82f-05e63d56f3d2", "Pugilist", RuleDefinitions.FeatureLimitedUsage.None,
                         RuleDefinitions.AdditionalDamageValueDetermination.Die, RuleDefinitions.AdditionalDamageTriggerCondition.AlwaysActive, RuleDefinitions.AdditionalDamageRequiredProperty.MeleeWeapon,
                         true, RuleDefinitions.DieType.D8, 1, RuleDefinitions.AdditionalDamageType.SameAsBaseDamage, "", RuleDefinitions.AdditionalDamageAdvancement.None, new List<DiceByRank>(),
                         false, AttributeDefinitions.Constitution, 15, RuleDefinitions.EffectSavingThrowType.None, new List<ConditionOperationDescription>(), gui.Build()).AddToDB(),
+                        offhandAttack,
                     },
                     gui.Build())
                     .SetIsActive(IsValid);
--- SolastaCommunityExpansion/Patches/CustomFightingStyle/RulesetCharacterHeroPatcher.cs
+++ SolastaCommunityExpansion/Patches/CustomFightingStyle/RulesetCharacterHeroPatcher.cs
@@ -1,39 +1,40 @@
 ﻿using HarmonyLib;
 using SolastaCommunityExpansion.CustomFeatureDefinitions;
-using SolastaModApi.Infrastructure;
-using System.Collections.Generic;
 
 namespace SolastaCommunityExpansion.Patches.CustomFightingStyle
 {
-    class RulesetCharacterHeroPatcher
-    {
+	class RulesetCharacterHeroPatcher
+	{
 		[HarmonyPatch(typeof(RulesetCharacterHero), "RefreshActiveFightingStyles")]
 		internal static class RulesetCharacterHero_RefreshActiveFightingStyles
 		{
 			internal static void Postfix(RulesetCharacterHero __instance)
 			{
 				foreach (FightingStyleDefinition fightingStyleDefinition in __instance.TrainedFightingStyles)
-                {
+				{
 					if (fightingStyleDefinition is ICustomFightingStyle)
-                    {
+					{
 						bool isActive = (fightingStyleDefinition as ICustomFightingStyle).IsActive(__instance);
 						// We don't know what normal fighting style condition was used or if it was met.
 						// The simplest thing to do is just make sure the active state of this fighting style is handled properly.
 						if (isActive)
-                        {
+						{
 							if (!__instance.ActiveFightingStyles.Contains(fightingStyleDefinition))
-                            {
+							{
 								__instance.ActiveFightingStyles.Add(fightingStyleDefinition);
-                            }
-                        } else
-                        {
+							}
+						} else
+						{
 							if (__instance.ActiveFightingStyles.Contains(fightingStyleDefinition))
 							{
 								__instance.ActiveFightingStyles.Remove(fightingStyleDefinition);
 							}
 						}
-                    }
-                }
+					}
+				}
+
+				// This allows fighting styles to grant powers!
+				__instance.GrantPowers();
 			}
 		}
 	}
