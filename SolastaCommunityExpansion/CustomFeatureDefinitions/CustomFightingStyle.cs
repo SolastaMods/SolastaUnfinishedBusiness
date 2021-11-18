35af09e491dd9d50c12cee536316a7252a88587b
Philip Lee <github@pjlconsultants.co.uk>
2021-11-18 22:20:42 +0000

    Revert change to CustomizableFightingStyle

--- SolastaCommunityExpansion/CustomFeatureDefinitions/CustomFightingStyle.cs
+++ SolastaCommunityExpansion/CustomFeatureDefinitions/CustomFightingStyle.cs
@@ -1,4 +1,5 @@
 ﻿using SolastaModApi;
+using SolastaModApi.Diagnostics;
 using SolastaModApi.Extensions;
 using SolastaModApi.Infrastructure;
 using System.Collections.Generic;
@@ -41,6 +42,40 @@ namespace SolastaCommunityExpansion.CustomFeatureDefinitions
         {
             Definition.SetIsActiveDelegate(del);
             return this;
+        }
+
+        // Can this be removed now?
+        /**
+         * The ModAPI doesn't handle adding extensions of things that aren't feature definitions well.
+         * Override the mod api logic and add this to the appropriate DB.
+         */
+        public new CustomizableFightingStyle AddToDB(bool assertIfDuplicate = true)
+        {
+            Preconditions.IsNotNull(Definition, nameof(Definition));
+            Preconditions.IsNotNullOrWhiteSpace(Definition.Name, "definition.Name");
+            Preconditions.IsNotNullOrWhiteSpace(Definition.GUID, "definition.GUID");
+
+            var db = DatabaseRepository.GetDatabase<FightingStyleDefinition>();
+
+            Assert.IsNotNull(db, $"Database '{typeof(FightingStyleDefinition).Name}' not found.");
+
+            if (assertIfDuplicate)
+            {
+                if (db.HasElement(Definition.name))
+                {
+                    throw new SolastaModApiException(
+                        $"The definition with name '{Definition.name}' already exists in database '{typeof(FightingStyleDefinition).Name}' by name.");
+                }
+
+                if (db.HasElementByGuid(Definition.GUID))
+                {
+                    throw new SolastaModApiException(
+                        $"The definition with name '{Definition.name}' and guid '{Definition.GUID}' already exists in database '{typeof(FightingStyleDefinition).Name}' by GUID.");
+                }
+            }
+
+            db.Add(Definition);
+            return Definition;
         }
     }
 }
