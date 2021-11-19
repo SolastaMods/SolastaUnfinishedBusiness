2c91547b48eb684c076055bbbc7f96d4e08dd34a
leprechaun7377 <chris.p.john@gmail.com>
2021-11-18 22:56:05 -0800

    Update credits for the new release.

--- SolastaCommunityExpansion/Viewers/Displays/CreditsDisplay.cs
+++ SolastaCommunityExpansion/Viewers/Displays/CreditsDisplay.cs
@@ -8,14 +8,14 @@ namespace SolastaCommunityExpansion.Viewers.Displays
         internal static Dictionary<string, string> CreditsTable = new Dictionary<string, string>
         {
             { "Zappastuff", "mod UI work, integration, community organization, level 20, respec" },
-            { "ImpPhil", "monster's health, pause UI, stocks prices, no attunement" },
+            { "ImpPhil", "monster's health, pause UI, stocks prices, no attunement, xp scaling" },
             { "DubhHerder", "crafty feats migration" },
             { "View619", "darkvision, superior dark vision" },
             { "SilverGriffon", "pickpocket, lore friendly names, crafty feats" },
             { "Boofat", "alwaysAlt" },
             { "Myztikrice", "faster time scale" },
-            { "AceHigh", "power attack, reckless fury, no identification" },
-            { "ElAntonious", "Torchbearer and Dual Flurry" },
+            { "AceHigh", "power attack, reckless fury, no identification, Tactician Subclass" },
+            { "ElAntonious", "Torchbearer and Dual Flurry, Arcanist Subclass" },
             { "Narria", "ModKit creator, developer" }
         };
 
