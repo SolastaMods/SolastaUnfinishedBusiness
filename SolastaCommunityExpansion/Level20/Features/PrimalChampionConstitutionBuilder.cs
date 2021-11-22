c473cf9519d2e2f853757f46d992bfae31dff112
Paulo Monteiro <pauloesquilo@gmail.com>
2021-11-22 11:26:48 -0800

    rename some classes to std nomenclature

--- SolastaCommunityExpansion/Level20/Features/PrimalChampionConstitutionBuilder.cs
+++ SolastaCommunityExpansion/Level20/Features/PrimalChampionConstitutionBuilder.cs
@@ -4,12 +4,12 @@ using static SolastaModApi.DatabaseHelper.FeatureDefinitionAttributeModifiers;
 
 namespace SolastaCommunityExpansion.Level20.Features
 {
-    internal class PrimalChampionConstitutionModifierBuilder : BaseDefinitionBuilder<FeatureDefinitionAttributeModifier>
+    internal class PrimalChampionConstitutionBuilder : BaseDefinitionBuilder<FeatureDefinitionAttributeModifier>
     {
         private const string PrimalChampionConstitutionName = "ZSPrimalChampionConstitution";
         private const string PrimalChampionConstitutionGuid = "8fd61e6f5ded4dfea2d8783b16c7ca1c";
 
-        protected PrimalChampionConstitutionModifierBuilder(string name, string guid) : base(AttributeModifierTomeOfAllThings_CON, name, guid)
+        protected PrimalChampionConstitutionBuilder(string name, string guid) : base(AttributeModifierTomeOfAllThings_CON, name, guid)
         {
             Definition.SetModifierValue(4);
             Definition.SetModifierType2(FeatureDefinitionAttributeModifier.AttributeModifierOperation.Additive);
@@ -18,7 +18,7 @@ namespace SolastaCommunityExpansion.Level20.Features
         }
 
         private static FeatureDefinitionAttributeModifier CreateAndAddToDB(string name, string guid)
-            => new PrimalChampionConstitutionModifierBuilder(name, guid).AddToDB();
+            => new PrimalChampionConstitutionBuilder(name, guid).AddToDB();
 
         internal static readonly FeatureDefinitionAttributeModifier PrimalChampionConstitution =
             CreateAndAddToDB(PrimalChampionConstitutionName, PrimalChampionConstitutionGuid);
