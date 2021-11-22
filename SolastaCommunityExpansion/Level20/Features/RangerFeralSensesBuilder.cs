8824a26a4552f7a3ccace2b3e5729eaa277c3ba6
Paulo Monteiro <pauloesquilo@gmail.com>
2021-11-21 22:54:37 -0800

    implement Ranger Feral Senses at level 16

---
+++ SolastaCommunityExpansion/Level20/Features/RangerFeralSensesBuilder.cs
@@ -0,0 +1,25 @@
+﻿using SolastaModApi;
+using SolastaModApi.Extensions;
+using static SolastaModApi.DatabaseHelper.FeatureDefinitionSenses;
+
+namespace SolastaCommunityExpansion.Level20.Features
+{
+    internal class RangerFeralSensesBuilder : BaseDefinitionBuilder<FeatureDefinitionSense>
+    {
+        private const string RangerFeralSensesName = "ZSRangerFeralSenses";
+        private const string RangerFeralSensesGuid = "0e3207505ac04a499477ca1185287117";
+
+        protected RangerFeralSensesBuilder(string name, string guid) : base(SenseSeeInvisible12, name, guid)
+        {
+            Definition.SetSenseRange(6);
+            Definition.GuiPresentation.Description = "Feature/&RangerFeralSensesDescription";
+            Definition.GuiPresentation.Title = "Feature/&RangerFeralSensesTitle";
+        }
+
+        private static FeatureDefinitionSense CreateAndAddToDB(string name, string guid)
+            => new RangerFeralSensesBuilder(name, guid).AddToDB();
+
+        internal static readonly FeatureDefinitionSense RangerFeralSenses =
+            CreateAndAddToDB(RangerFeralSensesName, RangerFeralSensesGuid);
+    }
+}
