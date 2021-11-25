2e1c6cf01e17de4ed9fc742081e51ab9d2cc29cd
Paulo Monteiro <pauloesquilo@gmail.com>
2021-11-24 23:18:02 -0800

    fix inventory UI official bug

---
+++ SolastaCommunityExpansion/Patches/GameUi/InventoryPanelPatcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches
+{
+    [HarmonyPatch(typeof(InventoryPanel), "EndInteraction")]
+    internal static class InventoryPanel_EndInteraction
+    {
+        internal static TA.int3 MyLocationPosition(GameLocationCharacter gameLocationCharacter)
+        {
+            return gameLocationCharacter == null ? TA.int3.zero : gameLocationCharacter.LocationPosition;
+        }
+
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var get_LocationPositionMethod = typeof(GameLocationCharacter).GetMethod("get_LocationPosition");
+            var myLocationPositionMethod = typeof(InventoryPanel_EndInteraction).GetMethod("MyLocationPosition", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.Calls(get_LocationPositionMethod))
+                {
+                    yield return new CodeInstruction(OpCodes.Call, myLocationPositionMethod);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
