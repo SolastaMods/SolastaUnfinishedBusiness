9c902ed649d43a4df017b45f3a86202092bc4ea8
Paulo Monteiro <pauloesquilo@gmail.com>
2021-11-26 21:27:11 -0800

    reorder action should reset sort order

---
+++ SolastaCommunityExpansion/Patches/InventoryManagement/ContainerPanelPatcher.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches
+{
+    internal static class ContainerPanelPatcher
+    {
+        [HarmonyPatch(typeof(ContainerPanel), "OnReorderCb")]
+        internal static class ContainerPanel_OnReorderCb
+        {
+            internal static void Prefix()
+            {
+                Models.InventoryManagementContenxt.Reset(true);
+            }
+        }
+    }
+}
