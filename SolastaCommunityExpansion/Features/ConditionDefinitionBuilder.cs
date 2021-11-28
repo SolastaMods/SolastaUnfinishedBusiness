3983da87933434f423e78081c6ac7f364d84a4b2
RedOrca <redorca.redorca@gmail.com>
2021-11-28 17:33:12 -0600

    Added Path of the Light barbarian subclass

--- SolastaCommunityExpansion/Features/ConditionDefinitionBuilder.cs
+++ SolastaCommunityExpansion/Features/ConditionDefinitionBuilder.cs
@@ -1,4 +1,5 @@
-﻿using SolastaModApi;
+﻿using System;
+using SolastaModApi;
 using SolastaModApi.Extensions;
 using SolastaModApi.Infrastructure;
 using System.Collections.Generic;
@@ -6,8 +7,43 @@ using UnityEngine.AddressableAssets;
 
 namespace SolastaCommunityExpansion.Features
 {
-    public class ConditionDefinitionBuilder : BaseDefinitionBuilder<ConditionDefinition>
+    public class ConditionDefinitionBuilder<TDefinition> : BaseDefinitionBuilder<TDefinition> where TDefinition : ConditionDefinition
     {
+        public ConditionDefinitionBuilder(string name, string guid, Action<TDefinition> modifyDefinition = null) : base(name, guid)
+        {
+            Definition
+                .SetConditionType(RuleDefinitions.ConditionType.Beneficial)
+                .SetAllowMultipleInstances(false)
+                .SetDurationType(RuleDefinitions.DurationType.Minute)
+                .SetDurationParameter(1)
+                .SetConditionStartParticleReference(new AssetReference())
+                .SetConditionParticleReference(new AssetReference())
+                .SetConditionEndParticleReference(new AssetReference())
+                .SetCharacterShaderReference(new AssetReference());
+
+            Definition.SetField("recurrentEffectForms", new List<EffectForm>());
+            Definition.SetField("cancellingConditions", new List<ConditionDefinition>());
+
+            if (modifyDefinition != null)
+            {
+                modifyDefinition(Definition);
+            }
+        }
+
+        public static TDefinition Build(string name, string guid, Action<TDefinition> modifyDefinition = null)
+        {
+            var conditionDefinitionBuilder = new ConditionDefinitionBuilder<TDefinition>(name, guid, modifyDefinition);
+
+            return conditionDefinitionBuilder.AddToDB();
+        }
+    }
+
+    public class ConditionDefinitionBuilder : ConditionDefinitionBuilder<ConditionDefinition>
+    {
+        public ConditionDefinitionBuilder(string name, string guid, Action<ConditionDefinition> modifyDefinition = null) : base(name, guid, modifyDefinition)
+        {
+        }
+
         public ConditionDefinitionBuilder(string name, string guid, List<FeatureDefinition> conditionFeatures, RuleDefinitions.DurationType durationType,
         int durationParameter, bool silent, GuiPresentation guiPresentation) : base(name, guid)
         {
---
+++ SolastaCommunityExpansion/Patches/NotifyConditionRemoval/RulesetCharacterPatcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using SolastaCommunityExpansion.CustomFeatureDefinitions;
+
+namespace SolastaCommunityExpansion.Patches.NotifyConditionRemoval
+{
+    internal static class RulesetCharacterPatcher
+    {
+        [HarmonyPatch(typeof(RulesetCharacter), "Kill")]
+        internal static class RulesetCharacter_Kill
+        {
+            internal static void Prefix(RulesetCharacter __instance)
+            {
+                foreach (KeyValuePair<string, List<RulesetCondition>> keyValuePair in __instance.ConditionsByCategory)
+                {
+                    foreach (RulesetCondition rulesetCondition in keyValuePair.Value)
+                    {
+                        var notifiedDefinition = rulesetCondition?.ConditionDefinition as INotifyConditionRemoval;
+
+                        if (notifiedDefinition != null)
+                        {
+                            notifiedDefinition.BeforeDyingWithCondition(__instance, rulesetCondition);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
