28a1df43cf5e7817f7012cf5a0540c27f98b0795
Philip Lee <github@pjlconsultants.co.uk>
2021-12-05 14:39:16 +0000

    Fix RuleDefinitions.ComputeAdvantage Patch ActionModifier.AttackAdvantageTrend

--- SolastaCommunityExpansion/Patches/SrdAndHouseRulesContext/RuleDefinitionsPatcher.cs
+++ SolastaCommunityExpansion/Patches/SrdAndHouseRulesContext/RuleDefinitionsPatcher.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
+using static RuleDefinitions;
 
 namespace SolastaCommunityExpansion.Patches.SrdAndHouseRulesContext
 {
@@ -9,26 +10,55 @@ namespace SolastaCommunityExpansion.Patches.SrdAndHouseRulesContext
         [HarmonyPatch(typeof(RuleDefinitions), "ComputeAdvantage")]
         internal static class RuleDefinitions_ComputeAdvantage_Patch
         {
-            public static void Postfix(List<RuleDefinitions.TrendInfo> trends, RuleDefinitions.AdvantageType __result)
+            public static void Postfix(List<TrendInfo> trends, ref AdvantageType __result)
             {
                 if (Main.Settings.EnableSRDAdvantageRules)
                 {
+                    var original = __result;
+
                     var hasAdvantage = trends.Any(t => t.value > 0);
                     var hasDisadvantage = trends.Any(t => t.value < 0);
 
                     if (!(hasAdvantage ^ hasDisadvantage))
                     {
-                        __result = RuleDefinitions.AdvantageType.None;
+                        __result = AdvantageType.None;
                     }
                     else if (hasAdvantage)
                     {
-                        __result = RuleDefinitions.AdvantageType.Advantage;
+                        __result = AdvantageType.Advantage;
                     }
                     else
                     {
-                        __result = RuleDefinitions.AdvantageType.Disadvantage;
+                        __result = AdvantageType.Disadvantage;
                     }
+
+                    Main.Log($"ComputeAdvantage:{trends.Where(t => t.value > 0).Sum(t => (int?)t.value) ?? 0}, {hasAdvantage}, Disadvantage:{trends.Where(t => t.value < 0).Sum(t => (int?)t.value) ?? 0}, {hasDisadvantage}, original={original}, updated={__result}.");
                 }
+                else
+                {
+                    Main.Log($"ComputeAdvantage:{trends.Where(t => t.value > 0).Sum(t => (int?)t.value) ?? 0}, Disadvantage:{trends.Where(t => t.value < 0).Sum(t => (int?)t.value) ?? 0}, result={__result}.");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(ActionModifier), "AttackAdvantageTrend", MethodType.Getter)]
+        internal static class ActionModifier_AttackAdvantageTrend_Patch
+        {
+            public static bool Prefix(ref int __result, List<TrendInfo> ___attackAdvantageTrends)
+            {
+                if (Main.Settings.EnableSRDAdvantageRules)
+                {
+                    var advantage = ___attackAdvantageTrends.Any(t => t.value > 0) ? 1 : 0;
+                    var disadvantage = ___attackAdvantageTrends.Any(t => t.value < 0) ? -1 : 0;
+
+                    __result = advantage + disadvantage;
+
+                    Main.Log($"AttackAdvantage:{___attackAdvantageTrends.Where(t => t.value > 0).Sum(t => (int?)t.value) ?? 0}, {advantage}, Disadvantage:{___attackAdvantageTrends.Where(t => t.value < 0).Sum(t => (int?)t.value) ?? 0}, {disadvantage}, updated={__result}.");
+                    return false;
+                }
+
+                Main.Log($"AttackAdvantage:{___attackAdvantageTrends.Where(t => t.value > 0).Sum(t => (int?)t.value) ?? 0}, Disadvantage:{___attackAdvantageTrends.Where(t => t.value < 0).Sum(t => (int?)t.value) ?? 0}, result={__result}.");
+                return true;
             }
         }
     }
