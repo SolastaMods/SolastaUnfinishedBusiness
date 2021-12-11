6823ad0e6213555c24784c525c1698a96a0dfa59
RealBazou <20.100.langlois@gmail.com>
2021-12-11 13:35:57 -0500

    Add 2 subclasses (empty for now)

---
+++ SolastaCommunityExpansion/Subclasses/Witch/GreenWitch.cs
@@ -0,0 +1,45 @@
+﻿using SolastaCommunityExpansion.Features;
+using SolastaModApi;
+using SolastaModApi.BuilderHelpers;
+using SolastaModApi.Extensions;
+using SolastaModApi.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace SolastaCommunityExpansion.Subclasses.Witch
+{
+    internal class GreenWitch : AbstractSubclass
+    {
+        private static Guid SubclassNamespace = new Guid("5d595308-bcf8-4a9f-a9a0-d2ae85c243e7");
+        private readonly CharacterSubclassDefinition Subclass;
+
+        internal override FeatureDefinitionSubclassChoice GetSubclassChoiceList()
+        {
+            DatabaseRepository.GetDatabase<FeatureDefinitionSubclassChoice>().TryGetElement("SubclassChoiceWitchCovens", out FeatureDefinitionSubclassChoice featureDefinitionSubclassChoice);
+            return featureDefinitionSubclassChoice;
+        }
+        internal override CharacterSubclassDefinition GetSubclass()
+        {
+            return Subclass;
+        }
+
+        internal GreenWitch()
+        {
+            // Make Blood Witch subclass
+            CharacterSubclassDefinitionBuilder greenWitch = new CharacterSubclassDefinitionBuilder("GreenWitch", GuidHelper.Create(SubclassNamespace, "GreenWitch").ToString());
+            GuiPresentationBuilder greenWitchPresentation = new GuiPresentationBuilder(
+                "Subclass/&GreenWitchDescription",
+                "Subclass/&GreenWitchTitle");
+            greenWitchPresentation.SetSpriteReference(DatabaseHelper.CharacterSubclassDefinitions.RoguishShadowCaster.GuiPresentation.SpriteReference);
+            greenWitch.SetGuiPresentation(greenWitchPresentation.Build());
+
+
+
+
+
+            // add subclass to db and add subclass to Witch class
+            Subclass = greenWitch.AddToDB();
+        }
+
+    }
+}
---
+++ SolastaCommunityExpansion/Subclasses/Witch/PurpleWitch.cs
@@ -0,0 +1,45 @@
+﻿using SolastaCommunityExpansion.Features;
+using SolastaModApi;
+using SolastaModApi.BuilderHelpers;
+using SolastaModApi.Extensions;
+using SolastaModApi.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace SolastaCommunityExpansion.Subclasses.Witch
+{
+    internal class PurpleWitch : AbstractSubclass
+    {
+        private static Guid SubclassNamespace = new Guid("5d595308-bcf8-4a9f-a9a0-d2ae85c243e7");
+        private readonly CharacterSubclassDefinition Subclass;
+
+        internal override FeatureDefinitionSubclassChoice GetSubclassChoiceList()
+        {
+            DatabaseRepository.GetDatabase<FeatureDefinitionSubclassChoice>().TryGetElement("SubclassChoiceWitchCovens", out FeatureDefinitionSubclassChoice featureDefinitionSubclassChoice);
+            return featureDefinitionSubclassChoice;
+        }
+        internal override CharacterSubclassDefinition GetSubclass()
+        {
+            return Subclass;
+        }
+
+        internal PurpleWitch()
+        {
+            // Make Blood Witch subclass
+            CharacterSubclassDefinitionBuilder purpleWitch = new CharacterSubclassDefinitionBuilder("PurpleWitch", GuidHelper.Create(SubclassNamespace, "PurpleWitch").ToString());
+            GuiPresentationBuilder purpleWitchPresentation = new GuiPresentationBuilder(
+                "Subclass/&PurpleWitchDescription",
+                "Subclass/&PurpleWitchTitle");
+            purpleWitchPresentation.SetSpriteReference(DatabaseHelper.CharacterSubclassDefinitions.RoguishShadowCaster.GuiPresentation.SpriteReference);
+            purpleWitch.SetGuiPresentation(purpleWitchPresentation.Build());
+
+
+
+
+
+            // add subclass to db and add subclass to Witch class
+            Subclass = purpleWitch.AddToDB();
+        }
+
+    }
+}
