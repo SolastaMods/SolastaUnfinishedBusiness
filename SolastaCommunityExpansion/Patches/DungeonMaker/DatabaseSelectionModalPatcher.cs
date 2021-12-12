f0b8649f53dd82d3336e6207d373c4dfa6c09fb3
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-12 06:44:48 -0800

    dungeon maker first commit

---
+++ SolastaCommunityExpansion/Patches/DungeonMaker/DatabaseSelectionModalPatcher.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using UnityEngine;
+
+namespace SolastaCommunityExpansion.Patches
+{
+    internal static class DatabaseSelectionModalPatcher
+    {
+        [HarmonyPatch(typeof(DatabaseSelectionModal), "SelectMonsterDefinition")]
+        [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+        internal static class DatabaseSelectionModalBuildMonsters
+        {
+            internal static void Prefix(DatabaseSelectionModal __instance, List<MonsterDefinition> ___allMonsters)
+            {
+                bool isShiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+                if (Main.Settings.UnleashAllMonsters)
+                {
+                    ___allMonsters.Clear();
+
+                    if (isShiftPressed)
+                    {
+                        foreach (var allElement in DatabaseRepository.GetDatabase<MonsterDefinition>().Where(x => !x.GuiPresentation.Hidden))
+                        {
+                            ___allMonsters.Add(allElement);
+                        }
+
+                        ___allMonsters.Sort(__instance);
+                    }
+                }
+            }
+        }
+
+        // this patch unleashes all monster definitions to be used as NPCs
+        [HarmonyPatch(typeof(DatabaseSelectionModal), "SelectNpcDefinition")]
+        [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+        internal static class DatabaseSelectionModalBuildNpcs
+        {
+            internal static void Prefix(DatabaseSelectionModal __instance, List<MonsterDefinition> ___allNpcs)
+            {
+                bool isShiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+                if (Main.Settings.UnleashAllNPCs)
+                {
+                    ___allNpcs.Clear();
+
+                    if (isShiftPressed)
+                    {
+                        foreach (var allElement in DatabaseRepository.GetDatabase<MonsterDefinition>().Where(x => !x.GuiPresentation.Hidden))
+                        {
+                            ___allNpcs.Add(allElement);
+                        }
+
+                        ___allNpcs.Sort(__instance);
+                    }
+                }
+            }
+        }
+    }
+}
