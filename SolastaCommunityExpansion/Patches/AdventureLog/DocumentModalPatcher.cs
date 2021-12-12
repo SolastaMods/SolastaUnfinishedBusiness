05ffadf553edae5684f006fb83258739a4344df6
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-12 04:18:01 -0800

    make patch a Postfix

--- SolastaCommunityExpansion/Patches/AdventureLog/DocumentModalPatcher.cs
+++ SolastaCommunityExpansion/Patches/AdventureLog/DocumentModalPatcher.cs
@@ -7,7 +7,7 @@ namespace SolastaCommunityExpansion.Patches
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     internal static class DocumentModal_Show
     {
-        internal static void Prefix(GuiEquipmentItem guiEquipmentItem)
+        internal static void Postfix(GuiEquipmentItem guiEquipmentItem)
         {
             if (Main.Settings.EnableAdventureLogDocuments)
             {
