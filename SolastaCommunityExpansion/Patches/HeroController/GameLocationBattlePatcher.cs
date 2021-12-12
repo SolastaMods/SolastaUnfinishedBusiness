1d105e73c9e6cd67035fc86b2c073d43463457e0
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-12 11:14:07 -0800

    fix class name

--- SolastaCommunityExpansion/Patches/HeroController/GameLocationBattlePatcher.cs
+++ SolastaCommunityExpansion/Patches/HeroController/GameLocationBattlePatcher.cs
@@ -7,7 +7,7 @@ namespace SolastaCommunityExpansion.Patches.HeroController
     {
         [HarmonyPatch(typeof(GameLocationBattle), "CheckVictoryOrDefeat")]
         [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-        internal static class GameLocationBattleCheckVictoryOrDefeat
+        internal static class GameLocationBattle_CheckVictoryOrDefeat
         {
             internal static void Prefix()
             {
