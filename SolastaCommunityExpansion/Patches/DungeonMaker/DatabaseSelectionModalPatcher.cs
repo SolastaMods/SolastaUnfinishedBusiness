d988c9be2ffde6991b8bf0c5ea6fa2413675a3ad
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-12 09:32:36 -0800

    fix nomenclature

--- SolastaCommunityExpansion/Patches/DungeonMaker/DatabaseSelectionModalPatcher.cs
+++ SolastaCommunityExpansion/Patches/DungeonMaker/DatabaseSelectionModalPatcher.cs
@@ -10,7 +10,7 @@ namespace SolastaCommunityExpansion.Patches
     {
         [HarmonyPatch(typeof(DatabaseSelectionModal), "SelectMonsterDefinition")]
         [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-        internal static class DatabaseSelectionModalBuildMonsters
+        internal static class DatabaseSelectionModal_BuildMonsters
         {
             internal static void Prefix(DatabaseSelectionModal __instance, List<MonsterDefinition> ___allMonsters)
             {
@@ -36,7 +36,7 @@ namespace SolastaCommunityExpansion.Patches
         // this patch unleashes all monster definitions to be used as NPCs
         [HarmonyPatch(typeof(DatabaseSelectionModal), "SelectNpcDefinition")]
         [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-        internal static class DatabaseSelectionModalBuildNpcs
+        internal static class DatabaseSelectionModal_BuildNpcs
         {
             internal static void Prefix(DatabaseSelectionModal __instance, List<MonsterDefinition> ___allNpcs)
             {
