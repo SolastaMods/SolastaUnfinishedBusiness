08a8f840536514b2f99812488cc1e6565103502d
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-12 19:09:14 -0800

    fix books display

---
+++ SolastaCommunityExpansion/Patches/BookDisplayFixers/TextBreakerPatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SolastaCommunityExpansion.Patches.BookDisplayFixes
+{
+    [HarmonyPatch(typeof(TextBreaker), "BreakdownFragments")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class TextBreaker_BreakdownFragments
+    {
+        internal static void Prefix(ref string textLine)
+        {
+            textLine = textLine.Replace($"\r", "");
+        }
+    }
+}
