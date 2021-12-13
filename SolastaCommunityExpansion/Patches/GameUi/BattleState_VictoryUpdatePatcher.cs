0f102a915f3267b2f28f18fbd65c1e45ad2e87ff
Philip Lee <github@pjlconsultants.co.uk>
2021-12-13 15:47:31 +0000

    Tidy

--- SolastaCommunityExpansion/Patches/GameUi/BattleState_VictoryUpdatePatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUi/BattleState_VictoryUpdatePatcher.cs
@@ -13,9 +13,7 @@ namespace SolastaCommunityExpansion.Patches.GameUi
 
             if (Gui.Battle == null) { return; }
 
-            INarrativeDirectionService narrativeService = ServiceRepository.GetService<INarrativeDirectionService>();
-
-            if (narrativeService != null && narrativeService.CurrentSequence != null)
+            if (ServiceRepository.GetService<INarrativeDirectionService>()?.CurrentSequence != null)
             {
                 // Don't pause in the middle of a narrative sequence it hangs the game.
                 // For example during the tutorial shoving the rock to destroy the bridge transitions
