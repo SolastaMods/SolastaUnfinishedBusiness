8b7b1283a1e3e4c20af6f9be5b617cf2f5ea7e3c
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-14 23:32:57 -0800

    reorg class names / split patches in feature folders

---
+++ SolastaCommunityExpansion/Patches/FutureFeatureSorting/CharacterStageDeitySelectionPanelPatcher.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SolastaCommunityExpansion.Patches.FutureFeatureSorting
+{
+    [HarmonyPatch(typeof(CharacterStageDeitySelectionPanel), "FillSubclassFeatures")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class CharacterStageDeitySelectionPanel_FillSubclassFeatures
+    {
+        internal static void Prefix(CharacterSubclassDefinition currentSubclassDefinition)
+        {
+            if (!Main.Settings.FutureFeatureSorting)
+            {
+                return;
+            }
+            currentSubclassDefinition.FeatureUnlocks.Sort((a, b) => a.Level - b.Level);
+        }
+    }
+}
