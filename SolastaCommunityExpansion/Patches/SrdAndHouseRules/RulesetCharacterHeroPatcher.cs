0cb92cc11a9a03187e913e981523db01cc1cf2fe
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-14 22:57:18 -0800

    reorg files

--- SolastaCommunityExpansion/Patches/SrdAndHouseRules/RulesetCharacterHeroPatcher.cs
+++ SolastaCommunityExpansion/Patches/SrdAndHouseRules/RulesetCharacterHeroPatcher.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
 
-namespace SolastaCommunityExpansion.Patches.GameUi
+namespace SolastaCommunityExpansion.Patches.SrdAndHouseRules
 {
     [HarmonyPatch(typeof(RulesetCharacterHero), "GrantItem")]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
