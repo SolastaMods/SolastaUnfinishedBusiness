5d33a716ef9460c2fc305762d4855486553e8677
leprechaun7377 <chris.p.john@gmail.com>
2021-12-14 22:54:36 -0800

    Make it an option to have the arcane weapon ability recharge on a short rest.

--- SolastaCommunityExpansion/Settings.cs
+++ SolastaCommunityExpansion/Settings.cs
@@ -200,5 +200,6 @@ namespace SolastaCommunityExpansion
         public const InputCommands.Id CTRL_SHIFT_E = (InputCommands.Id)44440005;
 
         public bool EnableControllersOverride { get; set; }
+        public bool ArcaneFighterEnchantWeaponRechargeShortRest { get; set; }
     }
 }
