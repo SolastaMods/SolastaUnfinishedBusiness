e84d457484e48109c7ad0f31efa5e9d55c345983
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-17 16:29:52 -0800

    keeps characters panel open with hero selected after level up / char. creation

---
+++ SolastaCommunityExpansion/Patches/GameUiLevelUp/CharacterEditionScreenPatcher.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SolastaCommunityExpansion.Patches
+{
+    [HarmonyPatch(typeof(CharacterEditionScreen), "OnFinishCb")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class CharacterEditionScreen_OnFinishCb
+    {
+        internal static string HeroName { get; set; }
+
+        internal static void Prefix()
+        {
+            if (Gui.Game == null && Main.Settings.KeepCharactersPanelOpenAndHeroSelectedOnLevelUp)
+            {
+                HeroName = ServiceRepository.GetService<ICharacterBuildingService>().HeroCharacter.Name;
+            }
+            else
+            {
+                HeroName = null;
+            }
+        }
+    }
+}
