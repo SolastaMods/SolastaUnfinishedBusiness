b16c4c393080a39921dd6461a4b1b0ba214e3f82
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-17 22:58:37 -0800

    Thug first commit

---
+++ SolastaCommunityExpansion/Patches/NegativeFeature/CharacterBuildingManagerPatcher.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SolastaCommunityExpansion.Patches
+{
+    [HarmonyPatch(typeof(CharacterBuildingManager), "GrantFeatures")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class CharacterBuildingManager_GrantFeatures
+    {
+        internal static void Prefix(CharacterBuildingManager __instance, List<FeatureDefinition> grantedFeatures)
+        {
+            foreach (var grantedFeature in grantedFeatures)
+            {
+                if (grantedFeature is Subclasses.Rogue.Thug.NegativeFeatureDefinition negativeFeatureDefinition)
+                {
+                    foreach (var activeFeature in __instance.HeroCharacter.ActiveFeatures)
+                    {
+                        activeFeature.Value.RemoveAll(feature => feature == negativeFeatureDefinition.FeatureToRemove);
+                    }
+                }
+            }
+        }
+    }
+
+}
