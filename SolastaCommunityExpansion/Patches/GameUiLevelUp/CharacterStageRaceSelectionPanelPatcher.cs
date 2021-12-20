57b79e115f7c5a70a7388beeb04f8833c70c1d21
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-20 09:59:38 -0800

    add more sorting / tweak existing ones

---
+++ SolastaCommunityExpansion/Patches/GameUiLevelUp/CharacterStageRaceSelectionPanelPatcher.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SolastaCommunityExpansion.Patches.GameUiLevelUp
+{
+    [HarmonyPatch(typeof(CharacterStageRaceSelectionPanel), "Compare")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class CharacterStageRaceSelectionPanel_Compare
+    {
+        internal static void Postfix(CharacterRaceDefinition left, CharacterRaceDefinition right, ref int __result)
+        {
+            if (!Main.Settings.FutureFeatureSorting)
+            {
+                return;
+            }
+
+            __result = left.FormatTitle().CompareTo(right.FormatTitle());
+        }
+    }
+}
