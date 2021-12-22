883c2ecd0c01f9e192499e172fb992ae948a81e2
Philip Lee <github@pjlconsultants.co.uk>
2021-12-22 10:02:18 +0000

    Fix exit

--- SolastaCommunityExpansion/Patches/Camera/CameraControllerLocationPatcher.cs
+++ SolastaCommunityExpansion/Patches/Camera/CameraControllerLocationPatcher.cs
@@ -16,48 +16,46 @@ namespace SolastaCommunityExpansion.Patches.Camera
         /// </summary>
         internal static bool Prefix(CameraControllerLocation __instance, GameLocationCharacter character)
         {
-            //
-            // TODO @IMPPHIL: FIX EXIT ROUTE
-            //
-            try
+            if (!Main.Settings.DontFollowCharacterInBattle)
             {
-                if (Main.Settings.DontFollowCharacterInBattle)
-                {
-                    if (!__instance 
-                        || character == null 
-                        || !__instance.CurrentCameraMode 
-                        || __instance.CurrentCameraMode.CameraService == null
-                        || !__instance.CurrentCameraMode.CameraService.MainCamera)
-                    {
-                        return true;
-                    }
-
-                    var lp = character.LocationPosition;
-                    var sp = __instance.CurrentCameraMode.CameraService.MainCamera.WorldToScreenPoint(new Vector3(lp.x, lp.y, lp.z));
-
-                    Main.Log($"{character.Name}: ({sp.x},{sp.y},{sp.z})");
-
-                    var w = Screen.width;
-                    var h = Screen.height;
-
-                    var margin = Main.Settings.DontFollowMargin / 100f;
-
-                    if (sp.x < w * margin || sp.x > w * (1 - margin) || sp.y < h * margin || sp.y > h * (1 - margin))
-                    {
-                        Main.Log($"CameraControllerLocation_FollowCharacterForBattle (off screen): {character.Name}");
-                        return true;
-                    }
-
-                    Main.Log($"CameraControllerLocation_FollowCharacterForBattle: {character.Name} - ignored.");
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                Main.Error(ex);
+                return true;
             }
 
-            return true;
+            // Ensure all Unity objects are valid
+            if (!__instance
+                || character == null
+                || !__instance.CurrentCameraMode
+                || __instance.CurrentCameraMode.CameraService == null
+                || !__instance.CurrentCameraMode.CameraService.MainCamera)
+            {
+                return true;
+            }
+
+            // Get character's location in screen co-ords
+            var characterLocation = character.LocationPosition;
+
+            var screenPoint = __instance
+                .CurrentCameraMode.CameraService.MainCamera
+                .WorldToScreenPoint(new Vector3(characterLocation.x, characterLocation.y, characterLocation.z));
+
+            var width = Screen.width;
+            var height = Screen.height;
+            var margin = Main.Settings.DontFollowMargin / 100f;
+
+            var followCharacter = ShouldFollowCharacter();
+
+            Main.Log($"CameraControllerLocation_FollowCharacterForBattle {(followCharacter ? "(follow)" : "don't follow")}: {character.Name}");
+
+            // return true to allow follow character code to run/false to disable
+            return followCharacter;
+
+            bool ShouldFollowCharacter()
+            {
+                return screenPoint.x < width * margin
+                    || screenPoint.x > width * (1 - margin)
+                    || screenPoint.y < height * margin
+                    || screenPoint.y > height * (1 - margin);
+            }
         }
     }
 }
