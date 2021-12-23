4ac5440c14f5c4db30b16daa1d54599371deb4ad
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-23 15:55:15 -0800

    disable this patch but keep file around in case the other patch gets deprecated

--- SolastaCommunityExpansion/Patches/GameUiCharactersPanel/CharacterFilteringGroupPatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUiCharactersPanel/CharacterFilteringGroupPatcher.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using HarmonyLib;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Reflection.Emit;
-using HarmonyLib;
 
 namespace SolastaCommunityExpansion.Patches.GameUiCharactersPanel
 {
     internal static class CharacterFilteringGroupPatcher
     {
+        //
+        // ATT: this patch is disabled as BugFix.CharacterSelectionModalPatcher is doing the same. Left here in case that bug gets fixed and we need this particular change as a standalone patch
+        //
+
         // ensures MC heroes are correctly sorted by level on character selection modal
-        [HarmonyPatch(typeof(CharacterFilteringGroup), "Compare")]
+        // [HarmonyPatch(typeof(CharacterFilteringGroup), "Compare")]
         [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
         internal static class CharacterFilteringGroup_Compare
         {
@@ -18,26 +20,13 @@ namespace SolastaCommunityExpansion.Patches.GameUiCharactersPanel
                 return levels.Sum();
             }
 
-            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            internal static void Postfix(RulesetCharacterHero.Snapshot left, RulesetCharacterHero.Snapshot right, bool ___sortInverted, SortGroup.Category ___sortCategory, ref int __result)
             {
-                var bypass = 0;
-                var myLevelMethod = typeof(CharacterFilteringGroup_Compare).GetMethod("MyLevels");
-                var levelsField = typeof(RulesetCharacterHero.Snapshot).GetField("Levels");
-
-                foreach (var instruction in instructions)
+                if (___sortCategory == SortGroup.Category.CharacterLevel)
                 {
-                    if (bypass-- > 0)
-                    {
-                        continue;
-                    }
+                    var sortSign = ___sortInverted ? -1 : 1;
 
-                    yield return instruction;
-
-                    if (instruction.LoadsField(levelsField))
-                    {
-                        yield return new CodeInstruction(OpCodes.Call, myLevelMethod);
-                        bypass = 2;
-                    }
+                    __result = sortSign * left.Levels.Sum().CompareTo(right.Levels.Sum());
                 }
             }
         }
