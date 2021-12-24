9e7684ffce0b34ac18123c4afe0787b2a3367490
leprechaun7377 <chris.p.john@gmail.com>
2021-12-24 00:48:33 -0800

    Rough idea for how to provide more flexibility in spell/power effects.

---
+++ SolastaCommunityExpansion/CustomFeatureDefinitions/CustomEffectForm.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace SolastaCommunityExpansion.CustomFeatureDefinitions
+{
+    class CustomEffectForm : EffectForm
+    {
+        public delegate bool ApplyDelegate(EffectForm effectForm,
+            RulesetImplementationDefinitions.ApplyFormsParams formsParams,
+            bool retargeting,
+            bool proxyOnly,
+            bool forceSelfConditionOnly);
+        private ApplyDelegate Apply;
+
+        internal void SetApplyDelegate(ApplyDelegate del)
+        {
+            Apply = del;
+        }
+
+        public void ApplyForm(EffectForm effectForm,
+            RulesetImplementationDefinitions.ApplyFormsParams formsParams,
+            bool retargeting,
+            bool proxyOnly,
+            bool forceSelfConditionOnly)
+        {
+            if (Apply != null)
+            {
+                Apply(effectForm, formsParams, retargeting, proxyOnly, forceSelfConditionOnly);
+            }
+        }
+    }
+}
---
+++ SolastaCommunityExpansion/Patches/CustomEffectForm/RulesetImplementationManagerPatcher.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches.CustomEffectForm
+{
+    [HarmonyPatch(typeof(RulesetImplementationManager), "ApplyEffectForms")]
+    internal static class RulesetImplementationManagerApplyCustomEffectForm
+    {
+        public static void Postfix(List<EffectForm> effectForms,
+            RulesetImplementationDefinitions.ApplyFormsParams formsParams,
+            bool retargeting = false,
+            bool proxyOnly = false,
+            bool forceSelfConditionOnly = false)
+        {
+           foreach(EffectForm effectForm in effectForms)
+            {
+                if (effectForm is CustomFeatureDefinitions.CustomEffectForm customEffect)
+                {
+                    customEffect.ApplyForm(effectForm, formsParams, retargeting, proxyOnly, forceSelfConditionOnly);
+                }
+            }
+        }
+    }
+}
