ba4c8f66c8f8807d4c65268a422478dc6bb8358d
Paulo Monteiro <pauloesquilo@gmail.com>
2021-12-24 09:51:11 -0800

    late load FightingStyles / Feats / Flex. Backgrounds & Races / Initial Feats

---
+++ SolastaCommunityExpansion/Patches/MainMenuScreenPatcher.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using SolastaCommunityExpansion.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SolastaCommunityExpansion.Patches
+{
+    [HarmonyPatch(typeof(MainMenuScreen), "OnEndShow")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class MainMenuScreen_OnEndShow
+    {
+        internal static void Postfix()
+        {
+            if (!Main.LateEnabled && Main.Enabled)
+            {
+
+            }
+
+            FightingStyleContext.Load(); // Fighting Styles should be loaded before feats in order to generate feats of new fighting styles
+            FeatsContext.Load();
+            FlexibleBackgroundsContext.Load();
+            FlexibleRacesContext.Load();
+            InitialChoicesContext.Load();
+
+            Main.LateEnabled = true;
+        }
+    }
+}
