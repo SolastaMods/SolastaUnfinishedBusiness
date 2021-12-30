76ebb3507bb800b2d4cd730f4221e5009f25d395
leprechaun7377 <chris.p.john@gmail.com>
2021-12-29 23:16:15 -0800

    New custom code feature definition. Using it to for the barbarian feature Primal Champion.

---
+++ SolastaCommunityExpansion/CustomFeatureDefinitions/FeatureDefinitionCustomCode.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SolastaCommunityExpansion.CustomFeatureDefinitions
+{
+    /**
+     * This adds the ability to do fully custom EffectForms. If possible you should use the standard EffectForms.
+     * Damage and healing done through this CustomEffectForm will not trigger the proper events.
+     */
+    public abstract class FeatureDefinitionCustomCode : FeatureDefinition
+    {
+        // Use this to add the feature to the character.
+        public abstract void ApplyFeature(RulesetCharacterHero hero);
+
+        // Use this to remove the feature from the character. In particular this is used to allow level down functionality.
+        public abstract void RemoveFeature(RulesetCharacterHero hero);
+    }
+}
---
+++ SolastaCommunityExpansion/Level20/Features/PrimalChampionBuilder.cs
@@ -0,0 +1,60 @@
+﻿using SolastaCommunityExpansion.CustomFeatureDefinitions;
+using SolastaModApi;
+using SolastaModApi.Extensions;
+using static SolastaModApi.DatabaseHelper.FeatureDefinitionAttributeModifiers;
+
+namespace SolastaCommunityExpansion.Level20.Features
+{
+    internal class PrimalChampionBuilder : BaseDefinitionBuilder<PrimalChampion>
+    {
+        private const string PrimalChampionName = "ZSPrimalChampion";
+        private const string PrimalChampionGuid = "118a5ea1-8a19-4bee-9db1-7a2464c8e7b5";
+
+        protected PrimalChampionBuilder(string name, string guid) : base(name, guid)
+        {
+
+            Definition.GuiPresentation.Description = "Feature/&PrimalChampionDescription";
+            Definition.GuiPresentation.Title = "Feature/&PrimalChampionTitle";
+        }
+
+        private static PrimalChampion CreateAndAddToDB(string name, string guid)
+            => new PrimalChampionBuilder(name, guid).AddToDB();
+
+        internal static readonly PrimalChampion PrimalChampion =
+            CreateAndAddToDB(PrimalChampionName, PrimalChampionGuid);
+    }
+
+    class PrimalChampion : FeatureDefinitionCustomCode
+    {
+        public override void ApplyFeature(RulesetCharacterHero hero)
+        {
+            ModifyAttributeAndMax(hero, AttributeDefinitions.Strength, 4);
+            ModifyAttributeAndMax(hero, AttributeDefinitions.Constitution, 4);
+
+            hero.RefreshAll();
+        }
+
+        public override void RemoveFeature(RulesetCharacterHero hero)
+        {
+            ModifyAttributeAndMax(hero, AttributeDefinitions.Strength, -4);
+            ModifyAttributeAndMax(hero, AttributeDefinitions.Constitution, -4);
+
+            hero.RefreshAll();
+        }
+
+        private void ModifyAttributeAndMax(RulesetCharacterHero hero, string attributeName, int amount)
+        {
+            RulesetAttribute attribute = hero.GetAttribute(attributeName);
+            attribute.BaseValue += amount;
+            attribute.MaxValue += amount;
+            attribute.MaxEditableValue += amount;
+            attribute.Refresh();
+
+            RulesetActor.AbilityScoreIncreasedHandler abilityScoreIncreased = hero.AbilityScoreIncreased;
+            if (abilityScoreIncreased != null)
+            {
+                abilityScoreIncreased((RulesetActor)hero, attributeName, amount, amount);
+            }
+        }
+    }
+}
