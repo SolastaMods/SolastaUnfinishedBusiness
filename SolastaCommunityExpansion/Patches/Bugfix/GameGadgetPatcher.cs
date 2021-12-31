ef69bd1402ed79808f84fb3b252aa0cfc0c477e1
Philip Lee <github@pjlconsultants.co.uk>
2021-12-31 15:45:26 +0000

    Fix GameGadget.CheckIsEnabled

---
+++ SolastaCommunityExpansion/Patches/Bugfix/GameGadgetPatcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using SolastaCommunityExpansion.Helpers;
+
+namespace SolastaCommunityExpansion.Patches.Bugfix
+{
+
+    [HarmonyPatch(typeof(GameGadget), "CheckIsEnabled")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class GameGadget_CheckIsEnabled
+    {
+        public static bool Prefix(GameGadget __instance, ref bool __result)
+        {
+            if (!Main.Settings.BugFixGameGadgetCheckIsEnabled)
+            {
+                return true;
+            }
+
+            var result = __result;
+            __result = __instance.IsEnabled();
+            Main.Log($"CheckIsEnabled: {__instance.UniqueNameId}, {result}, {__result}");
+            return false;
+        }
+    }
+}
