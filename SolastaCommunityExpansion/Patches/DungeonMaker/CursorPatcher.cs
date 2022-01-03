31ad63a60a1d17a0156c12ea84d7d67a8218e2b1
Philip Lee <github@pjlconsultants.co.uk>
2022-01-03 16:57:14 +0000

    Cancel drag on right click

---
+++ SolastaCommunityExpansion/Patches/DungeonMaker/CursorPatcher.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using SolastaModApi.Infrastructure;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SolastaCommunityExpansion.Patches.DungeonMaker
+{
+    [HarmonyPatch(typeof(Cursor), "OnClickSecondaryPointer")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class Cursor_OnClickSecondaryPointer
+    {
+        internal static void Postfix(Cursor __instance)
+        {
+            if (Main.Settings.EnableCancelCursorDragOnRightMouseClick && __instance is CursorEditor)
+            {
+                var userLocationEditorScreen = __instance.GetField<UserLocationEditorScreen>("userLocationEditorScreen");
+
+                userLocationEditorScreen?.HandleInput(InputCommands.Id.Cancel);
+            }
+        }
+    }
+}
