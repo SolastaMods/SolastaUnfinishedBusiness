5eda41ed439c498a5f02a7082950366eab4fd4ff
Paulo Monteiro <pauloesquilo@gmail.com>
2022-01-03 07:43:20 -0800

    consolidate Featssorting feature into FeatSubPanel Bind patch

--- SolastaCommunityExpansion/Patches/GameUiLevelUp/FeatSubPanelPatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUiLevelUp/FeatSubPanelPatcher.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Reflection.Emit;
 using HarmonyLib;
 using SolastaModApi.Infrastructure;
 using UnityEngine;
@@ -18,6 +17,11 @@ namespace SolastaCommunityExpansion.Patches.GameUiLevelUp
 
             ___relevantFeats.SetRange(dbFeatDefinition.Where(x => !x.GuiPresentation.Hidden));
 
+            if (Main.Settings.EnableSortingFeats)
+            {
+                ___relevantFeats?.Sort((a, b) => a.FormatTitle().CompareTo(b.FormatTitle()));
+            }
+
             while (___table.childCount < ___relevantFeats.Count)
             {
                 Gui.GetPrefabFromPool(___itemPrefab, ___table);
@@ -26,36 +30,6 @@ namespace SolastaCommunityExpansion.Patches.GameUiLevelUp
             while (___table.childCount > ___relevantFeats.Count)
             {
                 Gui.ReleaseInstanceToPool(___table.GetChild(___table.childCount - 1).gameObject);
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(FeatSubPanel), "RuntimeLoaded")]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class FeatSubPanel_RuntimeLoaded
-    {
-        internal static void Sort(List<FeatDefinition> relevantFeats)
-        {
-            relevantFeats?.Sort((a, b) => a.FormatTitle().CompareTo(b.FormatTitle()));
-        }
-
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            foreach (CodeInstruction instruction in instructions)
-            {
-                yield return instruction;
-
-                if (Main.Settings.EnableSortingFeats && instruction.opcode == OpCodes.Pop)
-                {
-#pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
-                    var relevantFeatsField = typeof(FeatSubPanel).GetField("relevantFeats", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                    var sortMethod = typeof(FeatSubPanel_RuntimeLoaded).GetMethod("Sort", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
-#pragma warning restore S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
-
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Ldfld, relevantFeatsField);
-                    yield return new CodeInstruction(OpCodes.Call, sortMethod);
-                }
             }
         }
     }
