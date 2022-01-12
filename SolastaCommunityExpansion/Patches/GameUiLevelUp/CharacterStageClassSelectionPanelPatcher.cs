5f86289c0a099fa0fc76175081c3af2b15449bbb
Paulo Monteiro <pauloesquilo@gmail.com>
2022-01-11 18:29:31 -0800

    reorg sorting

--- SolastaCommunityExpansion/Patches/GameUiLevelUp/CharacterStageClassSelectionPanelPatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUiLevelUp/CharacterStageClassSelectionPanelPatcher.cs
@@ -3,21 +3,6 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace SolastaCommunityExpansion.Patches.GameUiLevelUp
 {
-    [HarmonyPatch(typeof(CharacterStageClassSelectionPanel), "FillClassFeatures")]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class CharacterStageClassSelectionPanel_FillSubclassFeatures
-    {
-        internal static void Prefix(CharacterClassDefinition classDefinition)
-        {
-            if (!Main.Settings.EnableSortingFutureFeatures)
-            {
-                return;
-            }
-
-            classDefinition.FeatureUnlocks.Sort((a, b) => a.Level - b.Level);
-        }
-    }
-
     [HarmonyPatch(typeof(CharacterStageClassSelectionPanel), "Compare")]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     internal static class CharacterStageClassSelectionPanel_Compare
