3903965dc654fcbf455d6f714bb7f2a18879c951
Paulo Monteiro <pauloesquilo@gmail.com>
2022-01-14 22:36:41 -0800

    optimization

--- SolastaCommunityExpansion/Patches/GameUiScreenMap/WorldGadgetPatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUiScreenMap/WorldGadgetPatcher.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
-using static SolastaModApi.DatabaseHelper.GadgetDefinitions;
 
 namespace SolastaCommunityExpansion.Patches.GameUiScreenMap
 {
@@ -9,29 +8,6 @@ namespace SolastaCommunityExpansion.Patches.GameUiScreenMap
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     internal static class WorldGadget_SetHighlightVisibility
     {
-        //internal static void Prefix_Original_Solution_Faster_But_Not_Precise(WorldGadget __instance, ref bool visible)
-        //{
-        //    // IsCellPerceivedByCharacter returns false for any wall cell so ignoring wall placeable gadgets as well
-        //    if (!Main.Settings.HideGadgetsInFogOfWar || !visible || !__instance.IsUserGadget || __instance.UserGadget.GadgetBlueprint.WallPlacement)
-        //    {
-        //        return;
-        //    }
-
-        //    var gameLocationCharacterService = ServiceRepository.GetService<IGameLocationCharacterService>();
-        //    var gameLocationVisibilityService = ServiceRepository.GetService<IGameLocationVisibilityService>();
-        //    var position = new TA.int3((int)__instance.GameGadget.FeedbackPosition.x, (int)__instance.GameGadget.FeedbackPosition.y, (int)__instance.GameGadget.FeedbackPosition.z);
-
-        //    foreach (var gameLocationCharacter in gameLocationCharacterService.PartyCharacters)
-        //    {
-        //        visible = gameLocationVisibilityService.IsCellPerceivedByCharacter(position, gameLocationCharacter);
-
-        //        if (visible)
-        //        {
-        //            return;
-        //        }
-        //    }
-        //}
-
         internal static void Prefix(WorldGadget __instance, ref bool visible)
         {
             if (!Main.Settings.HideGadgetsInFogOfWar || !visible || !__instance.IsUserGadget)
@@ -39,31 +15,29 @@ namespace SolastaCommunityExpansion.Patches.GameUiScreenMap
                 return;
             }
 
-            TA.int3 position;
             var gameLocationCharacterService = ServiceRepository.GetService<IGameLocationCharacterService>();
             var gameLocationVisibilityService = ServiceRepository.GetService<IGameLocationVisibilityService>();
             var feedbackPosition = __instance.GameGadget.FeedbackPosition;
 
-            // scan the gadget cell
-            position = new TA.int3((int)feedbackPosition.x, (int)feedbackPosition.y, (int)feedbackPosition.z);
-
-            foreach (var gameLocationCharacter in gameLocationCharacterService.PartyCharacters)
+            // activators aren't detected in their original position so we handle them in a different way
+            if (!__instance.GadgetDefinition == SolastaModApi.DatabaseHelper.GadgetDefinitions.Activator)
             {
-                visible = gameLocationVisibilityService.IsCellPerceivedByCharacter(position, gameLocationCharacter);
+                var position = new TA.int3((int)feedbackPosition.x, (int)feedbackPosition.y, (int)feedbackPosition.z);
 
-                if (visible)
+                foreach (var gameLocationCharacter in gameLocationCharacterService.PartyCharacters)
                 {
-                    return;
-                }
-            }
+                    visible = gameLocationVisibilityService.IsCellPerceivedByCharacter(position, gameLocationCharacter);
 
-            // doors and wall gadgets need some special treatment as they aren't perceived in their original positions
-            if (!__instance.GadgetDefinition == Door || !__instance.UserGadget.GadgetBlueprint.WallPlacement)
-            {
+                    if (visible)
+                    {
+                        return;
+                    }
+                }
+
                 return;
             }
 
-            // scan doors or wall gadgets surrounding cells
+            // scan activators surrounding cells
             for (var x = -1; x <= 1; x++)
             {
                 for (var z = -1; z <= 1; z++)
@@ -74,7 +48,7 @@ namespace SolastaCommunityExpansion.Patches.GameUiScreenMap
                         continue;
                     }
 
-                    position = new TA.int3((int)feedbackPosition.x + x, (int)feedbackPosition.y, (int)feedbackPosition.z + z);
+                    var position = new TA.int3((int)feedbackPosition.x + x, (int)feedbackPosition.y, (int)feedbackPosition.z + z);
 
                     foreach (var gameLocationCharacter in gameLocationCharacterService.PartyCharacters)
                     {
