08278d431dff4fbf6be356801366b0e809990ea9
Paulo Monteiro <pauloesquilo@gmail.com>
2022-01-26 19:21:58 -0800

    refactor patch as transpiler

--- SolastaCommunityExpansion/Patches/SrdAndHouseRules/InventoryShortcutsPanelPatcher.cs
+++ SolastaCommunityExpansion/Patches/SrdAndHouseRules/InventoryShortcutsPanelPatcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Emit;
 using HarmonyLib;
 
 namespace SolastaCommunityExpansion.Patches.SrdAndHouseRules
@@ -9,64 +11,60 @@ namespace SolastaCommunityExpansion.Patches.SrdAndHouseRules
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     internal static class InventoryShortcutsPanel_OnCastLightCb
     {
-        public static bool InOnCastLightCb { get; private set; }
-
-        public static void Prefix()
+        public static bool MyTryFindTargetWieldedItem(RulesetCharacterHero rulesetCharacterHero, out RulesetItem targetItem)
         {
-            InOnCastLightCb = true;
-        }
-
-        public static void Postfix()
-        {
-            InOnCastLightCb = false;
-        }
-    }
-
-    [HarmonyPatch(typeof(RulesetCharacterHero), "TryFindTargetWieldedItem")]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class RulesetCharacterHero_TryFindTargetWieldedItem
-    {
-        public static bool Prefix(RulesetInventory ___characterInventory, out RulesetItem targetItem, out bool __result)
-        {
-            targetItem = null;
-            __result = false;
-
-            if (!Main.Settings.QuickCastLightCantripOnWornItemsFirst || !InventoryShortcutsPanel_OnCastLightCb.InOnCastLightCb)
+            if (!Main.Settings.QuickCastLightCantripOnWornItemsFirst)
             {
-                return true;
+                return rulesetCharacterHero.TryFindTargetWieldedItem(out targetItem);
             }
 
+            var characterInventory = rulesetCharacterHero.CharacterInventory;
+
             // Head
-            RulesetInventorySlot rulesetInventorySlot1 = ___characterInventory.InventorySlotsByName[EquipmentDefinitions.SlotTypeHead];
+            RulesetInventorySlot rulesetInventorySlot1 = characterInventory.InventorySlotsByName[EquipmentDefinitions.SlotTypeHead];
             if (rulesetInventorySlot1.EquipedItem != null)
             {
                 targetItem = rulesetInventorySlot1.EquipedItem;
-                __result = true;
-                return false;
+                return true;
             }
 
             // Neck
-            RulesetInventorySlot rulesetInventorySlot2 = ___characterInventory.InventorySlotsByName[EquipmentDefinitions.SlotTypeNeck];
+            RulesetInventorySlot rulesetInventorySlot2 = characterInventory.InventorySlotsByName[EquipmentDefinitions.SlotTypeNeck];
             if (rulesetInventorySlot1.EquipedItem != null)
             {
                 targetItem = rulesetInventorySlot2.EquipedItem;
-                __result = true;
-                return false;
+                return true;
             }
 
             // Torso
-            RulesetInventorySlot rulesetInventorySlote = ___characterInventory.InventorySlotsByName[EquipmentDefinitions.SlotTypeTorso];
+            RulesetInventorySlot rulesetInventorySlote = characterInventory.InventorySlotsByName[EquipmentDefinitions.SlotTypeTorso];
             if (rulesetInventorySlot2.EquipedItem != null)
             {
                 targetItem = rulesetInventorySlote.EquipedItem;
-                __result = true;
-                return false;
+                return true;
             }
 
             // Other...
 
             // Else default MainHand, OffHand
-            return true;
+            return rulesetCharacterHero.TryFindTargetWieldedItem(out targetItem);
+        }
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var tryFindTargetWieldedItemMethod = typeof(RulesetCharacterHero).GetMethod("TryFindTargetWieldedItem");
+            var myTryFindTargetWieldedItemMethod = typeof(InventoryShortcutsPanel_OnCastLightCb).GetMethod("MyTryFindTargetWieldedItem");
+
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.Calls(tryFindTargetWieldedItemMethod))
+                {
+                    yield return new CodeInstruction(OpCodes.Call, myTryFindTargetWieldedItemMethod);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
         }
     }
 }
