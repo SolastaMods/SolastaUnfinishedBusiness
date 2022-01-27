81543fa3773eee995af6f66b223c878d0dc1abc1
Paulo Monteiro <pauloesquilo@gmail.com>
2022-01-26 22:16:57 -0800

    tweaks

--- SolastaCommunityExpansion/Patches/Bugfix/InventoryPanelPatcher.cs
+++ SolastaCommunityExpansion/Patches/Bugfix/InventoryPanelPatcher.cs
@@ -6,34 +6,6 @@ using TA;
 
 namespace SolastaCommunityExpansion.Patches.Bugfix
 {
-    [HarmonyPatch(typeof(InventoryPanel), "OnPointerDown")]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class InventoryPanel_OnPointerDown
-    {
-        public static int3 MyLocationPosition(GameLocationCharacter gameLocationCharacter)
-        {
-            return gameLocationCharacter?.LocationPosition ?? int3.zero;
-        }
-
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var locationPositionGetter = typeof(GameLocationCharacter).GetMethod("get_LocationPosition");
-            var myLocationPositionMethod = typeof(InventoryPanel_OnPointerDown).GetMethod("MyLocationPosition");
-
-            foreach (CodeInstruction instruction in instructions)
-            {
-                if (Main.Settings.BugFixMainScreenInventoryPanelGround && instruction.Calls(locationPositionGetter))
-                {
-                    yield return new CodeInstruction(OpCodes.Call, myLocationPositionMethod);
-                }
-                else
-                {
-                    yield return instruction;
-                }
-            }
-        }
-    }
-
     [HarmonyPatch(typeof(InventoryPanel), "EndInteraction")]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     internal static class InventoryPanel_EndInteraction
@@ -47,34 +19,6 @@ namespace SolastaCommunityExpansion.Patches.Bugfix
         {
             var locationPositionGetter = typeof(GameLocationCharacter).GetMethod("get_LocationPosition");
             var myLocationPositionMethod = typeof(InventoryPanel_EndInteraction).GetMethod("MyLocationPosition");
-
-            foreach (CodeInstruction instruction in instructions)
-            {
-                if (Main.Settings.BugFixMainScreenInventoryPanelGround && instruction.Calls(locationPositionGetter))
-                {
-                    yield return new CodeInstruction(OpCodes.Call, myLocationPositionMethod);
-                }
-                else
-                {
-                    yield return instruction;
-                }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(InventoryPanel), "OnItemDroppedOnGround")]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class InventoryPanel_OnItemDroppedOnGround
-    {
-        public static int3 MyLocationPosition(GameLocationCharacter gameLocationCharacter)
-        {
-            return gameLocationCharacter?.LocationPosition ?? int3.zero;
-        }
-
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var locationPositionGetter = typeof(GameLocationCharacter).GetMethod("get_LocationPosition");
-            var myLocationPositionMethod = typeof(InventoryPanel_OnItemDroppedOnGround).GetMethod("MyLocationPosition");
 
             foreach (CodeInstruction instruction in instructions)
             {
