141b24bb45b0909b97f519c43825581cae5275ce
Philip Lee <github@pjlconsultants.co.uk>
2022-01-28 13:54:16 +0000

    Split CR on level = 1

---
+++ SolastaCommunityExpansion/Patches/Bugfix/GameBestiaryPatcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches.Bugfix
+{
+    [HarmonyPatch(typeof(GameBestiary), "SplitByUnknownVsKnown")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class GameBestiary_SplitByUnknownVsKnown
+    {
+        public static bool Prefix(int leftLevel, int rightLevel, BestiaryDefinitions.SortCategory ___sortCategory, ref int __result)
+        {
+            if (!Main.Settings.BugFixBestiarySorting)
+            {
+                return true;
+            }
+
+            switch (___sortCategory)
+            {
+                case BestiaryDefinitions.SortCategory.Alphabetical:
+                case BestiaryDefinitions.SortCategory.UpdateDate:
+                case BestiaryDefinitions.SortCategory.KnowledgeLevel:
+                case BestiaryDefinitions.SortCategory.Size:
+                    // These are all know if level > 0
+                    // Move all level = 0 to the end and only sort level > 0
+                    __result = leftLevel == 0 ? 1 : (rightLevel == 0 ? -1 : 0);
+                    break;
+                case BestiaryDefinitions.SortCategory.ChallengeRating:
+                    // Need at least level = 1 to know the CR
+                    // Move all level < 1 to the end and only sort level >= 1
+                    __result = leftLevel < 1 ? 1 : (rightLevel >= 1 ? -1 : 0);
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
