071d4cdf23f19d487ef56ffa6788630d166f8e30
Philip Lee <github@pjlconsultants.co.uk>
2022-01-31 21:31:41 +0000

    Fix warning suppression

--- SolastaCommunityExpansion/Patches/Bugfix/GameBestiaryPatcher.cs
+++ SolastaCommunityExpansion/Patches/Bugfix/GameBestiaryPatcher.cs
@@ -5,9 +5,9 @@ namespace SolastaCommunityExpansion.Patches.Bugfix
 {
     [HarmonyPatch(typeof(GameBestiary), "SplitByUnknownVsKnown")]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    [SuppressMessage("Minor Code Smell", "IDE0066:Use switch expression", Justification = "Prefer it as a switch")]
     internal static class GameBestiary_SplitByUnknownVsKnown
     {
+        [SuppressMessage("Minor Code Smell", "IDE0066:Use switch expression", Justification = "Prefer it as a switch")]
         public static bool Prefix(int leftLevel, int rightLevel, BestiaryDefinitions.SortCategory ___sortCategory, ref int __result)
         {
             if (!Main.Settings.BugFixBestiarySorting)
