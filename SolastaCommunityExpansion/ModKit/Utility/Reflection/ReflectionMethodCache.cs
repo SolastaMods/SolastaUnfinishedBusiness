979c9c629196ccf558551ad7fdfa4b10f95aa94c
Paulo Monteiro <pauloesquilo@gmail.com>
2022-02-05 20:32:31 -0800

    bring local modkit back

---
+++ SolastaCommunityExpansion/ModKit/Utility/Reflection/ReflectionMethodCache.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ModKit.Utility {
+    public static partial class ReflectionCache {
+        private static readonly HashSet<Type> ACTION_AND_FUNC_TYPES = new() {
+            typeof(Action),
+            typeof(Action<>),
+            typeof(Action<,>),
+            typeof(Action<,,>),
+            typeof(Action<,,,>),
+            typeof(Action<,,,,>),
+            typeof(Action<,,,,,>),
+            typeof(Action<,,,,,,>),
+            typeof(Action<,,,,,,,>),
+            typeof(Action<,,,,,,,,>),
+            typeof(Action<,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,,,,>),
+            typeof(Func<>),
+            typeof(Func<,>),
+            typeof(Func<,,>),
+            typeof(Func<,,,>),
+            typeof(Func<,,,,>),
+            typeof(Func<,,,,,>),
+            typeof(Func<,,,,,,>),
+            typeof(Func<,,,,,,,>),
+            typeof(Func<,,,,,,,,>),
+            typeof(Func<,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,,,>)
+        };
+
+        private static readonly TripleDictionary<Type, string, Type, WeakReference> _methodCache = new();
+
+        private static CachedMethod<TMethod> GetMethodCache<T, TMethod>(string name) where TMethod : Delegate {
+            object cache = null;
+            if (_methodCache.TryGetValue(typeof(T), name, typeof(TMethod), out var weakRef))
+                cache = weakRef.Target;
+            if (cache == null) {
+                cache = new CachedMethodOfNonStatic<T, TMethod>(name);
+                _methodCache[typeof(T), name, typeof(TMethod)] = new WeakReference(cache);
+                EnqueueCache(cache);
+            }
+            return cache as CachedMethod<TMethod>;
+        }
+
+        private static CachedMethod<TMethod> GetMethodCache<TMethod>(Type type, string name) where TMethod : Delegate {
+            object cache = null;
+            if (_methodCache.TryGetValue(type, name, typeof(TMethod), out var weakRef))
+                cache = weakRef.Target;
+            if (cache == null) {
+                cache =
+                    IsStatic(type) ?
+                    Activator.CreateInstance(typeof(CachedMethodOfStatic<>).MakeGenericType(typeof(TMethod)), type, name) :
+                    Activator.CreateInstance(typeof(CachedMethodOfNonStatic<,>).MakeGenericType(type, typeof(TMethod)), name);
+                _methodCache[type, name, typeof(TMethod)] = new WeakReference(cache);
+                EnqueueCache(cache);
+            }
+            return cache as CachedMethod<TMethod>;
+        }
+
+        public static MethodInfo GetMethodInfo<T, TMethod>(string name) where TMethod : Delegate => GetMethodCache<T, TMethod>(name).Info;
+
+        public static MethodInfo GetMethodInfo<TMethod>(Type type, string name) where TMethod : Delegate => GetMethodCache<TMethod>(type, name).Info;
+        public static TMethod GetMethodDel<T, TMethod>(string name) where TMethod : Delegate => GetMethodCache<T, TMethod>(name).Del;
+
+        public static TMethod GetMethodDel<TMethod>(Type type, string name) where TMethod : Delegate => GetMethodCache<TMethod>(type, name).Del;
+
+        public static TMethod GetMethod<T, TMethod>(string name) where TMethod : Delegate => GetMethodCache<T, TMethod>(name).Del;
+
+        public static TMethod GetMethod<TMethod>(Type type, string name) where TMethod : Delegate => GetMethodCache<TMethod>(type, name).Del;
+
+        private abstract class CachedMethod<TMethod> where TMethod : Delegate {
+            private TMethod _delegate;
+
+            public readonly MethodInfo Info;
+
+            protected CachedMethod(Type type, string name, bool hasThis) {
+                var delType = typeof(TMethod);
+                var delSign = delType.GetMethod("Invoke", ALL_FLAGS);
+                var delParams = delSign.GetParameters();
+
+                if (hasThis) {
+                    if (delParams.Length == 0)
+                        throw new InvalidOperationException();
+                    if (type.IsValueType) {
+                        if (!delParams[0].ParameterType.IsByRef || delParams[0].ParameterType.GetElementType() != type)
+                            throw new InvalidOperationException();
+                    }
+                    else if (delParams[0].ParameterType.IsByRef || delParams[0].ParameterType != type)
+                        throw new InvalidOperationException();
+                }
+
+                IEnumerable<MethodInfo> methods = type.GetMethods(ALL_FLAGS);
+                if (delType.IsGenericType && !ACTION_AND_FUNC_TYPES.Contains(delType.GetGenericTypeDefinition())) {
+                    if (hasThis)
+                        delParams = delParams.Skip(1).ToArray();
+                    var delGenericArgs = delType.GetGenericArguments();
+                    methods = methods.Where(m =>
+                        m.IsGenericMethod &&
+                        m.Name == name &&
+                        m.ReturnType == delSign.ReturnType &&
+                        m.GetGenericArguments().Length == delGenericArgs.Length &&
+                        CheckParamsOfGenericMethod(m.GetParameters(), delParams, delGenericArgs));
+                    if (methods.Count() > 1)
+                        throw new AmbiguousMatchException();
+                    Info = methods.FirstOrDefault()?.MakeGenericMethod(delGenericArgs);
+                }
+                else {
+                    var delParamTypes = hasThis ?
+                        delParams.Select(p => p.ParameterType).Skip(1) :
+                        delParams.Select(p => p.ParameterType);
+                    methods = methods.Where(m =>
+                        !m.IsGenericMethod &&
+                        m.Name == name &&
+                        m.ReturnType == delSign.ReturnType &&
+                        m.GetParameters().Select(p => p.ParameterType).SequenceEqual(delParamTypes));
+                    if (methods.Count() > 1)
+                        throw new AmbiguousMatchException();
+                    Info = methods.FirstOrDefault();
+                }
+                if (Info == null)
+                    throw new InvalidOperationException();
+            }
+
+            public TMethod Del
+                => _delegate ??= CreateDelegate();
+
+            private static bool CheckParamsOfGenericMethod(ParameterInfo[] @params, ParameterInfo[] delParams, Type[] delGenericArgs) {
+                if (@params.Length != delParams.Length) {
+                    return false;
+                }
+                for (var i = 0; i < @params.Length; i++) {
+                    if (!@params[i].ParameterType.IsGenericParameter) {
+                        if (@params[i].ParameterType != delParams[i].ParameterType) {
+                            return false;
+                        }
+                    }
+                    else {
+                        if (delGenericArgs[@params[i].ParameterType.GenericParameterPosition] != delParams[i].ParameterType) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+
+            protected abstract TMethod CreateDelegate();
+        }
+
+        private class CachedMethodOfStatic<TMethod> : CachedMethod<TMethod> where TMethod : Delegate {
+            public CachedMethodOfStatic(Type type, string name) : base(type, name, false) {
+                //if (!IsStatic(type))
+                //    throw new InvalidOperationException();
+            }
+
+            protected override TMethod CreateDelegate() {
+                var parameters = Info.GetParameters();
+                DynamicMethod method = new(
+                    name: Info.Name,
+                    returnType: Info.ReturnType,
+                    parameterTypes: parameters.Select(item => item.ParameterType).ToArray(),
+                    owner: typeof(CachedMethodOfStatic<TMethod>),
+                    skipVisibility: true);
+
+                var il = method.GetILGenerator();
+                for (var i = 0; i < parameters.Length; i++)
+                    il.Emit(OpCodes.Ldarg, i);
+                il.Emit(OpCodes.Call, Info);
+                il.Emit(OpCodes.Ret);
+
+                return method.CreateDelegate(typeof(TMethod)) as TMethod;
+            }
+        }
+
+        private class CachedMethodOfNonStatic<T, TMethod> : CachedMethod<TMethod> where TMethod : Delegate {
+            public CachedMethodOfNonStatic(string name) : base(typeof(T), name, true) {
+            }
+
+            protected override TMethod CreateDelegate() {
+                var type = typeof(T);
+                var parameters = Info.GetParameters();
+                DynamicMethod method = new(
+                    name: Info.Name,
+                    returnType: Info.ReturnType,
+                    parameterTypes: new[] { type.IsValueType ? type.MakeByRefType() : type }
+                                    .Concat(parameters.Select(item => item.ParameterType)).ToArray(),
+                    owner: typeof(CachedMethodOfNonStatic<T, TMethod>),
+                    skipVisibility: true);
+                method.DefineParameter(1, ParameterAttributes.In, "instance");
+
+                var il = method.GetILGenerator();
+                if (Info.IsStatic) {
+                    for (var i = 1; i <= parameters.Length; i++)
+                        il.Emit(OpCodes.Ldarg, i);
+                    il.Emit(OpCodes.Call, Info);
+                }
+                else {
+                    il.Emit(OpCodes.Ldarg_0);
+                    for (var i = 1; i <= parameters.Length; i++)
+                        il.Emit(OpCodes.Ldarg, i);
+                    if (Info.IsVirtual)
+                        il.Emit(OpCodes.Callvirt, Info);
+                    else
+                        il.Emit(OpCodes.Call, Info);
+                }
+                il.Emit(OpCodes.Ret);
+
+                return method.CreateDelegate(typeof(TMethod)) as TMethod;
+            }
+        }
+    }
+}
---
+++ SolastaCommunityExpansion/ModKit/Utility/Reflection/ReflectionPropertyCache.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ModKit.Utility {
+    public static partial class ReflectionCache {
+        private static readonly DoubleDictionary<Type, string, WeakReference> _propertieCache = new();
+
+        private static CachedProperty<TProperty> GetPropertyCache<T, TProperty>(string name) {
+            object cache = null;
+            if (_propertieCache.TryGetValue(typeof(T), name, out var weakRef))
+                cache = weakRef.Target;
+            if (cache == null) {
+                if (typeof(T).IsValueType)
+                    cache = new CachedPropertyOfStruct<T, TProperty>(name);
+                else
+                    cache = new CachedPropertyOfClass<T, TProperty>(name);
+                _propertieCache[typeof(T), name] = new WeakReference(cache);
+                EnqueueCache(cache);
+            }
+            return cache as CachedProperty<TProperty>;
+        }
+
+        private static CachedProperty<TProperty> GetPropertyCache<TProperty>(Type type, string name) {
+            object cache = null;
+            if (_propertieCache.TryGetValue(type, name, out var weakRef))
+                cache = weakRef.Target;
+            if (cache == null) {
+                cache =
+                    IsStatic(type) ?
+                    new CachedPropertyOfStatic<TProperty>(type, name) :
+                    type.IsValueType ?
+                    Activator.CreateInstance(typeof(CachedPropertyOfStruct<,>).MakeGenericType(type, typeof(TProperty)), name) :
+                    Activator.CreateInstance(typeof(CachedPropertyOfClass<,>).MakeGenericType(type, typeof(TProperty)), name);
+                _propertieCache[type, name] = new WeakReference(cache);
+                EnqueueCache(cache);
+            }
+            return cache as CachedProperty<TProperty>;
+        }
+
+        public static PropertyInfo GetPropertyInfo<T, TProperty>(string name) => GetPropertyCache<T, TProperty>(name).Info;
+
+        public static PropertyInfo GetPropertyInfo<TProperty>(this Type type, string name) => GetPropertyCache<TProperty>(type, name).Info;
+
+        public static TProperty GetPropertyValue<T, TProperty>(this ref T instance, string name) where T : struct => (GetPropertyCache<T, TProperty>(name) as CachedPropertyOfStruct<T, TProperty>).Get(ref instance);
+
+        public static TProperty GetPropertyValue<T, TProperty>(this T instance, string name) where T : class => (GetPropertyCache<T, TProperty>(name) as CachedPropertyOfClass<T, TProperty>).Get(instance);
+
+        public static TProperty GetPropertyValue<T, TProperty>(string name) => GetPropertyCache<T, TProperty>(name).Get();
+
+        public static TProperty GetPropertyValue<TProperty>(this Type type, string name) => GetPropertyCache<TProperty>(type, name).Get();
+
+        public static void SetPropertyValue<T, TProperty>(this ref T instance, string name, TProperty value) where T : struct => (GetPropertyCache<T, TProperty>(name) as CachedPropertyOfStruct<T, TProperty>).Set(ref instance, value);
+
+        public static void SetPropertyValue<T, TProperty>(this T instance, string name, TProperty value) where T : class => (GetPropertyCache<T, TProperty>(name) as CachedPropertyOfClass<T, TProperty>).Set(instance, value);
+
+        public static void SetPropertyValue<T, TProperty>(string name, TProperty value) => GetPropertyCache<T, TProperty>(name).Set(value);
+
+        public static void SetPropertyValue<TProperty>(this Type type, string name, TProperty value) => GetPropertyCache<TProperty>(type, name).Set(value);
+
+        private abstract class CachedProperty<TProperty> {
+            public readonly PropertyInfo Info;
+
+            protected CachedProperty(Type type, string name) {
+                Info = type.GetProperties(ALL_FLAGS).FirstOrDefault(item => item.Name == name);
+
+                if (Info == null || Info.PropertyType != typeof(TProperty))
+                    throw new InvalidOperationException();
+                else if (Info.DeclaringType != type)
+                    Info = Info.DeclaringType.GetProperties(ALL_FLAGS).FirstOrDefault(item => item.Name == name);
+            }
+
+            // for static property
+            public abstract TProperty Get();
+
+            // for static property
+            public abstract void Set(TProperty value);
+
+            protected Delegate CreateGetter(Type delType, MethodInfo getter, bool isInstByRef) {
+                if (getter.IsStatic) {
+                    DynamicMethod method = new(
+                    name: "get_" + Info.Name,
+                    returnType: Info.PropertyType,
+                    parameterTypes: new[] { isInstByRef ? Info.DeclaringType.MakeByRefType() : Info.DeclaringType },
+                    owner: typeof(CachedProperty<TProperty>),
+                    skipVisibility: true);
+                    method.DefineParameter(1, ParameterAttributes.In, "instance");
+                    var il = method.GetILGenerator();
+                    il.Emit(OpCodes.Call, getter);
+                    il.Emit(OpCodes.Ret);
+                    return method.CreateDelegate(delType);
+                }
+                else {
+                    return Delegate.CreateDelegate(delType, getter);
+                }
+            }
+
+            protected Delegate CreateSetter(Type delType, MethodInfo setter, bool isInstByRef) {
+                if (setter.IsStatic) {
+                    DynamicMethod method = new(
+                    name: "set_" + Info.Name,
+                    returnType: null,
+                    parameterTypes: new[] { isInstByRef ? Info.DeclaringType.MakeByRefType() : Info.DeclaringType, Info.PropertyType },
+                    owner: typeof(CachedProperty<TProperty>),
+                    skipVisibility: true);
+                    method.DefineParameter(1, ParameterAttributes.In, "instance");
+                    method.DefineParameter(2, ParameterAttributes.In, "value");
+                    var il = method.GetILGenerator();
+                    il.Emit(OpCodes.Ldarg_1);
+                    il.Emit(OpCodes.Call, setter);
+                    il.Emit(OpCodes.Ret);
+                    return method.CreateDelegate(delType);
+                }
+                else {
+                    return Delegate.CreateDelegate(delType, setter);
+                }
+            }
+        }
+
+        private class CachedPropertyOfStruct<T, TProperty> : CachedProperty<TProperty> {
+            private delegate TProperty Getter(ref T instance);
+            private delegate void Setter(ref T instance, TProperty value);
+
+            private T _dummy = default;
+            private Getter _getter;
+            private Setter _setter;
+
+            public CachedPropertyOfStruct(string name) : base(typeof(T), name) { }
+
+            public override TProperty Get() => (_getter ??= CreateGetter(typeof(Getter), Info.GetMethod, true) as Getter)(ref _dummy);
+
+            public TProperty Get(ref T instance) => (_getter ??= CreateGetter(typeof(Getter), Info.GetMethod, true) as Getter)(ref instance);
+
+            public override void Set(TProperty value) => (_setter ??= CreateSetter(typeof(Setter), Info.SetMethod, true) as Setter)(ref _dummy, value);
+
+            public void Set(ref T instance, TProperty value) => (_setter ??= CreateSetter(typeof(Setter), Info.SetMethod, true) as Setter)(ref instance, value);
+        }
+
+        private class CachedPropertyOfClass<T, TProperty> : CachedProperty<TProperty> {
+            private delegate TProperty Getter(T instance);
+            private delegate void Setter(T instance, TProperty value);
+
+            private readonly T _dummy = default;
+            private Getter _getter;
+            private Setter _setter;
+
+            public CachedPropertyOfClass(string name) : base(typeof(T), name) { }
+
+            public override TProperty Get() => (_getter ??= CreateGetter(typeof(Getter), Info.GetMethod, false) as Getter)(_dummy);
+
+            public TProperty Get(T instance) => (_getter ??= CreateGetter(typeof(Getter), Info.GetMethod, false) as Getter)(instance);
+
+            public override void Set(TProperty value) => (_setter ??= CreateSetter(typeof(Setter), Info.SetMethod, false) as Setter)(_dummy, value);
+
+            public void Set(T instance, TProperty value) => (_setter ??= CreateSetter(typeof(Setter), Info.SetMethod, false) as Setter)(instance, value);
+        }
+
+        private class CachedPropertyOfStatic<TProperty> : CachedProperty<TProperty> {
+            private delegate TProperty Getter();
+            private delegate void Setter(TProperty value);
+
+            private Getter _getter;
+            private Setter _setter;
+
+            public CachedPropertyOfStatic(Type type, string name) : base(type, name) {
+                //if (!IsStatic(type))
+                //    throw new InvalidOperationException();
+            }
+
+            public override TProperty Get() => (_getter ??= CreateGetter())();
+
+            public override void Set(TProperty value) => (_setter ??= CreateSetter())(value);
+
+            private Getter CreateGetter() => Delegate.CreateDelegate(typeof(Getter), Info.GetMethod) as Getter;
+
+            private Setter CreateSetter() => Delegate.CreateDelegate(typeof(Setter), Info.SetMethod) as Setter;
+        }
+    }
+}
