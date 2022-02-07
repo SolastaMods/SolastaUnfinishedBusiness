d47f72db7d5679552eca816f7b377c67647401e6
Philip Lee <github@pjlconsultants.co.uk>
2022-02-07 08:52:21 +0000

    Change SolastaCraftyFeats to CraftyFeats

--- SolastaCommunityExpansion/Feats/CraftyFeats.cs
+++ SolastaCommunityExpansion/Feats/CraftyFeats.cs
@@ -31,32 +31,32 @@ namespace SolastaCommunityExpansion.Feats
             FeatureDefinitionAttributeModifier crafty_int = BuildNewAttributeModifier(
                 "AttributeModifierFeatCraftyInt",
                 "b23c3b73-7690-42ba-aa49-7ca3451daa05",
-                "SolastaCraftyFeats/&AttributeIntTitle",
-                "SolastaCraftyFeats/&AttributeIntDescription",
+                "CraftyFeats/&AttributeIntTitle",
+                "CraftyFeats/&AttributeIntDescription",
                 DatabaseHelper.FeatureDefinitionAttributeModifiers.AttributeModifierCreed_Of_Pakri.GuiPresentation.SpriteReference,
                 DatabaseHelper.FeatureDefinitionAttributeModifiers.AttributeModifierCreed_Of_Pakri);
 
             FeatureDefinitionAttributeModifier crafty_wis = BuildNewAttributeModifier(
                 "AttributeModifierFeatCraftyWis",
                 "23f944c7-2359-43cc-8bdc-71833bf35302",
-                "SolastaCraftyFeats/&AttributeWisTitle",
-                "SolastaCraftyFeats/&AttributeWisDescription",
+                "CraftyFeats/&AttributeWisTitle",
+                "CraftyFeats/&AttributeWisDescription",
                 DatabaseHelper.FeatureDefinitionAttributeModifiers.AttributeModifierCreed_Of_Maraike.GuiPresentation.SpriteReference,
                 DatabaseHelper.FeatureDefinitionAttributeModifiers.AttributeModifierCreed_Of_Maraike);
 
             FeatureDefinitionAttributeModifier crafty_dex = BuildNewAttributeModifier(
                 "AttributeModifierFeatCraftyDex",
                 "4db12466-67da-47a4-8d96-a9bf9cf3a251",
-                "SolastaCraftyFeats/&AttributeDexTitle",
-                "SolastaCraftyFeats/&AttributeDexDescription",
+                "CraftyFeats/&AttributeDexTitle",
+                "CraftyFeats/&AttributeDexDescription",
                 DatabaseHelper.FeatureDefinitionAttributeModifiers.AttributeModifierCreed_Of_Misaye.GuiPresentation.SpriteReference,
                 DatabaseHelper.FeatureDefinitionAttributeModifiers.AttributeModifierCreed_Of_Misaye);
 
             FeatureDefinitionProficiency crafty_arcana = BuildNewProficiency(
                 "CraftyArcana",
                 "44a54666-80ba-475c-90b1-774e86f1a69a",
-                "SolastaCraftyFeats/&CraftySkillsTitle",
-                "SolastaCraftyFeats/&CraftyArcanaDescription",
+                "CraftyFeats/&CraftySkillsTitle",
+                "CraftyFeats/&CraftyArcanaDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
                 RuleDefinitions.ProficiencyType.SkillOrExpertise,
@@ -66,8 +66,8 @@ namespace SolastaCommunityExpansion.Feats
             FeatureDefinitionProficiency crafty_medicine = BuildNewProficiency(
                 "CraftyMedicine",
                 "1ac54869-a8ce-4a51-a858-1f7e34680b96",
-                "SolastaCraftyFeats/&CraftySkillsTitle",
-                "SolastaCraftyFeats/&CraftyMedicineDescription",
+                "CraftyFeats/&CraftySkillsTitle",
+                "CraftyFeats/&CraftyMedicineDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
                 RuleDefinitions.ProficiencyType.SkillOrExpertise,
@@ -77,8 +77,8 @@ namespace SolastaCommunityExpansion.Feats
             FeatureDefinitionProficiency crafty_nature = BuildNewProficiency(
                 "CraftyNature",
                 "7399b06a-bfda-4e60-8366-17e0d6cec0d0",
-                "SolastaCraftyFeats/&CraftySkillsTitle",
-                "SolastaCraftyFeats/&CraftyNatureDescription",
+                "CraftyFeats/&CraftySkillsTitle",
+                "CraftyFeats/&CraftyNatureDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
                 RuleDefinitions.ProficiencyType.SkillOrExpertise,
@@ -88,7 +88,7 @@ namespace SolastaCommunityExpansion.Feats
             FeatureDefinitionProficiency crafty_herbalism_kit = BuildNewProficiency(
                 "CraftyHerbalismKit",
                 "9345e1fd-ec4c-4509-acb5-3f3257b25ec4",
-                "SolastaCraftyFeats/&CraftyToolsTitle",
+                "CraftyFeats/&CraftyToolsTitle",
                 "Feature/&ToolProficiencyPluralShortDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
@@ -99,7 +99,7 @@ namespace SolastaCommunityExpansion.Feats
             FeatureDefinitionProficiency crafty_manacalon_rosary = BuildNewProficiency(
                 "CraftyManacalonRosary",
                 "0685a944-76cd-423a-81a1-9ceec507d69a",
-                "SolastaCraftyFeats/&CraftyToolsTitle",
+                "CraftyFeats/&CraftyToolsTitle",
                 "Feature/&ToolProficiencyPluralShortDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
@@ -109,7 +109,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatureDefinitionProficiency crafty_poisoners_kit = BuildNewProficiency(
                 "CraftyPoisonersKit", "32ddae84-66e7-4b56-b5ec-0ec91a713e2e",
-                "SolastaCraftyFeats/&CraftyToolsTitle",
+                "CraftyFeats/&CraftyToolsTitle",
                 "Feature/&ToolProficiencyPluralShortDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
@@ -120,7 +120,7 @@ namespace SolastaCommunityExpansion.Feats
             FeatureDefinitionProficiency crafty_scroll_kit = BuildNewProficiency(
                 "CraftyScrollKit",
                 "5309bd7f-b533-40ff-ae95-d977e02d61fe",
-                "SolastaCraftyFeats/&CraftyToolsTitle",
+                "CraftyFeats/&CraftyToolsTitle",
                 "Feature/&ToolProficiencyPluralShortDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
@@ -130,7 +130,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatureDefinitionProficiency crafty_smiths_tools = BuildNewProficiency(
                 "CraftySmithsTools", "48905450-4b35-480f-9868-f340c7902920",
-                "SolastaCraftyFeats/&CraftyToolsTitle",
+                "CraftyFeats/&CraftyToolsTitle",
                 "Feature/&ToolProficiencyPluralShortDescription",
                 null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
@@ -141,8 +141,8 @@ namespace SolastaCommunityExpansion.Feats
             FeatureDefinitionProficiency crafty_bows = BuildNewProficiency(
                 "CraftyBows",
                 "62a71277-b62d-41e6-9546-19f6faa2b5a7",
-                "SolastaCraftyFeats/&CraftyBowsTitle",
-                "SolastaCraftyFeats/&CraftyBowsDescription",
+                "CraftyFeats/&CraftyBowsTitle",
+                "CraftyFeats/&CraftyBowsDescription",
                  null,
                 DatabaseHelper.FeatureDefinitionProficiencys.ProficiencyAllLanguages,
                 RuleDefinitions.ProficiencyType.Weapon,
@@ -156,7 +156,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatDefinition ApothecaryIntFeat = CraftyFeatBuilder.CreateCopyFrom(
                 "ApothecaryInt", "ef387249-45e0-4899-aadd-44810f8aeb6d",
-                "SolastaCraftyFeats/&ApothecaryIntFeatTitle", "SolastaCraftyFeats/&ApothecaryIntFeatDescription",
+                "CraftyFeats/&ApothecaryIntFeatTitle", "CraftyFeats/&ApothecaryIntFeatDescription",
                 DatabaseHelper.FeatDefinitions.ArmorMaster);
 
             ApothecaryIntFeat.Features.Clear();
@@ -167,7 +167,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatDefinition ApothecaryWisFeat = CraftyFeatBuilder.CreateCopyFrom
                 ("ApothecaryWis", "4fd80bf9-7749-4c01-9d95-6eb56c644fe2",
-                "SolastaCraftyFeats/&ApothecaryWisFeatTitle", "SolastaCraftyFeats/&ApothecaryWisFeatDescription",
+                "CraftyFeats/&ApothecaryWisFeatTitle", "CraftyFeats/&ApothecaryWisFeatDescription",
                 DatabaseHelper.FeatDefinitions.ArmorMaster);
 
             ApothecaryWisFeat.Features.Clear();
@@ -178,7 +178,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatDefinition ManacalonCrafter = CraftyFeatBuilder.CreateCopyFrom
                 ("ManacalonCrafter", "290f73c8-201c-489e-bdcb-7a39ab40915c",
-                "SolastaCraftyFeats/&ManacalonCrafterFeatTitle", "SolastaCraftyFeats/&ManacalonCrafterFeatDescription",
+                "CraftyFeats/&ManacalonCrafterFeatTitle", "CraftyFeats/&ManacalonCrafterFeatDescription",
                 DatabaseHelper.FeatDefinitions.MasterEnchanter);
 
             ManacalonCrafter.Features.Clear();
@@ -189,7 +189,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatDefinition ToxicologistInt = CraftyFeatBuilder.CreateCopyFrom
                 ("ToxicologistInt", "702d1b4d-953c-406d-a900-d5d376ed29d3",
-                "SolastaCraftyFeats/&ToxicologistIntFeatTitle", "SolastaCraftyFeats/&ToxicologistIntFeatDescription",
+                "CraftyFeats/&ToxicologistIntFeatTitle", "CraftyFeats/&ToxicologistIntFeatDescription",
                 DatabaseHelper.FeatDefinitions.ArmorMaster);
 
             ToxicologistInt.Features.Clear();
@@ -200,7 +200,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatDefinition ToxicologistWis = CraftyFeatBuilder.CreateCopyFrom
                 ("ToxicologistWis", "1bb4acbd-1890-48ae-9f86-46c2cb95cb79",
-                "SolastaCraftyFeats/&ToxicologistWisFeatTitle", "SolastaCraftyFeats/&ToxicologistWisFeatDescription",
+                "CraftyFeats/&ToxicologistWisFeatTitle", "CraftyFeats/&ToxicologistWisFeatDescription",
                 DatabaseHelper.FeatDefinitions.ArmorMaster);
 
             ToxicologistWis.Features.Clear();
@@ -211,7 +211,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatDefinition CraftyScribe = CraftyFeatBuilder.CreateCopyFrom
                 ("CraftyScribe", "bd83e063-2751-4898-8070-f74ca925f8b5",
-                "SolastaCraftyFeats/&CraftyScribeFeatTitle", "SolastaCraftyFeats/&CraftyScribeFeatDescription",
+                "CraftyFeats/&CraftyScribeFeatTitle", "CraftyFeats/&CraftyScribeFeatDescription",
                 DatabaseHelper.FeatDefinitions.MasterEnchanter);
 
             CraftyScribe.Features.Clear();
@@ -222,7 +222,7 @@ namespace SolastaCommunityExpansion.Feats
 
             FeatDefinition CraftyFletcher = CraftyFeatBuilder.CreateCopyFrom
                 ("CraftyFletcher", "67c5f2d2-a98c-49a1-a1ab-16cc8f4b4ba4",
-                "SolastaCraftyFeats/&CraftyFletcherFeatTitle", "SolastaCraftyFeats/&CraftyFletcherFeatDescription",
+                "CraftyFeats/&CraftyFletcherFeatTitle", "CraftyFeats/&CraftyFletcherFeatDescription",
                 DatabaseHelper.FeatDefinitions.ArmorMaster);
             CraftyFletcher.Features.Clear();
             CraftyFletcher.Features.Add(crafty_dex);
