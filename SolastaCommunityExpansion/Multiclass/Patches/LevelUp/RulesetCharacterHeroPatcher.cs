169551398bbcced6679d9e80429ba87fcc8624c1
Paulo Monteiro <pauloesquilo@gmail.com>
2022-02-14 21:59:57 -0800

    patch hitpoints gain in MC scenario

---
+++ SolastaCommunityExpansion/Multiclass/Patches/LevelUp/RulesetCharacterHeroPatcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Multiclass.Patches.LevelUp
+{
+    internal static class RulesetCharacterHeroPatcher
+    {
+        [HarmonyPatch(typeof(RulesetCharacterHero), "AddClassLevel")]
+        internal static class RulesetCharacterHeroAddClassLevel
+        {
+            internal static bool Prefix(RulesetCharacterHero __instance, CharacterClassDefinition classDefinition, List<int> ___hitPointsGainHistory)
+            {
+                if (!Main.Settings.EnableMulticlass)
+                {
+                    return true;
+                }
+
+                __instance.ClassesHistory.Add(classDefinition);
+
+                if (__instance.ClassesAndLevels.ContainsKey(classDefinition))
+                {
+                    __instance.ClassesAndLevels[classDefinition]++;
+                } 
+                else
+                {
+                    __instance.ClassesAndLevels.Add(classDefinition, 1);
+                }
+
+                if (__instance.ClassesAndLevels[classDefinition] == 1 && !Models.LevelUpContext.IsMulticlass)
+                {
+                    ___hitPointsGainHistory.Add(RuleDefinitions.DiceMaxValue[(int)classDefinition.HitDice]);
+                }
+                else
+                {
+                    ___hitPointsGainHistory.Add(HeroDefinitions.RollHitPoints(classDefinition.HitDice));
+                }
+
+                __instance.ComputeCharacterLevel();
+                __instance.ComputeProficiencyBonus();
+
+                return false;
+            }
+        }
+    }
+}
