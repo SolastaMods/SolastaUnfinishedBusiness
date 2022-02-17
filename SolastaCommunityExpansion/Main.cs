6496586d5c3d582bf4478997930db8b89fe220cd
ThyWoof <pauloesquilo@gmail.com>
2022-02-16 19:20:27 -0800

    clean up

--- SolastaCommunityExpansion/Main.cs
+++ SolastaCommunityExpansion/Main.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using HarmonyLib;
 using ModKit;
 using UnityModManagerNet;
 
