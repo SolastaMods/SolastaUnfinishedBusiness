74a4dd6e4ba4eaf15cbad46f836768a559513a10
Chris John <chris.p.john@gmail.com>
2022-02-24 22:46:35 -0800

    Remove accidental character.

--- SolastaCommunityExpansion/Subclasses/Wizard/ArcaneFighter.cs
+++ SolastaCommunityExpansion/Subclasses/Wizard/ArcaneFighter.cs
@@ -138,7 +138,7 @@ namespace SolastaCommunityExpansion.Subclasses.Wizard
                 modifierType, attribute, amount, guiPresentation);
             return builder.AddToDB();
         }
-.
+
         public static FeatureDefinitionMagicAffinity BuildMagicAffinityConcentration(RuleDefinitions.ConcentrationAffinity concentrationAffinity, int threshold, string name, GuiPresentation guiPresentation)
         {
             FeatureDefinitionMagicAffinityBuilder builder = new FeatureDefinitionMagicAffinityBuilder(name, GuidHelper.Create(SubclassNamespace, name).ToString(),
