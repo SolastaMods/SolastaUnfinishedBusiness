4297ee76d57a346efe9b4d0becafc6b754f92731
Philip Lee <github@pjlconsultants.co.uk>
2022-02-26 09:02:50 +0000

    Add create methods

--- SolastaCommunityExpansion/Builders/ActionDefinitionBuilder.cs
+++ SolastaCommunityExpansion/Builders/ActionDefinitionBuilder.cs
@@ -35,11 +35,42 @@ namespace SolastaCommunityExpansion.Builders
         }
         #endregion
 
-        // TODO: add Create methods
-        public static ActionDefinitionBuilder Create(ActionDefinition original, string name, Guid namespaceGuid) 
+        #region Create
+        public static ActionDefinitionBuilder Create(ActionDefinition original)
         {
-            return new(original, name, namespaceGuid);
+            return new ActionDefinitionBuilder(original);
         }
+
+        public static ActionDefinitionBuilder Create(string name, Guid namespaceGuid) 
+        {
+            return new ActionDefinitionBuilder(name, namespaceGuid);
+        }
+
+        public static ActionDefinitionBuilder Create(string name, string definitionGuid) 
+        {
+            return new ActionDefinitionBuilder(name, definitionGuid);
+        }
+
+        public static ActionDefinitionBuilder Create(string name, bool createGuiPresentation = true) 
+        {
+            return new ActionDefinitionBuilder(name, createGuiPresentation);
+        }
+
+        public static ActionDefinitionBuilder Create(ActionDefinition original, string name, bool createGuiPresentation = true) 
+        {
+            return new ActionDefinitionBuilder(original, name, createGuiPresentation);
+        }
+
+        public static ActionDefinitionBuilder Create(ActionDefinition original, string name, Guid namespaceGuid)
+        {
+            return new ActionDefinitionBuilder(original, name, namespaceGuid);
+        }
+
+        public static ActionDefinitionBuilder Create(ActionDefinition original, string name, string definitionGuid)
+        {
+            return new ActionDefinitionBuilder(original, name, definitionGuid);
+        }
+        #endregion
 
         public ActionDefinitionBuilder SetId(ActionDefinitions.Id value)
         {
