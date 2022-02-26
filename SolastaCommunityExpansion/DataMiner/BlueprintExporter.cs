01579c44fa3029b676a663df3beca57cfadf0a5d
Paulo Monteiro <pauloesquilo@gmail.com>
2022-02-26 14:24:21 -0800

    remove export single

--- SolastaCommunityExpansion/DataMiner/BlueprintExporter.cs
+++ SolastaCommunityExpansion/DataMiner/BlueprintExporter.cs
@@ -10,6 +10,8 @@ namespace SolastaCommunityExpansion.DataMiner
 {
     internal class BlueprintExporter : MonoBehaviour
     {
+        private const int MAX_PATH_LENGTH = 260;
+
         // private const string BlueprintFolder = "SolastaBlueprints";
         private static BlueprintExporter Exporter;
 
@@ -49,8 +51,7 @@ namespace SolastaCommunityExpansion.DataMiner
             string exportName,
             HashSet<BaseDefinition> baseDefinitions,
             Dictionary<Type, List<BaseDefinition>> baseDefinitionsMap,
-            string path,
-            bool useSingleFile = false)
+            string path)
         {
             if (baseDefinitionsMap == null || baseDefinitions == null || PercentageComplete > 0)
             {
@@ -63,14 +64,7 @@ namespace SolastaCommunityExpansion.DataMiner
             BaseDefinitionsMap = baseDefinitionsMap;
             Path = path;
 
-            if (useSingleFile)
-            {
-                Singleton.StartCoroutine(ExportSingle());
-            }
-            else
-            {
-                Singleton.StartCoroutine(ExportMany());
-            }
+            Singleton.StartCoroutine(ExportMany());
         }
 
         private static IEnumerator ExportMany()
@@ -112,6 +106,7 @@ namespace SolastaCommunityExpansion.DataMiner
                 var dbType = d.Definition.GetType();
                 var value = d.Definition;
                 var subfolder = value.GetType().Name;
+
                 if (value.GetType() != dbType)
                 {
                     subfolder = $"{dbType.FullName}/{subfolder}";
@@ -121,9 +116,9 @@ namespace SolastaCommunityExpansion.DataMiner
 
                 var filename = $"{value.Name}.{value.GUID}.json";
                 var folder = $"{Path}/{subfolder}";
-
                 var path = $"{folder}/{filename}";
-                if (folder.Length + filename.Length > 250)
+
+                if (path.Length > MAX_PATH_LENGTH)
                 {
                     Main.Log($"Shortened path {path}, to {folder}/{value.GUID}.json");
                     path = $"{folder}/{value.GUID}.json";
@@ -144,44 +139,6 @@ namespace SolastaCommunityExpansion.DataMiner
 
                 yield return null;
             }
-
-            PercentageComplete = 0;
-            ExportName = "";
-        }
-
-        private static IEnumerator ExportSingle()
-        {
-            using StreamWriter sw = new StreamWriter(Path);
-            using JsonWriter writer = new JsonTextWriter(sw);
-
-            // NOTE: currently needs to be assembled one definition at a time into a single file
-            // Problem 1) serializing the whole array doesn't emit everything
-            // Problem 2) serializing into individual files exceeds the folder path limit because some CE definitions have very long namessssssssss....
-            sw.WriteLine("[");
-
-            var definitions = BaseDefinitions.OrderBy(d => d.Name).ThenBy(d => d.GetType().Name);
-            var lastDefinition = definitions.Last();
-            var total = definitions.Count();
-
-            foreach (var d in definitions.Select((d, i) => new { Definition = d, Index = i }))
-            {
-                // Don't put this outside the loop or it caches objects already serialized and then outputs a reference instead 
-                // of the whole object.
-                var serializer = JsonSerializer.Create(JsonUtil.CreateSettings(PreserveReferencesHandling.None));
-
-                serializer.Serialize(writer, d.Definition);
-
-                if (d.Definition != lastDefinition)
-                {
-                    sw.WriteLine(",");
-                }
-
-                PercentageComplete = ((float)d.Index / total);
-
-                yield return null;
-            }
-
-            sw.WriteLine("]");
 
             PercentageComplete = 0;
             ExportName = "";
