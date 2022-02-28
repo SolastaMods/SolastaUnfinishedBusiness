0124c1e3a88cd76b4f80bfbbc68ebb876cc33156
Paulo Monteiro <pauloesquilo@gmail.com>
2022-02-27 17:26:50 -0800

    remove scripts folder

--- Scripts/GenDependencies.py
+++
@@ -1,7 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir("D:\games\Slasta_COTM\Solasta_Data\Managed") if isfile(join("D:\games\Slasta_COTM\Solasta_Data\Managed", f))]
-
-for file in onlyfiles:
-    if file.endswith("dll") and not file.startswith("System"):
-        print(f"<Reference Include='{file}'>\n\t<HintPath>$([System.IO.Path]::Combine($(SolastaInstallDir), 'Solasta_Data\Managed\{file}'))</HintPath>\n\t<Private>false</Private>\n</Reference>")
