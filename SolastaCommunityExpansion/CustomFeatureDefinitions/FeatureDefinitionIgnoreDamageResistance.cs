7b224f3d247192268851043abcb79e4dfaf92bf9
Paulo Monteiro <pauloesquilo@gmail.com>
2022-02-28 19:30:50 -0800

    clean

--- SolastaCommunityExpansion/CustomFeatureDefinitions/FeatureDefinitionIgnoreDamageResistance.cs
+++ SolastaCommunityExpansion/CustomFeatureDefinitions/FeatureDefinitionIgnoreDamageResistance.cs
@@ -2,9 +2,6 @@
 
 namespace SolastaCommunityExpansion.CustomFeatureDefinitions
 {
-    /// <summary>
-    /// Implement on a FeatureDefinitionPower to allow it to recharge at the start of your turn.
-    /// </summary>
     public interface IIgnoreDamageAffinity
     {
         bool CanIgnoreDamageAffinity(IDamageAffinityProvider provider, string damageType);
