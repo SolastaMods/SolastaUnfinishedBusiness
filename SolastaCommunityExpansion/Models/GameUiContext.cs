a2e8710d6406f87e81d3831f21fcf588a99e0275
Philip Lee <github@pjlconsultants.co.uk>
2022-03-10 20:53:53 +0000

    All Shift-R to rotate item

--- SolastaCommunityExpansion/Models/GameUiContext.cs
+++ SolastaCommunityExpansion/Models/GameUiContext.cs
@@ -32,6 +32,9 @@ namespace SolastaCommunityExpansion.Models
         {
             var inputService = ServiceRepository.GetService<IInputService>();
 
+            // Dungeon Maker
+            inputService.RegisterCommand(InputCommands.Id.EditorRotate, 114, (int)KeyCode.LeftShift, -1, -1, -1, -1);
+
             // HUD
             inputService.RegisterCommand(Hotkeys.CTRL_SHIFT_C, (int)KeyCode.C, (int)KeyCode.LeftShift, (int)KeyCode.LeftControl, -1, -1, -1);
             inputService.RegisterCommand(Hotkeys.CTRL_SHIFT_L, (int)KeyCode.L, (int)KeyCode.LeftShift, (int)KeyCode.LeftControl, -1, -1, -1);
