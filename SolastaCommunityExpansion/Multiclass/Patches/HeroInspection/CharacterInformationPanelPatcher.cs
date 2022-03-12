f31f1d0051f2792693e073e64b502bbb95d8137d
ThyWoof <pauloesquilo@gmail.com>
2022-03-11 19:55:34 -0800

    fix parameter from GuiCharacter to RulesetCharacterHero

--- SolastaCommunityExpansion/Multiclass/Patches/HeroInspection/CharacterInformationPanelPatcher.cs
+++ SolastaCommunityExpansion/Multiclass/Patches/HeroInspection/CharacterInformationPanelPatcher.cs
@@ -24,9 +24,9 @@ namespace SolastaCommunityExpansion.Multiclass.Patches.HeroInspection
                 }
 
                 var containsMethod = typeof(string).GetMethod("Contains");
-                var getSelectedClassSearchTermMethod = typeof(Models.InspectionPanelContext).GetMethod("GetSelectedClassSearchTerm");
+                var getSelectedClassSearchTermMethod = typeof(InspectionPanelContext).GetMethod("GetSelectedClassSearchTerm");
                 var enumerateClassBadgesMethod = typeof(CharacterInformationPanel).GetMethod("EnumerateClassBadges", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-                var myEnumerateClassBadgesMethod = typeof(Models.InspectionPanelContext).GetMethod("EnumerateClassBadges");
+                var myEnumerateClassBadgesMethod = typeof(InspectionPanelContext).GetMethod("EnumerateClassBadges");
                 var found = 0;
 
                 foreach (var instruction in instructions)
@@ -77,6 +77,7 @@ namespace SolastaCommunityExpansion.Multiclass.Patches.HeroInspection
                 var subclassFilteredFeatureUnlocksMethod = typeof(LevelUpContext).GetMethod("SubclassFilteredFeatureUnlocks");
 
                 var inspectedCharacterMethod = typeof(CharacterInformationPanel).GetMethod("get_InspectedCharacter");
+                var rulesetCharacterHeroMethod = typeof(GuiCharacter).GetMethod("get_RulesetCharacterHero");
 
                 foreach (var instruction in instructions)
                 {
@@ -84,12 +85,14 @@ namespace SolastaCommunityExpansion.Multiclass.Patches.HeroInspection
                     {
                         yield return new CodeInstruction(OpCodes.Ldarg_0);
                         yield return new CodeInstruction(OpCodes.Call, inspectedCharacterMethod);
+                        yield return new CodeInstruction(OpCodes.Call, rulesetCharacterHeroMethod);
                         yield return new CodeInstruction(OpCodes.Call, classFilteredFeatureUnlocksMethod);
                     }
                     else if (instruction.Calls(subclassFeatureUnlocksMethod))
                     {
                         yield return new CodeInstruction(OpCodes.Ldarg_0);
                         yield return new CodeInstruction(OpCodes.Call, inspectedCharacterMethod);
+                        yield return new CodeInstruction(OpCodes.Call, rulesetCharacterHeroMethod);
                         yield return new CodeInstruction(OpCodes.Call, subclassFilteredFeatureUnlocksMethod);
                     }
                     else
