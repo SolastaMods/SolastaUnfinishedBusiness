1c660dcbb006cac31ccd444e59d3846930f84d7b
ThyWoof <pauloesquilo@gmail.com>
2022-03-13 21:13:33 -0700

    move Diagnostics folder off project subfolder

--- SolastaCommunityExpansion/Models/DiagnosticsContext.cs
+++ SolastaCommunityExpansion/Models/DiagnosticsContext.cs
@@ -34,7 +34,7 @@ namespace SolastaCommunityExpansion.Models
 
         private static string GetDiagnosticsFolder()
         {
-            var path = Path.Combine(ProjectFolder ?? GAME_FOLDER, @"SolastaCommunityExpansion\Diagnostics");
+            var path = Path.Combine(ProjectFolder ?? GAME_FOLDER, @"Diagnostics");
 
             EnsureFolderExists(path);
 
