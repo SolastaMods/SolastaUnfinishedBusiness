d52fb103b3bfe26bc84e4be4e3050cb57bb07404
ThyWoof <pauloesquilo@gmail.com>
2022-03-28 18:55:16 -0700

    add required pact magic patches

---
+++ SolastaCommunityExpansion/Patches/CustomFeatures/PactMagic/RulesetSpellRepertoirePatcher.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using static SolastaCommunityExpansion.Classes.Warlock.Warlock;
+
+namespace SolastaCommunityExpansion.Patches.CustomFeatures.PactMagic
+{
+    public static class RulesetSpellRepertoirePatcher
+    {
+        //
+        // TODO: move this over to a proper place once merge is done (otherwise it'll be a pain to merge settings or main...)
+        //
+        public static bool IsMulticlassInstalled { get; set; }
+
+        // handles all different scenarios to determine max slots numbers
+        [HarmonyPatch(typeof(RulesetSpellRepertoire), "GetMaxSlotsNumberOfAllLevels")]
+        internal static class RulesetSpellRepertoireGetMaxSlotsNumberOfAllLevels
+        {
+            internal static bool Prefix(
+                RulesetSpellRepertoire __instance,
+                ref int __result,
+                Dictionary<int, int> ___spellsSlotCapacities)
+            {
+                if (IsMulticlassInstalled || __instance.SpellCastingClass != ClassWarlock)
+                {
+                    return true;
+                }
+
+                ___spellsSlotCapacities.TryGetValue(1, out __result);
+
+                return false;
+            }
+        }
+
+        // handles all different scenarios to determine remaining slots numbers
+        [HarmonyPatch(typeof(RulesetSpellRepertoire), "GetRemainingSlotsNumberOfAllLevels")]
+        internal static class RulesetSpellRepertoireGetRemainingSlotsNumberOfAllLevels
+        {
+            internal static bool Prefix(
+                RulesetSpellRepertoire __instance, 
+                ref int __result, 
+                Dictionary<int, int> ___usedSpellsSlots,
+                Dictionary<int, int> ___spellsSlotCapacities)
+            {
+                if (IsMulticlassInstalled || __instance.SpellCastingClass != ClassWarlock)
+                {
+                    return true;
+                }
+
+                ___spellsSlotCapacities.TryGetValue(1, out var max);
+                ___usedSpellsSlots.TryGetValue(1, out var used);
+                __result = max - used;
+
+                return false;
+            }
+        }
+
+        // handles all different scenarios to determine slots numbers
+        [HarmonyPatch(typeof(RulesetSpellRepertoire), "GetSlotsNumber")]
+        internal static class RulesetSpellRepertoireGetSlotsNumber
+        {
+            internal static bool Prefix(
+                RulesetSpellRepertoire __instance,
+                Dictionary<int, int> ___usedSpellsSlots,
+                Dictionary<int, int> ___spellsSlotCapacities,
+                int spellLevel, 
+                ref int remaining, 
+                ref int max)
+            {
+                if (IsMulticlassInstalled || __instance.SpellCastingClass != ClassWarlock)
+                {
+                    return true;
+                }
+
+                max = 0;
+                remaining = 0;
+
+                if (spellLevel <= __instance.MaxSpellLevelOfSpellCastingLevel)
+                {
+                    {
+                        ___spellsSlotCapacities.TryGetValue(1, out max);
+                        ___usedSpellsSlots.TryGetValue(1, out var used);
+                        remaining = max - used;
+                    }
+                }
+
+                return false;
+            }
+        }
+ 
+        // handles all different scenarios of spell slots consumption (casts, smites, point buys)
+        [HarmonyPatch(typeof(RulesetSpellRepertoire), "SpendSpellSlot")]
+        internal static class RulesetSpellRepertoireSpendSpellSlot
+        {
+            internal static bool Prefix(RulesetSpellRepertoire __instance, Dictionary<int, int> ___usedSpellsSlots, int slotLevel)
+            {
+                if (IsMulticlassInstalled || slotLevel == 0 || __instance.SpellCastingClass != ClassWarlock)
+                {
+                    return true;
+                }
+
+                var maxSpellLevel = __instance.MaxSpellLevelOfSpellCastingLevel;
+
+                for (var i = 1; i <= maxSpellLevel; i++)
+                {
+                    if (i == 0)
+                    {
+                        continue;
+                    }
+
+                    if (!___usedSpellsSlots.ContainsKey(i))
+                    {
+                    ___usedSpellsSlots.Add(i, 0);
+                    }
+
+                    ___usedSpellsSlots[i]++;
+                }
+                
+                __instance.RepertoireRefreshed?.Invoke(__instance);
+
+                return false;
+            }
+        }
+    }
+}
