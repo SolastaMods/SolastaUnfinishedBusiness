14681845c37356ae6183421f00a7ad5a670a6e95
ThyWoof <pauloesquilo@gmail.com>
2022-03-29 11:06:50 -0700

    add proper Warlock credits

--- SolastaCommunityExpansion/Viewers/Displays/CreditsDisplay.cs
+++ SolastaCommunityExpansion/Viewers/Displays/CreditsDisplay.cs
@@ -10,12 +10,12 @@ namespace SolastaCommunityExpansion.Viewers.Displays
             { "ChrisJohnDigital".orange().bold(), "head developer, crafting, faction relations, feats, fighting styles, items, subclasses, progression" },
             { "Zappastuff", "multiclass, level 20, respec, level down, encounters, dungeon maker editor & pro, party size, screen map, inventory sorting, adventure log, epic points, teleport, mod UI, diagnostics, integration, arcane defense/precision, brutal thug, charismatic defense/precision, fighting surge, metamagic, primal, shady and wise defense/precision feats" },
             { "ImpPhil", "adv/dis rules, conjurations control, auto-equip, location rotation in DM, monster's health, pause UI, sorting, stocks prices, no attunement, xp scaling, character export, save by location, combat camera, diagnostics, custom icons, refactor, screen map" },
-            { "DubhHerder", "7th, 8th and 9th level spells, Crafty Feats migration, bug models replacement, additional high level monsters, class, subclasses" },
+            { "DubhHerder", "7th, 8th and 9th level spells, Crafty Feats migration, bug models replacement, additional high level monsters, Warlock class and subclasses, Tinkerer Artillerist subclass" },
             { "View619", "Darkvision, Superior Dark Vision" },
             { "SilverGriffon", "PickPocket, lore friendly names, crafty feats, face unlocks, sylvan armor unlock, empress garb skins, arcane foci items, belt of dwarvenkin, merchants" },
             { "Bazou", "Witch class" },
             { "Boofat", "alwaysAlt" },
-            { "AceHigh", "Tactician subclass, Power Attack and Reckless Fury feats, no identification" },
+            { "AceHigh", "original Warlock class, Tactician subclass, Power Attack and Reckless Fury feats, no identification" },
             { "ElAntonious", "Arcanist subclass, Torchbearer and Dual Flurry feats" },
             { "Scarlex", "Royal Knight subclass" },
             { "RedOrca", "Path of the Light subclass, Indomitable Might" },
@@ -23,6 +23,7 @@ namespace SolastaCommunityExpansion.Viewers.Displays
             { "Dreadmaker", "Circle of the Forest Guardian subclass" },
             { "Narria", "modKit creator, developer" },
             { "Spacehamster", "dataminer" },
+            { "Holic75", "pact magic slots idea" },
             { "sinai-dev", "Unity Explorer UI standalone" }
         };
 
