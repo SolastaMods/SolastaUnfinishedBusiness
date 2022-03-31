cae88a13498364dfbae34ab4e0d870d14bdcf3f0
ThyWoof <pauloesquilo@gmail.com>
2022-03-30 23:35:37 -0700

    add support for the Gnome race

--- SolastaCommunityExpansion/Models/RacesContext.cs
+++ SolastaCommunityExpansion/Models/RacesContext.cs
@@ -1,16 +1,16 @@
 ﻿using SolastaCommunityExpansion.Builders;
 using SolastaCommunityExpansion.Builders.Features;
-using SolastaModApi;
 using SolastaModApi.Extensions;
-using System.Collections.Generic;
 using System.Reflection;
-using UnityEngine;
+using static SolastaModApi.DatabaseHelper;
 
 namespace SolastaCommunityExpansion.Models
 {
     public class RacesContext
     {
-        internal static T MemberwiseClone<T>(T base_object)
+        internal static CharacterRaceDefinition GnomeRace { get; private set; } = BuildGnome();
+
+        private static T MemberwiseClone<T>(T base_object)
         {
             MethodInfo memberwiseCloneMethod = typeof(T).GetMethod("MemberwiseClone", BindingFlags.Instance | BindingFlags.NonPublic);
             var copy = (T)memberwiseCloneMethod.Invoke(base_object, null);
@@ -19,25 +19,38 @@ namespace SolastaCommunityExpansion.Models
 
         internal static void Load()
         {
-            CreateGnome();
+            //
+            // TODO: consider a setting on UI for this and add a Switch. It's a bit more complicated than it should as we need a patch
+            //
+            _ = GnomeRace;
         }
 
-        internal static void CreateGnome()
+        internal static CharacterRaceDefinition BuildGnome()
         {
-            var gnomeSpriteReference = Utils.CustomIcons.CreateAssetReferenceSprite("Warlock", Properties.Resources.Warlock, 1024, 576);
+            var gnomeSpriteReference = Utils.CustomIcons.CreateAssetReferenceSprite("Gnome", Properties.Resources.Gnome, 1024, 512);
 
-            var abilityScoreModifier = FeatureDefinitionAttributeModifierBuilder
-                .Create("AttributeModifierGnomeAbilityScoreIncrease", "")
+            var gnomeAbilityScoreModifier = FeatureDefinitionAttributeModifierBuilder
+                .Create("AttributeModifierGnomeAbilityScoreIncrease", "b1475c33-f9ba-4224-b4b1-a55621f4dcd1")
+                .SetGuiPresentationNoContent(true)
                 .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.Additive, AttributeDefinitions.Intelligence, 2)
                 .AddToDB();
 
-            var forestGnomeScoreModifier = FeatureDefinitionAttributeModifierBuilder
-                .Create("AttributeModifierForestGnomeAbilityScoreIncrease", "")
+            var forestGnomeAbilityScoreModifier = FeatureDefinitionAttributeModifierBuilder
+                .Create("AttributeModifierForestGnomeAbilityScoreIncrease", "b7f18e2f-532f-46bf-96d2-f3612026295f")
+                .SetGuiPresentationNoContent(true)
                 .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.Additive, AttributeDefinitions.Dexterity, 1)
                 .AddToDB();
 
+            var gnomeAbilityScoreModifierSet = FeatureDefinitionFeatureSetBuilder
+                .Create("AttributeModifierGnomeAbilityScoreSet", "b239a9b0-f964-48b4-8958-a631ee3d6178")
+                .SetGuiPresentation(Category.Feature)
+                .SetMode(FeatureDefinitionFeatureSet.FeatureSetMode.Union)
+                .SetFeatureSet(gnomeAbilityScoreModifier, forestGnomeAbilityScoreModifier)
+                .AddToDB();
+
             var gnomeCunning = FeatureDefinitionSavingThrowAffinityBuilder
-                .Create("GnomeCunningFeature", "")
+                .Create("GnomeCunningFeature", "8804963d-8214-480a-be62-98a5652f69e5")
+                .SetGuiPresentation(Category.Feature)
                 .SetAffinities(
                     RuleDefinitions.CharacterSavingThrowAffinity.Advantage,
                     true,
@@ -46,66 +59,57 @@ namespace SolastaCommunityExpansion.Models
                     AttributeDefinitions.Charisma)
                 .AddToDB();
 
-            var naturalIllusionistSpellList = SpellListDefinitionBuilder
-                .Create(DatabaseHelper.SpellListDefinitions.SpellListWizard, "NaturalIllusionistSpellList", "")
+            var gnomeNaturalIllusionistSpellList = SpellListDefinitionBuilder
+                .Create(SpellListDefinitions.SpellListWizard, "NaturalIllusionistSpellList", "ead60aeb-3a72-4296-af36-2e14b110bb0b")
                 .SetGuiPresentationNoContent()
-                .SetSpellsAtLevel(0, DatabaseHelper.SpellDefinitions.AnnoyingBee)
+                .SetSpellsAtLevel(0, SpellDefinitions.AnnoyingBee)
                 .AddToDB();
 
-            var naturalIllusionist = FeatureDefinitionCastSpellBuilder
-                .Create(DatabaseHelper.FeatureDefinitionCastSpells.CastSpellElfHigh, "GnomeNaturalIllusionist", "")
+            var gnomeNaturalIllusionist = FeatureDefinitionCastSpellBuilder
+                .Create(FeatureDefinitionCastSpells.CastSpellElfHigh, "GnomeNaturalIllusionist", "bfb1b55f-dbce-49b7-b76d-5b5b3c4fe992")
                 .SetGuiPresentation(Category.Feature)
-                .SetSpellList(naturalIllusionistSpellList)
+                .SetSpellList(gnomeNaturalIllusionistSpellList)
                 .AddToDB();
 
-            //
-            // TODO: write a builder for Languages
-            //
-            var languageGnomish = Object.Instantiate(DatabaseHelper.LanguageDefinitions.Language_Goblin);
+            var languageGnomish = LanguageDefinitionBuilder
+                .Create("GnomishLanguage", "ca2c78df-6308-4fd6-b0ae-0204b8e5ff6f")
+                .SetGuiPresentation(Category.Language)
+                .AddToDB();
 
-            languageGnomish.name = "Gnomish";
-            languageGnomish.GuiPresentation.SetTitle("Language/&GnomishTitle");
-            languageGnomish.GuiPresentation.SetDescription("Language/&GnomishDescription");
-
-            var languageGnomeProficiency = FeatureDefinitionProficiencyBuilder
-                .Create("ProficiencyGnomishLanguages", "")
+            var gnomeLanguageProficiency = FeatureDefinitionProficiencyBuilder
+                .Create("ProficiencyGnomishLanguages", "c497f363-c5d9-4255-9549-2db054976361")
+                .SetGuiPresentation(Category.Feature)
                 .SetProficiencies(RuleDefinitions.ProficiencyType.Language, "Language_Common", languageGnomish.Name)
                 .AddToDB();
 
-            //
-            // TODO: write a builder for Races
-            //
-            var gnome = Object.Instantiate(DatabaseHelper.CharacterRaceDefinitions.Human);
+            var gnomeRacePresentation = MemberwiseClone(CharacterRaceDefinitions.Halfling.RacePresentation);
 
-            gnome.name = "GnomeRace";
-            gnome.GuiPresentation.SetTitle("Race/&ForestGnomeTitle");
-            gnome.GuiPresentation.SetDescription("Race/&ForestGnomeDescription");
-            gnome.GuiPresentation.SetSpriteReference(gnomeSpriteReference);
-            gnome.SetSizeDefinition(DatabaseHelper.CharacterSizeDefinitions.Small);
-            gnome.SetMinimalAge(40);
-            gnome.SetMaximalAge(350);
-            gnome.SetBaseHeight(47);
-            gnome.SetBaseWeight(35);
-            gnome.FeatureUnlocks.Clear();
-            gnome.SetRacePresentation(MemberwiseClone(DatabaseHelper.CharacterRaceDefinitions.Halfling.RacePresentation));
-            gnome.RacePresentation.SetBodyAssetPrefix(DatabaseHelper.CharacterRaceDefinitions.Elf.RacePresentation.BodyAssetPrefix);
-            gnome.RacePresentation.SetMorphotypeAssetPrefix(DatabaseHelper.CharacterRaceDefinitions.Elf.RacePresentation.MorphotypeAssetPrefix);
-            gnome.RacePresentation.SetPreferedHairColors(new TA.RangedInt(26, 47));
-            gnome.FeatureUnlocks.AddRange(new List<FeatureUnlockByLevel>()
-            {
-                new FeatureUnlockByLevel(DatabaseHelper.FeatureDefinitionMoveModes.MoveModeMove5, 1),
-                new FeatureUnlockByLevel(abilityScoreModifier, 1),
-                new FeatureUnlockByLevel(forestGnomeScoreModifier, 1),
-                new FeatureUnlockByLevel(DatabaseHelper.FeatureDefinitionSenses.SenseNormalVision, 1),
-                new FeatureUnlockByLevel(DatabaseHelper.FeatureDefinitionSenses.SenseDarkvision, 1),
-                new FeatureUnlockByLevel(gnomeCunning, 1),
-                new FeatureUnlockByLevel(naturalIllusionist, 1),
-                new FeatureUnlockByLevel(languageGnomeProficiency, 1)
-            });
+            gnomeRacePresentation.SetBodyAssetPrefix(CharacterRaceDefinitions.Elf.RacePresentation.BodyAssetPrefix);
+            gnomeRacePresentation.SetMorphotypeAssetPrefix(CharacterRaceDefinitions.Elf.RacePresentation.MorphotypeAssetPrefix);
+            gnomeRacePresentation.SetPreferedHairColors(new TA.RangedInt(26, 47));
 
-            DatabaseHelper.FeatDefinitions.FocusedSleeper.CompatibleRacesPrerequisite.Add(gnome.name);
+            var gnome = CharacterRaceDefinitionBuilder
+                .Create(CharacterRaceDefinitions.Human, "GnomeRace", "ce63140e-c018-4f83-8e6e-bc7bbc815a17")
+                .SetGuiPresentation("Race/&ForestGnomeTitle", "Race/&ForestGnomeDescription", gnomeSpriteReference)
+                .SetRacePresentation(gnomeRacePresentation)
+                .SetSizeDefinition(CharacterSizeDefinitions.Small)
+                .SetMinimalAge(40)
+                .SetMaximalAge(350)
+                .SetBaseHeight(47)
+                .SetBaseWeight(35)
+                .SetFeatures(
+                    (FeatureDefinitionMoveModes.MoveModeMove5, 1),
+                    (gnomeAbilityScoreModifierSet, 1),
+                    (FeatureDefinitionSenses.SenseNormalVision, 1),
+                    (FeatureDefinitionSenses.SenseDarkvision, 1),
+                    (gnomeCunning, 1),
+                    (gnomeNaturalIllusionist, 1),
+                    (gnomeLanguageProficiency, 1))
+                .AddToDB();
 
-            DatabaseRepository.GetDatabase<CharacterRaceDefinition>().Add(gnome);
+            FeatDefinitions.FocusedSleeper.CompatibleRacesPrerequisite.Add(gnome.name);
+
+            return gnome;
         }
     }
 }
