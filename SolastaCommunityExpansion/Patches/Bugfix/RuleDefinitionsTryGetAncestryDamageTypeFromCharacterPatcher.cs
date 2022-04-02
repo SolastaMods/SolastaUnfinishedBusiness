d9207e78533b8d9ad3e052f9178b7e79e9c3252e
ThyWoof <pauloesquilo@gmail.com>
2022-04-02 09:59:27 -0700

    fix ancestry damage type resolution if Raging Warlock

---
+++ SolastaCommunityExpansion/Patches/Bugfix/RuleDefinitionsTryGetAncestryDamageTypeFromCharacterPatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches.Bugfix
+{
+    // Warlock has DamageForce set as ancestry damage type. This causes issues interacting with Rage powers so we fix this scenario here
+    [HarmonyPatch(typeof(RuleDefinitions), "TryGetAncestryDamageTypeFromCharacter")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class RuleDefinitions_TryGetAncestryDamageTypeFromCharacter
+        {
+            internal static void Postfix(ref bool __result, string ancestryDamageType)
+            {
+                if (ancestryDamageType == "DamageForce")
+                {
+                    __result = false;
+                }
+            }
+        }
+}
