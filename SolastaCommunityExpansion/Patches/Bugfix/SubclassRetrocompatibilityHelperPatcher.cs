9f073ed5cfc48205171cbf7749df2a944bb3a78d
ThyWoof <pauloesquilo@gmail.com>
2022-04-03 15:55:43 -0700

    fix patch standard

--- SolastaCommunityExpansion/Patches/Bugfix/SubclassRetrocompatibilityHelperPatcher.cs
+++ SolastaCommunityExpansion/Patches/Bugfix/SubclassRetrocompatibilityHelperPatcher.cs
@@ -1,22 +1,21 @@
-﻿using HarmonyLib;
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
 
 namespace SolastaCommunityExpansion.Patches.Bugfix
 {
-    internal static class SubclassRetrocompatibilityHelperPatcher
+    // fixes a false positive on MC heroes that have at least one class without a subclass assigned (i.e.: level 1 Paladin, level 1 Druid, etc)
+    [HarmonyPatch(typeof(SubclassRetrocompatibilityHelper), "RetrocompatibilityCheckMissingSubclasses")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class SubclassRetrocompatibilityHelper_RetrocompatibilityCheckMissingSubclasses
     {
-        // fixes a false positive on MC heroes that have at least one class without a subclass assigned (i.e.: level 1 Paladin, level 1 Druid, etc)
-        [HarmonyPatch(typeof(SubclassRetrocompatibilityHelper), "RetrocompatibilityCheckMissingSubclasses")]
-        internal static class SubclassRetrocompatibilityHelperRetrocompatibilityCheckMissingSubclasses
+        internal static bool Prefix(RulesetCharacterHero hero)
         {
-            internal static bool Prefix(RulesetCharacterHero hero)
+            if (!Main.Settings.BugFixDeityOfferingOnMulticlassHeroes)
             {
-                if (!Main.Settings.BugFixDeityOfferingOnMulticlassHeroes)
-                {
-                    return true;
-                }
-
-                return hero.ClassesAndLevels.Count == 1;
+                return true;
             }
+
+            return hero.ClassesAndLevels.Count == 1;
         }
     }
 }
