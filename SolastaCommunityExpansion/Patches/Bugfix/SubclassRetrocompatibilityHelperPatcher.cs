7825b0ec0b1bdcb1266d7c58c5d1d3cc873ff4bb
ThyWoof <pauloesquilo@gmail.com>
2022-04-03 15:53:42 -0700

    fixes a false positive on MC heroes

---
+++ SolastaCommunityExpansion/Patches/Bugfix/SubclassRetrocompatibilityHelperPatcher.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches.Bugfix
+{
+    internal static class SubclassRetrocompatibilityHelperPatcher
+    {
+        // fixes a false positive on MC heroes that have at least one class without a subclass assigned (i.e.: level 1 Paladin, level 1 Druid, etc)
+        [HarmonyPatch(typeof(SubclassRetrocompatibilityHelper), "RetrocompatibilityCheckMissingSubclasses")]
+        internal static class SubclassRetrocompatibilityHelperRetrocompatibilityCheckMissingSubclasses
+        {
+            internal static bool Prefix(RulesetCharacterHero hero)
+            {
+                if (!Main.Settings.BugFixDeityOfferingOnMulticlassHeroes)
+                {
+                    return true;
+                }
+
+                return hero.ClassesAndLevels.Count == 1;
+            }
+        }
+    }
+}
