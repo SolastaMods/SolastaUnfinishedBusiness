a4d06fad151fa7a78b762bc7c85533de4571c501
ThyWoof <pauloesquilo@gmail.com>
2022-04-10 14:27:30 -0700

    allows prepared spell casters to take metamagic feats and have a working UI

---
+++ SolastaCommunityExpansion/Patches/GameUi/CharacterInspection/SpellRepertoirePanelPatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SolastaCommunityExpansion.Patches.GameUi.CharacterInspection
+{
+    // allows prepared spell casters to take metamagic feats and have a working UI [otherwise sorcery points get off screen]
+    [HarmonyPatch(typeof(SpellRepertoirePanel), "OnBeginShow")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class SpellRepertoirePanel_OnBeginShow
+    {
+        internal static void Postfix(SpellRepertoirePanel __instance, RectTransform ___sorceryPointsBox)
+        {
+            if (!Main.Settings.EnableMoveSorceryPointsBox || __instance.SpellRepertoire.MaxPreparedSpell == 0)
+            {
+                return;
+            }
+
+            ___sorceryPointsBox.localPosition = new Vector3(-900, 38, 0);
+        }
+    }
+}
