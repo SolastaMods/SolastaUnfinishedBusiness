69e9df13bafe6b1c62cae3205b41ae786d7acbe6
Paulo Monteiro <pauloesquilo@gmail.com>
2022-04-15 07:30:32 -0700

    wip bolgrif

--- SolastaCommunityExpansion/Properties/Resources.Designer.cs
+++ SolastaCommunityExpansion/Properties/Resources.Designer.cs
@@ -79,7 +79,19 @@ namespace SolastaCommunityExpansion.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap Bolgrif
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("Bolgrif", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
