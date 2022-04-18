e6d96d28645a686e58a640816bf15c18d6c59fbd
ThyWoof <pauloesquilo@gmail.com>
2022-04-17 20:45:49 -0700

    add missing feature definitions

---
+++ SolastaCommunityExpansion/Builders/Features/FeatureDefinitionOnAttackFinishedEffectBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using SolastaCommunityExpansion.CustomDefinitions;
+
+namespace SolastaCommunityExpansion.Builders.Features
+{
+    public class FeatureDefinitionOnAttackFinishedEffectBuilder : FeatureDefinitionBuilder<FeatureDefinitionOnAttackFinishedEffect, FeatureDefinitionOnAttackFinishedEffectBuilder>
+    {
+        #region Constructors
+        protected FeatureDefinitionOnAttackFinishedEffectBuilder(string name, Guid namespaceGuid) : base(name, namespaceGuid)
+        {
+        }
+
+        protected FeatureDefinitionOnAttackFinishedEffectBuilder(string name, string definitionGuid) : base(name, definitionGuid)
+        {
+        }
+
+        protected FeatureDefinitionOnAttackFinishedEffectBuilder(FeatureDefinitionOnAttackFinishedEffect original, string name, Guid namespaceGuid) : base(original, name, namespaceGuid)
+        {
+        }
+
+        protected FeatureDefinitionOnAttackFinishedEffectBuilder(FeatureDefinitionOnAttackFinishedEffect original, string name, string definitionGuid) : base(original, name, definitionGuid)
+        {
+        }
+        #endregion
+
+        public FeatureDefinitionOnAttackFinishedEffectBuilder SetOnAttackFinishedDelegate(OnAttackFinishedDelegate del)
+        {
+            Definition.SetOnAttackFinishedDelegate(del);
+            return this;
+        }
+    }
+}
