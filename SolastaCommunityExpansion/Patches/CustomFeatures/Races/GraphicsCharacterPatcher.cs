1e1f0983de4f48c632d668e65c02011e3c3485ac
Silvergriffon <86208060+Silvergriffon@users.noreply.github.com>
2022-04-19 11:05:57 -0400

    Now properly scaling Bolgrif and Gnome

--- SolastaCommunityExpansion/Patches/CustomFeatures/Races/GraphicsCharacterPatcher.cs
+++ SolastaCommunityExpansion/Patches/CustomFeatures/Races/GraphicsCharacterPatcher.cs
@@ -1,41 +1,31 @@
 ﻿using HarmonyLib;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using static SolastaModApi.DatabaseHelper.CharacterRaceDefinitions;
 
 namespace SolastaCommunityExpansion.Patches.CustomFeatures.Races
 {
         [HarmonyPatch(typeof(GraphicsCharacter), "ResetScale")]
         class GraphicsCharacter_ResetScale
         {
-		    //Both scales work, just have to make them work together now
-            //const float bolgrifScaleMap = 7.8f /6.4f;
-
-            const float gnomeScaleMap = -0.02f /-0.03f;
-        
-			
             internal static void Postfix(GraphicsCharacter __instance, ref float __result)
             {
+                Dictionary<CharacterRaceDefinition, float> raceScaleMap = new Dictionary<CharacterRaceDefinition, float>();
+                
+                raceScaleMap[SolastaCommunityExpansion.Races.BolgrifRaceBuilder.BolgrifRace] = 8.8f / 6.4f;
+                raceScaleMap[SolastaCommunityExpansion.Races.GnomeRaceBuilder.GnomeRace] = -0.04f / -0.06f;
+                
                 var race = (__instance.RulesetCharacter as RulesetCharacterHero)?.RaceDefinition;
-				
-				if (/*race != SolastaCommunityExpansion.Races.BolgrifRaceBuilder.BolgrifRace &&*/ race != SolastaCommunityExpansion.Races.GnomeRaceBuilder.GnomeRace)
-				{
-					return;
-				}
+                
+                if (race != null && raceScaleMap.ContainsKey(race))
+                {
+                    __result *= raceScaleMap[race];
+                }
+                else
+                {
+                    __result *= 1.0f / 1.0f;
+                }
 
-                    //__result *= bolgrifScaleMap;
-                    //__instance.transform.localScale = new Vector3(__result, __result, __result);
-
-
-                    __result *= gnomeScaleMap;
-                    __instance.transform.localScale = new Vector3(__result, __result, __result);
-
-
+                __instance.transform.localScale = new Vector3(__result, __result, __result);
 
             }
         }
