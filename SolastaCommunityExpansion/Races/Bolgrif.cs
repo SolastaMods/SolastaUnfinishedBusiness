e6275f7cd7403a11585b0298973c1bf859e46960
ThyWoof <pauloesquilo@gmail.com>
2022-04-19 18:03:23 -0700

    keep repo standard

--- SolastaCommunityExpansion/Races/Bolgrif.cs
+++ SolastaCommunityExpansion/Races/Bolgrif.cs
@@ -78,7 +78,7 @@ namespace SolastaCommunityExpansion.Races
 
             bolgrifRacePresentation.SetPreferedSkinColors(new TA.RangedInt(45, 48));
             bolgrifRacePresentation.SetPreferedHairColors(new TA.RangedInt(16, 32));
-            
+
             var bolgrif = CharacterRaceDefinitionBuilder
                 .Create(CharacterRaceDefinitions.Human, "BolgrifRace", "346b7f90-973f-425f-8342-d534759e65aa")
                 .SetGuiPresentation(Category.Race, bolgrifSpriteReference)
