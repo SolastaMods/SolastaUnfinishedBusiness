4b74bc9c2ea1aa5a84cd1746ed81424d59ad5013
Ender Wiggin <andrii.gook@gmail.com>
2022-04-22 21:25:48 +0300

    added ability to create FeatureDefinitionFreeBonusCantrips and FeatureDefinitionPointPool that do not count their selections to overall progression

---
+++ SolastaCommunityExpansion/Builders/FeatureDefinitionFreeBonusCantrips.cs
@@ -0,0 +1,10 @@
+﻿using SolastaCommunityExpansion.CustomDefinitions;
+
+namespace SolastaCommunityExpansion.Builders
+{
+    public class FeatureDefinitionFreeBonusCantrips : FeatureDefinitionBonusCantrips, IPointPoolMaxBonus
+    {
+        public int MaxPointsBonus { get => BonusCantrips.Count; }
+        public HeroDefinitions.PointsPoolType PoolType { get => HeroDefinitions.PointsPoolType.Cantrip; }
+    }
+}
--- SolastaCommunityExpansion/Patches/CustomFeatures/Spells/CustomSpellPatches.cs
+++ SolastaCommunityExpansion/Patches/CustomFeatures/Spells/CustomSpellPatches.cs
@@ -1,4 +1,8 @@
-﻿using HarmonyLib;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using ModKit;
+using SolastaCommunityExpansion.CustomDefinitions;
 using SolastaCommunityExpansion.Models;
 
 namespace SolastaCommunityExpansion.Patches.CustomFeatures.Spells
@@ -23,6 +27,52 @@ namespace SolastaCommunityExpansion.Patches.CustomFeatures.Spells
             static void Postfix(ref EffectDescription __result, GuiSpellDefinition __instance)
             {
                 __result = CustomFeaturesContext.ModifySpellEffectGui(__result, __instance);
+            }
+        }
+
+        [HarmonyPatch(typeof(CharacterBuildingManager), "RegisterPoolStack")]
+        [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+        public static class CharacterBuildingManager_RegisterPoolStack
+        {
+            public static void Postfix(CharacterBuildingManager __instance, CharacterHeroBuildingData heroBuildingData,
+                List<FeatureDefinition> features,
+                string tag)
+            {
+                var hero = heroBuildingData.HeroCharacter;
+                var poolMods = CustomFeaturesContext.FeaturesByType<IPointPoolMaxBonus>(hero);
+
+                heroBuildingData.HeroCharacter.BrowseFeaturesOfType<FeatureDefinition>(features, (feature, _) =>
+                {
+                    if (feature is IPointPoolMaxBonus bonus)
+                    {
+                        poolMods.Remove(bonus);
+                    }
+                }, tag);
+
+                var values = new Dictionary<HeroDefinitions.PointsPoolType, int>();
+                foreach (var mod in poolMods)
+                {
+                    values.AddOrReplace(mod.PoolType, values.GetValueOrDefault(mod.PoolType) + mod.MaxPointsBonus);
+                }
+
+                foreach (var mod in values)
+                {
+                    var poolType = mod.Key;
+                    var value = mod.Value;
+
+                    var poolStack = heroBuildingData.PointPoolStacks[poolType] ?? new PointPoolStack(poolType);
+
+                    var pool = poolStack.ActivePools.GetValueOrDefault(tag);
+                    if (pool == null)
+                    {
+                        poolStack.ActivePools.Add(tag, new PointPool(value));
+                    }
+                    else
+                    {
+                        pool.MaxPoints += value;
+                        pool.RemainingPoints += value;
+                    }
+                }
             }
         }
     }
