97c44750252f01ea1dec811c6bb59f4dbd7fb661
ThyWoof <pauloesquilo@gmail.com>
2022-04-30 16:53:55 -0700

    add patch to support reactions

---
+++ SolastaCommunityExpansion/Patches/GameUi/Battle/ReactionTriggeredPatcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SolastaCommunityExpansion.Patches.GameUi.Battle
+{
+    [HarmonyPatch(typeof(ReactionModal), "ReactionTriggered")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class ReactionModal_ReactionTriggered
+    {
+        public static bool Prefix(ReactionRequest request)
+        {
+            var isCtrlPressed = Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl);
+
+            if (request is ReactionRequestSpendSpellSlot
+                && Main.Settings.EnableCtrlClickBypassSmiteReactionPanel
+                && isCtrlPressed)
+            {
+                ServiceRepository.GetService<ICommandService>().ProcessReactionRequest(request, false);
+                return false;
+            }
+
+            //
+            // wildshape heroes should not be able to cast spells
+            //
+            var rulesetCharacter = request.Character.RulesetCharacter;
+
+            if (rulesetCharacter.IsSubstitute
+                && (request is ReactionRequestCastSpell
+                || request is ReactionRequestCastFallPreventionSpell
+                || request is ReactionRequestCastImmunityToSpell))
+            {
+                ServiceRepository.GetService<ICommandService>().ProcessReactionRequest(request, false);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
