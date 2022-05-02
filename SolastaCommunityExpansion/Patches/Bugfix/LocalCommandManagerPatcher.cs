0741265577ffca91da3c2d4433ad30ee94896a4e
Ender Wiggin <andrii.gook@gmail.com>
2022-05-02 20:54:13 +0300

    fixed subclasses being lost on cancelled levelup

---
+++ SolastaCommunityExpansion/Patches/Bugfix/LocalCommandManagerPatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches.Bugfix
+{
+    // Default implementation of this metod doesn't pass 'onlyIfCurrentLevel' argument to ICharacterBuildingService
+    // This causes subclass to be lost on cancelled levelup
+    // Default classes are then silently fixed in CharacterBuildingManager.RepairSubclass, but custom ones break
+    [HarmonyPatch(typeof(LocalCommandManager), "UnassignLastSubclass")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class LocalCommandManager_UnassignLastSubclass
+    {
+        internal static bool Prefix(RulesetCharacterHero hero, bool onlyIfCurrentLevel)
+        {
+            ServiceRepository.GetService<ICharacterBuildingService>().UnassignLastSubclass(hero, onlyIfCurrentLevel);
+            return false;
+        }
+    }
+}
