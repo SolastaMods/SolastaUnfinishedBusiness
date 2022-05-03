a2cb775a893279f01300a65548c6cdc63b2d98dd
Ender Wiggin <andrii.gook@gmail.com>
2022-05-03 22:07:34 +0300

    added patch that formats auto prepared spells same way extra spell lists are formatted

--- SolastaCommunityExpansion/Patches/GameUi/FeatureDefinitionMagicAffinityPatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUi/FeatureDefinitionMagicAffinityPatcher.cs
@@ -6,57 +6,91 @@ using HarmonyLib;
 
 namespace SolastaCommunityExpansion.Patches.GameUi
 {
-    [HarmonyPatch(typeof(FeatureDefinitionMagicAffinity), "FormatDescription")]
-    internal static class FeatureDefinitionMagicAffinityPatcher
+    internal static class FeatureDefinitionDescriptionPatcher
     {
-        /**Formats spell list into list with spell levels, instead of 1 line of all spells like default does*/
-        public static string FormatSpellList(string format, string _, FeatureDefinitionMagicAffinity instance)
+        private static string FormatSpellLevel(int level)
         {
-            var description = instance.GuiPresentation.Description;
-            var spells = new Dictionary<int, List<SpellDefinition>>();
-            var levels = new HashSet<int>();
-            foreach (var duplet in instance.ExtendedSpellList.SpellsByLevel)
-            {
-                if (!spells.ContainsKey(duplet.Level))
-                {
-                    spells.Add(duplet.Level, new List<SpellDefinition>());
-                    levels.Add(duplet.Level);
-                }
-
-                spells[duplet.Level].AddRange(duplet.Spells);
-            }
-
-            foreach (var level in levels)
-            {
-                if (spells.ContainsKey(level) && spells[level].Empty())
-                {
-                    spells.Remove(level);
-                }
-            }
-            var spellList = "\n" +string.Join("\n",
-                spells.Select(e =>
-                    $"{Gui.Colorize($"{(e.Key == 0 ? 0 : Gui.ToRoman(e.Key))}", Gui.ColorHighEmphasis)}\t{string.Join(", ", e.Value.Select(s => s.FormatTitle()))}"));
-
-            return Gui.Format(Gui.Localize(description), spellList);
+            return Gui.Colorize((level == 0 ? "0" : Gui.ToRoman(level)), Gui.ColorHighEmphasis);
         }
 
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        [HarmonyPatch(typeof(FeatureDefinitionAutoPreparedSpells), "FormatDescription")]
+        internal static class FeatureDefinitionAutoPreparedSpells_FormatDescription
         {
-            var formatMethod = typeof(Gui).GetMethod("Format", BindingFlags.Static | BindingFlags.Public);
-            var myFormatMethod = typeof(FeatureDefinitionMagicAffinityPatcher).GetMethod("FormatSpellList");
-            var found = 0;
-
-            foreach (var instruction in instructions)
+            internal static bool Prefix(FeatureDefinitionAutoPreparedSpells __instance, ref string __result)
             {
-                //Find first call to Gui.Format and replace it with oour custom method
-                if (instruction.Calls(formatMethod) && ++found == 1)
+                string empty1 = string.Empty;
+                foreach (var group in __instance.AutoPreparedSpellsGroups)
                 {
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Call, myFormatMethod);
+                    var spells = string.Join(", ", group.SpellsList.Select(s => s.FormatTitle()));
+                    string str = $"{FormatSpellLevel(group.ClassLevel)}\t{spells}";
+                    if (!string.IsNullOrEmpty(empty1))
+                        empty1 += "\n";
+                    empty1 += str;
                 }
-                else
+
+                var description = __instance.GuiPresentation.Description;
+                description = description == Gui.NoLocalization ? string.Empty : Gui.Localize(description);
+
+                __result = Gui.Format(description, empty1);
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(FeatureDefinitionMagicAffinity), "FormatDescription")]
+        internal static class FeatureDefinitionMagicAffinity_FormatDescription
+        {
+            /**Formats spell list into list with spell levels, instead of 1 line of all spells like default does*/
+            public static string FormatSpellList(string format, string _, FeatureDefinitionMagicAffinity instance)
+            {
+                var description = instance.GuiPresentation.Description;
+                var spells = new Dictionary<int, List<SpellDefinition>>();
+                var levels = new HashSet<int>();
+                foreach (var duplet in instance.ExtendedSpellList.SpellsByLevel)
                 {
-                    yield return instruction;
+                    if (!spells.ContainsKey(duplet.Level))
+                    {
+                        spells.Add(duplet.Level, new List<SpellDefinition>());
+                        levels.Add(duplet.Level);
+                    }
+
+                    spells[duplet.Level].AddRange(duplet.Spells);
+                }
+
+                foreach (var level in levels)
+                {
+                    if (spells.ContainsKey(level) && spells[level].Empty())
+                    {
+                        spells.Remove(level);
+                    }
+                }
+
+                var spellList = "\n" + string.Join("\n",
+                    spells.Select(e =>
+                        $"{FormatSpellLevel(e.Key)}\t{string.Join(", ", e.Value.Select(s => s.FormatTitle()))}"));
+
+                return Gui.Format(Gui.Localize(description), spellList);
+            }
+
+           
+
+            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var formatMethod = typeof(Gui).GetMethod("Format", BindingFlags.Static | BindingFlags.Public);
+                var myFormatMethod = typeof(FeatureDefinitionMagicAffinity_FormatDescription).GetMethod("FormatSpellList");
+                var found = 0;
+
+                foreach (var instruction in instructions)
+                {
+                    //Find first call to Gui.Format and replace it with oour custom method
+                    if (instruction.Calls(formatMethod) && ++found == 1)
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        yield return new CodeInstruction(OpCodes.Call, myFormatMethod);
+                    }
+                    else
+                    {
+                        yield return instruction;
+                    }
                 }
             }
         }
