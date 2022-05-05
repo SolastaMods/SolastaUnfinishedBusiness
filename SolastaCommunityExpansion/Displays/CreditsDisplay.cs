9fdd9b0b6716a0929dcbf717600ba68efeb946f9
ThyWoof <pauloesquilo@gmail.com>
2022-05-04 21:36:00 -0700

    update credits

--- SolastaCommunityExpansion/Displays/CreditsDisplay.cs
+++ SolastaCommunityExpansion/Displays/CreditsDisplay.cs
@@ -17,27 +17,30 @@ namespace SolastaCommunityExpansion.Displays
 
         internal static readonly Dictionary<string, string> CreditsTable = new()
         {
-            { "AceHigh", "Warlock class, multiclass idea, Tactician subclass, Power Attack and Reckless Fury feats, no identification" },
+            { "AceHigh", "SoulBlade subclass, Tactician subclass, Power Attack and Reckless Fury feats, no identification" },
             { "Bazou", "Witch class" },
             { "Boofat", "alwaysAlt" },
             { "Burtsev-Alexey", "deep copy algorithm" },
             { "ChrisJohnDigital", "head developer, crafting, faction relations, feats, fighting styles, items, subclasses, progression" },
-            { "Dreadmaker", "Circle of the Forest Guardian subclass" },
-            { "DubhHerder", "7th, 8th and 9th level spells, Crafty Feats migration, bug models replacement, additional high level monsters, class, subclasses" },
+            { "Dreadmaker", "Forest Guardian subclass" },
+            { "DubhHerder", "7th, 8th and 9th level spells, Crafty Feats migration, bug models replacement, additional high level monsters, Warlock class, subclasses" },
             { "ElAntonious", "Arcanist subclass, Torchbearer and Dual Flurry feats" },
-            { "Holic75", "pact magic slots, Warlock class sprite, bolgrif and gnome races, acid claw, air blast burst of radiance, thunder strike, earth tremor and winter's breath spells" },
+            { "Esker", "Warlock class design, quality assurance" },
+            { "Holic75", "SolastaModHelpers patches, SolastaExtraContent races, cantrips, spells, sprites" },
             { "ImpPhil", "adv/dis rules, conjurations control, auto-equip, location rotation in DM, monster's health, pause UI, sorting, stocks prices, no attunement, xp scaling, character export, save by location, combat camera, diagnostics, custom icons, refactor, screen map" },
             { "Krisys", "Thug subclass" },
+            { "Lyraele", "Warlock class design, quality assurance" },
             { "Narria", "modKit creator, developer" },
             { "Nd", "Opportunist subclass" },
+            { "PraiseThyBus", "quality assurance" },
             { "RedOrca", "Path of the Light subclass, Indomitable Might" },
             { "Scarlex", "Royal Knight subclass" },
             { "SilverGriffon", "PickPocket, lore friendly names, crafty feats, face unlocks, sylvan armor unlock, empress garb skins, arcane foci items, belt of dwarvenkin, merchants" },
             { "Sinai-dev", "Unity Explorer UI standalone" },
             { "Spacehamster", "dataminer" },
-            { "TPABOBAP", "Warlock class" },
+            { "TPABOBAP", "Warlock class design, improvements and refactor, Level Up improvements, Tinkerer improvements, Holic patches " },
             { "View619", "Darkvision, Superior Dark Vision" },
-            { "Zappastuff", "multiclass, level 20, respec, level down, encounters, dungeon maker editor & pro, party size, screen gadgets highlights, inventory sorting, epic points, teleport, mod UI, diagnostics, integration, arcane defense/precision, brutal thug, charismatic defense/precision, fighting surge, metamagic, primal, shady and wise defense/precision feats, races and spells migration" }
+            { "Zappastuff", "multiclass, level 20, respec, level down, encounters, dungeon maker editor & pro, party size, screen gadgets highlights, inventory sorting, epic points, teleport, mod UI, diagnostics, integration, arcane defense/precision, brutal thug, charismatic defense/precision, fast hands, fighting surge, marksman, metamagic, primal, shady and wise defense/precision feats, races and spells migration, warlock pact magic, refactor" }
         };
 
         internal static void DisplayCredits()
