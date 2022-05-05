e91e6cf80e78abcb6968dcf44cafc2d9b7624587
ThyWoof <pauloesquilo@gmail.com>
2022-05-05 12:18:59 -0700

    fix reaction panel on SC Warlock

---
+++ SolastaCommunityExpansion/Patches/CustomFeatures/PactMagic/CharacterReactionSubitemPatcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using SolastaMulticlass.Models;
+using UnityEngine;
+
+namespace SolastaMulticlass.Patches.SlotsColors
+{
+
+    // creates different slots colors and pop up messages depending on slot types
+    [HarmonyPatch(typeof(CharacterReactionSubitem), "Bind")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class CharacterReactionSubitem_Bind
+    {
+        public static void Postfix(
+            RulesetSpellRepertoire spellRepertoire,
+            int slotLevel,
+            RectTransform ___slotStatusTable,
+            CanvasGroup ___canvasGroup)
+        {
+            var heroWithSpellRepertoire = SharedSpellsContext.GetHero(spellRepertoire.CharacterName);
+
+            spellRepertoire.GetSlotsNumber(slotLevel, out var totalSlotsRemainingCount, out var totalSlotsCount);
+
+            MulticlassGameUiContext.PaintPactSlots(
+                heroWithSpellRepertoire, totalSlotsCount, totalSlotsRemainingCount, slotLevel, ___slotStatusTable);
+
+            var warlockSpellLevel = SharedSpellsContext.GetWarlockSpellLevel(heroWithSpellRepertoire);
+
+            if (!SharedSpellsContext.IsMulticaster(heroWithSpellRepertoire) && warlockSpellLevel != slotLevel)
+            {
+                ___canvasGroup.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // ensures slot colors are white before getting back to pool
+    [HarmonyPatch(typeof(CharacterReactionSubitem), "Unbind")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class CharacterReactionSubitem_Unbind
+    {
+        public static void Prefix(RectTransform ___slotStatusTable)
+        {
+            MulticlassGameUiContext.PaintSlotsWhite(___slotStatusTable);
+        }
+    }
+}
