ceefdf00626dafb2a0bdec31e72362adb4de98f5
Ender Wiggin <andrii.gook@gmail.com>
2022-05-09 18:39:01 +0300

    made Divine Smite trigger only for melee attacks

---
+++ SolastaCommunityExpansion/Features/HouseFeatureTweaks.cs
@@ -0,0 +1,24 @@
+﻿using SolastaModApi;
+using SolastaModApi.Extensions;
+
+namespace SolastaCommunityExpansion.Features
+{
+    public static class HouseFeatureTweaks
+    {
+        public static void LateLoad()
+        {
+            FixDivineSmiteRestrictions();
+        }
+
+        /**
+         * Makes Divine Smite trigger only from melee attacks.
+         * This wasn't relevant until we changed how SpendSpellSlot trigger works.
+         */
+        private static void FixDivineSmiteRestrictions()
+        {
+            DatabaseHelper.FeatureDefinitionAdditionalDamages.AdditionalDamagePaladinDivineSmite
+                .SetAttackModeOnly(true)
+                .SetRequiredProperty(RuleDefinitions.AdditionalDamageRequiredProperty.MeleeWeapon);
+        }
+    }
+}
