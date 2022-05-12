cb5767bb39f7b06463749590ec7b26d3329e5ad9
Silvergriffon <86208060+Silvergriffon@users.noreply.github.com>
2022-05-12 11:05:07 -0400

    MorwennasSpellbook initial commit

--- SolastaCommunityExpansion/Models/SpellsContext.cs
+++ SolastaCommunityExpansion/Models/SpellsContext.cs
@@ -207,6 +207,7 @@ namespace SolastaCommunityExpansion.Models
             SrdSpells.Register();
             EWSpells.Register();
             HouseSpellTweaks.Register();
+            MorwennasSpellbook.Register();
 
             // caches which spells are toggleable per spell list
             Spells = Spells.OrderBy(x => x.SpellLevel).ThenBy(x => x.FormatTitle()).ToHashSet();
@@ -222,6 +223,7 @@ namespace SolastaCommunityExpansion.Models
             //BazouSpells.AddToDB();
             HolicSpells.AddToDB();
             SrdSpells.AddToDB();
+            MorwennasSpellbook.AddToDB();
         }
 
         internal static void RegisterSpell(
