69b7ca5040003e71a08d0489c103250abda2847d
Ender Wiggin <andrii.gook@gmail.com>
2022-05-15 10:14:05 +0300

    do not terminate 'summoned' effect from creatues if changing location

--- SolastaCommunityExpansion/Patches/Bugfix/GameLocationManagerPatcher.cs
+++ SolastaCommunityExpansion/Patches/Bugfix/GameLocationManagerPatcher.cs
@@ -5,6 +5,7 @@ using System.Reflection.Emit;
 using HarmonyLib;
 using SolastaCommunityExpansion.Api.AdditionalExtensions;
 using SolastaCommunityExpansion.CustomDefinitions;
+using SolastaModApi;
 
 namespace SolastaCommunityExpansion.Patches.Bugfix
 {
@@ -12,6 +13,32 @@ namespace SolastaCommunityExpansion.Patches.Bugfix
     [HarmonyPatch(typeof(GameLocationManager), "StopCharacterEffectsIfRelevant")]
     internal static class GameLocationManager_StopCharacterEffectsIfRelevant
     {
+        internal static void Prefix(GameLocationManager __instance, bool willEnterChainedLocation)
+        {
+            if (willEnterChainedLocation) { return; }
+
+            //remove summoned monsters upon entering new locations, since the game somehow removes corresponding summoned (and all other conditions)
+            //from them and thus they are no longer linked to the caster
+            var characterService = ServiceRepository.GetService<IGameLocationCharacterService>();
+
+            var gameLocationCharacters = characterService.GuestCharacters.ToArray();
+            foreach (var locationCharacter in gameLocationCharacters)
+            {
+                var rulesetCharacter = locationCharacter.RulesetCharacter;
+
+                var isSummon = rulesetCharacter.ConditionsByCategory.Any(c => c.Value.Any(cc =>
+                    cc.ConditionDefinition == DatabaseHelper.ConditionDefinitions.ConditionConjuredCreature));
+
+                var isKindredSpirit = (rulesetCharacter is RulesetCharacterMonster monster
+                                       && monster.MonsterDefinition.CreatureTags.Contains("KindredSpirit"));
+
+                if (isSummon && !isKindredSpirit)
+                {
+                    characterService.DestroyCharacterBody(locationCharacter);
+                }
+            }
+        }
+
         internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var code = instructions.ToList();
@@ -40,6 +67,13 @@ namespace SolastaCommunityExpansion.Patches.Bugfix
                 var skip = baseDefinition.GetFirstSubFeatureOfType<ISKipEffectRemovalOnLocationChange>();
 
                 if (skip != null && skip.Skip(willEnterChainedLocation))
+                {
+                    return;
+                }
+
+                var effectDescription = effect.EffectDescription;
+                if (willEnterChainedLocation
+                    && RuleDefinitions.MatchesMagicType(effectDescription, RuleDefinitions.MagicType.SummonsCreature))
                 {
                     return;
                 }
