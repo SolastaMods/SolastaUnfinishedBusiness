270df8898eda36cee4e34bacf7a922244d7f6552
Ender Wiggin <andrii.gook@gmail.com>
2022-05-16 18:13:50 +0300

    added patch and sub-feature that can modify speed based on conditions

---
+++ SolastaCommunityExpansion/Features/ConditionalMovementModifier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using SolastaCommunityExpansion.Api.AdditionalExtensions;
+
+namespace SolastaCommunityExpansion.Features;
+
+public interface IConditionalMovementModifier
+{
+    void AddModifiers(RulesetCharacter character, List<FeatureDefinition> modifiers);
+}
+
+public class ConditionalMovementModifier : IConditionalMovementModifier
+{
+    private readonly FeatureDefinition modifier;
+    private readonly CharacterValidator[] validators;
+
+    public ConditionalMovementModifier(FeatureDefinition modifier, params CharacterValidator[] validators)
+    {
+        this.modifier = modifier;
+        this.validators = validators;
+    }
+
+    public void AddModifiers(RulesetCharacter character, List<FeatureDefinition> modifiers)
+    {
+        if (character.IsValid(validators))
+        {
+            modifiers.Add(modifier);
+        }
+    }
+}
