f9cc905eb83a665f290ebc3d952e17d27df80767
Ender Wiggin <andrii.gook@gmail.com>
2022-05-16 17:11:07 +0300

    added `bonus unarmed attack`

---
+++ SolastaCommunityExpansion/Api/AdditionalExtensions/RulesetCharacterHeroExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using SolastaModApi.Infrastructure;
+
+namespace SolastaCommunityExpansion.Api.AdditionalExtensions;
+
+public static class RulesetCharacterHeroExtensions
+{
+    public static RulesetAttackMode RefreshAttackModePublic(
+        this RulesetCharacterHero instance,
+        ActionDefinitions.ActionType actionType,
+        ItemDefinition itemDefinition,
+        WeaponDescription weaponDescription,
+        bool freeOffHand,
+        bool canAddAbilityDamageBonus,
+        string slotName,
+        List<IAttackModificationProvider> attackModifiers,
+        Dictionary<FeatureDefinition, RuleDefinitions.FeatureOrigin> featuresOrigin,
+        RulesetItem weapon = null)
+    {
+        var attackMode = instance.InvokeMethod("RefreshAttackMode", actionType, itemDefinition, weaponDescription,
+            freeOffHand, canAddAbilityDamageBonus, slotName, attackModifiers, featuresOrigin, weapon);
+
+        return (RulesetAttackMode) attackMode;
+    }
+}
--- SolastaCommunityExpansion/Features/AddExtraAttack.cs
+++ SolastaCommunityExpansion/Features/AddExtraAttack.cs
@@ -1,6 +1,44 @@
-﻿namespace SolastaCommunityExpansion.Features;
+﻿using System.Collections.Generic;
+using SolastaCommunityExpansion.Api.AdditionalExtensions;
+using SolastaModApi.Infrastructure;
+
+namespace SolastaCommunityExpansion.Features;
 
 public interface IAddExtraAttack
 {
     void TryAddExtraAttack(RulesetCharacterHero hero);
+}
+
+public class AddBonusUnarmedAttack : IAddExtraAttack
+{
+    private readonly CharacterValidator[] validators;
+
+    public AddBonusUnarmedAttack(params CharacterValidator[] validators)
+    {
+        this.validators = validators;
+    }
+
+    public void TryAddExtraAttack(RulesetCharacterHero hero)
+    {
+        if (!hero.IsValid(validators))
+        {
+            return;
+        }
+
+        var strikeDefinition = hero.UnarmedStrikeDefinition;
+
+        var attackModifiers = hero.GetField<List<IAttackModificationProvider>>("attackModifiers");
+
+        hero.AttackModes.Add(hero.RefreshAttackModePublic(
+            ActionDefinitions.ActionType.Bonus,
+            strikeDefinition,
+            strikeDefinition.WeaponDescription,
+            false,
+            true,
+            EquipmentDefinitions.SlotTypeOffHand,
+            attackModifiers,
+            hero.FeaturesOrigin,
+            null
+        ));
+    }
 }
