6c15a705c586ab51acf86f71c16da899d1324b7d
Ender Wiggin <andrii.gook@gmail.com>
2022-05-16 22:00:32 +0300

    added power use validator

---
+++ SolastaCommunityExpansion/Features/PowerValidity.cs
@@ -0,0 +1,23 @@
+﻿using SolastaCommunityExpansion.Api.AdditionalExtensions;
+
+namespace SolastaCommunityExpansion.Features;
+
+public interface IPowerUseValidity
+{
+    bool CanUsePower(RulesetCharacter character);
+}
+
+public class PowerUseValidity : IPowerUseValidity
+{
+    private readonly CharacterValidator[] validators;
+
+    public PowerUseValidity(params CharacterValidator[] validators)
+    {
+        this.validators = validators;
+    }
+
+    public bool CanUsePower(RulesetCharacter character)
+    {
+        return character.IsValid(validators);
+    }
+}
