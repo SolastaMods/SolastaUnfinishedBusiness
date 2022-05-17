8536771dfedd713abd7899a076edfbe1ac80bac9
Ender Wiggin <andrii.gook@gmail.com>
2022-05-17 18:19:26 +0300

    added sub-feature that can restrict powers reacting to damage dealt

---
+++ SolastaCommunityExpansion/Features/ReactionAttackModeRestriction.cs
@@ -0,0 +1,19 @@
+﻿namespace SolastaCommunityExpansion.Features;
+
+public interface IReactionAttackModeRestriction
+{
+    bool ValidRactionMode(RulesetAttackMode attackMode, RulesetCharacter character, RulesetCharacter target);
+}
+
+public static class ReactionAttackModeRestriction
+{
+    public static readonly IReactionAttackModeRestriction MeleeOnly = new MeleeOnlyImpl();
+    
+    private class MeleeOnlyImpl: IReactionAttackModeRestriction
+    {
+        public bool ValidRactionMode(RulesetAttackMode attackMode, RulesetCharacter character, RulesetCharacter target)
+        {
+            return !attackMode.Ranged && attackMode.Reach;
+        }
+    }
+}
