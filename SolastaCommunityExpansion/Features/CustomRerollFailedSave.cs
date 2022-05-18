04594b165493046d3c45f6ce1de19debec23100b
Ender Wiggin <andrii.gook@gmail.com>
2022-05-18 21:09:33 +0300

    added patch and sub-feature that allow cusom power usage to reroll failed save

---
+++ SolastaCommunityExpansion/Features/CustomRerollFailedSave.cs
@@ -0,0 +1,25 @@
+﻿namespace SolastaCommunityExpansion.Features;
+
+public interface IUsePowerToRerollFailedSave
+{
+    string ReactionName { get; }
+    FeatureDefinitionPower GetPowerToRerollFailedSave(RulesetCharacter character, RuleDefinitions.RollOutcome saveOutcome);
+}
+
+public class CustomRerollFailedSave: IUsePowerToRerollFailedSave
+{
+    private readonly FeatureDefinitionPower power;
+
+    public CustomRerollFailedSave(FeatureDefinitionPower power, string reactionName = null)
+    {
+        this.power = power;
+        ReactionName = string.IsNullOrEmpty(reactionName) ? power.Name : reactionName;
+    }
+
+    public string ReactionName { get; }
+
+    public FeatureDefinitionPower GetPowerToRerollFailedSave(RulesetCharacter character, RuleDefinitions.RollOutcome saveOutcome)
+    {
+        return power;
+    }
+}
