fdb0e5ea1859919107394d73ae21770609996800
ThyWoof <pauloesquilo@gmail.com>
2022-05-17 21:48:44 -0700

    tweak fighting style UI

--- SolastaCommunityExpansion/Patches/GameUi/LevelUp/CharacterStageFightingStyleSelectionPanelPatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUi/LevelUp/CharacterStageFightingStyleSelectionPanelPatcher.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SolastaCommunityExpansion.Patches.GameUi.LevelUp
 {
@@ -9,6 +10,8 @@ namespace SolastaCommunityExpansion.Patches.GameUi.LevelUp
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     internal static class CharacterStageFightingStyleSelectionPanel_OnBeginShow
     {
+        private static Vector2 OriginalAnchoredPosition { get; set; } = Vector2.zero;
+
         internal static void Prefix(List<FightingStyleDefinition> ___compatibleFightingStyles, RectTransform ___fightingStylesTable)
         {
             if (Main.Settings.EnableSortingFightingStyles)
@@ -17,15 +20,25 @@ namespace SolastaCommunityExpansion.Patches.GameUi.LevelUp
                     .Sort((a, b) => a.FormatTitle().CompareTo(b.FormatTitle()));
             }
 
+            var gridLayoutGroup = ___fightingStylesTable.GetComponent<GridLayoutGroup>();
             var count = ___compatibleFightingStyles.Count;
+
+            if (OriginalAnchoredPosition == Vector2.zero)
+            {
+                OriginalAnchoredPosition = ___fightingStylesTable.anchoredPosition;
+            }
 
             if (count > 8)
             {
-                ___fightingStylesTable.localScale = new Vector3(0.8f, 0.8f, 1f);
+                gridLayoutGroup.constraintCount = 3;
+                ___fightingStylesTable.anchoredPosition = new Vector2(0, +15);
+                //___fightingStylesTable.localScale = new Vector3(0.8f, 0.8f, 1f);
             }
             else
             {
-                ___fightingStylesTable.localScale = new Vector3(1f, 1f, 1f);
+                gridLayoutGroup.constraintCount = 2;
+                ___fightingStylesTable.anchoredPosition = OriginalAnchoredPosition;
+                //___fightingStylesTable.localScale = new Vector3(1f, 1f, 1f);
             }
         }
     }
