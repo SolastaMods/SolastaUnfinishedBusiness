4cbf21416bdd798c7553e243c8e8c0ccd023e979
ThyWoof <pauloesquilo@gmail.com>
2022-05-20 19:47:20 -0700

    rollback

--- SolastaCommunityExpansion/Patches/Level20/HeroDefinitionsPatcher.cs
+++ SolastaCommunityExpansion/Patches/Level20/HeroDefinitionsPatcher.cs
@@ -18,31 +18,4 @@ namespace SolastaCommunityExpansion.Patches.Level20
             }
         }
     }
-
-    [HarmonyPatch(typeof(GraphicsResourceManager), "LateUpdate")]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class GraphicsResourceManager_LateUpdate
-    {
-        internal static bool Prefix(List<(UnityEngine.Object, byte)> ___syncAddressablesToRelease)
-        {
-            for (int index = ___syncAddressablesToRelease.Count - 1; index >= 0; --index)
-            {
-                var tuple = ___syncAddressablesToRelease[index];
-
-                if (tuple.Item2 == (byte)0)
-                {
-                    Main.Logger.Log(tuple.Item1.name);
-                    Addressables.Release(tuple.Item1);
-                    ___syncAddressablesToRelease.RemoveAt(index);
-                }
-                else
-                {
-                    --tuple.Item2;
-                    ___syncAddressablesToRelease[index] = tuple;
-                }
-            }
-
-            return false;
-        }
-    }
 }
