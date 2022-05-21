bf2a0e20a259e45e2aec53283f23e161931d05c5
Ender Wiggin <andrii.gook@gmail.com>
2022-05-21 15:42:32 +0300

    added patches to make power toltips show actual use numbers, including pool increases

---
+++ SolastaCommunityExpansion/Patches/GameUi/Tooltip/UsablePowerBoxPatcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.Patches.GameUi.Tooltip;
+
+internal static class UsablePowerBoxPatcher
+{
+    [HarmonyPatch(typeof(UsablePowerBox), "Bind")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class UsablePowerBox_Bind
+    {
+        internal static void Postfix(UsablePowerBox __instance)
+        {
+            var panel = __instance.GetComponentInParent<CharacterControlPanelExploration>();
+            if (panel == null)
+            {
+                return;
+            }
+
+            __instance.GuiTooltip.Context = panel.GuiCharacter?.RulesetCharacter;
+        }
+    }
+}
