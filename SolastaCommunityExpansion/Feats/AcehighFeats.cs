9c24f96dc423e5c22a1f1a4688bf5ab8eb2b3ca5
Ender Wiggin <andrii.gook@gmail.com>
2022-05-24 10:28:47 +0300

    removed unnecessary source

--- SolastaCommunityExpansion/Feats/AcehighFeats.cs
+++ SolastaCommunityExpansion/Feats/AcehighFeats.cs
@@ -266,11 +266,6 @@ namespace SolastaCommunityExpansion.Feats
         
         private class ModifyAttackPower : IModifyAttackModeForWeapon
         {
-            private static FeatureDefinitionPower _powerAttack;
-
-            private static FeatureDefinitionPower PowerAttack => _powerAttack ??=
-                DatabaseHelper.GetDefinition<FeatureDefinitionPower>("PowerAttack", null);
-            
             public void ModifyAttackMode(RulesetCharacter character, RulesetAttackMode attackMode, RulesetItem weapon)
             {
                 if (attackMode == null)
@@ -303,11 +298,11 @@ namespace SolastaCommunityExpansion.Feats
 
                 attackMode.ToHitBonus += toHit;
                 attackMode.ToHitBonusTrends.Add(new RuleDefinitions.TrendInfo(toHit,
-                    RuleDefinitions.FeatureSourceType.Power, "PowerAttack", PowerAttack));
+                    RuleDefinitions.FeatureSourceType.Power, "PowerAttack", null));
 
                 damage.BonusDamage += toDamage;
                 damage.DamageBonusTrends.Add(new RuleDefinitions.TrendInfo(toDamage,
-                    RuleDefinitions.FeatureSourceType.Power, "PowerAttack", PowerAttack));
+                    RuleDefinitions.FeatureSourceType.Power, "PowerAttack", null));
             }
 
             private static bool IsTwoHanded(ItemDefinition weapon)
