606d2c8068d407cbf1bfa0d07eb2c7602c44d29d
ThyWoof <pauloesquilo@gmail.com>
2022-05-24 17:52:29 -0700

    clean up

--- SolastaCommunityExpansion/Patches/CustomFeatures/Powers/RulesetEffectPowerPatcher.cs
+++ SolastaCommunityExpansion/Patches/CustomFeatures/Powers/RulesetEffectPowerPatcher.cs
@@ -2,20 +2,20 @@
 using HarmonyLib;
 using SolastaCommunityExpansion.Models;
 
-namespace SolastaCommunityExpansion.Patches.CustomFeatures.Powers;
-
-internal static class RulesetEffectPowerPatcher
+namespace SolastaCommunityExpansion.Patches.CustomFeatures.Powers
 {
-    [HarmonyPatch(typeof(RulesetEffectPower), "get_SaveDC")]
+    [HarmonyPatch(typeof(RulesetEffectPower), "SaveDC", MethodType.Getter)]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class RulesetCharacter_UsePower
+    internal static class RulesetCharacter_UsePower_Getter
     {
         public static void Postfix(RulesetEffectPower __instance, ref int __result)
         {
             var originItem = __instance.OriginItem;
+
             if (originItem != null && originItem.UsableDeviceDescription.SaveDC == -1)
             {
                 var usablePower = __instance.UsablePower;
+
                 UsablePowersProvider.UpdateSaveDC(__instance.User, usablePower);
                 __result = usablePower.SaveDC;
             }
