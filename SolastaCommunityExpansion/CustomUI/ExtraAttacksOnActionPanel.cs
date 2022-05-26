c4701ae284a6b1b3475be8e711287bbe9f4e6406
Ender Wiggin <andrii.gook@gmail.com>
2022-05-26 22:06:41 +0300

    added patch that fixes targeting when selecting extra attacks on action panel

---
+++ SolastaCommunityExpansion/CustomUI/ExtraAttacksOnActionPanel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace SolastaCommunityExpansion.CustomUI;
+
+public static class ExtraAttacksOnActionPanel
+{
+    //TODO: move other patch implementations for extra attacks here
+    
+    // Uses attack mode from cursor's ActionParams, instead of first one matching action type
+    // Without this when you click on any attack in actions panel targeting would work as if you clicked on the 1st
+    public static void ApplyCursorLocationSelectTargetTranspile(List<CodeInstruction> instructions)
+    {
+        var insertionIndex = instructions.FindIndex(x => x.opcode == OpCodes.Ldloc_2);
+
+        if (insertionIndex > 0)
+        {
+            var method = new Func<RulesetAttackMode, CursorLocationSelectTarget, RulesetAttackMode>(
+                GetAttackModeFromCursorActionParams).Method;
+
+            instructions.InsertRange(insertionIndex + 1, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, method),
+                new CodeInstruction(OpCodes.Stloc_2),
+                new CodeInstruction(OpCodes.Ldloc_2),
+            });
+        }
+    }
+
+    private static RulesetAttackMode GetAttackModeFromCursorActionParams(RulesetAttackMode def,
+        CursorLocationSelectTarget cursor)
+    {
+        if (cursor == null)
+        {
+            return def;
+        }
+
+        return cursor.ActionParams?.AttackMode ?? def;
+    }
+}
