999807e6a3428b53c8094830da097e8e10aa399a
Ender Wiggin <andrii.gook@gmail.com>
2022-05-26 15:55:28 +0300

    WIP on Zen Archer

--- SolastaCommunityExpansion/Classes/Monk/Subclasses/ZenArcher.cs
+++ SolastaCommunityExpansion/Classes/Monk/Subclasses/ZenArcher.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections.Generic;
 using SolastaCommunityExpansion.Builders;
 using SolastaCommunityExpansion.Builders.Features;
+using SolastaCommunityExpansion.CustomDefinitions;
+using SolastaCommunityExpansion.Models;
 using SolastaModApi.Extensions;
+using static RuleDefinitions;
 using static SolastaModApi.DatabaseHelper;
 
 namespace SolastaCommunityExpansion.Classes.Monk.Subclasses
 {
     public static class ZenArcher
     {
+        private const string ZenArrowTag = "ZenArrow";
+
         // Zen Archer's Monk weapons are all ranged weapons.
         private static readonly List<WeaponTypeDefinition> MonkWeapons = new()
         {
             WeaponTypeDefinitions.ShortbowType,
             WeaponTypeDefinitions.LongbowType,
-            WeaponTypeDefinitions.LightCrossbowType,
-            WeaponTypeDefinitions.HeavyCrossbowType,
             WeaponTypeDefinitions.DartType,
         };
 
@@ -51,7 +54,129 @@ namespace SolastaCommunityExpansion.Classes.Monk.Subclasses
 
         private static FeatureDefinition[] BuildLevel03Features()
         {
-            return System.Array.Empty<FeatureDefinition>();
+            return new[]
+            {
+                FeatureDefinitionBuilder
+                    .Create("ClassMonkZenArcherCombat", Monk.GUID)
+                    //TODO: add proficiency with bows
+                    .SetGuiPresentation(Category.Feature)
+                    .SetCustomSubFeatures(
+                        new ZenArcherMarker(),
+                        //TODO: add feature that doubles range of ranged monk attacks, but not more than to 16/32 cells
+                        //TODO: add bonus 1-handed thrown/ranged attack
+                        RangedAttackInMeleeDisadvantageRemover.Marker, //TODO: move to level 06 features
+                        new AddTagToWeaponAttack(ZenArrowTag, IsZenArrowAttack)
+                    )
+                    .AddToDB(),
+                BuildZenArrow()
+            };
+        }
+
+        private static FeatureDefinition BuildZenArrow()
+        {
+           var technique = FeatureDefinitionPowerBuilder
+                .Create("ClassMonkZenArrowTechnique", Monk.GUID)
+                .SetGuiPresentation(Category.Power)
+                .SetActivationTime(ActivationTime.OnAttackHit)
+                .SetRechargeRate(RechargeRate.AtWill)
+                .SetCostPerUse(0)
+                .SetCustomSubFeatures(new ReactionAttackModeRestriction(
+                    (mode, _, _) => mode.AttackTags.Contains(ZenArrowTag)
+                ))
+                .AddToDB();
+
+            var prone = FeatureDefinitionPowerSharedPoolBuilder
+                .Create("ClassMonkZenArrowProne", Monk.GUID)
+                .SetGuiPresentation(Category.Power)
+                .SetSharedPool(technique)
+                .SetActivationTime(ActivationTime.NoCost)
+                .SetEffectDescription(new EffectDescriptionBuilder()
+                    .SetDurationData(DurationType.Round, 1)
+                    .SetTargetingData(Side.Enemy, RangeType.Touch, 1, TargetType.Individuals)
+                    .SetTargetFiltering(TargetFilteringMethod.CharacterOnly)
+                    .SetSavingThrowData(
+                        true,
+                        true,
+                        AttributeDefinitions.Dexterity,
+                        true,
+                        EffectDifficultyClassComputation.AbilityScoreAndProficiency,
+                        AttributeDefinitions.Wisdom
+                    )
+                    .SetEffectForms(new EffectFormBuilder()
+                        .HasSavingThrow(EffectSavingThrowType.Negates)
+                        .SetLevelAdvancement(EffectForm.LevelApplianceType.No, LevelSourceType.ClassLevel)
+                        .SetMotionForm(MotionForm.MotionType.FallProne, 0)
+                        .Build())
+                    .Build())
+                .AddToDB();
+
+            var push = FeatureDefinitionPowerSharedPoolBuilder
+                .Create("ClassMonkZenArrowPush", Monk.GUID)
+                .SetGuiPresentation(Category.Power)
+                .SetSharedPool(technique)
+                .SetActivationTime(ActivationTime.NoCost)
+                .SetEffectDescription(new EffectDescriptionBuilder()
+                    .SetDurationData(DurationType.Round, 1)
+                    .SetTargetingData(Side.Enemy, RangeType.Touch, 1, TargetType.Individuals)
+                    .SetTargetFiltering(TargetFilteringMethod.CharacterOnly)
+                    .SetSavingThrowData(
+                        true,
+                        true,
+                        AttributeDefinitions.Strength,
+                        true,
+                        EffectDifficultyClassComputation.AbilityScoreAndProficiency,
+                        AttributeDefinitions.Wisdom
+                    )
+                    .SetEffectForms(new EffectFormBuilder()
+                        .HasSavingThrow(EffectSavingThrowType.Negates)
+                        .SetLevelAdvancement(EffectForm.LevelApplianceType.No, LevelSourceType.ClassLevel)
+                        .SetMotionForm(MotionForm.MotionType.PushFromOrigin, 2)
+                        .Build())
+                    .Build())
+                .AddToDB();
+
+            var distract = FeatureDefinitionPowerSharedPoolBuilder
+                .Create("ClassMonkZenArrowDistract", Monk.GUID)
+                .SetGuiPresentation(Category.Power)
+                .SetSharedPool(technique)
+                .SetActivationTime(ActivationTime.NoCost)
+                .SetEffectDescription(new EffectDescriptionBuilder()
+                    .SetDurationData(DurationType.Round, 1)
+                    .SetTargetingData(Side.Enemy, RangeType.Touch, 1, TargetType.Individuals)
+                    .SetTargetFiltering(TargetFilteringMethod.CharacterOnly)
+                    .SetSavingThrowData(
+                        true,
+                        true,
+                        AttributeDefinitions.Wisdom,
+                        true,
+                        EffectDifficultyClassComputation.AbilityScoreAndProficiency,
+                        AttributeDefinitions.Wisdom
+                    )
+                    .SetEffectForms(new EffectFormBuilder()
+                        .HasSavingThrow(EffectSavingThrowType.None)
+                        .SetLevelAdvancement(EffectForm.LevelApplianceType.No, LevelSourceType.ClassLevel)
+                        .HasSavingThrow(EffectSavingThrowType.Negates)
+                        .SetConditionForm(ConditionDefinitionBuilder
+                            .Create("ClassMonkZenArrowDistractCondition", Monk.GUID)
+                            .SetGuiPresentation(Category.Condition,
+                                ConditionDefinitions.ConditionDazzled.GuiPresentation.SpriteReference)
+                            .SetDuration(DurationType.Round, 1)
+                            .SetTurnOccurence(TurnOccurenceType.EndOfTurn)
+                            .SetConditionType(ConditionType.Detrimental)
+                            .SetSpecialInterruptions(ConditionInterruption.Attacks)
+                            .SetFeatures(FeatureDefinitionCombatAffinityBuilder
+                                .Create("ClassMonkZenArrowDistractFeature", Monk.GUID)
+                                .SetGuiPresentationNoContent(true)
+                                .SetMyAttackAdvantage(AdvantageType.Disadvantage)
+                                .AddToDB())
+                            .AddToDB(), ConditionForm.ConditionOperation.Add)
+                        .Build())
+                    .Build())
+                .AddToDB();
+
+            PowerBundleContext.RegisterPowerBundle(technique, true, prone, push, distract);
+
+            return technique;
         }
 
         private static FeatureDefinition[] BuildLevel06Features()
@@ -67,6 +192,11 @@ namespace SolastaCommunityExpansion.Classes.Monk.Subclasses
         private static FeatureDefinition[] BuildLevel17Features()
         {
             return System.Array.Empty<FeatureDefinition>();
+        }
+        
+        private static bool IsZenArrowAttack(RulesetAttackMode mode, RulesetItem weapon, RulesetCharacter character)
+        {
+            return mode is { Reach: false, Magical: false, Ranged: true } && Monk.IsMonkWeapon(character, mode);
         }
 
         private class ZenArcherMarker
---
+++ SolastaCommunityExpansion/CustomDefinitions/RangedAttackInMeleeDisadvantageRemover.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using SolastaModApi.Extensions;
+
+namespace SolastaCommunityExpansion.CustomDefinitions
+{
+    public class RangedAttackInMeleeDisadvantageRemover
+    {
+        public static readonly RangedAttackInMeleeDisadvantageRemover Marker = new();
+
+        private RangedAttackInMeleeDisadvantageRemover() { }
+
+        /**
+         * Patches `GameLocationBattleManager.CanAttack`
+         * Replaces starting value of a disadvantage on a ranged attack in melee flag from `true`
+         * with a result of a call of custom method 
+         */
+        public static void ApplyTranspile(List<CodeInstruction> instructions)
+        {
+            var foundConscious = false;
+            var insertionIndex = -1;
+            for (var i = 0; i < instructions.Count; i++)
+            {
+                var instruction = instructions[i];
+                if (foundConscious)
+                {
+                    if (instruction.opcode == OpCodes.Ldc_I4_1)
+                    {
+                        insertionIndex = i;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (instruction.opcode == OpCodes.Call
+                        && instruction.operand != null
+                        && instruction.operand.ToString().Contains("IsConsciousCharacterOfSideNextToCharacter"))
+                    {
+                        foundConscious = true;
+                    }
+                }
+            }
+
+            if (insertionIndex > 0)
+            {
+                var method = new Func<BattleDefinitions.AttackEvaluationParams, bool>(HasDisadvantage).Method;
+                instructions[insertionIndex] = new CodeInstruction(OpCodes.Call, method);
+                instructions.Insert(insertionIndex, new CodeInstruction(OpCodes.Ldarg_1));
+            }
+        }
+
+        private static bool HasDisadvantage(BattleDefinitions.AttackEvaluationParams attackParams)
+        {
+            if (attackParams.attackProximity != BattleDefinitions.AttackProximity.PhysicalRange)
+            {
+                return true;
+            }
+
+            var character = attackParams.attacker?.RulesetCharacter;
+            if (character == null)
+            {
+                return true;
+            }
+
+            return !character.HasSubFeatureOfType<RangedAttackInMeleeDisadvantageRemover>();
+        }
+    }
+}
