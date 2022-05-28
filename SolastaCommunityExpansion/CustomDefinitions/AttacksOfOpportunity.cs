649fe89e69a57dea78669391c63ac0eacf0ebdb9
Ender Wiggin <andrii.gook@gmail.com>
2022-05-28 16:19:08 +0300

    added patch and sub-feature that allows ignoring Disengage

---
+++ SolastaCommunityExpansion/CustomDefinitions/AttacksOfOpportunity.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SolastaModApi;
+using SolastaModApi.Extensions;
+
+namespace SolastaCommunityExpansion.CustomDefinitions;
+
+public interface ICanIgnoreAoOImmunity
+{
+    bool CanIgnoreAoOImmunity(RulesetCharacter character, RulesetCharacter attacker);
+}
+
+public static class AttacksOfOpportunity
+{
+    public static readonly ICanIgnoreAoOImmunity CanIgnoreDisengage = new CanIgnoreDisengage();
+
+    public static bool IsSubjectToAttackOfOpportunity(RulesetCharacter character, RulesetCharacter attacker,
+        bool def)
+    {
+        return def || attacker.GetSubFeaturesByType<ICanIgnoreAoOImmunity>()
+            .Any(f => f.CanIgnoreAoOImmunity(character, attacker));
+    }
+}
+
+internal class CanIgnoreDisengage : ICanIgnoreAoOImmunity
+{
+    public bool CanIgnoreAoOImmunity(RulesetCharacter character, RulesetCharacter attacker)
+    {
+        var FeaturesToBrowse = new List<FeatureDefinition>();
+        character.EnumerateFeaturesToBrowse<ICombatAffinityProvider>(FeaturesToBrowse);
+        var service = ServiceRepository.GetService<IRulesetImplementationService>();
+        var disengaging = DatabaseHelper.FeatureDefinitionCombatAffinitys.CombatAffinityDisengaging;
+        foreach (var feature in FeaturesToBrowse)
+        {
+            if (feature != disengaging
+                && feature is ICombatAffinityProvider affinityProvider
+                && service.IsSituationalContextValid(
+                    new RulesetImplementationDefinitions.SituationalContextParams(
+                        affinityProvider.SituationalContext, attacker,
+                        character, service.FindSourceIdOfFeature(character, feature),
+                        affinityProvider.RequiredCondition, null))
+                && affinityProvider.IsImmuneToOpportunityAttack(character, attacker))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ SolastaCommunityExpansion/_Patches/Insertion/RulesetCharacterPatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using SolastaCommunityExpansion.CustomDefinitions;
+
+namespace SolastaCommunityExpansion.Patches.Insertion;
+
+internal static class RulesetCharacterPatcher
+{
+    [HarmonyPatch(typeof(RulesetCharacter), "IsSubjectToAttackOfOpportunity")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class IsSubjectToAttackOfOpportunity
+    {
+        // ReSharper disable once RedundantAssignment
+        internal static void Postfix(RulesetCharacter __instance, ref bool __result, RulesetCharacter attacker)
+        {
+            __result = AttacksOfOpportunity.IsSubjectToAttackOfOpportunity(__instance, attacker, __result);
+        }
+    }
+}
