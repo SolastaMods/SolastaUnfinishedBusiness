021ad329e789111480ce26ab90af80079d7df6d2
ThyWoof <pauloesquilo@gmail.com>
2022-05-31 20:27:19 -0700

    Add Custom Campaigns Translations support

---
+++ SolastaCommunityExpansion/Displays/TranslationsDisplay.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using ModKit;
+using SolastaCommunityExpansion.Utils;
+
+namespace SolastaCommunityExpansion.Displays;
+
+public static class TranslationsDisplay
+{
+    internal static string SelectedLanguage = "en";
+
+    internal static void DisplayTranslations()
+    {
+        var userCampaignPoolService = ServiceRepository.GetService<IUserCampaignPoolService>();
+
+        UI.Label("");
+        UI.Label(Gui.Format("ModUi/&TranslationHelp", SelectedLanguage.yellow().bold()));
+        UI.Label("");
+
+        using (UI.HorizontalScope())
+        {
+            var intValue = Array.IndexOf(TranslationsExporter.AvailableLanguages, SelectedLanguage);
+
+            if (UI.SelectionGrid(ref intValue,
+                    TranslationsExporter.AvailableLanguages, TranslationsExporter.AvailableLanguages.Length, 6,
+                    UI.Width(500)))
+            {
+                SelectedLanguage = TranslationsExporter.AvailableLanguages[intValue];
+            }
+        }
+
+        UI.Label("");
+
+        foreach (var userCampaign in userCampaignPoolService.AllCampaigns
+                     .Where(x => x.IsWorkshopItem)
+                     .OrderBy(x => x.Title))
+        {
+            var exportName = userCampaign.Title;
+
+            using (UI.HorizontalScope())
+            {
+                string buttonLabel;
+
+                if (TranslationsExporter.CurrentExports.TryGetValue(exportName, out var status))
+                {
+                    buttonLabel = Gui.Format("ModUi/&CancelTranslate", status.LanguageCode.ToUpper(),
+                        $"{status.PercentageComplete:00.0%}").bold().yellow();
+                }
+                else
+                {
+                    buttonLabel = Gui.Format("ModUi/&Translate");
+                }
+
+                UI.ActionButton(buttonLabel, () =>
+                    {
+                        if (status == null)
+                        {
+                            TranslationsExporter.TranslateUserCampaign(SelectedLanguage, userCampaign.Title,
+                                userCampaign);
+                        }
+                        else
+                        {
+                            TranslationsExporter.Cancel(userCampaign.Title);
+                        }
+                    },
+                    UI.Width(200));
+
+                UI.Label(userCampaign.Author.bold().orange(), UI.Width(100));
+                UI.Label(userCampaign.Title.bold().italic(), UI.Width(400));
+            }
+        }
+
+        UI.Label("");
+    }
+}
---
+++ SolastaCommunityExpansion/Utils/TranslationsExporter.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using I2.Loc;
+using SolastaModApi.Infrastructure;
+using UnityEngine;
+
+namespace SolastaCommunityExpansion.Utils;
+
+internal class TranslationsExporter : MonoBehaviour
+{
+    private static TranslationsExporter _exporter;
+
+    internal static readonly Dictionary<string, ExportStatus> CurrentExports = new();
+
+    internal static readonly string[] AvailableLanguages = LocalizationManager.GetAllLanguagesCode().ToArray();
+
+    private static TranslationsExporter Exporter
+    {
+        get
+        {
+            if (_exporter == null)
+            {
+                _exporter = new GameObject().AddComponent<TranslationsExporter>();
+                DontDestroyOnLoad(_exporter.gameObject);
+            }
+
+            return _exporter;
+        }
+    }
+
+    internal static void Cancel(string exportName)
+    {
+        if (!CurrentExports.ContainsKey(exportName) || CurrentExports[exportName].Coroutine == null)
+        {
+            return;
+        }
+
+        Exporter.StopCoroutine(CurrentExports[exportName].Coroutine);
+        CurrentExports.Remove(exportName);
+    }
+
+    internal static void TranslateUserCampaign(string languageCode, string exportName, UserCampaign userCampaign)
+    {
+        var newUserCampaign = userCampaign.DeepCopy();
+
+        newUserCampaign.Title = $"{languageCode}_{userCampaign.Title}";
+        newUserCampaign.IsWorkshopItem = false;
+
+        var coroutine = TranslateUserCampaignRoutine(languageCode, exportName, newUserCampaign);
+
+        CurrentExports.Add(exportName,
+            new ExportStatus
+            {
+                Coroutine = Exporter.StartCoroutine(coroutine), PercentageComplete = 0f, LanguageCode = languageCode
+            });
+    }
+
+    private static IEnumerator TranslateUserCampaignRoutine(string languageCode, string exportName,
+        UserCampaign userCampaign)
+    {
+        var start = DateTime.UtcNow;
+
+        Main.Log($"Export started: {DateTime.UtcNow:G}");
+
+        yield return null;
+
+        var current = 0;
+        var total =
+            userCampaign.UserDialogs
+                .SelectMany(x => x.AllDialogStates)
+                .Count() +
+            userCampaign.UserLocations
+                .SelectMany(x => x.GadgetsByName)
+                .Count() +
+            userCampaign.UserQuests
+                .SelectMany(x => x.AllQuestStepDescriptions)
+                .SelectMany(x => x.OutcomesTable)
+                .Count();
+
+        IEnumerator Update()
+        {
+            current++;
+            CurrentExports[exportName].PercentageComplete = (float)current / total;
+
+            yield return null;
+        }
+
+        // USER DIALOGS
+        foreach (var dialog in userCampaign.UserDialogs)
+        {
+            dialog.Description = Translations.Translate(dialog.Description, languageCode);
+
+            foreach (var userDialogState in dialog.AllDialogStates)
+            {
+                yield return Update();
+
+                foreach (var dialogLine in userDialogState.DialogLines)
+                {
+                    dialogLine.TextLine = Translations.Translate(dialogLine.TextLine, languageCode);
+                }
+            }
+        }
+
+        // USER LOCATIONS
+        foreach (var userLocation in userCampaign.UserLocations)
+        {
+            foreach (var gadget in userLocation.GadgetsByName.Values)
+            {
+                yield return Update();
+
+                foreach (var parameterValue in gadget.ParameterValues)
+                {
+                    switch (parameterValue.GadgetParameterDescription.Type)
+                    {
+                        case GadgetBlueprintDefinitions.Type.Npc:
+                        case GadgetBlueprintDefinitions.Type.Speech:
+                        case GadgetBlueprintDefinitions.Type.SpeechList:
+                            var newStringsList = new List<string>();
+
+                            foreach (var stringValue in parameterValue.StringsList)
+                            {
+                                newStringsList.Add(Translations.Translate(stringValue, languageCode));
+                            }
+
+                            parameterValue.StringsList = newStringsList;
+
+                            if (parameterValue.StringValue != string.Empty)
+                            {
+                                parameterValue.StringValue =
+                                    Translations.Translate(parameterValue.StringValue, languageCode);
+                            }
+
+                            break;
+                    }
+                }
+            }
+        }
+
+        // USER QUESTS
+        foreach (var quest in userCampaign.UserQuests)
+        {
+            quest.Description = Translations.Translate(quest.Description, languageCode);
+
+            foreach (var userQuestStep in quest.AllQuestStepDescriptions)
+            {
+                userQuestStep.Title = Translations.Translate(userQuestStep.Title, languageCode);
+                userQuestStep.Description = Translations.Translate(userQuestStep.Description, languageCode);
+
+                foreach (var outcome in userQuestStep.OutcomesTable)
+                {
+                    yield return Update();
+
+                    outcome.DescriptionText = Translations.Translate(outcome.DescriptionText, languageCode);
+                }
+            }
+        }
+
+        CurrentExports.Remove(exportName);
+
+        var userCampaignPoolService = ServiceRepository.GetService<IUserCampaignPoolService>();
+
+        userCampaignPoolService.SaveUserCampaign(userCampaign);
+
+        Main.Log($"Export finished: {DateTime.UtcNow}, {DateTime.UtcNow - start}.");
+    }
+
+    internal class ExportStatus
+    {
+        internal Coroutine Coroutine;
+        internal string LanguageCode;
+        internal float PercentageComplete;
+    }
+}
