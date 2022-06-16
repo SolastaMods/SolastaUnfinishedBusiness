6871c7b5a027d7758f729dd484dde3bcd48a02a7
Silvergriffon <86208060+Silvergriffon@users.noreply.github.com>
2022-06-16 11:17:32 -0400

    custom darkelf hair colors

--- SolastaCommunityExpansion/Races/Darkelf.cs
+++ SolastaCommunityExpansion/Races/Darkelf.cs
@@ -180,31 +180,61 @@ internal static class DarkelfRaceBuilder
 
         _ = MorphotypeElementDefinitionBuilder
             .Create(HairColorSilver, "DarkelfHair2", "7dd1a932-69d3-4d51-b9c6-eccaaed90008")
-            .SetMainColor(FaceAndSkin_Neutral.SecondColor)
+            .SetMainColor(new UnityEngine.Color()
+            {
+                r = 0.945098039f,
+                g = 0.952941176f,
+                b = 0.980392157f,
+                a = 1.0f,
+            })
             .SetSortOrder(49)
             .AddToDB();
 
         _ = MorphotypeElementDefinitionBuilder
             .Create(HairColorSilver, "DarkelfHair3", "7dd1a932-69d3-4d51-b9c6-eccaaed90009")
-            .SetMainColor(HairColorBlue.MainColor)
+            .SetMainColor(new UnityEngine.Color()
+            {
+                r = 0.890196078f,
+                g = 0.933333333f,
+                b = 0.976470588f,
+                a = 1.0f,
+            })
             .SetSortOrder(50)
             .AddToDB();
 
         _ = MorphotypeElementDefinitionBuilder
             .Create(HairColorSilver, "DarkelfHair4", "7dd1a932-69d3-4d51-b9c6-eccaaed90010")
-            .SetMainColor(HairColorBlue.SecondColor)
+            .SetMainColor(new UnityEngine.Color()
+            {
+                r = 0.870588235f,
+                g = 0.894117647f,
+                b = 0.925490196f,
+                a = 1.0f,
+            })
             .SetSortOrder(51)
             .AddToDB();
 
         _ = MorphotypeElementDefinitionBuilder
             .Create(HairColorSilver, "DarkelfHair5", "7dd1a932-69d3-4d51-b9c6-eccaaed90011")
-            .SetMainColor(HairColorSilver.MainColor)
+            .SetMainColor(new UnityEngine.Color()
+            {
+                r = 0.968627451f,
+                g = 0.929411765f,
+                b = 0.937254902f,
+                a = 1.0f,
+            })
             .SetSortOrder(52)
             .AddToDB();
 
         _ = MorphotypeElementDefinitionBuilder
             .Create(HairColorSilver, "DarkelfHair6", "7dd1a932-69d3-4d51-b9c6-eccaaed90012")
-            .SetMainColor(HairColorSilver.SecondColor)
+            .SetMainColor(new UnityEngine.Color()
+            {
+                r = 0.996078431f,
+                g = 0.913725490f,
+                b = 0.913725490f,
+                a = 1.0f,
+            })
             .SetSortOrder(53)
             .AddToDB();
 
