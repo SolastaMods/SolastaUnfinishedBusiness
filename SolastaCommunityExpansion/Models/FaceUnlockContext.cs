4d16ab62bcb148a2935ff0319551e3c63ee69756
ThyWoof <pauloesquilo@gmail.com>
2022-06-28 10:43:04 -0700

    Revert "Fix sorcerer char creation"
    This reverts commit ac9148fd17e6dc5a16512184d45ac6eafa70adfc.

--- SolastaCommunityExpansion/Models/FaceUnlockContext.cs
+++ SolastaCommunityExpansion/Models/FaceUnlockContext.cs
@@ -75,7 +75,7 @@ internal static class FaceUnlockContext
             }
         }
 
-        if (Main.Settings.UnlockAllNpcFaces || Main.Settings.UnlockMarkAndTatoosForAllCharacters)
+        if (Main.Settings.UnlockMarkAndTatoosForAllCharacters)
         {
             foreach (var morphotype in dbMorphotypeElementDefinition.Where(x =>
                          x.Category == MorphotypeElementDefinition.ElementCategory.BodyDecoration))
@@ -84,13 +84,16 @@ internal static class FaceUnlockContext
             }
         }
 
-        SorcerousDraconicBloodline.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
-            .Default;
-        SorcerousManaPainter.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
-            .Default;
-        SorcerousChildRift.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
-            .Default;
-        SorcerousHauntedSoul.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
-            .Default;
+        if (Main.Settings.AllowUnmarkedSorcerers)
+        {
+            SorcerousDraconicBloodline.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
+                .Default;
+            SorcerousManaPainter.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
+                .Default;
+            SorcerousChildRift.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
+                .Default;
+            SorcerousHauntedSoul.morphotypeSubclassFilterTag = GraphicsDefinitions.MorphotypeSubclassFilterTag
+                .Default;
+        }
     }
 }
--- SolastaCommunityExpansion/Utils/Settings.cs
+++ SolastaCommunityExpansion/Utils/Settings.cs
@@ -94,6 +94,7 @@ public class Settings : UnityModManager.ModSettings
     // Visuals
     public bool OfferAdditionalLoreFriendlyNames { get; set; }
     public bool UnlockAllNpcFaces { get; set; }
+    public bool AllowUnmarkedSorcerers { get; set; }
     public bool UnlockMarkAndTatoosForAllCharacters { get; set; }
     public bool UnlockEyeStyles { get; set; }
     public bool AddNewBrightEyeColors { get; set; }
