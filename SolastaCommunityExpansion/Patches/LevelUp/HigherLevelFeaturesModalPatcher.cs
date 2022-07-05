afa573dad3eb44640c4c0843547133b22f893947
ThyWoof <pauloesquilo@gmail.com>
2022-07-05 11:21:54 -0700

    Finalize FeatureUnlocks fix

--- SolastaCommunityExpansion/Patches/LevelUp/HigherLevelFeaturesModalPatcher.cs
+++ SolastaCommunityExpansion/Patches/LevelUp/HigherLevelFeaturesModalPatcher.cs
@@ -1,20 +1,23 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using HarmonyLib;
 using SolastaCommunityExpansion.Models;
 
 namespace SolastaCommunityExpansion.Patches.LevelUp;
 
-// replaces the hard coded experience
 [HarmonyPatch(typeof(HigherLevelFeaturesModal), "Bind")]
 [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
 internal static class HigherLevelFeaturesModal_Bind
 {
-    internal static void Prefix(ref int achievementLevel)
+    internal static void Prefix(ref List<FeatureUnlockByLevel> featureUnlocks, ref int achievementLevel)
     {
         var hero = Global.ActiveLevelUpHero;
         var isLevelingUp = LevelUpContext.IsLevelingUp(hero);
         var isClassSelectionStage = LevelUpContext.IsClassSelectionStage(hero);
         var selectedClass = LevelUpContext.GetSelectedClass(hero);
+
+        featureUnlocks = MulticlassPatchingContext.ClassFilteredFeatureUnlocks(selectedClass, hero).ToList();
 
         if (isLevelingUp
             && isClassSelectionStage
