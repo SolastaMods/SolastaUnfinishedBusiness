f159f311503d5deda630f3036227422bc1e9eece
ThyWoof <pauloesquilo@gmail.com>
2022-07-13 20:56:48 -0700

    add OnCharacterKill
    - for future use on School of Dead / Cleave

---
+++ SolastaCommunityExpansion/CustomDefinitions/FeatureDefinitionOnCharacterKill.cs
@@ -0,0 +1,24 @@
+﻿using SolastaCommunityExpansion.CustomInterfaces;
+
+namespace SolastaCommunityExpansion.CustomDefinitions;
+
+public sealed class FeatureDefinitionOnCharacterKill : FeatureDefinition, IOnCharacterKill
+{
+    private OnCharacterKillDelegate onCharacterKill;
+
+    public void OnCharacterKill(
+        GameLocationCharacter character,
+        bool dropLoot,
+        bool removeBody,
+        bool forceRemove,
+        bool considerDead,
+        bool becomesDying)
+    {
+        onCharacterKill?.Invoke(character, dropLoot, removeBody, forceRemove, considerDead, becomesDying);
+    }
+
+    internal void SetOnCharacterKill(OnCharacterKillDelegate onCharacterKillSet)
+    {
+        onCharacterKill = onCharacterKillSet;
+    }
+}
---
+++ SolastaCommunityExpansion/Patches/Insertion/GameLocationCharacterManagerPatcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaCommunityExpansion.CustomInterfaces;
+using SolastaCommunityExpansion.Models;
+using TA;
+
+namespace SolastaCommunityExpansion.Patches.Insertion;
+
+[HarmonyPatch(typeof(GameLocationCharacterManager), "KillCharacter")]
+[SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+internal static class GameLocationCharacterManager_KillCharacter
+{
+    internal static void Prefix(
+        GameLocationCharacter character,
+        bool dropLoot,
+        bool removeBody,
+        bool forceRemove,
+        bool considerDead,
+        bool becomesDying)
+    {
+        var rulesetCharacter = Global.ActivePlayerCharacter.RulesetCharacter;
+
+        rulesetCharacter.EnumerateFeaturesToBrowse<IOnCharacterKill>(rulesetCharacter.FeaturesToBrowse);
+
+        // ReSharper disable once PossibleInvalidCastExceptionInForeachLoop
+        foreach (IOnCharacterKill characterKill in rulesetCharacter.FeaturesToBrowse)
+        {
+            characterKill.OnCharacterKill(character, dropLoot, removeBody, forceRemove, considerDead, becomesDying);
+        }
+    }
+}
