ebc153a1ff25b0d0c71dc774ddeb78d0b4ab7632
ThyWoof <pauloesquilo@gmail.com>
2022-07-15 21:38:33 -0700

    Wizard Dead Master WIP subclass

--- SolastaCommunityExpansion/Subclasses/Wizard/DeadMaster.cs
+++ SolastaCommunityExpansion/Subclasses/Wizard/DeadMaster.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using SolastaCommunityExpansion.Builders;
 using SolastaCommunityExpansion.Builders.Features;
+using SolastaCommunityExpansion.Models;
 using static SolastaCommunityExpansion.Api.DatabaseHelper;
 using static SolastaCommunityExpansion.Api.DatabaseHelper.CharacterSubclassDefinitions;
+using static SolastaCommunityExpansion.Api.DatabaseHelper.FeatureDefinitionDamageAffinitys;
 using static SolastaCommunityExpansion.Api.DatabaseHelper.SpellDefinitions;
 using static SolastaCommunityExpansion.Api.DatabaseHelper.MonsterDefinitions;
 using static SolastaCommunityExpansion.Api.DatabaseHelper.SchoolOfMagicDefinitions;
@@ -17,12 +19,12 @@ internal sealed class DeadMaster : AbstractSubclass
 
     private static readonly Dictionary<int, List<MonsterDefinition>> CreateDeadSpellMonsters = new()
     {
-        {3, new List<MonsterDefinition> {Ghast, Ghoul, Ogre_Zombie, Skeleton_Enforcer}},
-        {5, new List<MonsterDefinition> {Mummy, Skeleton_Knight, Skeleton_Marksman, Skeleton_Sorcerer}},
-        {7, new List<MonsterDefinition> {Brood_of_flesh, Brood_of_blood, Ghost, Wraith }}
+        {3, new List<MonsterDefinition> {Zombie, Skeleton, Skeleton_Archer}},
+        // {5, new List<MonsterDefinition> {Ghoul, Skeleton_Enforcer}},
+        // {7, new List<MonsterDefinition> {Skeleton_Knight, Skeleton_Marksman, Skeleton_Sorcerer}}
     };
 
-    private readonly CharacterSubclassDefinition Subclass;
+    private static CharacterSubclassDefinition Subclass { get; set; }
 
     internal DeadMaster()
     {
@@ -33,15 +35,113 @@ internal sealed class DeadMaster : AbstractSubclass
             .SetPreparedSpellGroups(GetDeadSpellAutoPreparedGroups())
             .AddToDB();
 
+        var featureStarkHarvest = FeatureDefinitionOnCharacterKillBuilder
+            .Create("FeatureStarkHarvest", SubclassNamespace)
+            .SetGuiPresentationNoContent()
+            .SetOnCharacterKill(OnStarkHarvestKill)
+            .AddToDB();
+
+        for (var i = 2; i < 7; i++)
+        {
+            _ = FeatureDefinitionAttackModifierBuilder
+                .Create($"FeatureUndeadChainsAttackModifier{i}", SubclassNamespace)
+                .SetGuiPresentation("FeatureUndeadChainsAttackModifier", Category.Feature)
+                .Configure(RuleDefinitions.AttackModifierMethod.FlatValue, i)
+                .AddToDB();
+        }
+
+        var featureUndeadChains = FeatureDefinitionOnCharacterKillBuilder
+            .Create("FeatureUndeadChains", SubclassNamespace)
+            .SetGuiPresentationNoContent()
+            .AddToDB();
+
         Subclass = CharacterSubclassDefinitionBuilder
             .Create("WizardDeadMaster", SubclassNamespace)
             .SetGuiPresentation(Category.Subclass, DomainMischief.GuiPresentation.SpriteReference)
-            .AddFeatureAtLevel(autoPreparedSpellsWizardDeadMaster, 2)
+            .AddFeatureAtLevel(featureStarkHarvest, 2)
+            .AddFeatureAtLevel(autoPreparedSpellsWizardDeadMaster, 6)
+            .AddFeatureAtLevel(featureUndeadChains, 6)
+            .AddFeatureAtLevel(DamageAffinityNecroticImmunity, 10)
             .AddToDB();
     }
 
+    internal static void OnMonsterCreated(RulesetCharacterMonster monster)
+    {
+        if (Global.CastedSpell == null || !Global.CastedSpell.Name.StartsWith("CreateDead"))
+        {
+            return;
+        }
+
+        var caster = Global.ActivePlayerCharacter.RulesetCharacter as RulesetCharacterHero
+                       ?? Global.ActivePlayerCharacter.RulesetCharacter.OriginalFormCharacter as
+                           RulesetCharacterHero;
+
+        if (caster == null
+            || !caster.ClassesAndSubclasses.TryGetValue(CharacterClassDefinitions.Wizard, out var subclassDefinition)
+            || subclassDefinition != Subclass)
+        {
+            return;
+        }
+
+        var casterLevel = caster.ClassesAndLevels[CharacterClassDefinitions.Wizard];
+
+        if (casterLevel < 6)
+        {
+            return;
+        }
+
+        var dbFeatureDefinitionAttackModifier = DatabaseRepository.GetDatabase<FeatureDefinitionAttackModifier>();
+        var proficiencyBonus = caster.GetAttribute(AttributeDefinitions.ProficiencyBonus).CurrentValue;
+
+        monster.GetAttribute(AttributeDefinitions.HitPoints).BaseValue += casterLevel;
+
+        if (dbFeatureDefinitionAttackModifier
+            .TryGetElement($"FeatureUndeadChainsAttackModifier{proficiencyBonus}", out var featureDefinitionAttackModifier))
+        {
+            monster.ActiveFeatures.Add(featureDefinitionAttackModifier);
+        }
+    }
+
+    private static void OnStarkHarvestKill(
+        GameLocationCharacter character,
+        bool dropLoot,
+        bool removeBody,
+        bool forceRemove,
+        bool considerDead,
+        bool becomesDying)
+    {
+        if (!considerDead || Global.CurrentAction is not CharacterActionCastSpell actionCastSpell)
+        {
+            return;
+        }
+
+        var characterFamily = character.RulesetCharacter.CharacterFamily;
+
+        if (characterFamily == CharacterFamilyDefinitions.Construct.Name ||
+            characterFamily == CharacterFamilyDefinitions.Undead.Name)
+        {
+            return;
+        }
+
+        var attacker = actionCastSpell.ActingCharacter.RulesetCharacter as RulesetCharacterHero
+                       ?? actionCastSpell.ActingCharacter.RulesetCharacter.OriginalFormCharacter as
+                           RulesetCharacterHero;
+
+        if (attacker == null)
+        {
+            return;
+        }
+
+        var spellLevel = actionCastSpell.ActiveSpell.SpellDefinition.SpellLevel;
+        var isNecromancy = actionCastSpell.ActiveSpell.SpellDefinition.SchoolOfMagic == SchoolNecromancy.Name;
+        var healingReceived = (isNecromancy ? 3 : 2) * spellLevel;
+
+        attacker.ReceiveHealing(healingReceived, true, attacker.Guid);
+    }
+
     [NotNull]
-    private static IEnumerable<FeatureDefinitionAutoPreparedSpells.AutoPreparedSpellsGroup> GetDeadSpellAutoPreparedGroups()
+    private static IEnumerable<FeatureDefinitionAutoPreparedSpells.AutoPreparedSpellsGroup>
+        GetDeadSpellAutoPreparedGroups()
     {
         var result = new List<FeatureDefinitionAutoPreparedSpells.AutoPreparedSpellsGroup>();
 
@@ -63,6 +163,8 @@ internal sealed class DeadMaster : AbstractSubclass
                     .AddToDB();
 
                 monster.fullyControlledWhenAllied = true;
+                subSpell.EffectDescription.durationType = RuleDefinitions.DurationType.UntilLongRest;
+                subSpell.EffectDescription.EffectAdvancement.additionalSummonsPerIncrement = 2;
                 subSpell.EffectDescription.EffectForms[0].SummonForm.monsterDefinitionName = monster.name;
                 spells.Add(subSpell);
             }
@@ -75,6 +177,10 @@ internal sealed class DeadMaster : AbstractSubclass
                 .SetCastingTime(RuleDefinitions.ActivationTime.Minute1)
                 .SetSubSpells(spells)
                 .AddToDB();
+
+            spell.EffectDescription.EffectAdvancement.additionalSummonsPerIncrement = 2;
+            spell.EffectDescription.EffectAdvancement.effectIncrementMethod =
+                RuleDefinitions.EffectIncrementMethod.PerAdditionalSlotLevel;
 
             var autoPreparedSpellsGroup =
                 new FeatureDefinitionAutoPreparedSpells.AutoPreparedSpellsGroup
