f9cbea6208747250f66884f61700b2b9f9b2e937
ThyWoof <pauloesquilo@gmail.com>
2022-07-15 23:13:19 -0700

    add support for more than 8 subs on UI

--- SolastaCommunityExpansion/Patches/GameUi/LevelUp/CharacterStageSubclassSelectionPanelPatcher.cs
+++ SolastaCommunityExpansion/Patches/GameUi/LevelUp/CharacterStageSubclassSelectionPanelPatcher.cs
@@ -1,21 +1,46 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace SolastaCommunityExpansion.Patches.GameUi.LevelUp;
 
-[HarmonyPatch(typeof(CharacterStageSubclassSelectionPanel), "Compare")]
+[HarmonyPatch(typeof(CharacterStageSubclassSelectionPanel), "OnBeginShow")]
 [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-internal static class CharacterStageSubclassSelectionPanel_Compare
+internal static class CharacterStageSubclassSelectionPanel_OnBeginShow
 {
-    internal static void Postfix(CharacterSubclassDefinition left, CharacterSubclassDefinition right,
-        ref int __result)
+    private static Vector2 OriginalAnchoredPosition { get; set; } = Vector2.zero;
+
+    internal static void Prefix([NotNull] CharacterStageSubclassSelectionPanel __instance)
     {
-        if (!Main.Settings.EnableSortingSubclasses)
+        if (Main.Settings.EnableSortingSubclasses)
         {
-            return;
+            __instance.compatibleSubclasses
+                .Sort((a, b) =>
+                    String.Compare(a.FormatTitle(), b.FormatTitle(), StringComparison.CurrentCultureIgnoreCase));
         }
 
-        __result = String.Compare(left.FormatTitle(), right.FormatTitle(), StringComparison.CurrentCultureIgnoreCase);
+        var gridLayoutGroup = __instance.subclassesTable.GetComponent<GridLayoutGroup>();
+        var count = __instance.compatibleSubclasses.Count;
+
+        if (OriginalAnchoredPosition == Vector2.zero)
+        {
+            OriginalAnchoredPosition = __instance.subclassesTable.anchoredPosition;
+        }
+
+        if (count > 8)
+        {
+            gridLayoutGroup.constraintCount = 3;
+            //__instance.subclassesTable.anchoredPosition = new Vector2(0, +15);
+            __instance.subclassesTable.localScale = new Vector3(0.8f, 0.8f, 1f);
+        }
+        else
+        {
+            gridLayoutGroup.constraintCount = 2;
+            //__instance.subclassesTable.anchoredPosition = OriginalAnchoredPosition;
+            __instance.subclassesTable.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
