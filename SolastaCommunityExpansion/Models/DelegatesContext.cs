31073dbcc51bafdaa831685e9737d0b4b00cb96f
ThyWoof <pauloesquilo@gmail.com>
2022-07-17 12:35:05 -0700

    add RulesetCharacter triggers

--- SolastaCommunityExpansion/Models/DelegatesContext.cs
+++ SolastaCommunityExpansion/Models/DelegatesContext.cs
@@ -66,16 +66,16 @@ internal static class DelegatesContext
 
         foreach (var gameLocationCharacter in gameLocationCharacterService.ValidCharacters)
         {
-            gameLocationCharacter.Placed += Placed;
+            //gameLocationCharacter.Placed += Placed;
             gameLocationCharacter.MoveStarted += MoveStarted;
             gameLocationCharacter.PrepareChargeStarted += PrepareChargeStarted;
             gameLocationCharacter.ChargeStarted += ChargeStarted;
             gameLocationCharacter.ChargeEnded += ChargeEnded;
             gameLocationCharacter.ChargeAborted += ChargeAborted;
-            gameLocationCharacter.UpdateMotions += UpdateMotions;
+            //gameLocationCharacter.UpdateMotions += UpdateMotions;
             gameLocationCharacter.TeleportStarted += TeleportStarted;
             gameLocationCharacter.Moved += Moved;
-            gameLocationCharacter.Rotated += Rotated;
+            //gameLocationCharacter.Rotated += Rotated;
             gameLocationCharacter.Stopped += Stopped;
             gameLocationCharacter.FallStarted += FallStarted;
             gameLocationCharacter.FallStopped += FallStopped;
@@ -106,7 +106,7 @@ internal static class DelegatesContext
             gameLocationCharacter.SpeechEnded += SpeechEnded;
             gameLocationCharacter.ListenStarted += ListenStarted;
             gameLocationCharacter.ListenEnded += ListenEnded;
-            gameLocationCharacter.AdditionalAnimationStarted += AdditionalAnimationStarted;
+            //gameLocationCharacter.AdditionalAnimationStarted += AdditionalAnimationStarted;
             gameLocationCharacter.TextFeedbackRequested += TextFeedbackRequested;
             gameLocationCharacter.InGameDialogLineRequested += InGameDialogLineRequested;
             gameLocationCharacter.AlreadySuccessful += AlreadySuccessful;
@@ -117,6 +117,39 @@ internal static class DelegatesContext
             gameLocationCharacter.CurrentMonsterAttackChanged += CurrentMonsterAttackChanged;
             gameLocationCharacter.DisolveStarted += DisolveStarted;
 
+            var rulesetCharacter = gameLocationCharacter.RulesetCharacter;
+
+            rulesetCharacter.ConditionAdded += ConditionAdded;
+            rulesetCharacter.ConditionRemoved += ConditionRemoved;
+            rulesetCharacter.ConditionRemovedForVisual += ConditionRemovedForVisual;
+            rulesetCharacter.ConditionOccurenceReached += ConditionOccurenceReached;
+            //rulesetCharacter.ConditionSaveRerollRequested += ConditionSaveRerollRequested;
+            rulesetCharacter.ImmuneToSpell += ImmuneToSpell;
+            rulesetCharacter.ImmuneToSpellLevel += ImmuneToSpellLevel;
+            rulesetCharacter.ImmuneToDamage += ImmuneToDamage;
+            rulesetCharacter.DamageAltered += DamageAltered;
+            rulesetCharacter.ImmuneToCondition += ImmuneToCondition;
+            rulesetCharacter.SaveRolled += SaveRolled;
+            rulesetCharacter.DieRerolled += DieRerolled;
+            rulesetCharacter.AttackInitiated += AttackInitiated;
+            rulesetCharacter.AttackRolled += AttackRolled;
+            rulesetCharacter.IncomingAttackRolled += IncomingAttackRolled;
+            rulesetCharacter.AttackAutomaticHit += AttackAutomaticHit;
+            rulesetCharacter.AttackAutomaticCritical += AttackAutomaticCritical;
+            rulesetCharacter.DamageFormsTriggered += DamageFormsTriggered;
+            rulesetCharacter.HealingFormsTriggered += HealingFormsTriggered;
+            rulesetCharacter.IncomingDamageNotified += IncomingDamageNotified;
+            rulesetCharacter.AbilityScoreIncreased += AbilityScoreIncreased;
+            rulesetCharacter.DamageHalved += DamageHalved;
+            rulesetCharacter.DamageReduced += DamageReduced;
+            rulesetCharacter.ReplacedAbilityScoreForSave += ReplacedAbilityScoreForSave;
+            rulesetCharacter.AdditionalSaveDieRolled += AdditionalSaveDieRolled;
+            rulesetCharacter.DamageReceived += DamageReceived;
+            rulesetCharacter.AlterationInflicted += AlterationInflicted;
+            rulesetCharacter.SpellDissipated += SpellDissipated;
+            rulesetCharacter.TagRevealed += TagRevealed;
+            rulesetCharacter.ActorReplaced += ActorReplaced;
+            
             if (gameLocationCharacter.RulesetCharacter is not RulesetCharacterHero rulesetCharacterHero)
             {
                 continue;
@@ -160,16 +193,16 @@ internal static class DelegatesContext
 
         foreach (var gameLocationCharacter in gameLocationCharacterService.ValidCharacters)
         {
-            gameLocationCharacter.Placed -= Placed;
+            //gameLocationCharacter.Placed -= Placed;
             gameLocationCharacter.MoveStarted -= MoveStarted;
             gameLocationCharacter.PrepareChargeStarted -= PrepareChargeStarted;
             gameLocationCharacter.ChargeStarted -= ChargeStarted;
             gameLocationCharacter.ChargeEnded -= ChargeEnded;
             gameLocationCharacter.ChargeAborted -= ChargeAborted;
-            gameLocationCharacter.UpdateMotions -= UpdateMotions;
+            //gameLocationCharacter.UpdateMotions -= UpdateMotions;
             gameLocationCharacter.TeleportStarted -= TeleportStarted;
             gameLocationCharacter.Moved -= Moved;
-            gameLocationCharacter.Rotated -= Rotated;
+            //gameLocationCharacter.Rotated -= Rotated;
             gameLocationCharacter.Stopped -= Stopped;
             gameLocationCharacter.FallStarted -= FallStarted;
             gameLocationCharacter.FallStopped -= FallStopped;
@@ -200,7 +233,7 @@ internal static class DelegatesContext
             gameLocationCharacter.SpeechEnded -= SpeechEnded;
             gameLocationCharacter.ListenStarted -= ListenStarted;
             gameLocationCharacter.ListenEnded -= ListenEnded;
-            gameLocationCharacter.AdditionalAnimationStarted -= AdditionalAnimationStarted;
+            //gameLocationCharacter.AdditionalAnimationStarted -= AdditionalAnimationStarted;
             gameLocationCharacter.TextFeedbackRequested -= TextFeedbackRequested;
             gameLocationCharacter.InGameDialogLineRequested -= InGameDialogLineRequested;
             gameLocationCharacter.AlreadySuccessful -= AlreadySuccessful;
@@ -210,6 +243,39 @@ internal static class DelegatesContext
             gameLocationCharacter.UsedTacticalMovesChanged -= UsedTacticalMovesChanged;
             gameLocationCharacter.CurrentMonsterAttackChanged -= CurrentMonsterAttackChanged;
             gameLocationCharacter.DisolveStarted -= DisolveStarted;
+
+            var rulesetCharacter = gameLocationCharacter.RulesetCharacter;
+
+            rulesetCharacter.ConditionAdded -= ConditionAdded;
+            rulesetCharacter.ConditionRemoved -= ConditionRemoved;
+            rulesetCharacter.ConditionRemovedForVisual -= ConditionRemovedForVisual;
+            rulesetCharacter.ConditionOccurenceReached -= ConditionOccurenceReached;
+            //rulesetCharacter.ConditionSaveRerollRequested -= ConditionSaveRerollRequested;
+            rulesetCharacter.ImmuneToSpell -= ImmuneToSpell;
+            rulesetCharacter.ImmuneToSpellLevel -= ImmuneToSpellLevel;
+            rulesetCharacter.ImmuneToDamage -= ImmuneToDamage;
+            rulesetCharacter.DamageAltered -= DamageAltered;
+            rulesetCharacter.ImmuneToCondition -= ImmuneToCondition;
+            rulesetCharacter.SaveRolled -= SaveRolled;
+            rulesetCharacter.DieRerolled -= DieRerolled;
+            rulesetCharacter.AttackInitiated -= AttackInitiated;
+            rulesetCharacter.AttackRolled -= AttackRolled;
+            rulesetCharacter.IncomingAttackRolled -= IncomingAttackRolled;
+            rulesetCharacter.AttackAutomaticHit -= AttackAutomaticHit;
+            rulesetCharacter.AttackAutomaticCritical -= AttackAutomaticCritical;
+            rulesetCharacter.DamageFormsTriggered -= DamageFormsTriggered;
+            rulesetCharacter.HealingFormsTriggered -= HealingFormsTriggered;
+            rulesetCharacter.IncomingDamageNotified -= IncomingDamageNotified;
+            rulesetCharacter.AbilityScoreIncreased -= AbilityScoreIncreased;
+            rulesetCharacter.DamageHalved -= DamageHalved;
+            rulesetCharacter.DamageReduced -= DamageReduced;
+            rulesetCharacter.ReplacedAbilityScoreForSave -= ReplacedAbilityScoreForSave;
+            rulesetCharacter.AdditionalSaveDieRolled -= AdditionalSaveDieRolled;
+            rulesetCharacter.DamageReceived -= DamageReceived;
+            rulesetCharacter.AlterationInflicted -= AlterationInflicted;
+            rulesetCharacter.SpellDissipated -= SpellDissipated;
+            rulesetCharacter.TagRevealed -= TagRevealed;
+            rulesetCharacter.ActorReplaced -= ActorReplaced;
 
             if (gameLocationCharacter.RulesetCharacter is not RulesetCharacterHero rulesetCharacterHero)
             {
@@ -771,5 +837,179 @@ internal static class DelegatesContext
     private static void DisolveStarted(GameLocationCharacter character)
     {
         Main.Logger.Log($"{character?.Name} Dissolve Started");
+    }
+
+    //
+    // RulesetCharacter
+    //
+
+    private static void ConditionAdded(RulesetActor character, RulesetCondition addedActiveCondition)
+    {
+        Main.Logger.Log("ConditionAdded");
+    }
+
+    private static void ConditionRemoved(RulesetActor character, RulesetCondition removedActiveCondition)
+    {
+        Main.Logger.Log("ConditionRemoved");
+    }
+
+    private static void ConditionRemovedForVisual(RulesetActor character, RulesetCondition removedActiveCondition,
+        bool showGraphics = true)
+    {
+        Main.Logger.Log("ConditionRemovedForVisual");
+    }
+
+    private static void ConditionOccurenceReached(RulesetActor character, RulesetCondition removedActiveCondition)
+    {
+        Main.Logger.Log("ConditionOccurenceReached");
+    }
+
+    // private static void ConditionSaveRerollRequested(RulesetActor character, RulesetCondition activeCondition,
+    //     RuleDefinitions.AdvantageType advantageType, bool removeOnSuccess, out bool success)
+    // {
+    //     Main.Logger.Log("ConditionSaveRerollRequested");
+    // }
+
+    private static void ImmuneToSpell(RulesetActor character, SpellDefinition spellDefinition)
+    {
+        Main.Logger.Log("ImmuneToSpell");
+    }
+
+    private static void ImmuneToSpellLevel(RulesetActor character, SpellDefinition spellDefinition, int maxSpellLevel)
+    {
+        Main.Logger.Log("ImmuneToSpellLevel");
+    }
+
+    private static void ImmuneToDamage(RulesetActor character, string damageType, bool silent)
+    {
+        Main.Logger.Log("ImmuneToDamage");
+    }
+
+    private static void DamageAltered(RulesetActor character, string damageType,
+        RuleDefinitions.DamageAffinityType damageAffinityType, bool silent)
+    {
+        Main.Logger.Log("DamageAltered");
+    }
+
+    private static void ImmuneToCondition(RulesetActor character, string conditionName, ulong sourceGuid)
+    {
+        Main.Logger.Log("ImmuneToCondition");
+    }
+
+    private static void SaveRolled(RulesetActor character, string ablityScoreName, BaseDefinition sourceDefinition,
+        RuleDefinitions.RollOutcome outcome, int saveDC, int totalRoll, int saveRoll, int firstRoll, int secondRoll,
+        int rollModifier, List<RuleDefinitions.TrendInfo> modifierTrends,
+        List<RuleDefinitions.TrendInfo> advantageTrends, bool hasHitVisual)
+    {
+        Main.Logger.Log("SaveRolled");
+    }
+
+    private static void DieRerolled(RulesetActor character, RuleDefinitions.DieType dieType, int previousValue,
+        int newValue, string localizationKey)
+    {
+        Main.Logger.Log("DieRerolled");
+    }
+
+    private static void AttackInitiated(RulesetActor character, int firstRoll, int secondRoll, int modifier,
+        RuleDefinitions.AdvantageType advantageType)
+    {
+        Main.Logger.Log("AttackInitiated");
+    }
+
+    private static void AttackRolled(RulesetActor character, RulesetActor target, BaseDefinition attackMethod,
+        RuleDefinitions.RollOutcome outcome, int attackRoll, int rawRoll, int modifier,
+        List<RuleDefinitions.TrendInfo> toHitTrends, List<RuleDefinitions.TrendInfo> advantageTrends,
+        bool opportunity = false)
+    {
+        Main.Logger.Log("AttackRolled");
+    }
+
+    private static void IncomingAttackRolled(RulesetActor character, RulesetActor target, BaseDefinition attackMethod,
+        bool rangeAttack, RuleDefinitions.RollOutcome outcome, int attackRoll, int rawRoll, int modifier,
+        List<RuleDefinitions.TrendInfo> toHitTrends, List<RuleDefinitions.TrendInfo> advantageTrends,
+        bool opportunity = false)
+    {
+        Main.Logger.Log("IncomingAttackRolled");
+    }
+
+    private static void AttackAutomaticHit(RulesetActor character, RulesetActor target, BaseDefinition attackMethod)
+    {
+        Main.Logger.Log("AttackAutomaticHit");
+    }
+
+    private static void AttackAutomaticCritical(RulesetActor target)
+    {
+        Main.Logger.Log("AttackAutomaticCritical");
+    }
+
+    private static void DamageFormsTriggered(RulesetActor character, List<EffectGroupInfo> damageInfos)
+    {
+        Main.Logger.Log("DamageFormsTriggered");
+    }
+
+    private static void HealingFormsTriggered(RulesetActor character, List<EffectGroupInfo> healingInfos)
+    {
+        Main.Logger.Log("HealingFormsTriggered");
+    }
+
+    private static void IncomingDamageNotified(RulesetActor character, RuleDefinitions.EffectSourceType damageType,
+        BaseDefinition attackVectorDefinition)
+    {
+        Main.Logger.Log("IncomingDamageNotified");
+    }
+
+    private static void AbilityScoreIncreased(RulesetActor character, string abilityScore, int valueIncrease,
+        int maxIncrease)
+    {
+        Main.Logger.Log("AbilityScoreIncreased");
+    }
+
+    private static void DamageHalved(RulesetActor character, FeatureDefinition feature)
+    {
+        Main.Logger.Log("DamageHalved");
+    }
+
+    private static void DamageReduced(RulesetActor character, FeatureDefinition feature, int reductionAmount)
+    {
+        Main.Logger.Log("DamageReduced");
+    }
+
+    private static void ReplacedAbilityScoreForSave(RulesetActor saver, FeatureDefinition feature,
+        string originalAbilityScore, string replacedAbilityScore)
+    {
+        Main.Logger.Log("ReplacedAbilityScoreForSave");
+    }
+
+    private static void AdditionalSaveDieRolled(RulesetActor character, RuleDefinitions.TrendInfo trendInfo)
+    {
+        Main.Logger.Log("AdditionalSaveDieRolled");
+    }
+
+    private static void DamageReceived(RulesetActor target, int damage, string damageType, ulong sourceGuid,
+        RollInfo rollInfo)
+    {
+        Main.Logger.Log("DamageReceived");
+    }
+
+    private static void AlterationInflicted(RulesetActor source, RulesetActor target,
+        AlterationForm.Type alterationType)
+    {
+        Main.Logger.Log("AlterationInflicted");
+    }
+
+    private static void SpellDissipated(RulesetActor source, RulesetActor target, SpellDefinition spellDefinition,
+        bool success)
+    {
+        Main.Logger.Log("SpellDissipated");
+    }
+
+    private static void TagRevealed(RulesetActor actor, string revealedTag)
+    {
+        Main.Logger.Log("TagRevealed");
+    }
+
+    private static void ActorReplaced(RulesetActor originalActor, RulesetActor newActor)
+    {
+        Main.Logger.Log("ActorReplaced");
     }
 }
