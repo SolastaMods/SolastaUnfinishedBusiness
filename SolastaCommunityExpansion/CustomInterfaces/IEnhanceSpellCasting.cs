7daf1c7ce42f31c2bd09d60932bb20dada263c53
nd <dolenam317@gmail.com>
2022-07-28 06:42:53 +0700

    Add STR/DEX modifier to attack roll

---
+++ SolastaCommunityExpansion/CustomInterfaces/IEnhanceSpellCasting.cs
@@ -0,0 +1,15 @@
+﻿namespace SolastaCommunityExpansion.CustomInterfaces;
+
+public interface IIncreaseSpellDC
+{
+    int GetSpellModifier(RulesetCharacter caster);
+}
+
+public interface IIncreaseSpellAttackRoll
+{
+    int GetSpellAttackRollModifier(RulesetCharacter caster);
+    RuleDefinitions.FeatureSourceType sourceType { get; set; }
+    string sourceName { get; set; }
+}
+
+
