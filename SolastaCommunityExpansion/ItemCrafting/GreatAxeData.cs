95d3454f62fd18af25f5457928dfdc3455b73306
Paulo Monteiro <pauloesquilo@gmail.com>
2022-08-09 07:51:26 -0700

    Add new recipes for GreatAxe and GreatSword

---
+++ SolastaCommunityExpansion/ItemCrafting/GreatAxeData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using SolastaCommunityExpansion.Api;
+using static SolastaCommunityExpansion.ItemCrafting.ItemCollection;
+
+namespace SolastaCommunityExpansion.ItemCrafting;
+
+internal static class GreatAxeData
+{
+    private static ItemCollection _items;
+
+    [NotNull]
+    internal static ItemCollection Items =>
+        _items ??= new ItemCollection
+        {
+            BaseGuid = new Guid("16757d1b-518f-4669-af43-1ddf5d23c223"),
+            BaseWeapons =
+                new List<ItemDefinition>
+                {
+                    DatabaseHelper.ItemDefinitions.Greataxe
+                },
+            PossiblePrimedItemsToReplace = new List<ItemDefinition>
+            {
+                DatabaseHelper.ItemDefinitions.Primed_Morningstar,
+                DatabaseHelper.ItemDefinitions.Primed_Mace,
+                DatabaseHelper.ItemDefinitions.Primed_Greatsword,
+                DatabaseHelper.ItemDefinitions.Primed_Battleaxe
+            },
+            MagicToCopy = new List<MagicItemDataHolder>
+            {
+                // Same as +1
+                new("Acuteness", DatabaseHelper.ItemDefinitions.Enchanted_Mace_Of_Acuteness,
+                    DatabaseHelper.RecipeDefinitions.Recipe_Enchantment_MaceOfAcuteness),
+                new("Bearclaw", DatabaseHelper.ItemDefinitions.Enchanted_Morningstar_Bearclaw,
+                    DatabaseHelper.RecipeDefinitions.Recipe_Enchantment_MorningstarBearclaw),
+                new("Power", DatabaseHelper.ItemDefinitions.Enchanted_Morningstar_Of_Power,
+                    DatabaseHelper.RecipeDefinitions.Recipe_Enchantment_MorningstarOfPower),
+                new("Lightbringer", DatabaseHelper.ItemDefinitions.Enchanted_Greatsword_Lightbringer,
+                    DatabaseHelper.RecipeDefinitions.Recipe_Enchantment_GreatswordLightbringer),
+                new("Punisher", DatabaseHelper.ItemDefinitions.Enchanted_Battleaxe_Punisher,
+                    DatabaseHelper.RecipeDefinitions.Recipe_Enchantment_BattleaxePunisher)
+            }
+        };
+}
---
+++ SolastaCommunityExpansion/ItemCrafting/GreatSwordData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using SolastaCommunityExpansion.Api;
+using static SolastaCommunityExpansion.ItemCrafting.ItemCollection;
+
+namespace SolastaCommunityExpansion.ItemCrafting;
+
+internal static class GreatSwordData
+{
+    private static ItemCollection _items;
+
+    [NotNull]
+    internal static ItemCollection Items =>
+        _items ??= new ItemCollection
+        {
+            BaseGuid = new Guid("16757d1b-518f-4669-af43-1ddf5d23c223"),
+            BaseWeapons =
+                new List<ItemDefinition>
+                {
+                    DatabaseHelper.ItemDefinitions.Greatsword
+                },
+            PossiblePrimedItemsToReplace = new List<ItemDefinition>
+            {
+                DatabaseHelper.ItemDefinitions.Battleaxe
+            },
+            MagicToCopy = new List<MagicItemDataHolder>
+            {
+                new("Punisher", DatabaseHelper.ItemDefinitions.Enchanted_Battleaxe_Punisher,
+                    DatabaseHelper.RecipeDefinitions.Recipe_Enchantment_BattleaxePunisher)
+            }
+        };
+}
