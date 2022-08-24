82ab8802bdeae42fd6468d55f4513999f8a3f916
Ender Wiggin <andrii.gook@gmail.com>
2022-08-24 12:34:12 +0300

    ported `hide reaction spells from spell panel` patch

---
+++ SolastaCommunityExpansion/Patches/SpellRepertoireLinePatcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace SolastaCommunityExpansion.Patches;
+
+internal static class SpellRepertoireLinePatcher
+{
+
+    [HarmonyPatch(typeof(SpellRepertoireLine), "FindAndSortRelevantSpells")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class FindAndSortRelevantSpells_Patch
+    {
+        internal static void Prefix([NotNull] List<SpellDefinition> spellDefinitions)
+        {
+            //PATCH: hide reaction spells from spell panel
+            spellDefinitions.RemoveAll(x => x.ActivationTime == RuleDefinitions.ActivationTime.Reaction);
+        }
+    }
+}
