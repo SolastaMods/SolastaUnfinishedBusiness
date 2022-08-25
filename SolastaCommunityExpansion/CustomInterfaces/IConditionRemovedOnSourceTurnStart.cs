b830eff683d4df2ed9a91f97377f46bebd3195b7
Ender Wiggin <andrii.gook@gmail.com>
2022-08-25 17:41:57 +0300

    ported `IConditionRemovedOnSourceTurnStart` patch

--- SolastaCommunityExpansion/CustomInterfaces/IConditionRemovedOnSourceTurnStart.cs
+++ SolastaCommunityExpansion/CustomInterfaces/IConditionRemovedOnSourceTurnStart.cs
@@ -1,8 +1,45 @@
-﻿namespace SolastaCommunityExpansion.CustomInterfaces;
+﻿using System.Linq;
+using SolastaCommunityExpansion.Api.Extensions;
+
+namespace SolastaCommunityExpansion.CustomInterfaces;
 
 /// <summary>
 ///     Implement on a ConditionDefinition to make it be removed when its source's turn starts.
 /// </summary>
 public interface IConditionRemovedOnSourceTurnStart
 {
+}
+
+internal static class ConditionRemovedOnSourceTurnStartPatch
+{
+    internal static void RemoveConditionIfNeeded(RulesetActor __instance,
+        RuleDefinitions.TurnOccurenceType occurenceType)
+    {
+        if (occurenceType != RuleDefinitions.TurnOccurenceType.StartOfTurn)
+        {
+            return;
+        }
+
+        var battleService = ServiceRepository.GetService<IGameLocationBattleService>();
+
+        if (battleService?.Battle == null)
+        {
+            return;
+        }
+
+        foreach (var contender in battleService.Battle.AllContenders
+                     .Where(x => x is {destroying: false, destroyedBody: false, RulesetActor: { }}))
+        {
+            var conditionsToRemove = (from keyValuePair in contender.RulesetActor.ConditionsByCategory
+                from rulesetCondition in keyValuePair.Value
+                where rulesetCondition.SourceGuid == __instance.Guid &&
+                      rulesetCondition.ConditionDefinition.HasSubFeatureOfType<IConditionRemovedOnSourceTurnStart>()
+                select rulesetCondition).ToList();
+
+            foreach (var conditionToRemove in conditionsToRemove)
+            {
+                contender.RulesetActor.RemoveCondition(conditionToRemove);
+            }
+        }
+    }
 }
---
+++ SolastaCommunityExpansion/Patches/RulesetActorPatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using SolastaCommunityExpansion.CustomInterfaces;
+
+namespace SolastaCommunityExpansion.Patches;
+
+internal static class RulesetActorPatcher
+{
+    [HarmonyPatch(typeof(RulesetActor), "ProcessConditionsMatchingOccurenceType")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class RulesetActor_ProcessConditionsMatchingOccurenceType
+    {
+        internal static void Postfix(RulesetActor __instance, RuleDefinitions.TurnOccurenceType occurenceType)
+        {
+            //PATCH: support for `IConditionRemovedOnSourceTurnStart` - removes appropriately marked conditions
+            ConditionRemovedOnSourceTurnStartPatch.RemoveConditionIfNeeded(__instance, occurenceType);
+        }
+    }
+}
