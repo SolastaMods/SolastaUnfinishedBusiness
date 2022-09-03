9d0ff91f7ecf535206d1f44c0bb8141d9441fa4d
Ender Wiggin <andrii.gook@gmail.com>
2022-09-03 18:06:45 +0300

    ported patch to support CustomSituationalContext

--- SolastaCommunityExpansion/Patches/RulesetImplementationManagerPatcher.cs
+++ SolastaCommunityExpansion/Patches/RulesetImplementationManagerPatcher.cs
@@ -2,19 +2,22 @@
 using HarmonyLib;
 using SolastaCommunityExpansion.CustomDefinitions;
 
-namespace SolastaCommunityExpansion.Patches.CustomFeatures.Insertion;
+namespace SolastaCommunityExpansion.Patches;
 
 internal static class RulesetImplementationManagerPatcher
 {
     [HarmonyPatch(typeof(RulesetImplementationManagerLocation), "IsSituationalContextValid")]
     [HarmonyPatch(new[] {typeof(RulesetImplementationDefinitions.SituationalContextParams)})]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    internal static class IsSituationalContextValid
+    internal static class IsSituationalContextValid_Patch
     {
         internal static void Postfix(
             ref bool __result,
             RulesetImplementationDefinitions.SituationalContextParams contextParams)
         {
+            //PATCH: supports custom situational context
+            //used to twean `Reckless` feat to properly work with reach weapons
+            //and for Blade Dancer subclass features
             __result = CustomSituationalContext.IsContextValid(contextParams, __result);
         }
     }
