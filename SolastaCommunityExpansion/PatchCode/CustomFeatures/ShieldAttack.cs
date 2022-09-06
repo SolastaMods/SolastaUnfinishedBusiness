cef65066b97f2437970dd7bb62e55c36ce445016
Ender Wiggin <andrii.gook@gmail.com>
2022-09-06 19:25:46 +0300

    ported patch that tweaks shield attack animation to use left hand

---
+++ SolastaCommunityExpansion/PatchCode/CustomFeatures/ShieldAttack.cs
@@ -0,0 +1,20 @@
+﻿using SolastaCommunityExpansion.Models;
+
+namespace SolastaCommunityExpansion.PatchCode.CustomFeatures;
+
+internal static class ShieldAttack
+{
+    public static void UseOffhandForShieldAttackAnimation(RulesetAttackMode attackMode, ref string animation,
+        ref bool isThrown,
+        ref bool leftHand)
+    {
+        if (!ShieldStrikeContext.IsShield(attackMode.SourceDefinition as ItemDefinition))
+        {
+            return;
+        }
+
+        leftHand = true;
+        isThrown = false;
+        animation = ShieldStrikeContext.ShieldWeaponType.AnimationTag;
+    }
+}
---
+++ SolastaCommunityExpansion/Patches/GraphicsCharacterHeroPatcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaCommunityExpansion.PatchCode.CustomFeatures;
+
+namespace SolastaCommunityExpansion.Patches;
+
+internal static class GraphicsCharacterHeroPatcher
+{
+    [HarmonyPatch(typeof(GraphicsCharacterHero), "GetAttackAnimationData")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class GetAttackAnimationData_Patch
+    {
+        internal static void Postfix(
+            ref string __result,
+            [NotNull] RulesetAttackMode attackMode,
+            ref bool isThrown,
+            ref bool leftHand)
+        {
+            ShieldAttack.UseOffhandForShieldAttackAnimation(attackMode, ref __result, ref isThrown, ref leftHand);
+        }
+    }
+}
