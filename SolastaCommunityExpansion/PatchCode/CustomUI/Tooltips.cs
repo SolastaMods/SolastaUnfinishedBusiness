d79c7819f91d7d458dee215de86a44263d8d86c5
Ender Wiggin <andrii.gook@gmail.com>
2022-09-06 17:10:46 +0300

    ported patch that shows proper usage limits of power in tooltip, including all power usage bunuses

--- SolastaCommunityExpansion/PatchCode/CustomUI/Tooltips.cs
+++ SolastaCommunityExpansion/PatchCode/CustomUI/Tooltips.cs
@@ -1,13 +1,14 @@
-﻿namespace SolastaCommunityExpansion.PatchCode.CustomUI;
+﻿using SolastaCommunityExpansion.Models;
+
+namespace SolastaCommunityExpansion.PatchCode.CustomUI;
 
 internal static class Tooltips
 {
-    
     internal static void AddContextToRecoveredFeature(RecoveredFeatureItem item, RulesetCharacterHero character)
     {
         item.GuiTooltip.Context = character;
     }
-    
+
     internal static void AddContextToPowerBoxTooltip(UsablePowerBox box)
     {
         CharacterControlPanel panel = box.GetComponentInParent<CharacterControlPanelExploration>();
@@ -17,5 +18,45 @@ internal static class Tooltips
         {
             box.GuiTooltip.Context = panel.GuiCharacter?.RulesetCharacter;
         }
+    }
+
+    public static void UpdatePowerUses(ITooltip tooltip, TooltipFeaturePowerParameters parameters)
+    {
+        if (tooltip.DataProvider is not GuiPowerDefinition guiPowerDefinition)
+        {
+            return;
+        }
+
+        if (tooltip.Context is not RulesetCharacter character)
+        {
+            return;
+        }
+
+        var power = guiPowerDefinition.PowerDefinition;
+        var usesLabel = parameters.usesLabel;
+        usesLabel.Text = FormatUses(power, character, usesLabel.Text);
+    }
+
+    private static string FormatUses(FeatureDefinitionPower power, RulesetCharacter character, string def)
+    {
+        if (power.UsesDetermination != RuleDefinitions.UsesDetermination.Fixed)
+        {
+            return def;
+        }
+
+        if (power.RechargeRate == RuleDefinitions.RechargeRate.AtWill)
+        {
+            return def;
+        }
+
+        if (power.CostPerUse == 0)
+        {
+            return def;
+        }
+
+        var usablePower = UsablePowersProvider.Get(power, character);
+        var maxUses = CustomFeaturesContext.GetMaxUsesForPool(usablePower, character);
+        var remainingUses = character.GetRemainingUsesOfPower(usablePower);
+        return $"{remainingUses}/{maxUses}";
     }
 }
---
+++ SolastaCommunityExpansion/Patches/TooltipFeaturePowerParametersPatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using SolastaCommunityExpansion.PatchCode.CustomUI;
+
+namespace SolastaCommunityExpansion.Patches;
+
+internal static class TooltipFeaturePowerParametersPatcher
+{
+    [HarmonyPatch(typeof(TooltipFeaturePowerParameters), "Bind")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class Bind_Patch
+    {
+        internal static void Postfix(TooltipFeaturePowerParameters __instance, ITooltip tooltip)
+        {
+            //PATCH: updates power uses in the tooltip to include all usage bonuses
+            Tooltips.UpdatePowerUses(tooltip, __instance);
+        }
+    }
+}
