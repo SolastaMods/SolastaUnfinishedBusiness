c6c608d48aa9c71a575cc24ed1c5696e3125d8ba
Ender Wiggin <andrii.gook@gmail.com>
2022-09-10 21:31:22 +0300

    ported bunch of patches related to power pools and powers that recharge on turn start

--- SolastaCommunityExpansion/Patches/RulesetCharacterPatcher.cs
+++ SolastaCommunityExpansion/Patches/RulesetCharacterPatcher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
@@ -270,18 +271,21 @@ internal static class RulesetCharacterPatcher
         }
     }
 
-    //PATCH: ensures that original character rage pool is in sync with substitute (Multiclass)
     [HarmonyPatch(typeof(RulesetCharacter), "UsePower")]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     internal static class UsePower_Patch
     {
         internal static void Postfix(RulesetCharacter __instance, RulesetUsablePower usablePower)
         {
+            //PATCH: ensures that original character rage pool is in sync with substitute (Multiclass)
             if (usablePower.PowerDefinition == PowerBarbarianRageStart
                 && __instance.OriginalFormCharacter is RulesetCharacterHero hero && hero != __instance)
             {
                 hero.SpendRagePoint();
             }
+
+            //PATCH: update usage for power pools 
+            __instance.UpdateUsageForPowerPool(usablePower, usablePower.PowerDefinition.CostPerUse);
         }
     }
 
@@ -620,5 +624,97 @@ internal static class RulesetCharacterPatcher
             warlockRepertoire?.RepertoireRefreshed?.Invoke(warlockRepertoire);
         }
     }
+
+
+    [HarmonyPatch(typeof(RulesetCharacter), "RechargePowersForTurnStart")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class RechargePowersForTurnStart_Patch
+    {
+        internal static void Postfix(RulesetCharacter __instance)
+        {
+            //PATCH: support for powers that recharge on turn start
+            foreach (var usablePower in __instance.UsablePowers)
+            {
+                if (usablePower?.PowerDefinition is not IStartOfTurnRecharge startOfTurnRecharge ||
+                    usablePower.RemainingUses >= usablePower.MaxUses)
+                {
+                    continue;
+                }
+
+                usablePower.Recharge();
+
+                if (!startOfTurnRecharge.IsRechargeSilent && __instance.PowerRecharged != null)
+                {
+                    __instance.PowerRecharged(__instance, usablePower);
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(RulesetCharacter), "RepayPowerUse")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class RepayPowerUse_Patch
+    {
+        public static void Postfix(RulesetCharacter __instance, RulesetUsablePower usablePower)
+        {
+            //PATCH: update usage for power pools
+            __instance.UpdateUsageForPowerPool(usablePower, -usablePower.PowerDefinition.CostPerUse);
+        }
+    }
+
+    [HarmonyPatch(typeof(RulesetCharacter), "GrantPowers")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class GrantPowers_Patch
+    {
+        public static void Postfix(RulesetCharacter __instance)
+        {
+            //PATCH: update usage for power pools
+            CustomFeaturesContext.RechargeLinkedPowers(__instance, RuleDefinitions.RestType.LongRest);
+        }
+    }
+
+    [HarmonyPatch(typeof(RulesetCharacter), "ApplyRest")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class ApplyRest_Patch
+    {
+        internal static void Postfix(
+            RulesetCharacter __instance, RuleDefinitions.RestType restType, bool simulate)
+        {
+            //PATCH: update usage for power pools
+            if (!simulate)
+            {
+                CustomFeaturesContext.RechargeLinkedPowers(__instance, restType);
+            }
+
+            // The player isn't recharging the shared pool features, just the pool.
+            // Hide the features that use the pool from the UI.
+            foreach (var feature in __instance.RecoveredFeatures.Where(f => f is IPowerSharedPool).ToArray())
+            {
+                __instance.RecoveredFeatures.Remove(feature);
+            }
+        }
+
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            //PATCH: Makes powers that have their max usage extended by pool modifiers show up correctly during rest
+            //replace calls to MaxUses getter to custom method that accounts for extended power usage
+            var codes = instructions.ToList();
+            var maxUses =
+                new Func<RulesetUsablePower, RulesetCharacter, int>(CustomFeaturesContext.GetMaxUsesForPool).Method;
+
+            var bind = typeof(RulesetUsablePower).GetMethod("get_MaxUses", BindingFlags.Public | BindingFlags.Instance);
+
+            var bindIndex = codes.FindIndex(x => x.Calls(bind));
+
+            if (bindIndex <= 0)
+            {
+                return codes.AsEnumerable();
+            }
+
+            codes[bindIndex] = new CodeInstruction(OpCodes.Call, maxUses);
+            codes.Insert(bindIndex, new CodeInstruction(OpCodes.Ldarg_0));
+
+            return codes.AsEnumerable();
+        }
+    }
 }
-    
