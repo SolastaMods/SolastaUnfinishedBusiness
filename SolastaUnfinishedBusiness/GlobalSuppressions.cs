72967657004f880085e3b85847d7c686e649bb01
Paulo Monteiro <pauloesquilo@gmail.com>
2022-09-12 06:17:27 -0700

    Fix modkit in DEBUG

--- SolastaUnfinishedBusiness/GlobalSuppressions.cs
+++ SolastaUnfinishedBusiness/GlobalSuppressions.cs
@@ -9,10 +9,6 @@
     SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Fighting analyzers",
         Scope = "namespaceanddescendants", Target = "~N:SolastaUnfinishedBusiness")]
 
-[assembly:
-    SuppressMessage("", "IDE0130", Justification = "3rd party source", Scope = "namespaceanddescendants",
-        Target = "~N:ModKit")]
-
 [assembly: SuppressMessage("Major Code Smell",
     "S3011:Reflection should not be used to increase accessibility of classes, methods, or fields",
     Justification = "Reflection is required", Scope = "namespaceanddescendants",
