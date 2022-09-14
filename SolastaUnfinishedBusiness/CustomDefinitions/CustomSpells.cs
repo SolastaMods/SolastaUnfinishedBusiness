b70c03fbb8139b577a29bad98507471e9cd71869
Paulo Monteiro <pauloesquilo@gmail.com>
2022-09-13 19:59:32 -0700

    Demote FeatureDefinitionSpellModifierBuilder

--- SolastaUnfinishedBusiness/CustomDefinitions/CustomSpells.cs
+++ SolastaUnfinishedBusiness/CustomDefinitions/CustomSpells.cs
@@ -6,38 +6,38 @@ using SolastaUnfinishedBusiness.CustomInterfaces;
 
 namespace SolastaUnfinishedBusiness.CustomDefinitions;
 
-public class SpellDefinitionWithDependentEffects : SpellDefinition, ICustomMagicEffectBasedOnCaster
-{
-    public List<(List<FeatureDefinition>, EffectDescription)> FeaturesEffectList { get; } = new();
+// public class SpellDefinitionWithDependentEffects : SpellDefinition, ICustomMagicEffectBasedOnCaster
+// {
+//     private List<(List<FeatureDefinition>, EffectDescription)> FeaturesEffectList { get; } = new();
+//
+//     public EffectDescription GetCustomEffect(RulesetCharacter caster)
+//     {
+//         var casterFeatures = caster.GetFeaturesByType<FeatureDefinition>().ToHashSet();
+//
+//         foreach (var (featureDefinitions, customEffect) in FeaturesEffectList)
+//         {
+//             if (featureDefinitions.All(f => casterFeatures.Contains(f)))
+//             {
+//                 return customEffect;
+//             }
+//         }
+//
+//         return EffectDescription;
+//     }
+// }
 
-    public EffectDescription GetCustomEffect(RulesetCharacter caster)
-    {
-        var casterFeatures = caster.GetFeaturesByType<FeatureDefinition>().ToHashSet();
-
-        foreach (var (featureDefinitions, customEffect) in FeaturesEffectList)
-        {
-            if (featureDefinitions.All(f => casterFeatures.Contains(f)))
-            {
-                return customEffect;
-            }
-        }
-
-        return EffectDescription;
-    }
-}
-
-public class SpellModifyingFeatureDefinition : FeatureDefinition, IModifySpellEffect
-{
-    public delegate EffectDescription ModifySpellEffectDelegate(SpellDefinition spell, EffectDescription effect,
-        RulesetCharacter caster);
-
-    public ModifySpellEffectDelegate SpellModifier { get; set; }
-
-    public EffectDescription ModifyEffect(SpellDefinition spell, EffectDescription effect, RulesetCharacter caster)
-    {
-        return SpellModifier != null ? SpellModifier(spell, effect, caster) : effect;
-    }
-}
+// public class SpellModifyingFeatureDefinition : FeatureDefinition, IModifySpellEffect
+// {
+//     public delegate EffectDescription ModifySpellEffectDelegate(SpellDefinition spell, EffectDescription effect,
+//         RulesetCharacter caster);
+//
+//     public ModifySpellEffectDelegate SpellModifier { get; set; }
+//
+//     public EffectDescription ModifyEffect(SpellDefinition spell, EffectDescription effect, RulesetCharacter caster)
+//     {
+//         return SpellModifier != null ? SpellModifier(spell, effect, caster) : effect;
+//     }
+// }
 
 internal sealed class UpgradeEffectFromLevel : ICustomMagicEffectBasedOnCaster
 {
