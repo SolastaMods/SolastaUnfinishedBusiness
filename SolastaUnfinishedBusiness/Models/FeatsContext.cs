015022015aeb6776abaa1692c050dfa710d8c485
Ender Wiggin <andrii.gook@gmail.com>
2022-09-19 09:50:36 +0300

    moved FeatSubPanel patches to FeatsContext

--- SolastaUnfinishedBusiness/Models/FeatsContext.cs
+++ SolastaUnfinishedBusiness/Models/FeatsContext.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Api;
 using SolastaUnfinishedBusiness.Api.Extensions;
+using SolastaUnfinishedBusiness.Api.Infrastructure;
 using SolastaUnfinishedBusiness.CustomInterfaces;
 using SolastaUnfinishedBusiness.Feats;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace SolastaUnfinishedBusiness.Models;
 
 internal static class FeatsContext
 {
     internal static HashSet<FeatDefinition> Feats { get; private set; } = new();
+    
+    private const int COLUMNS = 3;
+    public const int WIDTH = 300;
+    public const int HEIGHT = 44;
+    public const int SPACING = 5;
 
     internal static void LateLoad()
     {
@@ -70,5 +79,97 @@ internal static class FeatsContext
 
         UpdateFeatsVisibility(featDefinition);
         GuiWrapperContext.RecacheFeats();
+    }
+
+    public static void UpdatePanelChildren(FeatSubPanel panel)
+    {
+        //get missing children from pool
+        while (panel.table.childCount < panel.relevantFeats.Count)
+        {
+            Gui.GetPrefabFromPool(panel.itemPrefab, panel.table);
+        }
+
+        //release extra children to pool
+        while (panel.table.childCount > panel.relevantFeats.Count)
+        {
+            Gui.ReleaseInstanceToPool(panel.table.GetChild(panel.table.childCount - 1).gameObject);
+        }
+    }
+
+    //Called before Bind to hide sub-feats
+    public static void ProcessFeatGroups(FeatSubPanel panel)
+    {
+        var toRemove = new List<FeatDefinition>();
+        foreach (var group in panel.relevantFeats
+                     .Select(feat => feat.GetFirstSubFeatureOfType<IGroupedFeat>())
+                     .Where(group => group != null))
+        {
+            toRemove.AddRange(group.GetSubFeats());
+        }
+
+        panel.relevantFeats.RemoveAll(f => toRemove.Contains(f));
+    }
+
+    public static void SortFeats(FeatSubPanel panel)
+    {
+        if (Main.Settings.EnableSortingFeats)
+        {
+            panel.relevantFeats.Sort((a, b) =>
+                string.Compare(a.FormatTitle(), b.FormatTitle(), StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+
+    public static void UpdateRelevantFeatList(FeatSubPanel panel)
+    {
+        var dbFeatDefinition = DatabaseRepository.GetDatabase<FeatDefinition>();
+
+        panel.relevantFeats.SetRange(dbFeatDefinition.Where(x => !x.GuiPresentation.Hidden));
+    }
+
+    public static void ForceSameWidth(RectTransform table, bool active)
+    {
+        if (active && Main.Settings.EnableSameWidthFeatSelection)
+        {
+            var hero = Global.ActiveLevelUpHero;
+            var buildingData = hero?.GetHeroBuildingData();
+
+            if (buildingData == null)
+            {
+                return;
+            }
+
+            var trainedFeats = buildingData.LevelupTrainedFeats.SelectMany(x => x.Value).ToList();
+
+            trainedFeats.AddRange(hero.TrainedFeats);
+
+            var j = 0;
+            var rect = table.GetComponent<RectTransform>();
+
+            rect.sizeDelta = new Vector2(rect.sizeDelta.x, ((table.childCount / COLUMNS) + 1) * (HEIGHT + SPACING));
+
+            for (var i = 0; i < table.childCount; i++)
+            {
+                var child = table.GetChild(i);
+                var featItem = child.GetComponent<FeatItem>();
+
+                if (trainedFeats.Contains(featItem.GuiFeatDefinition.FeatDefinition))
+                {
+                    continue;
+                }
+
+                var x = j % COLUMNS;
+                var y = j / COLUMNS;
+                var posX = x * (WIDTH + (SPACING * 2));
+                var posY = -y * (HEIGHT + SPACING);
+
+                rect = child.GetComponent<RectTransform>();
+                rect.anchoredPosition = new Vector2(posX, posY);
+                rect.sizeDelta = new Vector2(WIDTH, HEIGHT);
+
+                j++;
+            }
+        }
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(table);
     }
 }
--- SolastaUnfinishedBusiness/Patches/LevelUp/FeatSubPanelPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/LevelUp/FeatSubPanelPatcher.cs
@@ -6,127 +6,53 @@ using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
-using SolastaUnfinishedBusiness.Api.Extensions;
-using SolastaUnfinishedBusiness.Api.Infrastructure;
-using SolastaUnfinishedBusiness.CustomInterfaces;
 using SolastaUnfinishedBusiness.Models;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace SolastaUnfinishedBusiness.Patches.LevelUp;
 
-[HarmonyPatch(typeof(FeatSubPanel), "Bind")]
-[SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-internal static class FeatSubPanel_Bind
+internal static class FeatSubPanelPatcher
 {
-    internal static void Prefix([NotNull] FeatSubPanel __instance)
+    [HarmonyPatch(typeof(FeatSubPanel), "Bind")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class FeatSubPanel_Bind
     {
-        //PATCH: avoids a restart when enabling / disabling feats on the Mod UI panel
-        var dbFeatDefinition = DatabaseRepository.GetDatabase<FeatDefinition>();
-
-        __instance.relevantFeats.SetRange(dbFeatDefinition.Where(x => !x.GuiPresentation.Hidden));
-
-        //PATCH: sorts the feats panel by Title
-        if (Main.Settings.EnableSortingFeats)
+        internal static void Prefix([NotNull] FeatSubPanel __instance)
         {
-            __instance.relevantFeats.Sort((a, b) =>
-                String.Compare(a.FormatTitle(), b.FormatTitle(), StringComparison.CurrentCultureIgnoreCase));
-        }
+            //PATCH: avoids a restart when enabling / disabling feats on the Mod UI panel
+            FeatsContext.UpdateRelevantFeatList(__instance);
 
-        //PATCH: grouped feats - hide feats that are in the group
-        var toRemove = new List<FeatDefinition>();
-        foreach (var group in __instance.relevantFeats
-                     .Select(feat => feat.GetFirstSubFeatureOfType<IGroupedFeat>())
-                     .Where(group => group != null))
-        {
-            toRemove.AddRange(group.GetSubFeats());
-        }
-        __instance.relevantFeats.RemoveAll(f => toRemove.Contains(f));
+            //PATCH: sorts the feats panel by Title
+            FeatsContext.SortFeats(__instance);
 
-        //get missing children from pool
-        while (__instance.table.childCount < __instance.relevantFeats.Count)
-        {
-            Gui.GetPrefabFromPool(__instance.itemPrefab, __instance.table);
-        }
+            //PATCH: grouped feats - select sub-feats into a separate list
+            FeatsContext.ProcessFeatGroups(__instance);
 
-        //release extra children to pool
-        while (__instance.table.childCount > __instance.relevantFeats.Count)
-        {
-            Gui.ReleaseInstanceToPool(__instance.table.GetChild(__instance.table.childCount - 1).gameObject);
+            //PATCH: grouped feats - update children according to current feat list
+            FeatsContext.UpdatePanelChildren(__instance);
         }
     }
-}
 
-//PATCH: enforces the feat selection panel to always display same-width columns
-[HarmonyPatch(typeof(FeatSubPanel), "SetState")]
-[SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-internal static class FeatSubPanel_SetState
-{
-    [NotNull]
-    internal static IEnumerable<CodeInstruction> Transpiler([NotNull] IEnumerable<CodeInstruction> instructions)
+    //PATCH: enforces the feat selection panel to always display same-width columns
+    [HarmonyPatch(typeof(FeatSubPanel), "SetState")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class FeatSubPanel_SetState
     {
-        var forceRebuildLayoutImmediateMethod = typeof(LayoutRebuilder)
-            .GetMethod("ForceRebuildLayoutImmediate", BindingFlags.Static | BindingFlags.Public);
-        var forceSameWidthMethod = new Action<RectTransform, bool>(ForceSameWidth).Method;
-
-        var code = instructions.ToList();
-        var index = code.FindIndex(x => x.Calls(forceRebuildLayoutImmediateMethod));
-
-        code[index] = new CodeInstruction(OpCodes.Ldarg_1);
-        code.Insert(index + 1, new CodeInstruction(OpCodes.Call, forceSameWidthMethod));
-
-        return code;
-    }
-
-    private static void ForceSameWidth(RectTransform table, bool active)
-    {
-        const int COLUMNS = 3;
-        const int WIDTH = 300;
-        const int HEIGHT = 44;
-        const int SPACING = 5;
-
-        if (active && Main.Settings.EnableSameWidthFeatSelection)
+        [NotNull]
+        internal static IEnumerable<CodeInstruction> Transpiler([NotNull] IEnumerable<CodeInstruction> instructions)
         {
-            var hero = Global.ActiveLevelUpHero;
-            var buildingData = hero?.GetHeroBuildingData();
+            var forceRebuildLayoutImmediateMethod = typeof(LayoutRebuilder)
+                .GetMethod("ForceRebuildLayoutImmediate", BindingFlags.Static | BindingFlags.Public);
+            var forceSameWidthMethod = new Action<RectTransform, bool>(FeatsContext.ForceSameWidth).Method;
 
-            if (buildingData == null)
-            {
-                return;
-            }
+            var code = instructions.ToList();
+            var index = code.FindIndex(x => x.Calls(forceRebuildLayoutImmediateMethod));
 
-            var trainedFeats = buildingData.LevelupTrainedFeats.SelectMany(x => x.Value).ToList();
+            code[index] = new CodeInstruction(OpCodes.Ldarg_1);
+            code.Insert(index + 1, new CodeInstruction(OpCodes.Call, forceSameWidthMethod));
 
-            trainedFeats.AddRange(hero.TrainedFeats);
-
-            var j = 0;
-            var rect = table.GetComponent<RectTransform>();
-
-            rect.sizeDelta = new Vector2(rect.sizeDelta.x, ((table.childCount / COLUMNS) + 1) * (HEIGHT + SPACING));
-
-            for (var i = 0; i < table.childCount; i++)
-            {
-                var child = table.GetChild(i);
-                var featItem = child.GetComponent<FeatItem>();
-
-                if (trainedFeats.Contains(featItem.GuiFeatDefinition.FeatDefinition))
-                {
-                    continue;
-                }
-
-                var x = j % COLUMNS;
-                var y = j / COLUMNS;
-                var posX = x * (WIDTH + (SPACING * 2));
-                var posY = -y * (HEIGHT + SPACING);
-
-                rect = child.GetComponent<RectTransform>();
-                rect.anchoredPosition = new Vector2(posX, posY);
-                rect.sizeDelta = new Vector2(WIDTH, HEIGHT);
-
-                j++;
-            }
+            return code;
         }
-
-        LayoutRebuilder.ForceRebuildLayoutImmediate(table);
     }
 }
