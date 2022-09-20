4fb7aa6d31882c91b1536af96b0d0bf1eccd6c46
Paulo Monteiro <pauloesquilo@gmail.com>
2022-09-19 19:34:20 -0700

    Remove children in groups from Mod UI selection:
    - We can also make this optional with a setting on UI.

--- SolastaUnfinishedBusiness/Feats/GroupFeats.cs
+++ SolastaUnfinishedBusiness/Feats/GroupFeats.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Builders;
 using SolastaUnfinishedBusiness.CustomInterfaces;
+using SolastaUnfinishedBusiness.Models;
 using static SolastaUnfinishedBusiness.Api.DatabaseHelper;
 
 namespace SolastaUnfinishedBusiness.Feats;
@@ -10,20 +11,25 @@ namespace SolastaUnfinishedBusiness.Feats;
 public static class GroupFeats
 {
     private static readonly List<FeatDefinition> Groups = new();
+    private static readonly List<FeatDefinition> Children = new();
 
     public static void CreateFeats([NotNull] List<FeatDefinition> feats)
     {
         feats.Add(BuildElementalTouchGroup());
         feats.Add(BuildCreedGroup());
         feats.AddRange(Groups);
+
+        //TODO: should this be an option on mod UI?
+        // remove children from mod UI selection
+        FeatsContext.Feats.RemoveWhere(x => Children.Contains(x));
     }
 
     public static FeatDefinition MakeGroup(string name, string family, params FeatDefinition[] feats)
     {
-        return MakeGroup(name, family, feats.AsEnumerable());
+        return MakeGroup(name, family, feats.ToList());
     }
 
-    public static FeatDefinition MakeGroup(string name, string family, IEnumerable<FeatDefinition> feats)
+    public static FeatDefinition MakeGroup(string name, string family, List<FeatDefinition> feats)
     {
         var group = FeatDefinitionBuilder
             .Create(name, DefinitionBuilder.CENamespaceGuid)
@@ -33,6 +39,7 @@ public static class GroupFeats
             .SetFeatures()
             .AddToDB();
         Groups.Add(group);
+        Children.AddRange(feats);
         return group;
     }
 
