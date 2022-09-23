0f7b1bd0e036ac3e5df445595e4f64e25b081dcf
Paulo Monteiro <pauloesquilo@gmail.com>
2022-09-22 19:17:24 -0700

    Bring back HandwrapsOfPulling

---
+++ SolastaUnfinishedBusiness/Builders/UsableDeviceDescriptionBuilder.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine.AddressableAssets;
+using static EquipmentDefinitions;
+
+namespace SolastaUnfinishedBusiness.Builders;
+
+public class UsableDeviceDescriptionBuilder
+{
+    private readonly UsableDeviceDescription description;
+
+    public UsableDeviceDescriptionBuilder()
+    {
+        description = new UsableDeviceDescription
+        {
+            usage = ItemUsage.ByFunction,
+            chargesCapital = ItemChargesCapital.Fixed,
+            chargesCapitalNumber = 1,
+            chargesCapitalDie = RuleDefinitions.DieType.D1,
+            chargesCapitalBonus = 0,
+            rechargeRate = RuleDefinitions.RechargeRate.Dawn,
+            rechargeNumber = 1,
+            rechargeDie = RuleDefinitions.DieType.D1,
+            rechargeBonus = 0,
+            outOfChargesConsequence = ItemOutOfCharges.Persist,
+            magicAttackBonus = 0,
+            saveDC = 10,
+            onUseParticle = new AssetReference()
+        };
+    }
+
+#if false
+    public UsableDeviceDescriptionBuilder SetUsage(ItemUsage usage)
+    {
+        description.usage = usage;
+        return this;
+    }
+    
+    public UsableDeviceDescriptionBuilder SetOutOfChargesConsequence(ItemOutOfCharges consequence)
+    {
+        description.outOfChargesConsequence = consequence;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetMagicAttackBonus(int bonus)
+    {
+        description.magicAttackBonus = bonus;
+        return this;
+    }
+    
+    public UsableDeviceDescriptionBuilder SetOnUseParticle(AssetReference asset)
+    {
+        description.onUseParticle = asset;
+        return this;
+    }
+#endif
+
+    public UsableDeviceDescriptionBuilder SetSaveDC(int DC)
+    {
+        description.saveDC = DC;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder AddFunctions(params DeviceFunctionDescription[] functions)
+    {
+        description.DeviceFunctions.AddRange(functions);
+        return this;
+    }
+
+    public UsableDeviceDescription Build()
+    {
+        return description;
+    }
+
+    #region Charge
+
+#if false
+    public UsableDeviceDescriptionBuilder SetCharges(
+        ItemChargesCapital capital = ItemChargesCapital.Fixed,
+        int number = 1,
+        RuleDefinitions.DieType dieType = RuleDefinitions.DieType.D1,
+        int bonus = 0)
+    {
+        description.chargesCapital = capital;
+        description.chargesCapitalNumber = number;
+        description.chargesCapitalDie = dieType;
+        description.chargesCapitalBonus = bonus;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetChargesCapital(ItemChargesCapital capital)
+    {
+        description.chargesCapital = capital;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetChargesCapitalNumber(int number)
+    {
+        description.chargesCapitalNumber = number;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetChargesCapitalDie(RuleDefinitions.DieType dieType)
+    {
+        description.chargesCapitalDie = dieType;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetChargesCapitalBonus(int bonus)
+    {
+        description.chargesCapitalBonus = bonus;
+        return this;
+    }
+#endif
+
+    #endregion
+
+    #region Recharge
+
+    public UsableDeviceDescriptionBuilder SetRecharge(
+        RuleDefinitions.RechargeRate rate = RuleDefinitions.RechargeRate.Dawn,
+        int number = 1,
+        RuleDefinitions.DieType dieType = RuleDefinitions.DieType.D1,
+        int bonus = 0)
+    {
+        description.rechargeRate = rate;
+        description.rechargeNumber = number;
+        description.rechargeDie = dieType;
+        description.rechargeBonus = bonus;
+        return this;
+    }
+
+#if false
+    public UsableDeviceDescriptionBuilder SetRechargeRate(RuleDefinitions.RechargeRate rate)
+    {
+        description.rechargeRate = rate;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetRechargeNumber(int number)
+    {
+        description.rechargeNumber = number;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetRechargeDie(RuleDefinitions.DieType dieType)
+    {
+        description.rechargeDie = dieType;
+        return this;
+    }
+
+    public UsableDeviceDescriptionBuilder SetRechargeBonus(int bonus)
+    {
+        description.rechargeBonus = bonus;
+        return this;
+    }
+#endif
+
+    #endregion
+}
--- SolastaUnfinishedBusiness/Models/CustomWeaponsContext.cs
+++ SolastaUnfinishedBusiness/Models/CustomWeaponsContext.cs
@@ -163,51 +163,51 @@ public static class CustomWeaponsContext
                 damageType: RuleDefinitions.DamageTypeForce)
             .Build());
 
-        // HandwrapsOfPulling = BuildHandwrapsCommon("HandwrapsOfPulling", 2000, true, false, Rare, WeaponPlus1AttackOnly);
-        // HandwrapsOfPulling.IsUsableDevice = true;
-        // HandwrapsOfPulling.usableDeviceDescription = new UsableDeviceDescriptionBuilder()
-        //     .SetRecharge(RuleDefinitions.RechargeRate.AtWill)
-        //     .SetSaveDC(-1)
-        //     .AddFunctions(new DeviceFunctionDescriptionBuilder()
-        //         .SetPower(FeatureDefinitionPowerBuilder
-        //             .Create("PowerFunctionHandwrapsOfPulling", Monk.Guid)
-        //             .SetGuiPresentation(Category.Feature)
-        //             .SetActivationTime(RuleDefinitions.ActivationTime.BonusAction)
-        //             .SetUsesFixed(1)
-        //             .SetRechargeRate(RuleDefinitions.RechargeRate.AtWill)
-        //             .SetEffectDescription(new EffectDescriptionBuilder()
-        //                 .SetTargetingData(RuleDefinitions.Side.All, RuleDefinitions.RangeType.Distance, 3,
-        //                     RuleDefinitions.TargetType.Individuals)
-        //                 .ExcludeCaster()
-        //                 .SetSavingThrowData(
-        //                     true,
-        //                     true,
-        //                     AttributeDefinitions.Strength,
-        //                     false,
-        //                     RuleDefinitions.EffectDifficultyClassComputation.AbilityScoreAndProficiency,
-        //                     AttributeDefinitions.Wisdom)
-        //                 .SetParticleEffectParameters(FeatureDefinitionPowers.PowerShadowTamerRopeGrapple
-        //                     .EffectDescription.EffectParticleParameters)
-        //                 .SetDurationData(RuleDefinitions.DurationType.Instantaneous)
-        //                 .SetEffectForms(new EffectFormBuilder()
-        //                     .SetMotionForm(MotionForm.MotionType.DragToOrigin, 2)
-        //                     .Build())
-        //                 .Build())
-        //             .AddToDB())
-        //         .Build())
-        //     .Build();
+        HandwrapsOfPulling = BuildHandwrapsCommon("HandwrapsOfPulling", 2000, true, false, Rare, WeaponPlus1AttackOnly);
+        HandwrapsOfPulling.IsUsableDevice = true;
+        HandwrapsOfPulling.usableDeviceDescription = new UsableDeviceDescriptionBuilder()
+            .SetRecharge(RuleDefinitions.RechargeRate.AtWill)
+            .SetSaveDC(-1)
+            .AddFunctions(new DeviceFunctionDescriptionBuilder()
+                .SetPower(FeatureDefinitionPowerBuilder
+                    .Create("PowerHandwrapsOfPulling")
+                    .SetGuiPresentation(Category.Feature)
+                    .SetActivationTime(RuleDefinitions.ActivationTime.BonusAction)
+                    .SetUsesFixed(1)
+                    .SetRechargeRate(RuleDefinitions.RechargeRate.AtWill)
+                    .SetEffectDescription(new EffectDescriptionBuilder()
+                        .SetTargetingData(RuleDefinitions.Side.All, RuleDefinitions.RangeType.Distance, 3,
+                            RuleDefinitions.TargetType.Individuals)
+                        .ExcludeCaster()
+                        .SetSavingThrowData(
+                            true,
+                            true,
+                            AttributeDefinitions.Strength,
+                            false,
+                            RuleDefinitions.EffectDifficultyClassComputation.AbilityScoreAndProficiency,
+                            AttributeDefinitions.Wisdom)
+                        .SetParticleEffectParameters(FeatureDefinitionPowers.PowerShadowTamerRopeGrapple
+                            .EffectDescription.EffectParticleParameters)
+                        .SetDurationData(RuleDefinitions.DurationType.Instantaneous)
+                        .SetEffectForms(new EffectFormBuilder()
+                            .SetMotionForm(MotionForm.MotionType.DragToOrigin, 2)
+                            .Build())
+                        .Build())
+                    .AddToDB())
+                .Build())
+            .Build();
 
-        // ShopItems.Add((BuildManual(BuildRecipe(HandwrapsPlus1, 24, 10, Monk.Guid,
-        //     ItemDefinitions.Ingredient_Enchant_Oil_Of_Acuteness), Monk.Guid), ShopCrafting));
-        //
-        // ShopItems.Add((BuildManual(BuildRecipe(HandwrapsPlus2, 48, 16, Monk.Guid,
-        //     ItemDefinitions.Ingredient_Enchant_Blood_Gem), Monk.Guid), ShopCrafting));
-        //
-        // ShopItems.Add((BuildManual(BuildRecipe(HandwrapsOfForce, 48, 16, Monk.Guid,
-        //     ItemDefinitions.Ingredient_Enchant_Soul_Gem), Monk.Guid), ShopCrafting));
-        //
-        // ShopItems.Add((BuildManual(BuildRecipe(HandwrapsOfPulling, 48, 16, Monk.Guid,
-        //     ItemDefinitions.Ingredient_Enchant_Slavestone), Monk.Guid), ShopCrafting));
+        ShopItems.Add((BuildManual(BuildRecipe(HandwrapsPlus1, 24, 10,
+            ItemDefinitions.Ingredient_Enchant_Oil_Of_Acuteness)), ShopCrafting));
+        
+        ShopItems.Add((BuildManual(BuildRecipe(HandwrapsPlus2, 48, 16,
+            ItemDefinitions.Ingredient_Enchant_Blood_Gem)), ShopCrafting));
+        
+        ShopItems.Add((BuildManual(BuildRecipe(HandwrapsOfForce, 48, 16,
+            ItemDefinitions.Ingredient_Enchant_Soul_Gem)), ShopCrafting));
+        
+        ShopItems.Add((BuildManual(BuildRecipe(HandwrapsOfPulling, 48, 16,
+            ItemDefinitions.Ingredient_Enchant_Slavestone)), ShopCrafting));
     }
 
     [NotNull]
