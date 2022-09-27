fa1207959e204eab6138fdd636034a8706fc4b70
Ender Wiggin <andrii.gook@gmail.com>
2022-09-27 12:54:10 +0300

    added patch to properly account for usage cost of overcharged powers

--- SolastaUnfinishedBusiness/Patches/CharacterActionUsePowerPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/CharacterActionUsePowerPatcher.cs
@@ -40,4 +40,41 @@ internal static class CharacterActionUsePowerPatcher
             return RulesetEffectPowerWithAdvancement.GetAdvancementData(__instance);
         }
     }
+
+    [HarmonyPatch(typeof(CharacterActionUsePower), "SpendMagicEffectUses")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    internal static class SpendMagicEffectUses_Patch
+    {
+        internal static bool Prefix([NotNull] CharacterActionUsePower __instance)
+        {
+            //PATCH: Calculate extra charge usage for `RulesetEffectPowerWithAdvancement`
+
+            if (__instance.activePower.OriginItem == null
+                || __instance.activePower is not RulesetEffectPowerWithAdvancement power)
+            {
+                return true;
+            }
+
+            var usableDevice = power.OriginItem;
+
+            foreach (RulesetDeviceFunction usableFunction in usableDevice.UsableFunctions)
+            {
+                var functionDescription = usableFunction.DeviceFunctionDescription;
+                if (functionDescription.Type != DeviceFunctionDescription.FunctionType.Power
+                    || functionDescription.FeatureDefinitionPower != power.PowerDefinition)
+                {
+                    continue;
+                }
+
+                __instance.ActingCharacter.RulesetCharacter
+                    .UseDeviceFunction(usableDevice, usableFunction, power.ExtraCharges);
+                break;
+            }
+
+            ServiceRepository.GetService<IGameLocationActionService>()
+                .ItemUsed?.Invoke(usableDevice.ItemDefinition.Name);
+
+            return false;
+        }
+    }
 }
