76f7ec9a707c9f936f6660d13607b7fd80d221d0
Paulo Monteiro <pauloesquilo@gmail.com>
2022-10-03 09:38:15 -0700

    Bring back original filter list on DB Helper auto gen blueprints
    - stuff like Tutorial, etc should not be in DLL

--- SolastaUnfinishedBusiness/Api/DatabaseHelperPartial.cs
+++ SolastaUnfinishedBusiness/Api/DatabaseHelperPartial.cs
@@ -1,5 +1,6 @@
 ﻿using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Api.Diagnostics;
+using TA.AI;
 
 namespace SolastaUnfinishedBusiness.Api;
 
@@ -37,5 +38,58 @@ internal static partial class DatabaseHelper
         definition = null;
 
         return false;
+    }
+    
+    internal static class CampaignDefinitions
+    {
+        internal static CampaignDefinition UserCampaign { get; } = GetDefinition<CampaignDefinition>("UserCampaign");
+    }
+    
+    internal static class DecisionPackageDefinitions
+    {
+        internal static DecisionPackageDefinition DefaultMeleeWithBackupRangeDecisions { get; } = GetDefinition<DecisionPackageDefinition>("DefaultMeleeWithBackupRangeDecisions");
+        internal static DecisionPackageDefinition DefaultSupportCasterWithBackupAttacksDecisions { get; } = GetDefinition<DecisionPackageDefinition>("DefaultSupportCasterWithBackupAttacksDecisions");
+        internal static DecisionPackageDefinition IdleGuard_Default { get; } = GetDefinition<DecisionPackageDefinition>("IdleGuard_Default");
+    }
+    internal static class FactionDefinitions
+    {
+        internal static FactionDefinition HostileMonsters { get; } = GetDefinition<FactionDefinition>("HostileMonsters");
+    }
+
+    internal static class FormationDefinitions
+    {
+        internal static FormationDefinition Squad4 { get; } = GetDefinition<FormationDefinition>("Squad4");
+        internal static FormationDefinition SingleCreature { get; } = GetDefinition<FormationDefinition>("SingleCreature");
+    }
+
+    internal static class GadgetBlueprints
+    {
+        internal static GadgetBlueprint TeleporterIndividual { get; } = GetDefinition<GadgetBlueprint>("TeleporterIndividual");
+        internal static GadgetBlueprint TeleporterParty { get; } = GetDefinition<GadgetBlueprint>("TeleporterParty");
+        internal static GadgetBlueprint VirtualExit { get; } = GetDefinition<GadgetBlueprint>("VirtualExit");
+        internal static GadgetBlueprint VirtualExitMultiple { get; } = GetDefinition<GadgetBlueprint>("VirtualExitMultiple");
+        internal static GadgetBlueprint Exit { get; } = GetDefinition<GadgetBlueprint>("Exit");
+        internal static GadgetBlueprint ExitMultiple { get; } = GetDefinition<GadgetBlueprint>("ExitMultiple");
+    }
+
+    internal static class GadgetDefinitions
+    {
+        internal static GadgetDefinition Activator { get; } = GetDefinition<GadgetDefinition>("Activator");
+    }
+
+    internal static class LootPackDefinitions
+    {
+        internal static LootPackDefinition Pickpocket_generic_loot_LowMoney { get; } = GetDefinition<LootPackDefinition>("Pickpocket_generic_loot_LowMoney");
+        internal static LootPackDefinition Pickpocket_generic_loot_MedMoney { get; } = GetDefinition<LootPackDefinition>("Pickpocket_generic_loot_MedMoney");
+    }
+
+    internal static class MonsterAttackDefinitions
+    {
+        internal static MonsterAttackDefinition Attack_Generic_Guard_Longsword { get; } = GetDefinition<MonsterAttackDefinition>("Attack_Generic_Guard_Longsword");
+    }
+    
+    internal static class ReactionDefinitions
+    {
+        internal static ReactionDefinition OpportunityAttack { get; } = GetDefinition<ReactionDefinition>("OpportunityAttack");
     }
 }
