be53fc3bcda9af4a17a2054eeed17b3ab9128712
Ender Wiggin <andrii.gook@gmail.com>
2022-10-05 10:32:50 +0300

    fixed collection modified while iterating exception

--- SolastaUnfinishedBusiness/Models/SpellsContext.cs
+++ SolastaUnfinishedBusiness/Models/SpellsContext.cs
@@ -170,7 +170,7 @@ internal static class SpellsContext
             var spellListName = kvp.Key.Name;
 
             foreach (var name in Main.Settings.SpellListSpellEnabled[spellListName]
-                         .Where(name => Spells.All(x => x.Name != name)))
+                         .Where(name => Spells.All(x => x.Name != name)).ToList())
             {
                 Main.Settings.SpellListSpellEnabled[spellListName].Remove(name);
             }
