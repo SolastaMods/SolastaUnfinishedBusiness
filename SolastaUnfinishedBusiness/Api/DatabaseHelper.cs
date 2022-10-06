50894912c7ef6d62bb3ed445838c6649a8300797
Paulo Monteiro <pauloesquilo@gmail.com>
2022-10-06 08:59:25 -0700

    Fix dup

--- SolastaUnfinishedBusiness/Api/DatabaseHelper.cs
+++ SolastaUnfinishedBusiness/Api/DatabaseHelper.cs
@@ -8086,9 +8086,6 @@ internal static partial class DatabaseHelper
             GetDefinition<FightingStyleDefinition>("TwoWeapon");
 
     }
-    internal static class GadgetBlueprints
-    {
-    }
     internal static class InvocationDefinitions
     {
         internal static InvocationDefinition AgonizingBlast { get; } =
