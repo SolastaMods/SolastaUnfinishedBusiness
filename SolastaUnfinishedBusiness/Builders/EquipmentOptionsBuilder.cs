8d88438782c017b34c126777b443eb3262c23656
Ender Wiggin <andrii.gook@gmail.com>
2022-10-07 11:55:38 +0300

    properly set default choice in HeroEquipmentOption

--- SolastaUnfinishedBusiness/Builders/EquipmentOptionsBuilder.cs
+++ SolastaUnfinishedBusiness/Builders/EquipmentOptionsBuilder.cs
@@ -6,7 +6,7 @@ internal static class EquipmentOptionsBuilder
 {
     internal static HeroEquipmentOption Option(ItemDefinition itemType, string optionType, int number)
     {
-        var itemOption = new HeroEquipmentOption { number = number, optionType = optionType, itemReference = itemType };
+        var itemOption = new HeroEquipmentOption { number = number, optionType = optionType, itemReference = itemType, defaultChoice = itemType.Name};
         return itemOption;
     }
 
