b09526be1a7f11250db472d44cbab769fdee4531
Ender Wiggin <andrii.gook@gmail.com>
2022-10-10 12:22:08 +0300

    added sub-feature and patch that would allow source character of a condition to use power to augment failed save roll

---
+++ SolastaUnfinishedBusiness/Api/Extensions/CharacterActionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using static RuleDefinitions;
+
+namespace SolastaUnfinishedBusiness.Api.Extensions;
+
+internal static class CharacterActionExtensions
+{
+    internal static int GetSaveDC(this CharacterAction action)
+    {
+        var actionParams = action.actionParams;
+        var magicEffect = actionParams.RulesetEffect;
+        var saveDC = 0;
+
+        if (magicEffect == null)
+        {
+            var attack = actionParams.AttackMode;
+            var effect = attack.effectDescription;
+            if (effect.DifficultyClassComputation == EffectDifficultyClassComputation.FixedValue)
+            {
+                saveDC = effect.FixedSavingThrowDifficultyClass;
+            }
+            else if (effect.DifficultyClassComputation == EffectDifficultyClassComputation.SpellCastingFeature)
+            {
+                var repertoire = action.ActingCharacter.RulesetCharacter?.GetClassSpellRepertoire();
+                if (repertoire != null)
+                {
+                    saveDC = repertoire.SaveDC;
+                }
+            }
+        }
+        else
+        {
+            saveDC = magicEffect.SaveDC;
+        }
+
+        return saveDC;
+    }
+}
---
+++ SolastaUnfinishedBusiness/CustomBehaviors/ConditionSourceCanUsePowerToImproveFailedSaveRoll.cs
@@ -0,0 +1,41 @@
+﻿namespace SolastaUnfinishedBusiness.CustomBehaviors;
+
+internal class ConditionSourceCanUsePowerToImproveFailedSaveRoll
+{
+    public ConditionSourceCanUsePowerToImproveFailedSaveRoll(FeatureDefinitionPower power, string reactionName)
+    {
+        Power = power;
+        ReactionName = reactionName;
+    }
+
+    internal FeatureDefinitionPower Power { get; }
+    internal string ReactionName { get; }
+
+    internal virtual bool ShouldTrigger(
+        CharacterAction action,
+        GameLocationCharacter attacker,
+        GameLocationCharacter defender,
+        RulesetCharacter helper,
+        ActionModifier saveModifier,
+        bool hasHitVisual,
+        bool hasBorrowedLuck,
+        RuleDefinitions.RollOutcome saveOutcome,
+        int saveOutcomeDelta)
+    {
+        return true;
+    }
+
+    internal virtual bool TryModifyRoll(
+        CharacterAction action,
+        GameLocationCharacter attacker,
+        GameLocationCharacter defender,
+        RulesetCharacter helper,
+        ActionModifier saveModifier,
+        bool hasHitVisual,
+        bool hasBorrowedLuck,
+        ref RuleDefinitions.RollOutcome saveOutcome,
+        ref int saveOutcomeDelta)
+    {
+        return true;
+    }
+}
