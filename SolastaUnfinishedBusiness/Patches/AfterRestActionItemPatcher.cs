bddea909377900741709f74a4ca135c07997f261
Ender Wiggin <andrii.gook@gmail.com>
2022-10-18 23:12:21 +0300

    make ret activity button with custom power functor use power tooltip class

--- SolastaUnfinishedBusiness/Patches/AfterRestActionItemPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/AfterRestActionItemPatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using HarmonyLib;
 using SolastaUnfinishedBusiness.Models;
 
@@ -14,6 +15,28 @@ public static class AfterRestActionItemPatcher
         {
             //PATCH: replaces callback execution for bundled powers to show sub-power selection
             return PowersBundleContext.ExecuteAfterRestCb(__instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(AfterRestActionItem), "Bind")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    public static class Bind_Patch
+    {
+        public static void Postfix(AfterRestActionItem __instance)
+        {
+            var activity = __instance.RestActivityDefinition;
+            var hero = __instance.Hero;
+
+            if (activity.functor != PowersBundleContext.UseCustomRestPowerFunctorName) { return; }
+
+            var power = hero.UsablePowers.FirstOrDefault(usablePower =>
+                usablePower.PowerDefinition.Name == activity.StringParameter);
+
+            if (power == null) { return; }
+
+            ServiceRepository.GetService<IGuiWrapperService>()
+                .GetGuiPowerDefinition(power.PowerDefinition.Name)
+                .SetupTooltip(__instance.GuiTooltip, hero);
         }
     }
 }
