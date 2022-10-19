ba3191a718dbeb1e15abb3ebe0a8be2031e0348a
Paulo Monteiro <pauloesquilo@gmail.com>
2022-10-18 18:31:10 -0700

    Fix CreateOfflineRoomIfNeeded patch

--- SolastaUnfinishedBusiness/Patches/NetworkingManagerPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/NetworkingManagerPatcher.cs
@@ -18,21 +18,21 @@ public static class NetworkingManagerPatcher
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     public static class CreateOfflineRoomIfNeeded_Patch
     {
-        private static bool CreateRoom(string name, RoomOptions roomOptions, TypedLobby typedLobby)
+        private static bool CreateRoom(string name, RoomOptions roomOptions, TypedLobby typedLobby, string[] expectedUsers)
         {
             //PATCH: allows up to 6 players to join the game if there are enough heroes available (PARTYSIZE)
             var maxValue = Math.Max(DungeonMakerContext.GamePartySize, Main.Settings.OverridePartySize);
 
             roomOptions.MaxPlayers = (byte)maxValue;
 
-            return PhotonNetwork.CreateRoom(name, roomOptions, typedLobby);
+            return PhotonNetwork.CreateRoom(name, roomOptions, typedLobby, expectedUsers);
         }
 
         public static IEnumerable<CodeInstruction> Transpiler([NotNull] IEnumerable<CodeInstruction> instructions)
         {
             var createRoomMethod =
                 typeof(PhotonNetwork).GetMethod("CreateRoom", BindingFlags.Public | BindingFlags.Static);
-            var myCreateRoomMethod = new Func<string, RoomOptions, TypedLobby, bool>(CreateRoom).Method;
+            var myCreateRoomMethod = new Func<string, RoomOptions, TypedLobby, string[], bool>(CreateRoom).Method;
 
             foreach (var instruction in instructions)
             {
