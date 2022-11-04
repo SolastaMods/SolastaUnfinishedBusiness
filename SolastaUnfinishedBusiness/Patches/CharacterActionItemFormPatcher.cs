fb6d3cb1bee8c6360b78ff136469c2b3d4200c5a
Ender Wiggin <andrii.gook@gmail.com>
2022-11-04 10:37:39 +0200

    fixed ammunition and dynamic properties of the main hand weapon showing over icon of the attack action of the built-in weapons of the Armorer

---
+++ SolastaUnfinishedBusiness/Patches/CharacterActionItemFormPatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+public static class CharacterActionItemFormPatcher
+{
+    
+    [HarmonyPatch(typeof(CharacterActionItemForm), "Bind")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    public static class Bind_Patch
+    {
+        public static void Postfix(CharacterActionItemForm __instance)
+        {
+            //PATCH: Get dynamic properties from forced attack
+            if (__instance.guiCharacterAction.forcedAttackMode == null) { return;}
+            
+            __instance.dynamicItemPropertiesEnumerator.Unbind();
+            __instance.dynamicItemPropertiesEnumerator.Bind(__instance.guiCharacterAction.forcedAttackMode.sourceObject as RulesetItem);
+        }
+    }
+}
---
+++ SolastaUnfinishedBusiness/Patches/GuiCharacterActionPatcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+public static class GuiCharacterActionPatcher
+{
+    [HarmonyPatch(typeof(GuiCharacterAction), "LimitedUses", MethodType.Getter)]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    public static class LimitedUses_Getter_Patch
+    {
+        public static void Postfix(GuiCharacterAction __instance, ref int __result)
+        {
+            //PATCH: Get remaining attack uses (ammunition) from forced attack mode
+            if (__instance.forcedAttackMode == null) { return; }
+            
+            __result = __instance.ActingCharacter.RulesetCharacter.GetRemainingAttackUses(__instance.forcedAttackMode);
+        }
+    }
+}
