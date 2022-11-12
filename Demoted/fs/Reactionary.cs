3c65d8b2119a9725d5a35f58d55c1c5d337b519d
Paulo Monteiro <pauloesquilo@gmail.com>
2022-11-12 14:41:30 -0800

    update diags

---
+++ Demoted/fs/Reactionary.cs
@@ -0,0 +1,57 @@
+﻿#if false
+// FightingStyle/&ReactionaryDescription=You gain a +PB/2 (round up) to hit with attacks of opportunity.
+// FightingStyle/&ReactionaryTitle=Reactionary
+using System.Collections.Generic;
+using SolastaUnfinishedBusiness.Builders;
+using SolastaUnfinishedBusiness.Builders.Features;
+using SolastaUnfinishedBusiness.CustomInterfaces;
+using static SolastaUnfinishedBusiness.Api.DatabaseHelper.CharacterSubclassDefinitions;
+using static SolastaUnfinishedBusiness.Api.DatabaseHelper.FeatureDefinitionFightingStyleChoices;
+
+namespace SolastaUnfinishedBusiness.FightingStyles;
+
+internal sealed class Reactionary : AbstractFightingStyle
+{
+    private const string ReactionaryName = "Reactionary";
+
+    internal override FightingStyleDefinition FightingStyle { get; } = FightingStyleBuilder
+        .Create(ReactionaryName)
+        .SetGuiPresentation(Category.FightingStyle, PathBerserker)
+        .SetFeatures(
+            FeatureDefinitionBuilder
+                .Create("OnComputeAttackModifierFightingStyleReactionary")
+                .SetGuiPresentationNoContent(true)
+                .SetCustomSubFeatures(new OnComputeAttackModifierFightingStyleReactionary())
+                .AddToDB())
+        .AddToDB();
+
+    internal override List<FeatureDefinitionFightingStyleChoice> FightingStyleChoice => new()
+    {
+        FightingStyleChampionAdditional, FightingStyleFighter, FightingStylePaladin, FightingStyleRanger
+    };
+
+    private sealed class OnComputeAttackModifierFightingStyleReactionary : IOnComputeAttackModifier
+    {
+        public void ComputeAttackModifier(
+            RulesetCharacter myself,
+            RulesetCharacter defender,
+            RulesetAttackMode attackMode,
+            ref ActionModifier attackModifier)
+        {
+            // grant +PB if attacker is performing an opportunity attack
+            if (attackMode == null || defender == null ||
+                attackMode.actionType != ActionDefinitions.ActionType.Reaction)
+            {
+                return;
+            }
+
+            var proficiencyBonus =
+                (myself.GetAttribute(AttributeDefinitions.ProficiencyBonus).CurrentValue + 1) / 2;
+
+            attackModifier.attackRollModifier += proficiencyBonus;
+            attackModifier.attackToHitTrends.Add(new RuleDefinitions.TrendInfo(
+                proficiencyBonus, RuleDefinitions.FeatureSourceType.FightingStyle, ReactionaryName, myself));
+        }
+    }
+}
+#endif
