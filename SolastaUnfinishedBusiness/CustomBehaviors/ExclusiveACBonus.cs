cfd0ad54a9f30cc0212886fb563a5bfeb5c870b5
Paulo Monteiro <pauloesquilo@gmail.com>
2022-11-13 14:48:31 -0800

    remove ExclusiveACBonus leftover

--- SolastaUnfinishedBusiness/CustomBehaviors/ExclusiveACBonus.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SolastaUnfinishedBusiness.CustomBehaviors;
-
-internal sealed class ExclusiveAcBonus
-{
-    //AC formula is `Value`
-    internal const string TagNaturalArmor = "AC_NATURAL_ARMOR";
-}
