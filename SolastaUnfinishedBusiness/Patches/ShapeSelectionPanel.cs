b350a0ecc6c11e592b36c16895637b6aeace1caf
Paulo Monteiro <pauloesquilo@gmail.com>
2022-11-23 04:51:38 -0800

    shrink the shape selection panel if too many shapes offered

---
+++ SolastaUnfinishedBusiness/Patches/ShapeSelectionPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using UnityEngine;
+using static SolastaUnfinishedBusiness.Api.DatabaseHelper.CharacterClassDefinitions;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+public static class ShapeSelectionPanelPatcher
+{
+    [HarmonyPatch(typeof(ShapeSelectionPanel), "OnEndShow")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    public static class OnEndShow_Patch
+    {
+        public static void Postfix(ShapeSelectionPanel __instance)
+        {
+            //PATCH: shrink the shape selection panel if too many shapes offered
+            var rect = __instance.GetComponent<RectTransform>();
+            var shapesCount = __instance.shapeOptionTable.childCount;
+
+            rect.localScale = shapesCount > 10 ? new Vector3(0.9f, 0.9f, 0.9f) : Vector3.one;
+        }
+    }
+}
