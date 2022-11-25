0e05324faa5e5fcea5053a56764c3341b27b7bd9
Paulo Monteiro <pauloesquilo@gmail.com>
2022-11-24 21:07:36 -0800

    change to class levels 2, 6, 10 and 14

--- SolastaUnfinishedBusiness/Models/CharacterContext.cs
+++ SolastaUnfinishedBusiness/Models/CharacterContext.cs
@@ -610,7 +610,7 @@ internal static class CharacterContext
 
     internal static void SwitchFighterArmamentAdroitness()
     {
-        var levels = new[] { 4, 6, 8, 12, 14, 16, 19 };
+        var levels = new[] { 2, 6, 10, 14 };
 
         if (Main.Settings.EnableFighterArmamentAdroitness)
         {
