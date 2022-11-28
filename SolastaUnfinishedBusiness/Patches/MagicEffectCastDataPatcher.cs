6517873e52031ed504069cd971da75bdd85e27ed
agook <andrii.gook@dgngames.com>
2022-11-28 17:49:25 +0200

    added patch that allows any magic effect be cast with quick mode animations

---
+++ SolastaUnfinishedBusiness/Patches/MagicEffectCastDataPatcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+public static class MagicEffectCastDataPatcher
+{
+    [HarmonyPatch(typeof(ActionDefinitions.MagicEffectCastData), "IsQuickSpell", MethodType.Getter)]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    public static class IsQuickSpell_Patch
+    {
+        public static void Postfix(ActionDefinitions.MagicEffectCastData __instance, ref bool __result)
+        {
+            if (__instance.EffectDescription.SpeedParameter < 0)
+            {
+                __result = true;
+            }
+        }
+    }
+}
