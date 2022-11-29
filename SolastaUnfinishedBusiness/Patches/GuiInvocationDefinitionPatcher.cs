6fa678f25bcb4cd48170f40e3c7b8283d519da3b
Paulo Monteiro <pauloesquilo@gmail.com>
2022-11-28 21:57:36 -0800

    fix invocations offering under MC scenarios

---
+++ SolastaUnfinishedBusiness/Patches/GuiInvocationDefinitionPatcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Emit;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaUnfinishedBusiness.Api;
+using SolastaUnfinishedBusiness.Api.Extensions;
+using SolastaUnfinishedBusiness.Api.Helpers;
+using SolastaUnfinishedBusiness.CustomDefinitions;
+using SolastaUnfinishedBusiness.CustomInterfaces;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+public static class GuiInvocationDefinitionPatcher
+{
+    //NOTE: There is a typo on official method name
+    [HarmonyPatch(typeof(GuiInvocationDefinition), "IsInvocationMacthingPrerequisites")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    public static class IsInvocationMacthingPrerequisites_Patch
+    {
+        //PATCH: return class level instead of char level on invocations selection (MULTICLASS)
+        private static int TryGetAttributeValue(
+            RulesetCharacterHero hero,
+            string attributeName)
+        {
+            hero.ClassesAndLevels.TryGetValue(DatabaseHelper.CharacterClassDefinitions.Warlock, out var levels);
+
+            return levels;
+        }
+
+        [NotNull]
+        public static IEnumerable<CodeInstruction> Transpiler([NotNull] IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions
+                .ReplaceCalls(typeof(RulesetEntity).GetMethod("TryGetAttributeValue"),
+                    "GuiInvocationDefinition.IsInvocationMacthingPrerequisites",
+                    new CodeInstruction(OpCodes.Call,
+                        new Func<RulesetCharacterHero, string, int>(TryGetAttributeValue).Method));
+        }
+    }
+}
