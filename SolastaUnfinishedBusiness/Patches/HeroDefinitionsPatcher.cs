107f5b0bbf2f3b5a584d2974ca3b96f510b91830
Paulo Monteiro <pauloesquilo@gmail.com>
2022-12-11 17:40:38 -0800

    fix Level 20 allowing level up past 12

--- SolastaUnfinishedBusiness/Patches/HeroDefinitionsPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/HeroDefinitionsPatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
+using UnityEngine;
 using static SolastaUnfinishedBusiness.Models.Level20Context;
 
 namespace SolastaUnfinishedBusiness.Patches;
@@ -10,9 +11,9 @@ public static class HeroDefinitionsPatcher
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     public static class MaxHeroExperience_Patch
     {
+        //PATCH: overrides the max experience allowed under Level 20 scenarios
         public static bool Prefix(ref int __result)
         {
-            //PATCH: overrides the max experience allowed under Level 20 scenarios
             if (!Main.Settings.EnableLevel20)
             {
                 return true;
