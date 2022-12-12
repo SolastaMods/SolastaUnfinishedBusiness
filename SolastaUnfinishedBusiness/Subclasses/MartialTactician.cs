bbb5435aa179980b4d40622179ade278410a0cad
Paulo Monteiro <pauloesquilo@gmail.com>
2022-12-11 23:55:20 -0800

    replace damageRollReduction and prefer same pattern as Heavy Armor Master and Dance of Defense

--- SolastaUnfinishedBusiness/Subclasses/MartialTactician.cs
+++ SolastaUnfinishedBusiness/Subclasses/MartialTactician.cs
@@ -1484,17 +1484,24 @@ internal sealed class MartialTactician : AbstractSubclass
             var dieRoll = RollDie(dieType, AdvantageType.None, out _, out _);
 
             var hitTrends = attackModifier.AttacktoHitTrends;
-            if (hitTrends != null)
+
+            hitTrends?.Add(new TrendInfo(dieRoll, FeatureSourceType.Power, pool.Name, null)
             {
-                hitTrends.Add(new TrendInfo(dieRoll, FeatureSourceType.Power, pool.Name, null)
-                {
-                    dieType = dieType, dieFlag = TrendInfoDieFlag.None
-                });
-            }
+                dieType = dieType, dieFlag = TrendInfoDieFlag.None
+            });
 
             var pb = 2 * character.TryGetAttributeValue(AttributeDefinitions.ProficiencyBonus);
             var reduction = dieRoll + pb;
-            attackModifier.damageRollReduction += reduction;
+
+            // attackModifier.damageRollReduction += reduction;
+            var damage = attackMode.EffectDescription?.FindFirstDamageForm();
+            var trendInfo = new TrendInfo(reduction, FeatureSourceType.CharacterFeature, feature.FormatTitle(), feature);
+
+            if (damage != null)
+            {
+                damage.bonusDamage -= reduction;
+                damage.DamageBonusTrends.Add(trendInfo);
+            }
 
             character.ShowDieRoll(dieType, dieRoll,
                 title: feature.GuiPresentation.Title,
