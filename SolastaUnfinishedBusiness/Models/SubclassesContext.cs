d6b8a85b6cdaa69a43036adbe88294a392c4bfff
ThyWolf <pauloesquilo@gmail.com>
2022-12-21 19:43:55 -0800

    update diagnostics

--- SolastaUnfinishedBusiness/Models/SubclassesContext.cs
+++ SolastaUnfinishedBusiness/Models/SubclassesContext.cs
@@ -24,7 +24,7 @@ internal static class SubclassesContext
 
         // Bard
         LoadSubclass(new CollegeOfGuts());
-        LoadSubclass(new CollegeOfHarlequin(), true);
+        LoadSubclass(new CollegeOfHarlequin());
         LoadSubclass(new CollegeOfLife());
         LoadSubclass(new CollegeOfWarDancer());
 
@@ -37,10 +37,10 @@ internal static class SubclassesContext
         LoadSubclass(new MartialMarshal());
         LoadSubclass(new MartialRoyalKnight());
         LoadSubclass(new MartialSpellShield());
-        LoadSubclass(new MartialTactician(), true);
+        LoadSubclass(new MartialTactician());
 
         // Paladin
-        LoadSubclass(new OathOfHatred(), true);
+        LoadSubclass(new OathOfHatred());
 
         // Ranger
         LoadSubclass(new RangerArcanist());
