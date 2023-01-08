68a20f01d34e46c86dc7d5376ebfc41837ff3c1b
ThyWolf <pauloesquilo@gmail.com>
2023-01-07 18:35:24 -0800

    fix MC Warlock to use a correct slot level when using spell slots

---
+++ SolastaUnfinishedBusiness/Patches/SpellsByLevelBoxPatcher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Emit;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaUnfinishedBusiness.Api.Helpers;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+public class SpellsByLevelBoxPatcher
+{
+    [HarmonyPatch(typeof(SpellsByLevelBox), "OnActivateStandardBox")]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    public static class OnActivateStandardBox_Patch
+    {
+        private static bool UniqueLevelSlots()
+        {
+            return false;
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler([NotNull] IEnumerable<CodeInstruction> instructions)
+        {
+            //PATCH: ensures multiclass warlock will use the correct slot level when casting spells using spell slots (MULTICLASS)
+            var uniqueLevelSlotsMethod = typeof(FeatureDefinitionCastSpell).GetMethod("get_UniqueLevelSlots");
+            var myUniqueLevelSlotsMethod = new Func<bool>(UniqueLevelSlots).Method;
+
+            return instructions.ReplaceCalls(uniqueLevelSlotsMethod, "SpellsByLevelBox.OnActivateStandardBox",
+                new CodeInstruction(OpCodes.Pop),
+                new CodeInstruction(OpCodes.Call, myUniqueLevelSlotsMethod));
+        }
+    }
+}
