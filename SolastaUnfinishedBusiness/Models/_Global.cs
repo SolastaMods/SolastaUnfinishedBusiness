eadba34d6b4563a9bea893ea00c6612556ae2825
ThyWolf <pauloesquilo@gmail.com>
2023-01-31 23:29:54 -0800

    add fell handed and blade mastery feats

--- SolastaUnfinishedBusiness/Models/_Global.cs
+++ SolastaUnfinishedBusiness/Models/_Global.cs
@@ -61,6 +61,11 @@ internal static class Global
     // special case for our powers that add a turn off stop provider
     internal static HashSet<FeatureDefinitionPower> PowersThatIgnoreInterruptions { get; } = new();
 
+    // keep a tab on last rolled dices
+    internal static int FirstRoll { get; set; }
+
+    internal static int SecondRoll { get; set; }
+
     // restate globals on every new action
     internal static void ActionStarted([NotNull] CharacterAction characterAction)
     {
