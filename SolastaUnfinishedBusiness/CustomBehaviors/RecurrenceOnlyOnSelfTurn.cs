8923ccf7623e8e12c38636510d107dd9f19545c8
Ender Wiggin <andrii.gook@gmail.com>
2023-02-04 15:48:33 +0200

    added sub-feature to mark re-occuring effects that happen on turn start to happen only on source character's turn

---
+++ SolastaUnfinishedBusiness/CustomBehaviors/RecurrenceOnlyOnSelfTurn.cs
@@ -0,0 +1,10 @@
+﻿namespace SolastaUnfinishedBusiness.CustomBehaviors;
+
+public class RecurrenceOnlyOnSelfTurn
+{
+    public static RecurrenceOnlyOnSelfTurn Mark { get; } = new();
+
+    private RecurrenceOnlyOnSelfTurn()
+    {
+    }
+}
