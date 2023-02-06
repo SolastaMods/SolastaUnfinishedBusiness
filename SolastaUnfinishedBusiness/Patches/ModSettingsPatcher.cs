10b2fbe801864757af469baa8bc0e5d73f417af3
ThyWolf <pauloesquilo@gmail.com>
2023-02-05 21:00:06 -0800

    added Gameplay > Tools > Settings to allow export/import settings files

---
+++ SolastaUnfinishedBusiness/Patches/ModSettingsPatcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrains.Annotations;
+using UnityModManagerNet;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+[UsedImplicitly]
+public static class ModSettingsPatcher
+{
+    //PATCH: supports exports / imports mod settings
+    [HarmonyPatch(typeof(UnityModManager.ModSettings), nameof(UnityModManager.ModSettings.GetPath))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class GetPath_Patch
+    {
+        [UsedImplicitly]
+        public static bool Prefix(ref string __result)
+        {
+            if (Main.SettingsFilename == String.Empty)
+            {
+                return true;
+            }
+
+            __result = Main.SettingsFilename;
+
+            return false;
+        }
+    }
+}
