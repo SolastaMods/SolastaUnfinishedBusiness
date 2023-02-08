c3ad2d861b13ae095a885168cb76fdf8a08a17bd
ThyWolf <pauloesquilo@gmail.com>
2023-02-08 13:12:48 -0800

    bump 1.4.33.31

--- SolastaUnfinishedBusiness/CustomBehaviors/ModifyAttackModeForWeapon.cs
+++ SolastaUnfinishedBusiness/CustomBehaviors/ModifyAttackModeForWeapon.cs
@@ -156,7 +156,7 @@ internal sealed class UpgradeWeaponDice : ModifyAttackModeForWeaponBase
 
         var newDamage = RuleDefinitions.DieAverage(newDie) * newNumber;
         var oldDamage = RuleDefinitions.DieAverage(damage.DieType) * damage.DiceNumber;
-        
+
         if (newDamage > oldDamage)
         {
             damage.DieType = newDie;
@@ -165,7 +165,7 @@ internal sealed class UpgradeWeaponDice : ModifyAttackModeForWeaponBase
 
         newDamage = RuleDefinitions.DieAverage(newVersatileDie) * newNumber;
         oldDamage = RuleDefinitions.DieAverage(damage.VersatileDieType) * damage.DiceNumber;
-        
+
         if (newDamage > oldDamage)
         {
             damage.VersatileDieType = newVersatileDie;
