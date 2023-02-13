18d878f914cc2ff23f683c9f7e68e90c2a6c0a54
Ender Wiggin <andrii.gook@gmail.com>
2023-02-13 17:14:14 +0200

    WIP on making Altruistic metamagic consist of 2 options

--- SolastaUnfinishedBusiness/Metamagic/MetamagicBuilders.cs
+++ SolastaUnfinishedBusiness/Metamagic/MetamagicBuilders.cs
@@ -1,5 +1,6 @@
 using System;
 using SolastaUnfinishedBusiness.Builders;
+using SolastaUnfinishedBusiness.CustomBehaviors;
 using SolastaUnfinishedBusiness.CustomInterfaces;
 using static RuleDefinitions;
 
@@ -11,15 +12,29 @@ internal static class MetamagicBuilders
 
     internal static MetamagicOptionDefinition BuildMetamagicAltruisticSpell()
     {
+        var altruisticAlly = MetamagicOptionDefinitionBuilder
+            .Create($"{MetamagicAltruistic}Ally")
+            .SetGuiPresentation(Category.Feature, hidden: true)
+            .SetCost()
+            .SetCustomSubFeatures(new MetamagicApplicationValidatorMetamagicAltruisticAlly())
+            .AddToDB();
+
+        var altruisticSelf = MetamagicOptionDefinitionBuilder
+            .Create($"{MetamagicAltruistic}Self")
+            .SetGuiPresentation(Category.Feature, hidden: true)
+            .SetCost(sorceryPointsCost: 3)
+            .SetCustomSubFeatures(new MetamagicApplicationValidatorMetamagicAltruisticSelf())
+            .AddToDB();
+
         return MetamagicOptionDefinitionBuilder
             .Create(MetamagicAltruistic)
             .SetGuiPresentation(Category.Feature)
-            .SetCost()
-            .SetCustomSubFeatures(new MetamagicApplicationValidatorMetamagicAltruisticSpell())
+            .SetCost(MetamagicCostMethod.SpellLevel)
+            .SetCustomSubFeatures(new ReplaceMetamagicOption(altruisticAlly, altruisticSelf))
             .AddToDB();
     }
 
-    private sealed class MetamagicApplicationValidatorMetamagicAltruisticSpell : IMetamagicApplicationValidator,
+    private sealed class MetamagicApplicationValidatorMetamagicAltruisticAlly : IMetamagicApplicationValidator,
         IModifyMagicEffect
     {
         public bool IsMetamagicOptionValid(
@@ -50,6 +65,42 @@ internal static class MetamagicBuilders
             effect.targetType = TargetType.IndividualsUnique;
             effect.targetParameter = 1;
             effect.targetExcludeCaster = true;
+
+            return effect;
+        }
+    }
+
+    private sealed class MetamagicApplicationValidatorMetamagicAltruisticSelf : IMetamagicApplicationValidator,
+        IModifyMagicEffect
+    {
+        public bool IsMetamagicOptionValid(
+            RulesetCharacter caster,
+            RulesetEffectSpell rulesetEffectSpell,
+            MetamagicOptionDefinition metamagicOption,
+            ref string failure)
+        {
+            var effect = rulesetEffectSpell.EffectDescription;
+
+            if (effect.rangeType != RangeType.Self || effect.targetType != TargetType.Self)
+            {
+                failure = "Failure/&FailureFlagSpellRangeMustBeSelf";
+
+                return false;
+            }
+
+            failure = String.Empty;
+
+            return true;
+        }
+
+        public EffectDescription ModifyEffect(BaseDefinition definition, EffectDescription effect,
+            RulesetCharacter character)
+        {
+            effect.rangeType = RangeType.Distance;
+            effect.rangeParameter = 6;
+            effect.targetType = TargetType.IndividualsUnique;
+            effect.targetParameter = 1;
+            effect.inviteOptionalAlly = true;
 
             return effect;
         }
