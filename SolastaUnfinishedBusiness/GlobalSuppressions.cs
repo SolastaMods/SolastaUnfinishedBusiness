4557f7e36fc80e0e79289bd72a275b34d4a2b039
ThyWolf <pauloesquilo@gmail.com>
2023-02-19 20:09:19 -0800

    minor tweaks and diagnostics update

--- SolastaUnfinishedBusiness/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Roslynator", "ROS0003")]
-
-[assembly: SuppressMessage("Major Code Smell",
-    "S3011:Reflection should not be used to increase accessibility of classes, methods, or fields",
-    Justification = "Reflection is required", Scope = "namespaceanddescendants",
-    Target = "~N:SolastaUnfinishedBusiness")]
