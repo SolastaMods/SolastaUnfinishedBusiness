99e8e2ef49b24ed7834efda031150ec06a8dae27
ThyWolf <pauloesquilo@gmail.com>
2023-02-21 13:37:33 -0800

    expand `Potent Spellcaster` to any cantrip caster class except Warlocks

--- SolastaUnfinishedBusiness/CustomBehaviors/ValidatorsFeat.cs
+++ SolastaUnfinishedBusiness/CustomBehaviors/ValidatorsFeat.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Api.Extensions;
+using SolastaUnfinishedBusiness.Classes.Inventor;
 using SolastaUnfinishedBusiness.Races;
 using static SolastaUnfinishedBusiness.Api.DatabaseHelper.CharacterClassDefinitions;
 using static SolastaUnfinishedBusiness.Api.DatabaseHelper.CharacterRaceDefinitions;
@@ -14,17 +15,26 @@ internal static class ValidatorsFeat
     // validation routines for FeatDefinitionWithPrerequisites
     //
 
+    internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsBardLevel4 =
+        ValidateIsClass(Bard.FormatTitle(), 4, Bard);
+
     internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsClericLevel4 =
         ValidateIsClass(Cleric.FormatTitle(), 4, Cleric);
 
     internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsDruidLevel4 =
         ValidateIsClass(Druid.FormatTitle(), 4, Druid);
 
+    internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsInventorLevel4 =
+        ValidateIsClass(InventorClass.Class.FormatTitle(), 4, InventorClass.Class);
+
     internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsPaladinLevel1 =
         ValidateIsClass(Paladin.FormatTitle(), 1, Paladin);
 
     internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsRogueLevel4 =
         ValidateIsClass(Rogue.FormatTitle(), 4, Rogue);
+
+    internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsSorcererLevel4 =
+        ValidateIsClass(Sorcerer.FormatTitle(), 4, Sorcerer);
 
     internal static readonly Func<FeatDefinition, RulesetCharacterHero, (bool result, string output)> IsWizardLevel4 =
         ValidateIsClass(Wizard.FormatTitle(), 4, Wizard);
