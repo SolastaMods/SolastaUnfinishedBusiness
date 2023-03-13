591ee2f60b69e094a8ae9e28cb866a55390bbd05
ThyWolf <pauloesquilo@gmail.com>
2023-03-12 19:17:09 -0700

    add `HasQuarterstaff` validator

--- SolastaUnfinishedBusiness/CustomBehaviors/ValidatorsCharacter.cs
+++ SolastaUnfinishedBusiness/CustomBehaviors/ValidatorsCharacter.cs
@@ -41,6 +41,10 @@ internal static class ValidatorsCharacter
     internal static readonly IsCharacterValidHandler HasPolearm = character =>
         ValidatorsWeapon.IsPolearm(character.GetMainWeapon()) ||
         ValidatorsWeapon.IsPolearm(character.GetOffhandWeapon());
+    
+    internal static readonly IsCharacterValidHandler HasQuarterstaff = character =>
+        ValidatorsWeapon.IsWeaponType(character.GetMainWeapon(), QuarterstaffType) ||
+        ValidatorsWeapon.IsWeaponType(character.GetOffhandWeapon(), QuarterstaffType);
 
     internal static readonly IsCharacterValidHandler HasTwoHandedRangedWeapon = character =>
         ValidatorsWeapon.IsWeaponType(character.GetMainWeapon(),
