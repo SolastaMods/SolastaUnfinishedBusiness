f5ae06a83f3440ba8ddd1b967d1ad38f1da3c3f3
ThyWolf <pauloesquilo@gmail.com>
2023-03-14 08:37:12 -0700

    always displays a sign on attribute modifiers

---
+++ SolastaUnfinishedBusiness/Patches/GuiPatcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+[UsedImplicitly]
+public static class GuiPatcher
+{
+    //PATCH: always displays a sign on attribute modifiers
+    [HarmonyPatch(typeof(Gui), nameof(Gui.FormatTrendsList))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class FormatTrendsList_Patch
+    {
+        [UsedImplicitly]
+        public static bool Prefix(
+            ref string __result,
+            string output,
+            List<RuleDefinitions.TrendInfo> trends,
+            bool ignoreZero)
+        {
+            foreach (var trend in trends
+                         .Where(trend => trend.value != 0 || !ignoreZero))
+            {
+                if (!string.IsNullOrEmpty(output))
+                {
+                    output += "\n";
+                }
+
+                output += Gui.Format("{0}: {1}", trend.value.ToString("+0;-#"), Gui.FormatTrendInfo(trend));
+            }
+
+            __result = output;
+
+            return false;
+        }
+    }
+}
