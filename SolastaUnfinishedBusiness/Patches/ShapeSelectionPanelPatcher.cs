dc35c02e0c5c7312cf3b25ae7609d447d9361b5e
ThyWolf <pauloesquilo@gmail.com>
2023-04-08 04:56:34 -0700

    consolidate shape selection patches

--- SolastaUnfinishedBusiness/Patches/ShapeSelectionPanelPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/ShapeSelectionPanelPatcher.cs
@@ -9,22 +9,6 @@ namespace SolastaUnfinishedBusiness.Patches;
 [UsedImplicitly]
 public static class ShapeSelectionPanelPatcher
 {
-    [HarmonyPatch(typeof(ShapeSelectionPanel), nameof(ShapeSelectionPanel.OnEndShow))]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    [UsedImplicitly]
-    public static class OnEndShow_Patch
-    {
-        [UsedImplicitly]
-        public static void Postfix(ShapeSelectionPanel __instance)
-        {
-            //PATCH: shrink the shape selection panel if too many shapes offered
-            var rect = __instance.GetComponent<RectTransform>();
-            var shapesCount = __instance.shapeOptionTable.childCount;
-
-            rect.localScale = shapesCount > 10 ? new Vector3(0.8f, 0.8f, 0.8f) : Vector3.one;
-        }
-    }
-
     //PATCH: allows shapes to be displayed regardless of the campaign max level
     [HarmonyPatch(typeof(ShapeSelectionPanel), nameof(ShapeSelectionPanel.Show))]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
@@ -99,6 +83,16 @@ public static class ShapeSelectionPanelPatcher
             __instance.Show();
 
             return false;
+        }
+
+        [UsedImplicitly]
+        public static void Postfix(ShapeSelectionPanel __instance)
+        {
+            //PATCH: shrink the shape selection panel if too many shapes offered
+            var rect = __instance.RectTransform;
+            var shapesCount = __instance.shapeOptionTable.childCount;
+
+            rect.localScale = shapesCount > 10 ? new Vector3(0.8f, 0.8f, 0.8f) : Vector3.one;
         }
     }
 }
