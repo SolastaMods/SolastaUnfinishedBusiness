cf970481e49878bae634ef93a461828db8bd5967
ThyWolf <pauloesquilo@gmail.com>
2023-04-10 10:14:05 -0700

    add CharacterFamilyDefinitionBuilder

---
+++ SolastaUnfinishedBusiness/Builders/CharacterFamilyDefinitionBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace SolastaUnfinishedBusiness.Builders;
+
+[UsedImplicitly]
+internal class CharacterFamilyDefinitionBuilder
+    : DefinitionBuilder<CharacterFamilyDefinition, CharacterFamilyDefinitionBuilder>
+{
+    internal CharacterFamilyDefinitionBuilder IsExtraPlanar()
+    {
+        Definition.extraplanar = true;
+        return this;
+    }
+
+    #region Constructors
+
+    protected CharacterFamilyDefinitionBuilder(string name, Guid namespaceGuid) : base(name, namespaceGuid)
+    {
+    }
+
+    protected CharacterFamilyDefinitionBuilder(CharacterFamilyDefinition original, string name, Guid namespaceGuid)
+        : base(original, name, namespaceGuid)
+    {
+    }
+
+    #endregion
+}
