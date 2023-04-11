c6e93cfb8a288d131de3d7cf797bb92ffb86ce87
ThyWolf <pauloesquilo@gmail.com>
2023-04-10 22:26:21 -0700

    deprecate `EnableCommandAllUndead`

--- SolastaUnfinishedBusiness/Settings.cs
+++ SolastaUnfinishedBusiness/Settings.cs
@@ -53,7 +53,6 @@ public class Settings : UnityModManager.ModSettings
     // SETTINGS HIDDEN ON UI
     //
 
-    public bool EnableCommandAllUndead { get; set; } = true;
     public bool EnableCtrlClickOnlySwapsMainHand { get; set; } = true;
     public bool EnableDisplaySorceryPointBoxSorcererOnly { get; set; } = true;
     public bool EnableSameWidthFeatSelection { get; set; } = true;
