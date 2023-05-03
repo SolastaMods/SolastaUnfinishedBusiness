ef2c73841421e4132f5af908d65e7b79978aca51
ThyWolf <pauloesquilo@gmail.com>
2023-05-03 15:18:24 -0700

    refactor toggles logic

--- SolastaUnfinishedBusiness/Patches/GuiCharacterActionPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/GuiCharacterActionPatcher.cs
@@ -25,26 +25,4 @@ public static class GuiCharacterActionPatcher
             __result = __instance.ActingCharacter.RulesetCharacter.GetRemainingAttackUses(__instance.forcedAttackMode);
         }
     }
-
-    [HarmonyPatch(typeof(GuiCharacterAction), nameof(GuiCharacterAction.OnGoing), MethodType.Getter)]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    [UsedImplicitly]
-    public static class OnGoing_Getter_Patch
-    {
-        [UsedImplicitly]
-        public static bool Prefix(GuiCharacterAction __instance, ref bool __result)
-        {
-            if (__instance.actionId != (ActionDefinitions.Id)ExtraActionId.FeatCrusherToggle &&
-                __instance.actionId != (ActionDefinitions.Id)ExtraActionId.MonkKiPointsToggle &&
-                __instance.actionId != (ActionDefinitions.Id)ExtraActionId.PaladinSmiteToggle &&
-                __instance.actionId != (ActionDefinitions.Id)ExtraActionId.QuiveringPalmToggle)
-            {
-                return true;
-            }
-
-            __result = __instance.actingCharacter.RulesetCharacter.IsToggleEnabled(__instance.actionId);
-
-            return false;
-        }
-    }
 }
