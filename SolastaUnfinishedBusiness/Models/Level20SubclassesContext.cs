1b577422fb66c889160098933fe2332041b1dc42
ThyWolf <pauloesquilo@gmail.com>
2023-05-06 09:25:09 -0700

    review RemoveCondition scenarios

--- SolastaUnfinishedBusiness/Models/Level20SubclassesContext.cs
+++ SolastaUnfinishedBusiness/Models/Level20SubclassesContext.cs
@@ -977,19 +977,25 @@ internal static class Level20SubclassesContext
             ref RollOutcome saveOutcome,
             ref int saveOutcomeDelta)
         {
-            if (target is not RulesetCharacter rulesetCharacter)
+            if (target is not RulesetCharacter rulesetTarger)
             {
                 return;
             }
 
-            rulesetCharacter.RemoveAllConditionsOfCategory("ConditionTraditionOpenHandQuiveringPalm");
+            var rulesetCondition = rulesetTarger.AllConditions.FirstOrDefault(x =>
+                x.ConditionDefinition.Name == "ConditionTraditionOpenHandQuiveringPalm");
+
+            if (rulesetCondition != null)
+            {
+                rulesetTarger.RemoveCondition(rulesetCondition);
+            }
 
             if (saveOutcome is not (RollOutcome.Failure or RollOutcome.CriticalFailure))
             {
                 return;
             }
 
-            var totalDamage = rulesetCharacter.CurrentHitPoints + rulesetCharacter.TemporaryHitPoints - 1;
+            var totalDamage = rulesetTarger.CurrentHitPoints + rulesetTarger.TemporaryHitPoints - 1;
 
             target.SustainDamage(totalDamage, "DamagePure", false, caster.Guid, null, out _);
             effectForms.SetRange(
