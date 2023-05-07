7090f85b7ddeefc4b6092482de831b92a84a679a
ThyWolf <pauloesquilo@gmail.com>
2023-05-07 11:07:54 -0700

    tweak Duelist reflexive party

--- SolastaUnfinishedBusiness/Subclasses/RoguishDuelist.cs
+++ SolastaUnfinishedBusiness/Subclasses/RoguishDuelist.cs
@@ -62,6 +62,7 @@ internal sealed class RoguishDuelist : AbstractSubclass
             .SetGuiPresentationNoContent(true)
             .SetSilent(Silent.WhenAddedOrRemoved)
             .SetSpecialDuration(DurationType.Round, 0, TurnOccurenceType.StartOfTurn)
+            .SetSpecialInterruptions(ConditionInterruption.AnyBattleTurnEnd)
             .AddToDB();
 
         var actionAffinityReflexiveParry = FeatureDefinitionBuilder
