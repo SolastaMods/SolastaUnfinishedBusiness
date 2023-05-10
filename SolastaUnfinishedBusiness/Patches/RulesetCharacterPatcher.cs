09b61097e48f10ad3a4ce3ea2f7c6009c67acbab
ThyWolf <pauloesquilo@gmail.com>
2023-05-09 19:26:57 -0700

    fix wrong method name

--- SolastaUnfinishedBusiness/Patches/RulesetCharacterPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/RulesetCharacterPatcher.cs
@@ -872,39 +872,6 @@ public static class RulesetCharacterPatcher
         }
     }
 
-    [HarmonyPatch(typeof(RulesetCharacter), nameof(RulesetCharacter.RechargePowersForTurnStart))]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    [UsedImplicitly]
-    public static class RechargePowersForTurnStart_Patch
-    {
-        [UsedImplicitly]
-        public static void Postfix(RulesetCharacter __instance)
-        {
-            //PATCH: support for powers that recharge on turn start
-            foreach (var usablePower in __instance.UsablePowers)
-            {
-                if (usablePower.RemainingUses >= usablePower.MaxUses)
-                {
-                    continue;
-                }
-
-                var startOfTurnRecharge = usablePower.PowerDefinition.GetFirstSubFeatureOfType<IStartOfTurnRecharge>();
-
-                if (startOfTurnRecharge == null)
-                {
-                    continue;
-                }
-
-                usablePower.Recharge();
-
-                if (!startOfTurnRecharge.IsRechargeSilent && __instance.PowerRecharged != null)
-                {
-                    __instance.PowerRecharged(__instance, usablePower);
-                }
-            }
-        }
-    }
-
     [HarmonyPatch(typeof(RulesetCharacter), nameof(RulesetCharacter.RepayPowerUse))]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     [UsedImplicitly]
