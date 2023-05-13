6052c44890d4fd793fee7bc3811dd78e164d4d84
ThyWolf <pauloesquilo@gmail.com>
2023-05-12 19:35:58 -0700

    fix wizard builder call

--- SolastaUnfinishedBusiness/Models/SubclassesContext.cs
+++ SolastaUnfinishedBusiness/Models/SubclassesContext.cs
@@ -98,7 +98,7 @@ internal static class SubclassesContext
         LoadSubclass(new WizardArcaneFighter());
         LoadSubclass(new WizardBladeDancer());
         LoadSubclass(new WizardDeadMaster());
-        LoadSubclass(new WizardGravityMage());
+        LoadSubclass(new WizardGraviturgist());
         LoadSubclass(new WizardSpellMaster());
 
         // settings paring
