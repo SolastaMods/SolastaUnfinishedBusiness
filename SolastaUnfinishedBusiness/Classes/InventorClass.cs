5ffaf668ecbd60d6b874254c80e2fb2fa3c25d32
ThyWolf <pauloesquilo@gmail.com>
2023-05-13 22:00:53 -0700

    fix wit display

--- SolastaUnfinishedBusiness/Classes/InventorClass.cs
+++ SolastaUnfinishedBusiness/Classes/InventorClass.cs
@@ -901,7 +901,7 @@ internal class FlashOfGenius : ConditionSourceCanUsePowerToImproveFailedSaveRoll
 
         //reuse DC modifier from previous checks, not 100% sure this is correct
         var saveDc = action.GetSaveDC() + saveModifier.SaveDCModifier;
-        var rolled = saveDc + saveOutcomeDelta - bonus;
+        var rolled = saveDc + saveOutcomeDelta;
         var success = saveOutcomeDelta >= 0;
 
         const string TEXT = "Feedback/&CharacterGivesBonusToSaveWithDCFormat";
