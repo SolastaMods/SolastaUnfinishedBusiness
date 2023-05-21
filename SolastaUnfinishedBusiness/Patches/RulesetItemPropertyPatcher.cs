eb64adcd5801b8c4dda37baac63da46a3845acaa
Ender Wiggin <andrii.gook@gmail.com>
2023-05-21 17:02:34 +0300

    added custom unicity tag sub-feature to customize how item properties are processed for uniqueness

---
+++ SolastaUnfinishedBusiness/Patches/RulesetItemPropertyPatcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaUnfinishedBusiness.Api.GameExtensions;
+using SolastaUnfinishedBusiness.CustomBehaviors;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+[UsedImplicitly]
+public static class RulesetItemPropertyPatcher
+{
+    [HarmonyPatch(typeof(RulesetItemProperty), nameof(RulesetItemProperty.UnicityTag), MethodType.Getter)]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class UnicityTag_Patch
+    {
+        [UsedImplicitly]
+        public static void Postfix(RulesetItemProperty __instance, ref string __result)
+        {
+            var custom = __instance.featureDefinition.GetFirstSubFeatureOfType<ICustomUnicityTag>();
+            if (custom != null)
+            {
+                __result = custom.UnicityTag;
+            }
+        }
+    }
+}
