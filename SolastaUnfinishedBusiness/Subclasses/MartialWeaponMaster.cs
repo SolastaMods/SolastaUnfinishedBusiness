352f7fcb127a33db4fa8660dba70ea1bbe16a8f6
ThyWolf <pauloesquilo@gmail.com>
2023-05-25 20:55:38 -0700

    fix Perfect Strikes not checking for condition Focused Strikes

--- SolastaUnfinishedBusiness/Subclasses/MartialWeaponMaster.cs
+++ SolastaUnfinishedBusiness/Subclasses/MartialWeaponMaster.cs
@@ -175,7 +175,7 @@ internal sealed class MartialWeaponMaster : AbstractSubclass
             .SetGuiPresentation(Category.Feature)
             .AddToDB();
 
-        featurePerfectStrikes.SetCustomSubFeatures(new PerfectStrikes(featurePerfectStrikes));
+        featurePerfectStrikes.SetCustomSubFeatures(new PerfectStrikes(conditionFocusedStrikes, featurePerfectStrikes));
 
         // MAIN
 
@@ -506,10 +506,12 @@ internal sealed class MartialWeaponMaster : AbstractSubclass
 
     private sealed class PerfectStrikes : IChangeDiceRoll
     {
+        private readonly ConditionDefinition _conditionDefinition;
         private readonly FeatureDefinition _featureDefinition;
 
-        public PerfectStrikes(FeatureDefinition featureDefinition)
+        public PerfectStrikes(ConditionDefinition conditionDefinition, FeatureDefinition featureDefinition)
         {
+            _conditionDefinition = conditionDefinition;
             _featureDefinition = featureDefinition;
         }
 
@@ -517,7 +519,9 @@ internal sealed class MartialWeaponMaster : AbstractSubclass
             RollContext rollContext,
             RulesetCharacter rulesetCharacter)
         {
-            return rollContext == RollContext.AttackDamageValueRoll && HasSpecializedWeapon(rulesetCharacter);
+            return rollContext == RollContext.AttackDamageValueRoll &&
+                   HasSpecializedWeapon(rulesetCharacter) &&
+                   rulesetCharacter.HasConditionOfType(_conditionDefinition.Name);
         }
 
         public void BeforeRoll(
