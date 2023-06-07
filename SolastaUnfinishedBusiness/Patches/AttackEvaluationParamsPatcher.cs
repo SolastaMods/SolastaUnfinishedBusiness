eaf9fca4a1e6d701644776bc1cd46921a6d7fa99
Ender Wiggin <andrii.gook@gmail.com>
2023-06-07 23:17:56 +0300

    moved flanking rules patches from attack initiated to much earlier AttackEvaluationParams Fill methods - now surrounded advantage is shown on cursor and in theory can be considered by AI

--- SolastaUnfinishedBusiness/Patches/AttackEvaluationParamsPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/AttackEvaluationParamsPatcher.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
 using JetBrains.Annotations;
+using SolastaUnfinishedBusiness.Models;
 
 namespace SolastaUnfinishedBusiness.Patches;
 
@@ -53,6 +54,72 @@ public static class AttackEvaluationParamsPatcher
             }
 
             __instance.maxRange = Math.Max(effectDescription.rangeParameter, 1f);
+
+            //PATCH: apply flanking rules
+            if (Main.Settings.EnableFlankingRules)
+            {
+                FlankingRules.HandleFlanking(__instance);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleDefinitions.AttackEvaluationParams),
+        nameof(BattleDefinitions.AttackEvaluationParams.FillForMagicRangeAttack))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class FillForMagicRangeAttack_Patch
+    {
+        [UsedImplicitly]
+        public static void Postfix(
+            // Since `AttackEvaluationParams` is a struct, we need to use ref to get actual object, instead of a copy
+            ref BattleDefinitions.AttackEvaluationParams __instance
+        )
+        {
+            //PATCH: apply flanking rules
+            if (Main.Settings.EnableFlankingRules)
+            {
+                FlankingRules.HandleFlanking(__instance);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleDefinitions.AttackEvaluationParams),
+        nameof(BattleDefinitions.AttackEvaluationParams.FillForPhysicalReachAttack))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class FillForPhysicalReachAttack_Patch
+    {
+        [UsedImplicitly]
+        public static void Postfix(
+            // Since `AttackEvaluationParams` is a struct, we need to use ref to get actual object, instead of a copy
+            ref BattleDefinitions.AttackEvaluationParams __instance
+        )
+        {
+            //PATCH: apply flanking rules
+            if (Main.Settings.EnableFlankingRules)
+            {
+                FlankingRules.HandleFlanking(__instance);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleDefinitions.AttackEvaluationParams),
+        nameof(BattleDefinitions.AttackEvaluationParams.FillForPhysicalRangeAttack))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class FillForPhysicalRangeAttack_Patch
+    {
+        [UsedImplicitly]
+        public static void Postfix(
+            // Since `AttackEvaluationParams` is a struct, we need to use ref to get actual object, instead of a copy
+            ref BattleDefinitions.AttackEvaluationParams __instance
+        )
+        {
+            //PATCH: apply flanking rules
+            if (Main.Settings.EnableFlankingRules)
+            {
+                FlankingRules.HandleFlanking(__instance);
+            }
         }
     }
 }
