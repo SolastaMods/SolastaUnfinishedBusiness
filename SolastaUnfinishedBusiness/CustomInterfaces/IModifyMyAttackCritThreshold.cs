41cf636e256a51e8c2c3873c22a988134943a73c
Ender Wiggin <andrii.gook@gmail.com>
2023-06-07 18:39:56 +0300

    added `IModifyMyAttackCritThreshold` that can try to modify critical threshold of specific attack against specific target

---
+++ SolastaUnfinishedBusiness/CustomInterfaces/IModifyMyAttackCritThreshold.cs
@@ -0,0 +1,7 @@
+﻿namespace SolastaUnfinishedBusiness.CustomInterfaces;
+
+public interface IModifyMyAttackCritThreshold
+{
+    public int TryModifyMyAttackCritThreshold(int current, RulesetCharacter me, RulesetCharacter target,
+        BaseDefinition attackMethod);
+}
