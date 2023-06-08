3bcee59067de2209112071db28d6f75616359787
ThyWolf <pauloesquilo@gmail.com>
2023-06-07 19:46:13 -0700

    minor fixes

--- SolastaUnfinishedBusiness/Displays/CreditsDisplay.cs
+++ SolastaUnfinishedBusiness/Displays/CreditsDisplay.cs
@@ -20,7 +20,7 @@ internal static class CreditsDisplay
             "Circle of the Ancient Forest, Circle of the Eternal Grove, Wizard Bladedancer, Wizard Deadmaster, Sorcerous Field Manipulator, " +
             "Sorcerous Forceblade, Sorcerous Sorr-Akkath, Oath of Dread, Oath of Hatred, Path of the Elements, Path of the Reaver, " +
             "Path of the Savagery, Path of the Yeoman, Ranger Hellwalker, Ranger Lightbearer, Ranger Survivalist, Ranger Wildmaster, Martial Royal Knight, " +
-            "Martial Weapon Master, Way of the Discordance, Way of the Silhouette, Way of the Tempest, Innovation Artillerist, Level 20, Multiclass"),
+            "Martial Weapon Master, Way of the Discordance, Way of the Silhouette, Way of the Tempest, Innovation Artillerist, Innovation Artillerist, Vivisectionist, Level 20, Multiclass"),
         ("TPABOBAP",
             "custom behaviors, game UI, infrastructure, gameplay, rules, quality of life, feats, fighting styles, invocations, metamagic, spells, " +
             "quality of life, Patron Elementalist, Patron Moonlit, Patron Riftwalker, Patron Soulblade, Martial Tactician, Way of Distant Hand, " +
