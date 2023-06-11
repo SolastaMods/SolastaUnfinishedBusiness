035cdef0e98c560e08a6b3ab89ac667b0d162101
Ender Wiggin <andrii.gook@gmail.com>
2023-06-11 19:10:24 +0300

    added custom sub-feature and patch for it that allows marking FeatureDefinition to be enumerable multiple times, if it is present multiple times in the character features

---
+++ SolastaUnfinishedBusiness/Patches/FeatureDefinitionPatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaUnfinishedBusiness.Api.GameExtensions;
+using SolastaUnfinishedBusiness.CustomBehaviors;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+[UsedImplicitly]
+public static class FeatureDefinitionPatcher
+{
+    [HarmonyPatch(typeof(FeatureDefinition), nameof(FeatureDefinition.AllowsDuplicate), MethodType.Getter)]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class AllowsDuplicate_Getter_Patch
+    {
+        public static void Postfix(FeatureDefinition __instance, ref bool __result)
+        {
+            __result = __instance.HasSubFeatureOfType<AllowDuplicates>();
+        }
+    }
+}
