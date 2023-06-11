32f204ae97208f80f54b9ce0f272f78c6632fa27
Ender Wiggin <andrii.gook@gmail.com>
2023-06-11 19:12:38 +0300

    added IActionExecutionHandled

---
+++ SolastaUnfinishedBusiness/CustomInterfaces/IActionExecutionHandled.cs
@@ -0,0 +1,12 @@
+﻿namespace SolastaUnfinishedBusiness.CustomInterfaces;
+
+public interface IActionExecutionHandled
+{
+    /**
+     * Called after action execution has been accounted for - actions spent, attack numbers upodated, etc
+     */
+    void OnActionExecutionHandled(
+        GameLocationCharacter character, 
+        CharacterActionParams actionParams,
+        ActionDefinitions.ActionScope scope);
+}
