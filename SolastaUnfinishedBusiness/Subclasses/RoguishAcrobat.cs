d8ea34a9b94838510b89548f44f67a3471a9bef7
ThyWolf <pauloesquilo@gmail.com>
2023-06-18 18:58:47 -0700

    change Trooper feature to leverage IncreaseWeaponReach

--- SolastaUnfinishedBusiness/Subclasses/RoguishAcrobat.cs
+++ SolastaUnfinishedBusiness/Subclasses/RoguishAcrobat.cs
@@ -50,7 +50,7 @@ internal sealed class RoguishAcrobat : AbstractSubclass
             .SetCustomSubFeatures(
                 new AddPolearmFollowUpAttack(QuarterstaffType),
                 new AddTagToWeapon(TagsDefinitions.WeaponTagFinesse, TagsDefinitions.Criticity.Important, validWeapon),
-                new ModifyWeaponAttackModeTypeQuarterstaff(validWeapon))
+                new IncreaseWeaponReach(1, validWeapon, Name)) // should stack with Lunger or Wendigo
             .AddToDB();
 
         // LEVEL 09 - Swift as the Wind
@@ -172,27 +172,6 @@ internal sealed class RoguishAcrobat : AbstractSubclass
 
     // ReSharper disable once UnassignedGetOnlyAutoProperty
     internal override DeityDefinition DeityDefinition { get; }
-
-    private sealed class ModifyWeaponAttackModeTypeQuarterstaff : IModifyWeaponAttackMode
-    {
-        private readonly IsWeaponValidHandler _isWeaponValid;
-
-        public ModifyWeaponAttackModeTypeQuarterstaff(IsWeaponValidHandler isWeaponValid)
-        {
-            _isWeaponValid = isWeaponValid;
-        }
-
-        public void ModifyAttackMode(RulesetCharacter character, RulesetAttackMode attackMode)
-        {
-            if (!_isWeaponValid(attackMode, null, character))
-            {
-                return;
-            }
-
-            attackMode.reach = true;
-            attackMode.reachRange = 2;
-        }
-    }
 
     private class AttackBeforeHitConfirmedOnMeHeroicUncannyDodge : IAttackBeforeHitConfirmedOnMe
     {
