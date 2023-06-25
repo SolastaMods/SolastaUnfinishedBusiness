7108c5c09a038c29ada2d89e37856d57b6fcb1d3
Paulo Monteiro <pauloesquilo@gmail.com>
2023-06-25 09:38:08 -0700

    demote IActionFinishedOnMe in favor of IActionFinishedByEnemy

--- SolastaUnfinishedBusiness/CustomInterfaces/IActionFinishedOnMe.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-
-namespace SolastaUnfinishedBusiness.CustomInterfaces;
-
-public interface IActionFinishedOnMe
-{
-    public IEnumerator OnActionFinishedOnMe(GameLocationCharacter me, CharacterAction action);
-}
