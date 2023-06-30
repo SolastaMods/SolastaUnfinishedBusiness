5a07af1b3921c3a8612e0897d41a5c673a79ce5e
Paulo Monteiro <pauloesquilo@gmail.com>
2023-06-30 11:59:15 -0700

    add AddAsActivePowerToSource to InstantiateEffectPower

--- SolastaUnfinishedBusiness/CustomUI/ReactionRequestSpendBundlePower.cs
+++ SolastaUnfinishedBusiness/CustomUI/ReactionRequestSpendBundlePower.cs
@@ -120,7 +120,9 @@ internal sealed class ReactionRequestSpendBundlePower : ReactionRequest
         var rulesService = ServiceRepository.GetService<IRulesetImplementationService>();
         var rulesetCharacter = actingCharacter.RulesetCharacter;
         var usablePower = UsablePowersProvider.Get(power, rulesetCharacter);
-        var powerEffect = rulesService.InstantiateEffectPower(rulesetCharacter, usablePower, false);
+        var powerEffect = rulesService
+            .InstantiateEffectPower(rulesetCharacter, usablePower, false)
+            .AddAsActivePowerToSource();
 
         ReactionParams.RulesetEffect = powerEffect;
 
