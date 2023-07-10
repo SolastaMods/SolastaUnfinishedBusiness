68bb76a3558e9ca06dd4eeb06fa7eeca9ca0e8fd
Andargor <andargor@yahoo.com>
2023-07-09 21:48:59 -0400

    Update EnumExtensions.cs - corrected mistake
    Messed up on the resolved conflicts, fixed enums
    
    Signed-off-by: Andargor <andargor@yahoo.com>

--- SolastaUnfinishedBusiness/Api/GameExtensions/EnumExtensions.cs
+++ SolastaUnfinishedBusiness/Api/GameExtensions/EnumExtensions.cs
@@ -225,8 +225,7 @@ internal enum ExtraActionId
     FlightSuspend = 9031,
     FlightResume = 9032,
     ArcaneArcherToggle = 9033,
-    ArcaneArcherToggle = 9034,
-    CunningStrikeToggle = 9035,
+    CunningStrikeToggle = 9034,
     PrioritizeAction
 }
 
