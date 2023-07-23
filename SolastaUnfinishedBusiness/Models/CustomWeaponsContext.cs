0ea09eca1f3e1fb503a414c07d8b3be177428bbe
Paulo Monteiro <pauloesquilo@gmail.com>
2023-07-22 21:24:25 -0700

    improve Gauntlet and Lightning effects

--- SolastaUnfinishedBusiness/Models/CustomWeaponsContext.cs
+++ SolastaUnfinishedBusiness/Models/CustomWeaponsContext.cs
@@ -651,6 +651,9 @@ internal static class CustomWeaponsContext
                 .AddToDB(), ConditionForm.ConditionOperation.Add)
             .Build());
 
+        baseDescription.EffectDescription.effectParticleParameters =
+            SpellDefinitions.Shatter.EffectDescription.EffectParticleParameters;
+
         ThunderGauntlet = BuildWeapon("CEThunderGauntlet", baseItem, 0, true, Common, basePresentation, baseDescription,
             Sprites.ItemThunderGauntlet, properties: new[] { ThunderImpactVFX });
         ThunderGauntlet.inDungeonEditor = false;
@@ -687,6 +690,9 @@ internal static class CustomWeaponsContext
                 .SetSpecialDuration(DurationType.Round, 1, TurnOccurenceType.StartOfTurn)
                 .AddToDB(), ConditionForm.ConditionOperation.Add, true)
             .Build());
+
+        baseDescription.EffectDescription.effectParticleParameters =
+            SpellDefinitions.LightningBolt.EffectDescription.EffectParticleParameters;
 
         LightningLauncher = BuildWeapon("CELightningLauncher", baseItem, 0, true, Common, basePresentation,
             baseDescription, Sprites.ItemGemLightning, properties: new[] { LightningImpactVFX });
