22c91993aeefe335cd66c322ace69e8a82fc0623
ThyWolf <pauloesquilo@gmail.com>
2023-09-10 19:47:39 -0700

    update IModifySavingThrow signature

---
+++ SolastaUnfinishedBusiness/CustomInterfaces/IModifySavingThrow.cs
@@ -0,0 +1,11 @@
+﻿namespace SolastaUnfinishedBusiness.CustomInterfaces;
+
+public interface IModifySavingThrow
+{
+    public bool IsValid(RulesetActor rulesetActor, RulesetActor rulesetCaster, string attributeScore);
+
+    public string AttributeAndActionModifier(
+        RulesetActor rulesetActor,
+        ActionModifier actionModifier,
+        string attribute);
+}
