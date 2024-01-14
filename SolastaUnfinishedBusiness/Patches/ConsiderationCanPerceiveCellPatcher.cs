b07f97d80c82124ab2eecdad42aad3f7a6fb7a05
ThyWolf <pauloesquilo@gmail.com>
2024-01-14 06:23:17 -0800

    add additional patches on AI brain to learn the better place to insert new obscurement rules on enemies

--- SolastaUnfinishedBusiness/Patches/ConsiderationCanPerceiveCellPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/ConsiderationCanPerceiveCellPatcher.cs
@@ -3,34 +3,38 @@ using HarmonyLib;
 using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Api.GameExtensions;
 using TA.AI;
+using TA.AI.Considerations;
 
 namespace SolastaUnfinishedBusiness.Patches;
 
 [UsedImplicitly]
 public static class ConsiderationCanPerceiveCellPatcher
 {
-    [HarmonyPatch(typeof(TA.AI.Considerations.CanPerceiveCell), nameof(TA.AI.Considerations.CanPerceiveCell.Score))]
+    [HarmonyPatch(typeof(CanPerceiveCell), nameof(CanPerceiveCell.Score))]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     [UsedImplicitly]
-    public static class Clear_Patch
+    public static class Score_Patch
     {
         [UsedImplicitly]
+#pragma warning disable IDE0060
         public static bool Prefix(
-            TA.AI.Considerations.CanPerceiveCell __instance,
+            CanPerceiveCell __instance,
             DecisionContext context,
             ConsiderationDescription consideration,
             DecisionParameters parameters,
             ScoringResult scoringResult)
+#pragma warning restore IDE0060
         {
             var locationCharacter = parameters.character.GameLocationCharacter;
             var position = context.position;
             var visibilityService =
                 parameters.situationalInformation.VisibilityService as GameLocationVisibilityManager;
 
-            bool flag = Main.Settings.UseOfficialObscurementRules
+            var flag = Main.Settings.UseOfficialObscurementRules
                 ? visibilityService.MyIsCellPerceivedByCharacter(position, locationCharacter)
                 : visibilityService!.IsCellPerceivedByCharacter(position, locationCharacter);
 
+            Main.Info($"{locationCharacter.Name} => {position} : {scoringResult.Score}");
             scoringResult.Score = flag ? 1f : 0.0f;
 
             return false;
