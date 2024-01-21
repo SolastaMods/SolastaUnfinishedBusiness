c2e570a96fad9fc51e292bd68197bda9c2741184
ThyWolf <pauloesquilo@gmail.com>
2024-01-21 08:05:47 -0800

    add IForceConditionParent to have a soft parent behavior

---
+++ SolastaUnfinishedBusiness/CustomInterfaces/IForceConditionParent.cs
@@ -0,0 +1,8 @@
+﻿namespace SolastaUnfinishedBusiness.CustomInterfaces;
+
+// allows a condition to have a soft parent
+// a soft parent allows the condition to be considered on checks but don't inherit parent features
+public interface IForceConditionParent
+{
+    string Parent(ConditionDefinition source);
+}
---
+++ SolastaUnfinishedBusiness/Patches/ConditionDefinitionPatcher.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaUnfinishedBusiness.Api.GameExtensions;
+using SolastaUnfinishedBusiness.CustomInterfaces;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+[UsedImplicitly]
+public static class ConditionDefinitionPatcher
+{
+    //PATCH: mainly supports lighting and obscurement rules use case
+    [HarmonyPatch(typeof(ConditionDefinition), nameof(ConditionDefinition.IsSubtypeOf))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class IsSubtypeOf_Patch
+    {
+        [UsedImplicitly]
+        public static void Postfix(ConditionDefinition __instance, ref bool __result, string parentConditionName)
+        {
+            if (__result)
+            {
+                return;
+            }
+
+            var modifier = __instance.GetFirstSubFeatureOfType<IForceConditionParent>();
+
+            if (modifier != null && modifier.Parent(__instance) == parentConditionName)
+            {
+                __result = true;
+            }
+        }
+    }
+
+    //PATCH: mainly supports lighting and obscurement rules use case
+    [HarmonyPatch(typeof(ConditionDefinition), nameof(ConditionDefinition.HasParentCondition))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class HasParentCondition_Patch
+    {
+        [UsedImplicitly]
+        public static void Postfix(ConditionDefinition __instance, ref bool __result)
+        {
+            if (__result)
+            {
+                return;
+            }
+
+            var modifier = __instance.HasSubFeatureOfType<IForceConditionParent>();
+
+            if (modifier)
+            {
+                __result = true;
+            }
+        }
+    }
+}
