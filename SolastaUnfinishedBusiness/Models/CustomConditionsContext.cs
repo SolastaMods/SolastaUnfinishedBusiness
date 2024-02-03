ed0bfbd0ab54b975dfe04e09450c319a45d27373
ThyWolf <pauloesquilo@gmail.com>
2024-02-02 20:38:12 -0800

    Revert content removal

--- SolastaUnfinishedBusiness/Models/CustomConditionsContext.cs
+++ SolastaUnfinishedBusiness/Models/CustomConditionsContext.cs
@@ -23,14 +23,22 @@ namespace SolastaUnfinishedBusiness.Models;
 internal static class CustomConditionsContext
 {
     internal static ConditionDefinition Distracted;
+
     internal static ConditionDefinition FlightSuspended;
+
+    internal static ConditionDefinition InvisibilityEveryRound;
+
     internal static ConditionDefinition LightSensitivity;
+
     internal static ConditionDefinition StopMovement;
+
     internal static ConditionDefinition Taunted;
 
     private static ConditionDefinition _taunter;
     private static FeatureDefinitionPower _flightSuspend;
     private static FeatureDefinitionPower _flightResume;
+    private static ConditionDefinition _conditionInvisibilityEveryRoundRevealed;
+    private static ConditionDefinition _conditionInvisibilityEveryRoundHidden;
 
     internal static void Load()
     {
@@ -48,6 +56,8 @@ internal static class CustomConditionsContext
             .AddToDB();
 
         FlightSuspended = BuildFlightSuspended();
+
+        InvisibilityEveryRound = BuildInvisibilityEveryRound();
 
         LightSensitivity = BuildLightSensitivity();
 
@@ -83,28 +93,60 @@ internal static class CustomConditionsContext
             .AddToDB();
     }
 
+    private static ConditionDefinition BuildInvisibilityEveryRound()
+    {
+        _conditionInvisibilityEveryRoundRevealed = ConditionDefinitionBuilder
+            .Create("ConditionInvisibilityEveryRoundRevealed")
+            .SetGuiPresentationNoContent(true)
+            .SetSilent(Silent.WhenAddedOrRemoved)
+            .AddToDB();
+
+        _conditionInvisibilityEveryRoundHidden = ConditionDefinitionBuilder
+            .Create(ConditionDefinitions.ConditionInvisible, "ConditionInvisibilityEveryRoundHidden")
+            .SetCancellingConditions(_conditionInvisibilityEveryRoundRevealed)
+            .SetSilent(Silent.WhenAddedOrRemoved)
+            .SetSpecialDuration(DurationType.Round, 1, TurnOccurenceType.StartOfTurn)
+            .SetSpecialInterruptions(
+                ConditionInterruption.Attacks,
+                ConditionInterruption.CastSpell,
+                ConditionInterruption.UsePower)
+            .AddToDB();
+
+        var conditionInvisibilityEveryRound = ConditionDefinitionBuilder
+            .Create("ConditionInvisibilityEveryRound")
+            .SetGuiPresentationNoContent(true)
+            .SetSilent(Silent.WhenAddedOrRemoved)
+            .SetSpecialDuration(DurationType.Round, 1, TurnOccurenceType.StartOfTurn)
+            .AddCustomSubFeatures(new InvisibilityEveryRoundBehavior())
+            .AddToDB();
+
+        return conditionInvisibilityEveryRound;
+    }
+
     private static ConditionDefinition BuildLightSensitivity()
     {
+        var abilityCheckAffinityLightSensitivity = FeatureDefinitionAbilityCheckAffinityBuilder
+            .Create("AbilityCheckAffinityLightSensitivity")
+            .SetGuiPresentation(Category.Feature)
+            .BuildAndSetAffinityGroups(CharacterAbilityCheckAffinity.Disadvantage, DieType.D1, 0,
+                (AttributeDefinitions.Wisdom, SkillDefinitions.Perception))
+            .AddToDB();
+
+        var combatAffinityDarkelfLightSensitivity = FeatureDefinitionCombatAffinityBuilder
+            .Create(CombatAffinitySensitiveToLight, "CombatAffinityLightSensitivity")
+            .SetGuiPresentation(Category.Feature)
+            .SetMyAttackAdvantage(AdvantageType.None)
+            .SetMyAttackModifierSign(AttackModifierSign.Substract)
+            .SetMyAttackModifierDieType(DieType.D4)
+            .AddToDB();
+
         var conditionLightSensitive = ConditionDefinitionBuilder
             .Create(ConditionDefinitions.ConditionLightSensitive, "ConditionLightSensitivity")
             .SetOrUpdateGuiPresentation(Category.Condition)
             .SetSilent(Silent.WhenAddedOrRemoved)
             .SetPossessive()
             .SetConditionType(ConditionType.Detrimental)
-            .SetFeatures(
-                FeatureDefinitionAbilityCheckAffinityBuilder
-                    .Create("AbilityCheckAffinityLightSensitivity")
-                    .SetGuiPresentation(Category.Feature)
-                    .BuildAndSetAffinityGroups(CharacterAbilityCheckAffinity.Disadvantage, DieType.D1, 0,
-                        (AttributeDefinitions.Wisdom, SkillDefinitions.Perception))
-                    .AddToDB(),
-                FeatureDefinitionCombatAffinityBuilder
-                    .Create(CombatAffinitySensitiveToLight, "CombatAffinityLightSensitivity")
-                    .SetGuiPresentation(Category.Feature)
-                    .SetMyAttackAdvantage(AdvantageType.None)
-                    .SetMyAttackModifierSign(AttackModifierSign.Substract)
-                    .SetMyAttackModifierDieType(DieType.D4)
-                    .AddToDB())
+            .SetFeatures(abilityCheckAffinityLightSensitivity, combatAffinityDarkelfLightSensitivity)
             .AddToDB();
 
         conditionLightSensitive.GuiPresentation.description = Gui.NoLocalization;
@@ -138,8 +180,7 @@ internal static class CustomConditionsContext
         return conditionFlightSuspend;
     }
 
-    private static void BuildFlightSuspendAction(
-        ConditionDefinition conditionFlightSuspend,
+    private static void BuildFlightSuspendAction(ConditionDefinition conditionFlightSuspend,
         ConditionDefinition conditionFlightSuspendConcentrationTracker)
     {
         const string FlightSuspendName = "FlightSuspend";
@@ -291,6 +332,124 @@ internal static class CustomConditionsContext
             "PowerMartialGuardianGrandChallenge" => 6,
             _ => 18 // Thunder Gauntlet
         };
+    }
+
+    private sealed class InvisibilityEveryRoundBehavior : IActionFinishedByMe, IOnConditionAddedOrRemoved
+    {
+        private const string CategoryRevealed = "InvisibilityEveryRoundRevealed";
+        private const string CategoryHidden = "InvisibilityEveryRoundHidden";
+
+        public IEnumerator OnActionFinishedByMe(CharacterAction action)
+        {
+            if (action is not (CharacterActionUsePower or CharacterActionCastSpell or CharacterActionAttack))
+            {
+                yield break;
+            }
+
+            var actingCharacter = action.ActingCharacter;
+            var rulesetCharacter = actingCharacter.RulesetCharacter;
+            var actionParams = action.ActionParams;
+            var rulesetEffect = actionParams.RulesetEffect;
+
+            if (rulesetEffect == null || !IsAllowedEffect(rulesetEffect.EffectDescription))
+            {
+                BecomeRevealed(rulesetCharacter);
+            }
+        }
+
+        public void OnConditionAdded(RulesetCharacter target, RulesetCondition rulesetCondition)
+        {
+            if (target is not RulesetCharacterMonster &&
+                !target.HasConditionOfType(_conditionInvisibilityEveryRoundRevealed))
+            {
+                BecomeHidden(target);
+            }
+        }
+
+        public void OnConditionRemoved(RulesetCharacter target, RulesetCondition rulesetCondition)
+        {
+            if (target is not RulesetCharacterMonster)
+            {
+                target.RemoveAllConditionsOfCategory(CategoryHidden, false);
+            }
+        }
+
+        // returns true if effect is self teleport or any self targeting spell that is self-buff
+        private static bool IsAllowedEffect(EffectDescription effect)
+        {
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+            switch (effect.TargetType)
+            {
+                case TargetType.Position:
+                {
+                    foreach (var form in effect.EffectForms)
+                    {
+                        if (form.FormType != EffectForm.EffectFormType.Motion)
+                        {
+                            return false;
+                        }
+
+                        if (form.MotionForm.Type != MotionForm.MotionType.TeleportToDestination)
+                        {
+                            return false;
+                        }
+                    }
+
+                    break;
+                }
+                case TargetType.Self:
+                {
+                    foreach (var form in effect.EffectForms)
+                    {
+                        // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
+                        switch (form.FormType)
+                        {
+                            case EffectForm.EffectFormType.Damage:
+                            case EffectForm.EffectFormType.Healing:
+                            case EffectForm.EffectFormType.ShapeChange:
+                            case EffectForm.EffectFormType.Summon:
+                            case EffectForm.EffectFormType.Counter:
+                            case EffectForm.EffectFormType.Motion:
+                                return false;
+                        }
+                    }
+
+                    break;
+                }
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static void BecomeRevealed(RulesetCharacter hero)
+        {
+            hero.AddConditionOfCategory(CategoryRevealed,
+                RulesetCondition.CreateActiveCondition(
+                    hero.Guid,
+                    _conditionInvisibilityEveryRoundRevealed,
+                    DurationType.Round,
+                    1,
+                    TurnOccurenceType.StartOfTurn,
+                    hero.Guid,
+                    hero.CurrentFaction.Name
+                ));
+        }
+
+        private static void BecomeHidden(RulesetCharacter hero)
+        {
+            hero.AddConditionOfCategory(CategoryHidden,
+                RulesetCondition.CreateActiveCondition(
+                    hero.Guid,
+                    _conditionInvisibilityEveryRoundHidden,
+                    DurationType.Permanent,
+                    0,
+                    TurnOccurenceType.EndOfTurn,
+                    hero.Guid,
+                    hero.CurrentFaction.Name),
+                false);
+        }
     }
 
     private sealed class OnConditionAddedOrRemovedFlightSuspendBehavior : IOnConditionAddedOrRemoved
