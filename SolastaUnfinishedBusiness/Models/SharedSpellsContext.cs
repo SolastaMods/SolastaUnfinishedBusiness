b3456706e1ef386f38b782c8f2757cefcd22b684
ThyWolf <pauloesquilo@gmail.com>
2024-02-16 08:04:38 -0800

    fix half caster round down shared spells progression
    - fix #4357

--- SolastaUnfinishedBusiness/Models/SharedSpellsContext.cs
+++ SolastaUnfinishedBusiness/Models/SharedSpellsContext.cs
@@ -270,7 +270,7 @@ internal static class SharedSpellsContext
         // first index is for absence of levels. always 0
         private static readonly int[] FromHalfCaster =
         [
-            0, 0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10
+            0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10
         ];
 
         private static readonly int[] FromHalfRoundUpCaster =
