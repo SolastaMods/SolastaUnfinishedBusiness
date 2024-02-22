d225c07e91e83e922c15aed912c7a9a5612b6841
ThyWolf <pauloesquilo@gmail.com>
2024-02-21 18:56:54 -0800

    update credits

--- SolastaUnfinishedBusiness/Displays/CreditsDisplay.cs
+++ SolastaUnfinishedBusiness/Displays/CreditsDisplay.cs
@@ -19,70 +19,88 @@ internal static class CreditsDisplay
     internal static readonly List<(string, string)> CreditsTable =
     [
         ("Zappastuff",
-            "maintenance, mod UI, infrastructure, gameplay, rules, quality of life, backgrounds, " +
-            "feats, fighting styles, invocations, metamagic, spells, Fairy, Half-elf, Tiefling, " +
-            "Roguish Acrobat, Roguish Arcane Scoundrel, Roguish Blade Caller, Roguish Duelist, Roguish Slayer, Roguish Umbral Stalker, " +
+            "maintenance, mod UI, infrastructure, gameplay, quality of life, rules, " +
+            "backgrounds, feats, fighting styles, gambits, invocations, metamagic, spells, " +
+            "Fairy, Half-elf, Tiefling, " +
+            "Circle of the Ancient Forest, Circle of the Cosmos, Circle of the Eternal Grove, " +
             "College of Audacity, College of Guts, College of Life, College of Thespian, College of Valiance, " +
-            "Circle of the Ancient Forest, Circle of the Eternal Grove, Wizard Bladedancer, Wizard Deadmaster, " +
-            "Sorcerous Field Manipulator, Sorcerous Forceblade, Sorcerous Sorr-Akkath, Oath of Dread, Oath of Thunder, " +
+            "Innovation Artillerist, Innovation Vitriolist, Innovation Vivisectionist, " +
+            "Martial Arcane Archer, Martial Force Knight, Martial Guardian, Martial Royal Knight, Martial Warlord, Martial Weapon Master, " +
+            "Oath of Altruism, Oath of Dread, Oath of Thunder, " +
             "Path of the Elements, Path of the Reaver, Path of the Savagery, Path of the Yeoman, " +
+            "Patron Celestial, Patron Moonlit, Patron Mountain, Patron Riftwalker, " +
             "Ranger Hellwalker, Ranger Lightbearer, Ranger Sky Warrior, Ranger Survivalist, Ranger Wildmaster, " +
-            "Martial Arcane Archer, Martial Guardian, Martial Royal Knight, Martial Warlord, Martial Weapon Master, " +
-            "Patron Moonlit, Way of the Discordance, Way of the Silhouette, Way of the Tempest, Way of Weal and Woe, Way of Zen Archery, " +
-            "Innovation Artillerist, Innovation Vitriolist, Innovation Vivisectionist, Patron Celestial, Patron Mountain, " +
-            "Lighting and Obscurement, Level 20, Multiclass"),
+            "Roguish Acrobat, Roguish Arcane Scoundrel, Roguish Blade Caller, Roguish Duelist, Roguish Slayer, Roguish Umbral Stalker, " +
+            "Sorcerous Field Manipulator, Sorcerous Forceblade, Sorcerous Scion, Sorcerous Sorr-Akkath, Sorcerous Spellblade, " +
+            "Way of the Discordance, Way of the Silhouette, Way of the Tempest, Way of Weal and Woe, Way of Zen Archery, " +
+            "Wizard Bladedancer, Wizard Deadmaster, " +
+            "Level 20, Lighting and Obscurement, Multiclass"),
+
+        ("HiddenHax",
+            "QA master, SFX, sprites, homebrew design [feats, gambits, invocations, spells, " +
+            "Circle of the Eternal Grove, " +
+            "College of Audacity, College of Thespian, College of Valiance, " +
+            "Martial Arcane Archer, Martial Force Knight, Martial Guardian, Martial Warlord, Martial Weapon Master, " +
+            "Oath of Ancients, Oath of Dread, " +
+            "Path of the Elements, Path of the Reaver, Path of the Savagery, " +
+            "Patron Moonlit, " +
+            "Roguish Arcane Scoundrel, Roguish Blade Caller, Roguish Duelist, Roguish Slayer, Roguish Umbral Stalker, " +
+            "Sorcerous Field Manipulator, Sorcerous Forceblade, Sorcerous Psion, Sorcerous Sorr-Akkath, Sorcerous Spellblade, " +
+            "Way of the Discordance, Way of the Silhouette, Way of the Tempest, Way of Dragon, Way of Tempest, Way of Zen Archery]"),
 
         ("TPABOBAP",
-            "custom behaviors, game UI, infrastructure, gameplay, rules, quality of life, " +
-            "feats, fighting styles, invocations, metamagic, spells, quality of life, " +
+            "custom behaviors, game UI, infrastructure, gameplay, quality of life, rules, " +
+            "feats, fighting styles, gambits, invocations, metamagic, spells, " +
             "Innovation Armor, Innovation Grenadier, Innovation Weapon, " +
-            "Patron Elementalist, Patron Moonlit, Patron Riftwalker, Patron Soulblade, " +
+            "Patron Elementalist, Patron Soulblade, " +
             "Martial Tactician, Inventor, Multiclass"),
 
         ("ImpPhil", "api, builders, gameplay, rules, quality of life"),
-        ("ChrisJohnDigital",
-            "builders, gameplay, feats, fighting styles, Wizard Arcane Fighter, Wizard Spellmaster, Martial Spell Shield"),
 
-        ("HiddenHax",
-            "QA, sprites [spells, powers], homebrew design [feats, spells, Circle of the Eternal Grove, " +
-            "College of Audacity, College of Thespian, College of Valiance, Oath of Dread, " +
-            "Path of the Elemental Fury, Path of the Reaver, Path of the Savagery, Patron Moonlit, " +
-            "Roguish Arcane Scoundrel, Roguish Blade Caller, Roguish Duelist, Roguish Slayer, Roguish Umbral Stalker, " +
-            "Sorcerous Field Manipulator, Sorcerous Forceblade, Sorcerous Psion, Sorcerous Sorr-Akkath, " +
-            "Martial Guardian, Martial Warlord, Martial Weapon Master, " +
-            "Way of the Discordance, Way of Dragon, Way of Tempest, Way of Zen Archery]"),
+        ("ChrisJohnDigital",
+            "builders, gameplay, feats, fighting styles, " +
+            "Wizard Arcane Fighter, Wizard Spellmaster, Martial Spell Shield"),
+
+        ("Haxermn", "spells, Domain Defiler, Domain Smith, Oath of Ancients, Oath of Hatred, Way of Dragon"),
+
+        ("Nd", "College of Wardancer, Roguish Opportunist, Roguish Raven"),
+        ("tivie", "Circle of the Night, Path of the Spirits"),
+        ("ElAntonius", "feats, fighting styles, Ranger Arcanist"),
+        ("Kloranger", "feats, spells, Wizard Graviturgist"),
+        ("magicskysword", "chinese translations, asian languages support, Oath of Demon Hunter"),
+        ("DreadMaker", "Circle of the Forest Guardian"),
+        ("RedOrca", "Path of the Light"),
+        ("HIEROT", "Patron Eldritch Surge"),
+
+        ("SilverGriffon",
+            "gameplay, visuals, spells, " +
+            "Dark Elf, Draconic Kobold, Grey Dwarf, Sorcerous Divine Heart"),
+        ("Otearaisu", "Battleborn, Malakh, Oligath, Oni, Wendigo, Wildling, Wyrmkin"),
+        ("Remunos", "Ironborn Dwarf, Obsidian Dwarf"),
+        ("Holic75", "spells, Bolgrif"),
+
+        ("Bazou", "fighting styles, rules, spells"),
+        ("Andargor", "rules, quality of life"),
+        ("TheRev", "quality of life"),
+        ("Kiloku", "quality of life"),
 
         ("DemonicDuck",
-            "QA, homebrew design [rules, feats, fighting styles, spells, Innovation Vivisectionist, Oath of the Hammer, Sorcerous Sorr-Akkath, Way of Weal and Woe]"),
+            "QA, homebrew design [rules, feats, fighting styles, spells, Innovation Vivisectionist, Oath of the Thunder, Sorcerous Sorr-Akkath, Way of Weal and Woe]"),
+
+        ("Vess", "QA, homebrew design [Innovation Vitriolist]"),
 
         ("Earandil",
             "homebrew design [Patron Mountain, Path of the Savagery, Path of the Yeoman, Ranger Sky Warrior, Ranger Survivalist]"),
 
-        ("Nd", "College of Wardancer, Roguish Opportunist, Roguish Raven"),
-        ("Haxermn", "spells, Domain Defiler, Domain Smith, Oath of Ancient, Oath of Hatred, Way of Dragon"),
-        ("Otearaisu", "Battleborn, Malakh, Oligath, Oni, Wendigo, Wildling, Wyrmkin"),
-        ("SilverGriffon", "gameplay, visuals, spells, Dark Elf, Draconic Kobold, Grey Dwarf, Sorcerous Divine Heart"),
-        ("Narria", "modKit, mod UI improvements, Party Editor"),
-        ("tivie", "Circle of the Night, Path of the Spirits"),
-        ("Remunos", "Ironborn Dwarf, Obsidian Dwarf"),
-        ("ElAntonius", "feats, fighting styles, Ranger Arcanist"),
-        ("Kloranger", "feats, spells, Wizard Graviturgist"),
-        ("magicskysword", "chinese translations, asian languages support, Oath of Demon Hunter"),
-        ("HIEROT", "fixes, Patron Eldritch Surge"),
-        ("DreadMaker", "Circle of the Forest Guardian"),
-        ("RedOrca", "Path of the Light"),
-        ("Kiloku", "quality of life"),
-        ("Andargor", "rules, quality of life"),
-        ("TheRev", "quality of life"),
-        ("Bazou", "fighting styles, rules, spells"),
+        ("Taco", "sprites, homebrew design [feats, Roguish Acrobat, Domain Defiler, Oath of Altruism]"),
+        ("DubhHerder", "quality of life, spells, homebrew design [Patron Elementalist, Patron Riftwalker]"),
         ("Stuffies12", "homebrew design [Ranger Hellwalker, Ranger Lightbearer]"),
-        ("Vess", "QA, homebrew design [Innovation Vitriolist]"),
-        ("Holic75", "spells, Bolgrif"),
+
+        ("Narria", "modKit, mod UI improvements, Party Editor"),
+
         ("Artyoan", "pre-gen heroes and sample portraits"),
         ("Thaladar", "sample portraits"),
-        ("Taco",
-            "sprites [fighting styles, powers, spells, subclasses], homebrew design [feats, Roguish Acrobat, Defiler Domain, Oath of Altruism]"),
-        ("DubhHerder", "quality of life, spells, homebrew design [Patron Elementalist, Patron Riftwalker]"),
+
         ("team-waldo", "official korean font and translations"),
         ("akintos", "korean translations"),
         ("Dovel", "russian and non-official russian translations"),
