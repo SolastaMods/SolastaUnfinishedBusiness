d3aa22a426c0c2ad53836cb5f9fdbadcf0002d30
ThyWolf <pauloesquilo@gmail.com>
2024-03-01 21:14:23 -0800

    add IMagicalAttackInitiatedOnMe

---
+++ SolastaUnfinishedBusiness/Interfaces/IMagicalAttackInitiatedOnMe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace SolastaUnfinishedBusiness.Interfaces;
+
+public interface IMagicalAttackInitiatedOnMe
+{
+    [UsedImplicitly]
+    IEnumerator OnMagicalAttackInitiatedOnMe(
+        CharacterActionMagicEffect action,
+        RulesetEffect activeEffect,
+        GameLocationCharacter target,
+        ActionModifier attackModifier,
+        List<EffectForm> actualEffectForms,
+        bool firstTarget,
+        bool checkMagicalAttackDamage);
+}
