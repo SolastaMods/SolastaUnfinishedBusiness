5f7ede57f0e35a2489428223a69680935d162c99
ThyWolf <pauloesquilo@gmail.com>
2024-03-24 19:23:58 -0700

    change Heroic Uncanny Dodge to use IAttackBeforeHitPossibleOnMeOrAlly instead of  IPhysicalAttackBeforeHitConfirmedOnMe, IMagicEffectBeforeHitConfirmedOnMe

--- SolastaUnfinishedBusiness/Subclasses/RoguishAcrobat.cs
+++ SolastaUnfinishedBusiness/Subclasses/RoguishAcrobat.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Api.GameExtensions;
 using SolastaUnfinishedBusiness.Behaviors;
@@ -144,18 +142,17 @@ public sealed class RoguishAcrobat : AbstractSubclass
 
         //  Heroic Uncanny Dodge
 
-        var reduceDamageHeroicUncannyDodge = FeatureDefinitionReduceDamageBuilder
-            .Create($"ReduceDamage{Name}HeroicUncannyDodge")
-            .SetGuiPresentation($"Power{Name}HeroicUncannyDodge", Category.Feature)
-            .SetAlwaysActiveReducedDamage((_, _) => Int32.MaxValue)
-            .AddToDB();
-
         var conditionHeroicUncannyDodge = ConditionDefinitionBuilder
             .Create($"Condition{Name}HeroicUncannyDodge")
             .SetGuiPresentationNoContent(true)
             .SetSilent(Silent.WhenAddedOrRemoved)
-            .SetSpecialInterruptions(ConditionInterruption.Attacked)
-            .SetFeatures(reduceDamageHeroicUncannyDodge)
+            .SetSpecialInterruptions(ExtraConditionInterruption.AfterWasAttacked)
+            .SetFeatures(
+                FeatureDefinitionReduceDamageBuilder
+                    .Create($"ReduceDamage{Name}HeroicUncannyDodge")
+                    .SetGuiPresentation($"Power{Name}HeroicUncannyDodge", Category.Feature)
+                    .SetAlwaysActiveReducedDamage((_, _) => 999)
+                    .AddToDB())
             .AddToDB();
 
         var powerHeroicUncannyDodge = FeatureDefinitionPowerBuilder
@@ -173,7 +170,7 @@ public sealed class RoguishAcrobat : AbstractSubclass
 
         powerHeroicUncannyDodge.AddCustomSubFeatures(
             ModifyPowerVisibility.Hidden,
-            new CustomBehaviorHeroicUncannyDodge(powerHeroicUncannyDodge));
+            new AttackBeforeHitPossibleOnMeOrAllyUncannyDodge(powerHeroicUncannyDodge));
 
         // MAIN
 
@@ -201,50 +198,24 @@ public sealed class RoguishAcrobat : AbstractSubclass
     // ReSharper disable once UnassignedGetOnlyAutoProperty
     internal override DeityDefinition DeityDefinition { get; }
 
-    private class CustomBehaviorHeroicUncannyDodge(FeatureDefinitionPower powerHeroicUncannyDodge)
-        : IPhysicalAttackBeforeHitConfirmedOnMe, IMagicEffectBeforeHitConfirmedOnMe
+    private class AttackBeforeHitPossibleOnMeOrAllyUncannyDodge(FeatureDefinitionPower powerHeroicUncannyDodge)
+        : IAttackBeforeHitPossibleOnMeOrAlly
+
     {
-        public IEnumerator OnMagicEffectBeforeHitConfirmedOnMe(
+        public IEnumerator OnAttackBeforeHitPossibleOnMeOrAlly(
             GameLocationBattleManager battleManager,
             GameLocationCharacter attacker,
             GameLocationCharacter defender,
-            ActionModifier actionModifier,
-            RulesetEffect rulesetEffect,
-            List<EffectForm> actualEffectForms,
-            bool firstTarget,
-            bool criticalHit)
-        {
-            if (rulesetEffect.EffectDescription.RangeType is not (RangeType.MeleeHit or RangeType.RangeHit))
-            {
-                yield break;
-            }
-
-            yield return HandleReaction(battleManager, attacker, defender);
-        }
-
-        public IEnumerator OnAttackBeforeHitConfirmedOnMe(
-            GameLocationBattleManager battleManager,
-            GameLocationCharacter attacker,
-            GameLocationCharacter defender,
+            GameLocationCharacter helper,
             ActionModifier actionModifier,
             RulesetAttackMode attackMode,
-            bool rangedAttack,
-            AdvantageType advantageType,
-            List<EffectForm> actualEffectForms,
-            bool firstTarget,
-            bool criticalHit)
-        {
-            yield return HandleReaction(battleManager, attacker, defender);
-        }
-
-        private IEnumerator HandleReaction(
-            GameLocationBattleManager battleManager,
-            GameLocationCharacter attacker,
-            GameLocationCharacter defender)
+            RulesetEffect rulesetEffect,
+            int attackRoll)
         {
             var rulesetDefender = defender.RulesetCharacter;
 
-            if (defender.IsMyTurn() ||
+            if (defender != helper ||
+                defender.IsMyTurn() ||
                 !defender.CanReact() ||
                 !defender.CanPerceiveTarget(attacker) ||
                 rulesetDefender.GetRemainingPowerUses(powerHeroicUncannyDodge) == 0)
