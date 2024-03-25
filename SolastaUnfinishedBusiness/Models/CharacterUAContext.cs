00ed52417ac3a1e07153910376e8c1f45d38b494
ThyWolf <pauloesquilo@gmail.com>
2024-03-24 20:31:33 -0700

    fix SwitchBarbarianBrutalCritical

--- SolastaUnfinishedBusiness/Models/CharacterUAContext.cs
+++ SolastaUnfinishedBusiness/Models/CharacterUAContext.cs
@@ -582,12 +582,26 @@ internal static partial class CharacterContext
         }
         else
         {
-            Barbarian.FeatureUnlocks.TryAdd(
-                new FeatureUnlockByLevel(FeatureSetBarbarianBrutalCritical, 9));
-            Barbarian.FeatureUnlocks.TryAdd(
-                new FeatureUnlockByLevel(AttributeModifierBarbarianBrutalCriticalAdd, 13));
-            Barbarian.FeatureUnlocks.TryAdd(
-                new FeatureUnlockByLevel(AttributeModifierBarbarianBrutalCriticalAdd, 17));
+            if (!Barbarian.FeatureUnlocks.Exists(x =>
+                    x.level == 9 && x.FeatureDefinition == FeatureSetBarbarianBrutalCritical))
+            {
+                Barbarian.FeatureUnlocks.TryAdd(
+                    new FeatureUnlockByLevel(FeatureSetBarbarianBrutalCritical, 9));
+            }
+
+            if (!Barbarian.FeatureUnlocks.Exists(x =>
+                    x.level == 13 && x.FeatureDefinition == AttributeModifierBarbarianBrutalCriticalAdd))
+            {
+                Barbarian.FeatureUnlocks.TryAdd(
+                    new FeatureUnlockByLevel(AttributeModifierBarbarianBrutalCriticalAdd, 13));
+            }
+
+            if (!Barbarian.FeatureUnlocks.Exists(x =>
+                    x.level == 17 && x.FeatureDefinition == AttributeModifierBarbarianBrutalCriticalAdd))
+            {
+                Barbarian.FeatureUnlocks.TryAdd(
+                    new FeatureUnlockByLevel(AttributeModifierBarbarianBrutalCriticalAdd, 17));
+            }
         }
 
         if (Main.Settings.EnableSortingFutureFeatures)
