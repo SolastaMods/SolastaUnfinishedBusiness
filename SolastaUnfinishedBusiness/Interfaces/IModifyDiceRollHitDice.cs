e98af343bf204aa55257c83ba35e1a41b172f4f9
ThyWolf <pauloesquilo@gmail.com>
2024-04-20 13:55:34 -0700

    add IModifyDiceRollHitDice interface

---
+++ SolastaUnfinishedBusiness/Interfaces/IModifyDiceRollHitDice.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using static RuleDefinitions;
+
+namespace SolastaUnfinishedBusiness.Interfaces;
+
+public interface IModifyDiceRollHitDice
+{
+    [UsedImplicitly]
+    public void BeforeRoll(
+        RulesetCharacterHero __instance,
+        ref DieType die,
+        ref int modifier,
+        ref AdvantageType advantageType,
+        ref bool healKindred,
+        ref bool isBonus);
+}
