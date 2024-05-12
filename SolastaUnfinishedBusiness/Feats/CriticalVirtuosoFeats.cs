8db50c02eb7827b67a5e7a9801c3358e5e8cd912
ThyWolf <pauloesquilo@gmail.com>
2024-05-12 13:33:49 -0700

    review critical virtuoso gui presentation bleeding into source

--- SolastaUnfinishedBusiness/Feats/CriticalVirtuosoFeats.cs
+++ SolastaUnfinishedBusiness/Feats/CriticalVirtuosoFeats.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections.Generic;
 using JetBrains.Annotations;
-using SolastaUnfinishedBusiness.Api;
 using SolastaUnfinishedBusiness.Api.LanguageExtensions;
 using SolastaUnfinishedBusiness.Builders;
 using SolastaUnfinishedBusiness.Builders.Features;
 using SolastaUnfinishedBusiness.Validators;
+using static SolastaUnfinishedBusiness.Api.DatabaseHelper.FeatureDefinitionAttributeModifiers;
 
 namespace SolastaUnfinishedBusiness.Feats;
 
@@ -14,16 +14,18 @@ internal static class CriticalVirtuosoFeats
     {
         var improved = FeatureDefinitionAttributeModifierBuilder
             .Create("AttributeModifierFeatImprovedCritical")
-            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
-                .AttributeModifierMartialChampionImprovedCritical.GuiPresentation)
+            .SetGuiPresentation(
+                AttributeModifierMartialChampionImprovedCritical.GuiPresentation.Title,
+                AttributeModifierMartialChampionImprovedCritical.GuiPresentation.Description)
             .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.ForceIfWorse,
                 AttributeDefinitions.CriticalThreshold, 19)
             .AddToDB();
 
         var superior = FeatureDefinitionAttributeModifierBuilder
             .Create("AttributeModifierFeatSuperiorCritical")
-            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
-                .AttributeModifierMartialChampionSuperiorCritical.GuiPresentation)
+            .SetGuiPresentation(
+                AttributeModifierMartialChampionSuperiorCritical.GuiPresentation.Title,
+                AttributeModifierMartialChampionSuperiorCritical.GuiPresentation.Description)
             .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.ForceIfWorse,
                 AttributeDefinitions.CriticalThreshold, 18)
             .AddToDB();
@@ -31,8 +33,9 @@ internal static class CriticalVirtuosoFeats
         // Improved Critical
         var featImprovedCritical = FeatDefinitionWithPrerequisitesBuilder
             .Create("FeatImprovedCritical")
-            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
-                .AttributeModifierMartialChampionImprovedCritical.GuiPresentation)
+            .SetGuiPresentation(
+                AttributeModifierMartialChampionImprovedCritical.GuiPresentation.Title,
+                AttributeModifierMartialChampionImprovedCritical.GuiPresentation.Description)
             .SetFeatures(improved)
             .SetValidators(ValidatorsFeat.IsLevel4)
             .AddToDB();
@@ -40,8 +43,9 @@ internal static class CriticalVirtuosoFeats
         // Superior Critical
         var featSuperiorCritical = FeatDefinitionWithPrerequisitesBuilder
             .Create("FeatSuperiorCritical")
-            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
-                .AttributeModifierMartialChampionSuperiorCritical.GuiPresentation)
+            .SetGuiPresentation(
+                AttributeModifierMartialChampionSuperiorCritical.GuiPresentation.Title,
+                AttributeModifierMartialChampionSuperiorCritical.GuiPresentation.Description)
             .SetFeatures(superior)
             .SetValidators(ValidatorsFeat.IsLevel16, ValidatorsFeat.ValidateHasFeature(improved))
             .AddToDB();
