54c6df1311969a904b34638b86360df52b87408d
ThyWolf <pauloesquilo@gmail.com>
2024-05-12 09:00:39 -0700

    make critical virtuoso feats work on critical range instead of decrementing it

--- SolastaUnfinishedBusiness/Feats/CriticalVirtuosoFeats.cs
+++ SolastaUnfinishedBusiness/Feats/CriticalVirtuosoFeats.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Api;
+using SolastaUnfinishedBusiness.Api.LanguageExtensions;
 using SolastaUnfinishedBusiness.Builders;
 using SolastaUnfinishedBusiness.Builders.Features;
 using SolastaUnfinishedBusiness.Validators;
@@ -11,53 +12,44 @@ internal static class CriticalVirtuosoFeats
 {
     internal static void CreateFeats([NotNull] List<FeatDefinition> feats)
     {
-        const string Description = "Feat/&FeatCriticalVirtuosoDescription";
-        var nameImproved = DatabaseHelper.FeatureDefinitionAttributeModifiers
-            .AttributeModifierMartialChampionImprovedCritical.GuiPresentation.Title;
-        var nameSuperior = DatabaseHelper.FeatureDefinitionAttributeModifiers
-            .AttributeModifierMartialChampionSuperiorCritical.GuiPresentation.Title;
-
         var improved = FeatureDefinitionAttributeModifierBuilder
             .Create("AttributeModifierFeatImprovedCritical")
-            .SetGuiPresentation(nameImproved, Description)
-            .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.Additive,
-                AttributeDefinitions.CriticalThreshold, -1)
+            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
+                .AttributeModifierMartialChampionImprovedCritical.GuiPresentation)
+            .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.ForceIfBetter,
+                AttributeDefinitions.CriticalThreshold, 19)
             .AddToDB();
 
         var superior = FeatureDefinitionAttributeModifierBuilder
             .Create("AttributeModifierFeatSuperiorCritical")
-            .SetGuiPresentation(nameSuperior, Description)
-            .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.Additive,
-                AttributeDefinitions.CriticalThreshold, -1)
+            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
+            .AttributeModifierMartialChampionSuperiorCritical.GuiPresentation)
+            .SetModifier(FeatureDefinitionAttributeModifier.AttributeModifierOperation.ForceIfBetter,
+                AttributeDefinitions.CriticalThreshold, 18)
             .AddToDB();
 
-        // kept for backward compatibility
         // Improved Critical
         var featImprovedCritical = FeatDefinitionWithPrerequisitesBuilder
             .Create("FeatImprovedCritical")
-            .SetGuiPresentation(nameImproved, Description)
+            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
+                .AttributeModifierMartialChampionImprovedCritical.GuiPresentation)
             .SetFeatures(improved)
             .SetValidators(ValidatorsFeat.IsLevel4)
             .AddToDB();
 
-        featImprovedCritical.GuiPresentation.hidden = true;
-
-        // kept for backward compatibility
         // Superior Critical
         var featSuperiorCritical = FeatDefinitionWithPrerequisitesBuilder
             .Create("FeatSuperiorCritical")
-            .SetGuiPresentation(nameSuperior, Description)
+            .SetGuiPresentation(DatabaseHelper.FeatureDefinitionAttributeModifiers
+                .AttributeModifierMartialChampionSuperiorCritical.GuiPresentation)
             .SetFeatures(superior)
             .SetValidators(ValidatorsFeat.IsLevel16, ValidatorsFeat.ValidateHasFeature(improved))
             .AddToDB();
 
-        featSuperiorCritical.GuiPresentation.hidden = true;
-#if false
         feats.AddRange(featImprovedCritical, featSuperiorCritical);
 
         GroupFeats.FeatGroupSupportCombat.AddFeats(
             featImprovedCritical,
             featSuperiorCritical);
-#endif
     }
 }
