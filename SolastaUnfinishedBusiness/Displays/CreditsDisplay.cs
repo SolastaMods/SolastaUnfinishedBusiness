d9f129fc577be85037ebfb507441d78685c01b63
ThyWolf <pauloesquilo@gmail.com>
2024-05-29 22:53:59 -0700

    update Archfey textx

--- SolastaUnfinishedBusiness/Displays/CreditsDisplay.cs
+++ SolastaUnfinishedBusiness/Displays/CreditsDisplay.cs
@@ -28,7 +28,7 @@ internal static class CreditsDisplay
             "Martial Arcane Archer, Martial Banneret, Martial Guardian, Martial Psi Warrior, Martial Warlord, Martial Weapon Master, " +
             "Oath of Altruism, Oath of Dread, Oath of Thunder, " +
             "Path of the Elements, Path of the Ravager, Path of the Reaver, Path of the Savagery, Path of the Yeoman, " +
-            "Patron Celestial, Patron Moonlit, Patron Mountain, Patron Riftwalker, " +
+            "Patron Archfey, Patron Celestial, Patron Moonlit, Patron Mountain, Patron Riftwalker, " +
             "Ranger Fey Wanderer, Ranger Gloom Stalker, Ranger Hellwalker, Ranger Lightbearer, Ranger Sky Warrior, Ranger Survivalist, Ranger Wildmaster, " +
             "Roguish Acrobat, Roguish Arcane Scoundrel, Roguish Blade Caller, Roguish Duelist, Roguish Raven, Roguish Slayer, Roguish Umbral Stalker, " +
             "Sorcerous Field Manipulator, Sorcerous Forceblade, Sorcerous Scion, Sorcerous Sorr-Akkath, Sorcerous Spellblade, " +
