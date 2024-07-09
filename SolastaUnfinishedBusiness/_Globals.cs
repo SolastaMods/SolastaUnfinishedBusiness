e31a37433f39906f5edb62aaf83eac4559b89609
ThyWolf <pauloesquilo@gmail.com>
2024-07-08 19:51:24 -0700

    remove gameLocationScreenBattle visibility check from SelectedLocationCharacter
    - looking to fix some weirdo issues under MP

--- SolastaUnfinishedBusiness/_Globals.cs
+++ SolastaUnfinishedBusiness/_Globals.cs
@@ -6,13 +6,12 @@ internal static class Global
 {
     // true if in a multiplayer game
     internal static bool IsMultiplayer =>
-        IsSettingUpMultiplayer
-        || ServiceRepository.GetService<INetworkingService>().IsMultiplayerGame;
+        IsSettingUpMultiplayer || ServiceRepository.GetService<INetworkingService>().IsMultiplayerGame;
 
     // true if on multiplayer setup screen
     internal static bool IsSettingUpMultiplayer { get; set; }
 
-    //PATCH: Keeps last level up hero selected
+    // last level up hero name
     internal static string LastLevelUpHeroName { get; set; }
 
     // level up hero
@@ -28,25 +27,17 @@ internal static class Global
     {
         get
         {
-            var exploration = Gui.GuiService.GetScreen<GameLocationScreenExploration>();
+            var gameLocationScreenExploration = Gui.GuiService.GetScreen<GameLocationScreenExploration>();
+            var gameLocationScreenBattle = Gui.GuiService.GetScreen<GameLocationScreenBattle>();
 
-            if (exploration.Visible)
-            {
-                return exploration.CharacterControlPanel.GuiCharacter?.GameLocationCharacter;
-            }
-
-            var battle = Gui.GuiService.GetScreen<GameLocationScreenBattle>();
-
-            return battle.Visible
-                ? battle.CharacterControlPanel.GuiCharacter?.GameLocationCharacter
-                : null;
+            return gameLocationScreenExploration.Visible
+                ? gameLocationScreenExploration.CharacterControlPanel.GuiCharacter?.GameLocationCharacter
+                : gameLocationScreenBattle.CharacterControlPanel.GuiCharacter?.GameLocationCharacter;
         }
     }
 
-    //PATCH: used in UI references
+    // used in UI references
     [CanBeNull]
     internal static RulesetCharacter CurrentCharacter =>
-        InspectedHero
-        ?? LevelUpHero
-        ?? SelectedLocationCharacter?.RulesetCharacter;
+        InspectedHero ?? LevelUpHero ?? SelectedLocationCharacter?.RulesetCharacter;
 }
