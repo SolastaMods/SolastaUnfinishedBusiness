e036486162868df9725cd7d8b9fa7bc051d65c29
ThyWolf <pauloesquilo@gmail.com>
2024-08-06 21:02:52 -0700

    change Patron Moonlit moonlight guise to use MyReactToUsePower

--- SolastaUnfinishedBusiness/Subclasses/PatronMoonlitScion.cs
+++ SolastaUnfinishedBusiness/Subclasses/PatronMoonlitScion.cs
@@ -585,33 +585,23 @@ public sealed class PatronMoonlitScion : AbstractSubclass
             }
 
             var rulesetDefender = defender.RulesetCharacter;
+            var usablePower = PowerProvider.Get(powerMoonlightGuise, rulesetDefender);
 
-            if (!defender.CanReact() ||
-                rulesetDefender.GetRemainingPowerUses(powerMoonlightGuise) == 0)
+            if (actualEffectForms.All(x => x.FormType != EffectForm.EffectFormType.Damage) ||
+                !defender.CanReact() ||
+                rulesetDefender.GetRemainingUsesOfPower(usablePower) == 0)
             {
                 yield break;
             }
 
-            var actionService = ServiceRepository.GetService<IGameLocationActionService>();
-            var implementationManager =
-                ServiceRepository.GetService<IRulesetImplementationService>() as RulesetImplementationManager;
 
-            var usablePower = PowerProvider.Get(powerMoonlightGuise, rulesetDefender);
-            var reactionParams =
-                new CharacterActionParams(defender, ActionDefinitions.Id.PowerReaction)
-                {
-                    StringParameter = "MoonlightGuise",
-                    ActionModifiers = { new ActionModifier() },
-                    RulesetEffect = implementationManager
-                        .MyInstantiateEffectPower(rulesetDefender, usablePower, false),
-                    UsablePower = usablePower,
-                    TargetCharacters = { defender }
-                };
-            var count = actionService.PendingReactionRequestGroups.Count;
-
-            actionService.ReactToUsePower(reactionParams, "UsePower", defender);
-
-            yield return battleManager.WaitForReactions(attacker, actionService, count);
+            yield return defender.MyReactToUsePower(
+                ActionDefinitions.Id.PowerReaction,
+                usablePower,
+                [defender],
+                attacker,
+                "MoonlightGuise",
+                battleManager: battleManager);
         }
     }
 }
