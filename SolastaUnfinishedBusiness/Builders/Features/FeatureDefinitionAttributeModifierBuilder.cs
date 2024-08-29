dc0ca8e55057f4fb5a0804770edc5e87fffb695d
ThyWolf <pauloesquilo@gmail.com>
2024-08-29 16:43:37 -0700

    fix SetAddConditionAmount to no add a +1 on modifierValue

--- SolastaUnfinishedBusiness/Builders/Features/FeatureDefinitionAttributeModifierBuilder.cs
+++ SolastaUnfinishedBusiness/Builders/Features/FeatureDefinitionAttributeModifierBuilder.cs
@@ -25,7 +25,6 @@ internal class FeatureDefinitionAttributeModifierBuilder
     {
         Definition.modifierOperation = AttributeModifierOperation.AddConditionAmount;
         Definition.modifiedAttribute = attribute;
-        Definition.modifierValue = 1;
         return this;
     }
 
