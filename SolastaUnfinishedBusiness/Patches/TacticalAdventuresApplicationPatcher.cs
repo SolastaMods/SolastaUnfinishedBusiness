7c28937730e036130ef088d824ecc99d97cdcd83
ThyWolf <pauloesquilo@gmail.com>
2024-09-04 21:57:42 -0700

    fix save by location/campaign setting on multiplayer load

--- SolastaUnfinishedBusiness/Patches/TacticalAdventuresApplicationPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/TacticalAdventuresApplicationPatcher.cs
@@ -8,6 +8,32 @@ namespace SolastaUnfinishedBusiness.Patches;
 [UsedImplicitly]
 public static class TacticalAdventuresApplicationPatcher
 {
+    private static bool EnableSaveByLocation(ref string __result)
+    {
+        //PATCH: EnableSaveByLocation
+        if (!Main.Settings.EnableSaveByLocation)
+        {
+            return true;
+        }
+
+        // Modify the value returned by TacticalAdventuresApplication.SaveGameDirectory so that saves
+        // end up where we want them (by location/campaign)
+        var selectedCampaignService = ServiceRepository.GetService<SelectedCampaignService>();
+
+        // handle exception when saving from world map or encounters on a user campaign
+        if (Gui.GameCampaign?.campaignDefinition?.IsUserCampaign == true &&
+            selectedCampaignService is { LocationType: LocationType.StandardCampaign })
+        {
+            (__result, _) = GetMostRecent();
+
+            return false;
+        }
+
+        __result = selectedCampaignService?.SaveGameDirectory ?? DefaultSaveGameDirectory;
+        
+        return false;
+    }
+    
     [HarmonyPatch(typeof(TacticalAdventuresApplication), nameof(TacticalAdventuresApplication.SaveGameDirectory),
         MethodType.Getter)]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
@@ -17,28 +43,20 @@ public static class TacticalAdventuresApplicationPatcher
         [UsedImplicitly]
         public static bool Prefix(ref string __result)
         {
-            //PATCH: EnableSaveByLocation
-            if (!Main.Settings.EnableSaveByLocation)
-            {
-                return true;
-            }
-
-            // Modify the value returned by TacticalAdventuresApplication.SaveGameDirectory so that saves
-            // end up where we want them (by location/campaign)
-            var selectedCampaignService = ServiceRepository.GetService<SelectedCampaignService>();
-
-            // handle exception when saving from world map or encounters on a user campaign
-            if (Gui.GameCampaign?.campaignDefinition?.IsUserCampaign == true &&
-                selectedCampaignService is { LocationType: LocationType.StandardCampaign })
-            {
-                (__result, _) = GetMostRecent();
-
-                return false;
-            }
-
-            __result = selectedCampaignService?.SaveGameDirectory ?? DefaultSaveGameDirectory;
-
-            return false;
+            return EnableSaveByLocation(ref __result);
+        }
+    }
+    
+    [HarmonyPatch(typeof(TacticalAdventuresApplication), nameof(TacticalAdventuresApplication.MultiplayerFilesDirectory),
+        MethodType.Getter)]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class MultiplayerFilesDirectory_Getter_Patch
+    {
+        [UsedImplicitly]
+        public static bool Prefix(ref string __result)
+        {
+            return EnableSaveByLocation(ref __result);
         }
     }
 }
