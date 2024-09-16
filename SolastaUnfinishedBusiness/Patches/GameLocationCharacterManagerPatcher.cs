35114aea99ba43f00128b25ca1f8d8a5261a56c9
ThyWolf <pauloesquilo@gmail.com>
2024-09-15 18:15:43 -0700

    demote GameLocationCharacterManager.UnlockCharactersForLoading patch as no longer required

--- SolastaUnfinishedBusiness/Patches/GameLocationCharacterManagerPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/GameLocationCharacterManagerPatcher.cs
@@ -256,27 +256,6 @@ public static class GameLocationCharacterManagerPatcher
         }
     }
 
-    //PATCH: recalculates additional party members positions (PARTYSIZE)
-    [HarmonyPatch(typeof(GameLocationCharacterManager),
-        nameof(GameLocationCharacterManager.UnlockCharactersForLoading))]
-    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
-    [UsedImplicitly]
-    public static class UnlockCharactersForLoading_Patch
-    {
-        [UsedImplicitly]
-        public static void Prefix([NotNull] GameLocationCharacterManager __instance)
-        {
-            var partyCharacters = __instance.PartyCharacters;
-
-            for (var idx = ToolsContext.GamePartySize; idx < partyCharacters.Count; idx++)
-            {
-                var position = partyCharacters[idx % ToolsContext.GamePartySize].LocationPosition;
-
-                partyCharacters[idx].LocationPosition = new int3(position.x, position.y, position.z);
-            }
-        }
-    }
-
     [HarmonyPatch(typeof(GameLocationCharacterManager), nameof(GameLocationCharacterManager.LoseWildShapeAndRefund))]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     [UsedImplicitly]
