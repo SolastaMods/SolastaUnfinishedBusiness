69beccd00f726fcfe04b5ce56c52cad47c705cf8
ThyWolf <pauloesquilo@gmail.com>
2024-11-07 11:34:40 -0800

    fix 'Enable all Casters to use new prepared spells tables' on Rangers

--- SolastaUnfinishedBusiness/Models/Tabletop2024Context.cs
+++ SolastaUnfinishedBusiness/Models/Tabletop2024Context.cs
@@ -692,6 +692,8 @@ internal static class Tabletop2024Context
         {
             FeatureDefinitionCastSpells.CastSpellBard.knownSpells =
                 [4, 5, 6, 7, 9, 10, 11, 12, 14, 15, 16, 16, 17, 17, 18, 18, 19, 20, 21, 22];
+            FeatureDefinitionCastSpells.CastSpellRanger.knownSpells =
+                [2, 3, 4, 5, 6, 6, 7, 7, 9, 9, 10, 10, 11, 11, 12, 12, 14, 14, 15, 15];
             FeatureDefinitionCastSpells.CastSpellSorcerer.knownSpells =
                 [2, 4, 6, 7, 9, 10, 11, 12, 14, 15, 16, 16, 17, 17, 18, 18, 19, 20, 21, 22];
         }
@@ -699,6 +701,8 @@ internal static class Tabletop2024Context
         {
             FeatureDefinitionCastSpells.CastSpellBard.knownSpells =
                 [4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 15, 16, 18, 19, 19, 20, 22, 22, 22];
+            FeatureDefinitionCastSpells.CastSpellRanger.knownSpells =
+                [0, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11];
             FeatureDefinitionCastSpells.CastSpellSorcerer.knownSpells =
                 [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13, 13, 14, 14, 15, 15, 15, 15];
         }
