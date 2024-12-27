7060c3273414bc7fd208fb5d8b012e1f57943715
ThyWolf <pauloesquilo@gmail.com>
2024-12-27 07:43:32 -0800

    update resources

--- SolastaUnfinishedBusiness/Properties/Resources.Designer.cs
+++ SolastaUnfinishedBusiness/Properties/Resources.Designer.cs
@@ -3685,6 +3685,16 @@ namespace SolastaUnfinishedBusiness.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
+        public static byte[] PowerPatientDefenseNoCost {
+            get {
+                object obj = ResourceManager.GetObject("PowerPatientDefenseNoCost", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
         public static byte[] PowerPerniciousCloak {
             get {
                 object obj = ResourceManager.GetObject("PowerPerniciousCloak", resourceCulture);
@@ -3828,6 +3838,16 @@ namespace SolastaUnfinishedBusiness.Properties {
         public static byte[] PowerSteadyAim {
             get {
                 object obj = ResourceManager.GetObject("PowerSteadyAim", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        public static byte[] PowerStepOfTheWindNoCost {
+            get {
+                object obj = ResourceManager.GetObject("PowerStepOfTheWindNoCost", resourceCulture);
                 return ((byte[])(obj));
             }
         }
